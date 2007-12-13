199a0c043a0d35f0191fff3f64852f49acf4e161
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2007-12-13 17:37:28 +0000

    8X61V - Artifact types are not filtered by branch in Search dialog

--- org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/attribute/ArtifactSubtypeDescriptorCache.java
+++ org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/attribute/ArtifactSubtypeDescriptorCache.java
@@ -30,116 +30,140 @@ import org.eclipse.osee.framework.ui.plugin.util.db.Query;
  * @author Robert A. Fisher
  */
 public class ArtifactSubtypeDescriptorCache {
-   private static final TransactionIdManager transactionIdManager = TransactionIdManager.getInstance();
-   private final HashCollection<TransactionId, ArtifactSubtypeDescriptor> allDescriptors;
-   private final DoubleKeyHashMap<String, TransactionId, ArtifactSubtypeDescriptor> nameToDescriptors;
-   private final DoubleKeyHashMap<Integer, TransactionId, ArtifactSubtypeDescriptor> idToDescriptors;
+	private static final TransactionIdManager transactionIdManager = TransactionIdManager
+			.getInstance();
+	private final HashCollection<TransactionId, ArtifactSubtypeDescriptor> allDescriptors;
+	private final DoubleKeyHashMap<String, TransactionId, ArtifactSubtypeDescriptor> nameToDescriptors;
+	private final DoubleKeyHashMap<Integer, TransactionId, ArtifactSubtypeDescriptor> idToDescriptors;
 
-   private static final String SELECT_ARTIFACT_TYPES =
-         "SELECT aty1.art_type_id, aty1.factory_key, aty1.name, aty1.image, fac2.factory_class FROM osee_define_artifact_type aty1, osee_define_factory fac2 WHERE aty1.factory_id = fac2.factory_id AND EXISTS (SELECT MAX(txs3.transaction_id) FROM osee_define_txs txs3, osee_define_tx_details txd4 WHERE aty1.gamma_id = txs3.gamma_id AND txs3.transaction_id = txd4.transaction_id AND txd4.branch_id = ? AND txd4.transaction_id <= ?) ORDER BY aty1.name";
+	private static final String SELECT_ARTIFACT_TYPES = "SELECT * FROM osee_define_artifact_type aty1, osee_define_factory fac2, osee_define_txs txs3 WHERE aty1.factory_id = fac2.factory_id AND aty1.gamma_id = txs3.gamma_id AND txs3.transaction_id = (SELECT MAX(txs4.transaction_id) FROM osee_define_txs txs4, osee_define_tx_details txd5 WHERE txs4.gamma_id = aty1.gamma_id AND txs4.transaction_id <= ? AND txs4.transaction_id = txd5.transaction_id AND txd5.branch_id = ?) ORDER BY aty1.name";
 
-   protected ArtifactSubtypeDescriptorCache() {
-      this.allDescriptors = new HashCollection<TransactionId, ArtifactSubtypeDescriptor>(false, LinkedHashSet.class);
-      this.nameToDescriptors = new DoubleKeyHashMap<String, TransactionId, ArtifactSubtypeDescriptor>();
-      this.idToDescriptors = new DoubleKeyHashMap<Integer, TransactionId, ArtifactSubtypeDescriptor>();
-   }
+	protected ArtifactSubtypeDescriptorCache() {
+		this.allDescriptors = new HashCollection<TransactionId, ArtifactSubtypeDescriptor>(
+				false, LinkedHashSet.class);
+		this.nameToDescriptors = new DoubleKeyHashMap<String, TransactionId, ArtifactSubtypeDescriptor>();
+		this.idToDescriptors = new DoubleKeyHashMap<Integer, TransactionId, ArtifactSubtypeDescriptor>();
+	}
 
-   private synchronized void checkPopulated(TransactionId transactionId) throws SQLException {
-      if (!allDescriptors.containsKey(transactionId)) {
-         populateCache(transactionId);
-      }
-   }
+	private synchronized void checkPopulated(TransactionId transactionId)
+			throws SQLException {
+		if (!allDescriptors.containsKey(transactionId)) {
+			populateCache(transactionId);
+		}
+	}
 
-   private void populateCache(TransactionId transactionId) throws SQLException {
-      Collection<ArtifactSubtypeDescriptor> descriptors = new LinkedList<ArtifactSubtypeDescriptor>();
-      Query.acquireCollection(descriptors, new ArtifactSubtypeProcessor(transactionId), SELECT_ARTIFACT_TYPES,
-            SQL3DataType.INTEGER, transactionId.getBranch().getBranchId(), SQL3DataType.INTEGER,
-            transactionId.getTransactionNumber());
+	private void populateCache(TransactionId transactionId) throws SQLException {
+		Collection<ArtifactSubtypeDescriptor> descriptors = new LinkedList<ArtifactSubtypeDescriptor>();
+		Query.acquireCollection(descriptors, new ArtifactSubtypeProcessor(
+				transactionId), SELECT_ARTIFACT_TYPES, SQL3DataType.INTEGER,
+				transactionId.getTransactionNumber(), SQL3DataType.INTEGER,
+				transactionId.getBranch().getBranchId());
 
-      for (ArtifactSubtypeDescriptor descriptor : descriptors) {
-         cache(descriptor);
-      }
-   }
+		for (ArtifactSubtypeDescriptor descriptor : descriptors) {
+			cache(descriptor);
+		}
+	}
 
-   /**
-    * @return Returns all of the descriptors.
-    * @throws SQLException
-    */
-   public Collection<ArtifactSubtypeDescriptor> getAllDescriptors(Branch branch) throws SQLException {
-      TransactionId transactionId = transactionIdManager.getEditableTransactionId(branch);
+	/**
+	 * @return Returns all of the descriptors.
+	 * @throws SQLException
+	 */
+	public Collection<ArtifactSubtypeDescriptor> getAllDescriptors(Branch branch)
+			throws SQLException {
+		TransactionId transactionId = transactionIdManager
+				.getEditableTransactionId(branch);
 
-      return getAllDescriptors(transactionId);
-   }
+		return getAllDescriptors(transactionId);
+	}
 
-   /**
-    * @return Returns the descriptor with a particular name, null if it does not exist.
-    * @throws SQLException
-    */
-   public ArtifactSubtypeDescriptor getDescriptor(String name, Branch branch) throws SQLException {
-      return getDescriptor(name, transactionIdManager.getEditableTransactionId(branch));
-   }
+	/**
+	 * @return Returns the descriptor with a particular name, null if it does
+	 *         not exist.
+	 * @throws SQLException
+	 */
+	public ArtifactSubtypeDescriptor getDescriptor(String name, Branch branch)
+			throws SQLException {
+		return getDescriptor(name, transactionIdManager
+				.getEditableTransactionId(branch));
+	}
 
-   /**
-    * @return Returns the descriptor with a particular name, null if it does not exist.
-    * @throws SQLException
-    */
-   public ArtifactSubtypeDescriptor getDescriptor(int artTypeId, Branch branch) throws SQLException {
-      TransactionId transactionId = transactionIdManager.getEditableTransactionId(branch);
+	/**
+	 * @return Returns the descriptor with a particular name, null if it does
+	 *         not exist.
+	 * @throws SQLException
+	 */
+	public ArtifactSubtypeDescriptor getDescriptor(int artTypeId, Branch branch)
+			throws SQLException {
+		TransactionId transactionId = transactionIdManager
+				.getEditableTransactionId(branch);
 
-      return getDescriptor(artTypeId, transactionId);
-   }
+		return getDescriptor(artTypeId, transactionId);
+	}
 
-   /**
-    * @return Returns all of the descriptors.
-    * @throws SQLException
-    */
-   public Set<ArtifactSubtypeDescriptor> getAllDescriptors(TransactionId transactionId) throws SQLException {
-      checkPopulated(transactionId);
-      return (Set<ArtifactSubtypeDescriptor>) allDescriptors.getValues(transactionId);
-   }
+	/**
+	 * @return Returns all of the descriptors.
+	 * @throws SQLException
+	 */
+	public Set<ArtifactSubtypeDescriptor> getAllDescriptors(
+			TransactionId transactionId) throws SQLException {
+		checkPopulated(transactionId);
+		return (Set<ArtifactSubtypeDescriptor>) allDescriptors
+				.getValues(transactionId);
+	}
 
-   /**
-    * @return Returns the descriptor with a particular name, null if it does not exist.
-    * @throws SQLException
-    */
-   public ArtifactSubtypeDescriptor getDescriptor(String name, TransactionId transactionId) throws SQLException {
-      checkPopulated(transactionId);
-      return nameToDescriptors.get(name, transactionId);
-   }
+	/**
+	 * @return Returns the descriptor with a particular name, null if it does
+	 *         not exist.
+	 * @throws SQLException
+	 */
+	public ArtifactSubtypeDescriptor getDescriptor(String name,
+			TransactionId transactionId) throws SQLException {
+		checkPopulated(transactionId);
+		return nameToDescriptors.get(name, transactionId);
+	}
 
-   /**
-    * @return Returns the descriptor with a particular name, null if it does not exist.
-    * @throws SQLException
-    */
-   public ArtifactSubtypeDescriptor getDescriptor(int artTypeId, TransactionId transactionId) throws SQLException {
-      checkPopulated(transactionId);
+	/**
+	 * @return Returns the descriptor with a particular name, null if it does
+	 *         not exist.
+	 * @throws SQLException
+	 */
+	public ArtifactSubtypeDescriptor getDescriptor(int artTypeId,
+			TransactionId transactionId) throws SQLException {
+		checkPopulated(transactionId);
 
-      ArtifactSubtypeDescriptor artifactSubtypeDescriptor = idToDescriptors.get(artTypeId, transactionId);
+		ArtifactSubtypeDescriptor artifactSubtypeDescriptor = idToDescriptors
+				.get(artTypeId, transactionId);
 
-      if (artifactSubtypeDescriptor == null) {
-         throw new IllegalArgumentException(
-               "Atrifact type: " + artTypeId + " is not available for transaction: " + transactionId);
-      }
-      return artifactSubtypeDescriptor;
-   }
+		if (artifactSubtypeDescriptor == null) {
+			throw new IllegalArgumentException("Atrifact type: " + artTypeId
+					+ " is not available for transaction: " + transactionId);
+		}
+		return artifactSubtypeDescriptor;
+	}
 
-   /**
-    * Cache a newly created descriptor.
-    * 
-    * @param descriptor The descriptor to cache
-    * @throws IllegalArgumentException if descriptor is null.
-    */
-   public void cache(ArtifactSubtypeDescriptor descriptor) {
-      if (descriptor == null) {
-         throw new IllegalArgumentException("The descriptor parameter can not be null");
-      }
+	/**
+	 * Cache a newly created descriptor.
+	 * 
+	 * @param descriptor
+	 *            The descriptor to cache
+	 * @throws IllegalArgumentException
+	 *             if descriptor is null.
+	 */
+	public void cache(ArtifactSubtypeDescriptor descriptor) {
+		if (descriptor == null) {
+			throw new IllegalArgumentException(
+					"The descriptor parameter can not be null");
+		}
 
-      if (nameToDescriptors.containsKey(descriptor.getName(), descriptor.getTransactionId())) {
-         System.out.println("bad");
-      }
+		if (nameToDescriptors.containsKey(descriptor.getName(), descriptor
+				.getTransactionId())) {
+			System.out.println("bad");
+		}
 
-      allDescriptors.put(descriptor.getTransactionId(), descriptor);
-      nameToDescriptors.put(descriptor.getName(), descriptor.getTransactionId(), descriptor);
-      idToDescriptors.put(descriptor.getArtTypeId(), descriptor.getTransactionId(), descriptor);
-   }
+		allDescriptors.put(descriptor.getTransactionId(), descriptor);
+		nameToDescriptors.put(descriptor.getName(), descriptor
+				.getTransactionId(), descriptor);
+		idToDescriptors.put(descriptor.getArtTypeId(), descriptor
+				.getTransactionId(), descriptor);
+	}
 }
