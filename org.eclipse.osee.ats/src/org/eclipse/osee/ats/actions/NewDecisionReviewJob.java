e52da901eca605dcc507cd4e43f150e22dc256df
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2007-12-14 05:05:20 +0000

    AR6F7 - Creating a review from workflow editor results in a null exception

--- org.eclipse.osee.ats/src/org/eclipse/osee/ats/actions/NewDecisionReviewJob.java
+++ org.eclipse.osee.ats/src/org/eclipse/osee/ats/actions/NewDecisionReviewJob.java
@@ -12,6 +12,7 @@
 package org.eclipse.osee.ats.actions;
 
 import java.sql.SQLException;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -35,68 +36,82 @@ import org.eclipse.osee.framework.ui.skynet.ats.AtsOpenOption;
  * @author Donald G. Dunne
  */
 public class NewDecisionReviewJob extends Job {
-   private static final BranchPersistenceManager branchManager = BranchPersistenceManager.getInstance();
-   private final TeamWorkFlowArtifact teamParent;
-   private final boolean againstCurrentState;
+	private static final BranchPersistenceManager branchManager = BranchPersistenceManager
+			.getInstance();
+	private final TeamWorkFlowArtifact teamParent;
+	private final boolean againstCurrentState;
+	private DecisionReviewArtifact decisionReviewArtifact;
 
-   public NewDecisionReviewJob(TeamWorkFlowArtifact teamParent, boolean againstCurrentState) {
-      super("Creating New Decision Review");
-      this.teamParent = teamParent;
-      this.againstCurrentState = againstCurrentState;
-   }
+	public NewDecisionReviewJob(TeamWorkFlowArtifact teamParent,
+			boolean againstCurrentState) {
+		super("Creating New Decision Review");
+		this.teamParent = teamParent;
+		this.againstCurrentState = againstCurrentState;
+	}
 
-   public IStatus run(final IProgressMonitor monitor) {
-      try {
-         AbstractSkynetTxTemplate newDecisionReviewTx = new AbstractSkynetTxTemplate(branchManager.getAtsBranch()) {
+	@Override
+	public IStatus run(final IProgressMonitor monitor) {
+		try {
+			AbstractSkynetTxTemplate newDecisionReviewTx = new AbstractSkynetTxTemplate(
+					branchManager.getAtsBranch()) {
 
-            @Override
-            protected void handleTxWork() throws Exception {
-               DecisionReviewArtifact decRev = createNewDecisionReview(teamParent, againstCurrentState);
-               decRev.persist(true);
+				@Override
+				protected void handleTxWork() throws Exception {
+					decisionReviewArtifact = createNewDecisionReview(
+							teamParent, againstCurrentState);
+					decisionReviewArtifact.persist(true);
+				}
 
-               monitor.subTask("Persisting");
+			};
+			newDecisionReviewTx.execute();
+			AtsLib.openAtsAction(decisionReviewArtifact,
+					AtsOpenOption.OpenOneOrPopupSelect);
+		} catch (Exception ex) {
+			monitor.done();
+			return new Status(Status.ERROR, AtsPlugin.PLUGIN_ID, -1,
+					"Error creating Decision Review", ex);
+		} finally {
+			monitor.done();
+		}
+		return Status.OK_STATUS;
+	}
 
-               // Because this is a job, it will automatically kill any popups that are created during.
-               // Thus, if multiple teams were selected to create, don't popup on openAction or
-               // dialog will exception out when it is killed at the end of this job.
-               AtsLib.openAtsAction(decRev, AtsOpenOption.OpenOneOrPopupSelect);
-            }
+	public static DecisionReviewArtifact createNewDecisionReview(
+			StateMachineArtifact teamParent, boolean againstCurrentState)
+			throws SQLException {
+		DecisionReviewArtifact decRev = (DecisionReviewArtifact) ConfigurationPersistenceManager
+				.getInstance().getArtifactSubtypeDescriptor(
+						DecisionReviewArtifact.ARTIFACT_NAME,
+						branchManager.getAtsBranch()).makeNewArtifact();
 
-         };
-         newDecisionReviewTx.execute();
-      } catch (Exception ex) {
-         monitor.done();
-         return new Status(Status.ERROR, AtsPlugin.PLUGIN_ID, -1, "Error creating Decision Review", ex);
-      } finally {
-         monitor.done();
-      }
-      return Status.OK_STATUS;
-   }
+		if (teamParent != null)
+			teamParent.relate(RelationSide.TeamWorkflowToReview_Review, decRev);
+		if (againstCurrentState)
+			decRev.setSoleAttributeValue(
+					ATSAttributes.RELATED_TO_STATE_ATTRIBUTE.getStoreName(),
+					teamParent.getCurrentStateName());
 
-   public static DecisionReviewArtifact createNewDecisionReview(StateMachineArtifact teamParent, boolean againstCurrentState) throws SQLException {
-      DecisionReviewArtifact decRev =
-            (DecisionReviewArtifact) ConfigurationPersistenceManager.getInstance().getArtifactSubtypeDescriptor(
-                  DecisionReviewArtifact.ARTIFACT_NAME, branchManager.getAtsBranch()).makeNewArtifact();
+		decRev.getLog().addLog(LogType.Originated, "", "");
+		decRev
+				.setDescriptiveName("Should we do this?  Yes will require followup, No will not");
+		decRev.setSoleAttributeValue(ATSAttributes.DESCRIPTION_ATTRIBUTE
+				.getStoreName(), "Enter description of the decision, if any");
+		decRev.setSoleAttributeValue(
+				ATSAttributes.DECISION_REVIEW_OPTIONS_ATTRIBUTE.getStoreName(),
+				"Yes;Followup;<"
+						+ SkynetAuthentication.getInstance()
+								.getAuthenticatedUser().getUserId() + ">\n"
+						+ "No;Completed;");
 
-      if (teamParent != null) teamParent.relate(RelationSide.TeamWorkflowToReview_Review, decRev);
-      if (againstCurrentState) decRev.setSoleAttributeValue(ATSAttributes.RELATED_TO_STATE_ATTRIBUTE.getStoreName(),
-            teamParent.getCurrentStateName());
+		// Set state
+		// Set current state and POCs
+		decRev.getCurrentStateDam().setState(
+				new SMAState(DecisionReviewArtifact.StateNames.Prepare.name(),
+						SkynetAuthentication.getInstance()
+								.getAuthenticatedUser()));
+		decRev.getLog().addLog(LogType.StateEntered,
+				DecisionReviewArtifact.StateNames.Prepare.name(), "");
 
-      decRev.getLog().addLog(LogType.Originated, "", "");
-      decRev.setDescriptiveName("Should we do this?  Yes will require followup, No will not");
-      decRev.setSoleAttributeValue(ATSAttributes.DESCRIPTION_ATTRIBUTE.getStoreName(),
-            "Enter description of the decision, if any");
-      decRev.setSoleAttributeValue(
-            ATSAttributes.DECISION_REVIEW_OPTIONS_ATTRIBUTE.getStoreName(),
-            "Yes;Followup;<" + SkynetAuthentication.getInstance().getAuthenticatedUser().getUserId() + ">\n" + "No;Completed;");
-
-      // Set state
-      // Set current state and POCs
-      decRev.getCurrentStateDam().setState(
-            new SMAState(DecisionReviewArtifact.StateNames.Prepare.name(),
-                  SkynetAuthentication.getInstance().getAuthenticatedUser()));
-      decRev.getLog().addLog(LogType.StateEntered, DecisionReviewArtifact.StateNames.Prepare.name(), "");
-
-      return decRev;
-   }
+		return decRev;
+	}
 }
--- org.eclipse.osee.ats/src/org/eclipse/osee/ats/actions/NewPeerToPeerReviewJob.java
+++ org.eclipse.osee.ats/src/org/eclipse/osee/ats/actions/NewPeerToPeerReviewJob.java
@@ -18,7 +18,6 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.osee.ats.AtsPlugin;
 import org.eclipse.osee.ats.artifact.PeerToPeerReviewArtifact;
 import org.eclipse.osee.ats.artifact.TeamWorkFlowArtifact;
-import org.eclipse.osee.ats.editor.SMAManager;
 import org.eclipse.osee.ats.util.AtsLib;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchPersistenceManager;
 import org.eclipse.osee.framework.skynet.core.transaction.AbstractSkynetTxTemplate;
@@ -28,46 +27,46 @@ import org.eclipse.osee.framework.ui.skynet.ats.AtsOpenOption;
  * @author Donald G. Dunne
  */
 public class NewPeerToPeerReviewJob extends Job {
-   private static final BranchPersistenceManager branchManager = BranchPersistenceManager.getInstance();
+	private static final BranchPersistenceManager branchManager = BranchPersistenceManager
+			.getInstance();
 
-   private final TeamWorkFlowArtifact teamParent;
-   private final String againstState;
+	private final TeamWorkFlowArtifact teamParent;
+	private final String againstState;
+	private PeerToPeerReviewArtifact peerToPeerReviewArtifact;
 
-   public NewPeerToPeerReviewJob(TeamWorkFlowArtifact teamParent, String againstState) {
-      super("Creating New PeerToPeer Review");
-      this.teamParent = teamParent;
-      this.againstState = againstState;
-   }
+	public NewPeerToPeerReviewJob(TeamWorkFlowArtifact teamParent,
+			String againstState) {
+		super("Creating New PeerToPeer Review");
+		this.teamParent = teamParent;
+		this.againstState = againstState;
+	}
 
-   public IStatus run(final IProgressMonitor monitor) {
-      try {
-         AbstractSkynetTxTemplate newPeerToPeerTx = new AbstractSkynetTxTemplate(branchManager.getAtsBranch()) {
+	@Override
+	public IStatus run(final IProgressMonitor monitor) {
+		try {
+			AbstractSkynetTxTemplate newPeerToPeerTx = new AbstractSkynetTxTemplate(
+					branchManager.getAtsBranch()) {
 
-            @Override
-            protected void handleTxWork() throws Exception {
+				@Override
+				protected void handleTxWork() throws Exception {
+					peerToPeerReviewArtifact = teamParent.getSmaMgr()
+							.getReviewManager().createNewPeerToPeerReview(
+									againstState);
+					peerToPeerReviewArtifact.persist(true);
+				}
 
-               SMAManager smaMgr = new SMAManager(teamParent);
-               PeerToPeerReviewArtifact peerToPeerRev =
-                     smaMgr.getReviewManager().createNewPeerToPeerReview(againstState);
+			};
+			newPeerToPeerTx.execute();
 
-               monitor.subTask("Persisting");
-
-               // Because this is a job, it will automatically kill any popups that are created
-               // during.
-               // Thus, if multiple teams were selected to create, don't popup on openAction or
-               // dialog
-               // will exception out when it is killed at the end of this job.
-               AtsLib.openAtsAction(peerToPeerRev, AtsOpenOption.OpenOneOrPopupSelect);
-            }
-         };
-         newPeerToPeerTx.execute();
-      } catch (Exception ex) {
-         monitor.done();
-         return new Status(Status.ERROR, AtsPlugin.PLUGIN_ID, -1, "Error creating PeerToPeer Review", ex);
-      } finally {
-         monitor.done();
-      }
-      return Status.OK_STATUS;
-   }
-
+			AtsLib.openAtsAction(peerToPeerReviewArtifact,
+					AtsOpenOption.OpenOneOrPopupSelect);
+		} catch (Exception ex) {
+			monitor.done();
+			return new Status(Status.ERROR, AtsPlugin.PLUGIN_ID, -1,
+					"Error creating PeerToPeer Review", ex);
+		} finally {
+			monitor.done();
+		}
+		return Status.OK_STATUS;
+	}
 }
