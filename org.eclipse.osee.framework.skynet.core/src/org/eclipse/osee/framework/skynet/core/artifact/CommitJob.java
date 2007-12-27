be6eceb7e4130221b9cbe570216578bd30cfa234
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2007-12-27 01:09:06 +0000

    8MMC5 - Convert ATS to use branch associated art id and commit art id instead of storing in workflow

--- org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/CommitJob.java
+++ org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/CommitJob.java
@@ -260,13 +260,12 @@ class CommitJob extends Job {
       }
 
       private void tagArtifacts(Branch toBranch, int fromBranchId, int commitTransactionId) throws SQLException {
-         System.err.println("CommitJob: Add this back in.");
          //Delete toBranch artifact tags
-         //         ConnectionHandler.runPreparedQuery(DELETE_TO_BRANCH_TAG_DATA, SQL3DataType.INTEGER, toBranch.getBranchId(),
-         //               SQL3DataType.INTEGER, fromBranchId);
-         //         //move artifact tags from fromBranch to toBranch
-         //         ConnectionHandler.runPreparedQuery(MOVE_TAG_DATA, SQL3DataType.INTEGER, toBranch.getBranchId(),
-         //               SQL3DataType.INTEGER, fromBranchId);
+         ConnectionHandler.runPreparedUpdate(DELETE_TO_BRANCH_TAG_DATA, SQL3DataType.INTEGER, toBranch.getBranchId(),
+               SQL3DataType.INTEGER, fromBranchId);
+         //move artifact tags from fromBranch to toBranch
+         ConnectionHandler.runPreparedUpdate(MOVE_TAG_DATA, SQL3DataType.INTEGER, toBranch.getBranchId(),
+               SQL3DataType.INTEGER, fromBranchId);
       }
    }
 }
