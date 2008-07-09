341560aef187ccc0eb1f00008abebea858c55379
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-07-09 19:59:29 +0000

    svn merge -r 18363:18974 "https://dev.eclipse.org/svnroot/technology/org.eclipse.osee/branches/MS_0.2.0_20080619/org.eclipse.osee.framework.jdk.core@HEAD"

--- org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/type/CompositeKeyHashMap.java
+++ org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/type/CompositeKeyHashMap.java
@@ -25,140 +25,148 @@ import java.util.Set;
  */
 public class CompositeKeyHashMap<KeyOne, KeyTwo, Value> implements Map<CompositeKeyHashMap.CompositeKey<KeyOne, KeyTwo>, Value> {
 
-    private final Map<CompositeKey<KeyOne, KeyTwo>, Value> map;
+   private final Map<CompositeKey<KeyOne, KeyTwo>, Value> map;
 
-    private final ThreadLocal<CompositeKey<KeyOne, KeyTwo>> threadLocalKey = new ThreadLocal<CompositeKey<KeyOne, KeyTwo>>() {
+   private final ThreadLocal<CompositeKey<KeyOne, KeyTwo>> threadLocalKey =
+         new ThreadLocal<CompositeKey<KeyOne, KeyTwo>>() {
 
-	@Override
-	protected CompositeKey<KeyOne, KeyTwo> initialValue() {
-	    return new CompositeKey<KeyOne, KeyTwo>();
-	}
-	
-    };
+            @Override
+            protected CompositeKey<KeyOne, KeyTwo> initialValue() {
+               return new CompositeKey<KeyOne, KeyTwo>();
+            }
 
-    public static final class CompositeKey<A, B> {
-	private A key1;
-	private B key2;
+         };
 
-	public CompositeKey() {
+   public static final class CompositeKey<A, B> {
+      private A key1;
+      private B key2;
 
-	}
+      public CompositeKey() {
 
-	public CompositeKey(A key1, B key2) {
-	    setKeys(key1, key2);
-	}
+      }
 
-	public A getKey1() {
-	    return key1;
-	}
+      public CompositeKey(A key1, B key2) {
+         setKeys(key1, key2);
+      }
 
-	public B getKey2() {
-	    return key2;
-	}
+      public A getKey1() {
+         return key1;
+      }
 
-	public CompositeKey<A, B> setKeys(A key1, B key2) {
-	    this.key1 = key1;
-	    this.key2 = key2;
-	    return this;
-	}
+      public B getKey2() {
+         return key2;
+      }
 
-	@Override
-	public boolean equals(Object obj) {
-	    if (obj instanceof CompositeKey) {
-		final CompositeKey<A, B> otherKey = (CompositeKey<A, B>) obj;
-		return otherKey.key1.equals(key1) && otherKey.key2.equals(key2);
-	    }
-	    return false;
-	}
+      public CompositeKey<A, B> setKeys(A key1, B key2) {
+         this.key1 = key1;
+         this.key2 = key2;
+         return this;
+      }
 
-	@Override
-	public int hashCode() {
-	    int hashCode = 11;
-	    hashCode = hashCode * 31 + key1.hashCode();
-	    hashCode = hashCode * 31 + key2.hashCode();
-	    return hashCode;
-	}
+      @Override
+      public boolean equals(Object obj) {
+         if (obj instanceof CompositeKey) {
+            final CompositeKey<A, B> otherKey = (CompositeKey<A, B>) obj;
+            return otherKey.key1.equals(key1) && otherKey.key2.equals(key2);
+         }
+         return false;
+      }
 
-    }
+      @Override
+      public int hashCode() {
+         int hashCode = 11;
+         hashCode = hashCode * 31 + key1.hashCode();
+         hashCode = hashCode * 31 + key2.hashCode();
+         return hashCode;
+      }
 
-    public CompositeKeyHashMap() {
-	map = new HashMap<CompositeKey<KeyOne, KeyTwo>, Value>();
-    }
+      /* (non-Javadoc)
+       * @see java.lang.Object#toString()
+       */
+      @Override
+      public String toString() {
+         return "key1: \"" + key1 + "\" + key2: \"" + key2 + "\"";
+      }
+   }
 
-    public CompositeKeyHashMap(Map<CompositeKey<KeyOne, KeyTwo>, Value> map) {
-	this.map = map;
-    }
+   public CompositeKeyHashMap() {
+      map = new HashMap<CompositeKey<KeyOne, KeyTwo>, Value>();
+   }
 
-    public CompositeKeyHashMap(int initialCapacity) {
-	map = new HashMap<CompositeKey<KeyOne, KeyTwo>, Value>(initialCapacity);
-    }
+   public CompositeKeyHashMap(Map<CompositeKey<KeyOne, KeyTwo>, Value> map) {
+      this.map = map;
+   }
 
-    public void clear() {
-	map.clear();
-    }
+   public CompositeKeyHashMap(int initialCapacity) {
+      map = new HashMap<CompositeKey<KeyOne, KeyTwo>, Value>(initialCapacity);
+   }
 
-    public boolean containsKey(Object key) {
-	return map.containsKey(key);
-    }
+   public void clear() {
+      map.clear();
+   }
 
-    public boolean containsKey(KeyOne a, KeyTwo b) {
-	return map.containsKey(threadLocalKey.get().setKeys(a, b));
-    }
+   public boolean containsKey(Object key) {
+      return map.containsKey(key);
+   }
 
-    public boolean containsValue(Object value) {
-	return map.containsValue(value);
-    }
+   public boolean containsKey(KeyOne a, KeyTwo b) {
+      return map.containsKey(threadLocalKey.get().setKeys(a, b));
+   }
 
-    public Set<Map.Entry<CompositeKey<KeyOne, KeyTwo>, Value>> entrySet() {
-	return map.entrySet();
-    }
+   public boolean containsValue(Object value) {
+      return map.containsValue(value);
+   }
 
-    public Value get(Object key) {
-	if (CompositeKey.class.isInstance(key)) {
-	    return map.get(key);
-	} else {
-	    throw new IllegalArgumentException(String.format("Expected Type [CompositeKey], got type [%s].",
-		    key.getClass().getName()));
-	}
-    }
+   public Set<Map.Entry<CompositeKey<KeyOne, KeyTwo>, Value>> entrySet() {
+      return map.entrySet();
+   }
 
-    public Value get(KeyOne a, KeyTwo b) {
-	return map.get(threadLocalKey.get().setKeys(a, b));
-    }
+   public Value get(Object key) {
+      if (CompositeKey.class.isInstance(key)) {
+         return map.get(key);
+      } else {
+         throw new IllegalArgumentException(String.format("Expected Type [CompositeKey], got type [%s].",
+               key.getClass().getName()));
+      }
+   }
 
-    public boolean isEmpty() {
-	return map.isEmpty();
-    }
+   public Value get(KeyOne a, KeyTwo b) {
+      return map.get(threadLocalKey.get().setKeys(a, b));
+   }
 
-    public Set<CompositeKey<KeyOne, KeyTwo>> keySet() {
-	return map.keySet();
-    }
+   public boolean isEmpty() {
+      return map.isEmpty();
+   }
 
-    public Value put(CompositeKey<KeyOne, KeyTwo> key, Value value) {
-	return map.put(key, value);
-    }
+   public Set<CompositeKey<KeyOne, KeyTwo>> keySet() {
+      return map.keySet();
+   }
 
-    public Value put(KeyOne a, KeyTwo b, Value value) {
-	return map.put(new CompositeKey<KeyOne, KeyTwo>(a, b), value);
-    }
+   public Value put(CompositeKey<KeyOne, KeyTwo> key, Value value) {
+      return map.put(key, value);
+   }
 
-    public void putAll(Map<? extends CompositeKey<KeyOne, KeyTwo>, ? extends Value> m) {
-	map.putAll(m);
-    }
+   public Value put(KeyOne a, KeyTwo b, Value value) {
+      return map.put(new CompositeKey<KeyOne, KeyTwo>(a, b), value);
+   }
 
-    public Value remove(Object key) {
-	return map.remove(key);
-    }
+   public void putAll(Map<? extends CompositeKey<KeyOne, KeyTwo>, ? extends Value> m) {
+      map.putAll(m);
+   }
 
-    public Value remove(KeyOne a, KeyTwo b) {
-	return map.remove(threadLocalKey.get().setKeys(a, b));
-    }
+   public Value remove(Object key) {
+      return map.remove(key);
+   }
 
-    public int size() {
-	return map.size();
-    }
+   public Value remove(KeyOne a, KeyTwo b) {
+      return map.remove(threadLocalKey.get().setKeys(a, b));
+   }
 
-    public Collection<Value> values() {
-	return map.values();
-    }
+   public int size() {
+      return map.size();
+   }
+
+   public Collection<Value> values() {
+      return map.values();
+   }
 }
