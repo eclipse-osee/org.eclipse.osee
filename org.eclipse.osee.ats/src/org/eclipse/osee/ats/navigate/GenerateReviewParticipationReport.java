afd67de8157cff7da4b06b79a27ebac9a00aef45
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-07-16 22:38:45 +0000

    Review participation report

--- org.eclipse.osee.ats/src/org/eclipse/osee/ats/navigate/GenerateReviewParticipationReport.java
+++ org.eclipse.osee.ats/src/org/eclipse/osee/ats/navigate/GenerateReviewParticipationReport.java
@@ -13,38 +13,29 @@ package org.eclipse.osee.ats.navigate;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.osee.ats.AtsPlugin;
-import org.eclipse.osee.ats.artifact.ATSAttributes;
-import org.eclipse.osee.ats.artifact.ReviewSMArtifact;
-import org.eclipse.osee.ats.util.widgets.role.UserRole;
 import org.eclipse.osee.ats.util.xviewer.column.XViewerReviewCompletedDateColumn;
 import org.eclipse.osee.ats.util.xviewer.column.XViewerReviewRoleColumn;
 import org.eclipse.osee.ats.util.xviewer.column.XViewerSmaStateColumn;
 import org.eclipse.osee.ats.world.AtsXColumn;
 import org.eclipse.osee.ats.world.search.MyReviewWorkflowItem;
 import org.eclipse.osee.ats.world.search.MyReviewWorkflowItem.ReviewState;
-import org.eclipse.osee.framework.jdk.core.util.AHTML;
 import org.eclipse.osee.framework.skynet.core.User;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.ui.plugin.util.AWorkbench;
 import org.eclipse.osee.framework.ui.skynet.artifact.massEditor.MassArtifactEditor;
 import org.eclipse.osee.framework.ui.skynet.artifact.massEditor.MassArtifactEditorInput;
-import org.eclipse.osee.framework.ui.skynet.util.OSEELog;
 import org.eclipse.osee.framework.ui.skynet.widgets.XDate;
 import org.eclipse.osee.framework.ui.skynet.widgets.dialog.UserListDialog;
 import org.eclipse.osee.framework.ui.skynet.widgets.xnavigate.XNavigateItem;
 import org.eclipse.osee.framework.ui.skynet.widgets.xnavigate.XNavigateItemAction;
 import org.eclipse.osee.framework.ui.skynet.widgets.xnavigate.XNavigateComposite.TableLoadOption;
-import org.eclipse.osee.framework.ui.skynet.widgets.xresults.XResultPage;
-import org.eclipse.osee.framework.ui.skynet.widgets.xresults.XResultView;
-import org.eclipse.osee.framework.ui.skynet.widgets.xresults.XResultPage.Manipulations;
 import org.eclipse.osee.framework.ui.skynet.widgets.xviewer.XViewerColumn;
 import org.eclipse.osee.framework.ui.skynet.widgets.xviewer.skynet.column.XViewerArtifactNameColumn;
 import org.eclipse.osee.framework.ui.skynet.widgets.xviewer.skynet.column.XViewerArtifactTypeColumn;
@@ -90,58 +81,18 @@ public class GenerateReviewParticipationReport extends XNavigateItemAction {
 
       public IStatus run(IProgressMonitor monitor) {
          try {
-            final String html =
-                  getReleaseReportHtml(getName() + " - " + XDate.getDateNow(XDate.MMDDYYHHMM), user, monitor);
-            Display.getDefault().asyncExec(new Runnable() {
-               public void run() {
-                  XResultView.getResultView().addResultPage(
-                        new XResultPage(getName(), html, Manipulations.HTML_MANIPULATIONS));
-                  AWorkbench.popup("Complete", getName() + " Complete...Results in ATS Results");
-               }
-            });
-
-         } catch (SQLException ex) {
+            MyReviewWorkflowItem srch = new MyReviewWorkflowItem("", user, ReviewState.All);
+            Collection<Artifact> reviewArts = srch.performSearchGetResults();
+            MassArtifactEditorInput input =
+                  new MassArtifactEditorInput(getName() + " as of " + XDate.getDateNow(), reviewArts, getColumns(user));
+            MassArtifactEditor.editArtifacts(input);
+         } catch (Exception ex) {
             return new Status(Status.ERROR, AtsPlugin.PLUGIN_ID, -1, ex.toString(), ex);
          }
          monitor.done();
          return Status.OK_STATUS;
       }
 
-   }
-
-   public static String getReleaseReportHtml(String title, User user, IProgressMonitor monitor) throws SQLException {
-      if (user == null) {
-         AWorkbench.popup("ERROR", "Must select product, config and version.");
-         return null;
-      }
-      StringBuilder sb = new StringBuilder();
-      sb.append(AHTML.heading(3, title + " as of " + XDate.getDateNow(), user.getDescriptiveName()));
-      sb.append(AHTML.beginMultiColumnTable(100, 1));
-      sb.append(AHTML.addHeaderRowMultiColumnTable(new String[] {"Id", "Legacy Id", "Date", "Type", "Role", "State",
-            "Title"}));
-      MyReviewWorkflowItem srch = new MyReviewWorkflowItem("", user, ReviewState.All);
-      try {
-         Collection<Artifact> reviewArts = srch.performSearchGetResults();
-         for (Artifact art : reviewArts) {
-            ReviewSMArtifact reviewArt = (ReviewSMArtifact) art;
-            try {
-               sb.append(AHTML.addRowMultiColumnTable(new String[] {reviewArt.getHumanReadableId(),
-                     reviewArt.getSoleAttributeValue(ATSAttributes.LEGACY_PCR_ID_ATTRIBUTE.getStoreName(), ""),
-                     getDateString(reviewArt.getWorldViewCompletedDate()), reviewArt.getArtifactTypeName(),
-                     getRolesStr(reviewArt, user), reviewArt.getSmaMgr().getStateMgr().getCurrentStateName(),
-                     reviewArt.getDescriptiveName()}));
-            } catch (Exception ex) {
-               OSEELog.logException(AtsPlugin.class, ex, false);
-            }
-         }
-         MassArtifactEditorInput input =
-               new MassArtifactEditorInput(title + " as of " + XDate.getDateNow(), reviewArts, getColumns(user));
-         MassArtifactEditor.editArtifacts(input);
-      } catch (Exception ex) {
-         OSEELog.logException(AtsPlugin.class, ex, false);
-      }
-      sb.append(AHTML.endMultiColumnTable());
-      return sb.toString();
    }
 
    private static List<XViewerColumn> getColumns(User user) throws OseeCoreException, SQLException {
@@ -160,19 +111,6 @@ public class GenerateReviewParticipationReport extends XNavigateItemAction {
       //               SWT.LEFT, false, XViewerAttributeSortDataType.get(attributeType)));
       //      }
       return columns;
-   }
-
-   private static String getRolesStr(ReviewSMArtifact reviewArt, User user) throws OseeCoreException, SQLException {
-      String str = "";
-      for (UserRole role : reviewArt.getUserRoleManager().getUserRoles()) {
-         if (role.getUser().equals(user)) str += role.getRole().name() + ", ";
-      }
-      return str.replaceFirst(", $", "");
-   }
-
-   private static String getDateString(Date date) {
-      if (date == null) return "";
-      return XDate.getDateStr(date, XDate.MMDDYY);
    }
 
 }
