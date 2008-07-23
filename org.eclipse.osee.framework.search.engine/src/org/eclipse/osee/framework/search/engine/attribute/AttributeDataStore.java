b65b89d8f4bf8c88d30b5a0cdd1b6aac61253626
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-07-23 23:27:16 +0000

    Improved search query.

--- org.eclipse.osee.framework.search.engine/src/org/eclipse/osee/framework/search/engine/attribute/AttributeDataStore.java
+++ org.eclipse.osee.framework.search.engine/src/org/eclipse/osee/framework/search/engine/attribute/AttributeDataStore.java
@@ -31,7 +31,7 @@ public class AttributeDataStore {
    private static final String RESTRICT_BRANCH = " AND txd1.branch_id = ?";
 
    private static final String SELECT_ATTRIBUTE_BY_TAG =
-         "select attr1.art_id, attr1.gamma_id, attr1.VALUE, attr1.uri, attrtype.tagger_id, txd1.branch_id from osee_define_attribute attr1, osee_define_attribute_type attrtype, osee_search_tags ost1, osee_define_txs txs1, osee_define_tx_details txd1, osee_define_artifact_version artv1, osee_define_txs txs2, osee_define_tx_details txd2 where attrtype.attr_type_id = attr1.attr_type_id AND ost1.gamma_id = attr1.gamma_id AND ost1.gamma_id = txs1.gamma_id AND txd1.transaction_id = txs1.transaction_id AND txs2.gamma_id = artv1.gamma_id AND artv1.art_id = attr1.art_id AND txd2.transaction_id = txs2.transaction_id AND txd1.branch_id = txd2.branch_id AND 1 = (SELECT count(1) FROM osee.osee_join_search_tags jq1 WHERE jq1.query_id = ? AND ost1.coded_tag_id = jq1.coded_tag_id)";
+         "select attr1.art_id, attr1.gamma_id, attr1.VALUE, attr1.uri, attrtype.tagger_id, txd1.branch_id from osee_define_attribute attr1, osee_define_attribute_type attrtype, osee_search_tags ost1, osee_define_txs txs1, osee_define_tx_details txd1, osee_define_artifact_version artv1, osee_define_txs txs2, osee_define_tx_details txd2, osee.osee_join_search_tags jq1 where attrtype.attr_type_id = attr1.attr_type_id AND ost1.gamma_id = attr1.gamma_id AND ost1.gamma_id = txs1.gamma_id AND txd1.transaction_id = txs1.transaction_id AND txs2.gamma_id = artv1.gamma_id AND artv1.art_id = attr1.art_id AND txd2.transaction_id = txs2.transaction_id AND txd1.branch_id = txd2.branch_id AND ost1.coded_tag_id = jq1.coded_tag_id AND jq1.query_id = ?";
 
    private AttributeDataStore() {
    }
