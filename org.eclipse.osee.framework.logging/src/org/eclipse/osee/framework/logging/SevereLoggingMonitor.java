8436388bd47085327f4afb4a6cb942062d75be4b
afinkbein <afinkbein@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-07-23 14:17:58 +0000

    Added Functionality

---
+++ org.eclipse.osee.framework.logging/src/org/eclipse/osee/framework/logging/SevereLoggingMonitor.java
@@ -0,0 +1,46 @@
+package org.eclipse.osee.framework.logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.regex.Pattern;
+
+public class SevereLoggingMonitor implements ILoggerListener {
+
+	private List<IHealthStatus> status = new ArrayList<IHealthStatus>();
+	private ILoggerFilter filter = new ILoggerFilter(){
+
+		@Override
+		public Pattern bundleId() {
+			return null;
+		}
+
+		@Override
+		public Level getLoggerLevel() {
+			return Level.SEVERE;
+		}
+
+		@Override
+		public Pattern name() {
+			return null;
+		}
+		
+	};
+	
+	@Override
+	public ILoggerFilter getFilter() {
+		return filter;
+	}
+
+	@Override
+	public void log(String loggerName, String bundleId, Level level,
+			String message, Throwable th) {
+		status.add(new BaseStatus(level, message, th));
+	}
+
+	public List<IHealthStatus> getSevereLogs(){
+		return status;
+	}
+	
+	
+}
