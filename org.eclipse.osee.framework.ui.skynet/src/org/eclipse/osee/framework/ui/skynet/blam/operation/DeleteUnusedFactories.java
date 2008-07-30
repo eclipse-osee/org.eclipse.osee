df8cc8edd483fb43468c8cba8d2777af7c3c1280
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-07-30 23:17:35 +0000

    [Y9TZW] - "Create BLAM to remove unused artifact factories"

---
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/DeleteUnusedFactories.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.db.connection.ConnectionHandler;
+import org.eclipse.osee.framework.ui.skynet.blam.BlamVariableMap;
+
+/**
+ * @author Ryan D. Brooks
+ */
+public class DeleteUnusedFactories extends AbstractBlam {
+   private static final String DELETE_UNUSED_FACTORIES =
+         "delete from osee_define_factory f1 where not exists (select '1' from osee_define_artifact_type f2 where f1.factory_id = f2.factory_id)";
+
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.BlamOperation#runOperation(org.eclipse.osee.framework.ui.skynet.blam.BlamVariableMap, org.eclipse.osee.framework.skynet.core.artifact.Branch, org.eclipse.core.runtime.IProgressMonitor)
+    */
+   public void runOperation(BlamVariableMap variableMap, IProgressMonitor monitor) throws Exception {
+      ConnectionHandler.runPreparedUpdate(DELETE_UNUSED_FACTORIES);
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.BlamOperation#getXWidgetXml()
+    */
+   public String getXWidgetsXml() {
+      return emptyXWidgetsXml;
+   }
+}
