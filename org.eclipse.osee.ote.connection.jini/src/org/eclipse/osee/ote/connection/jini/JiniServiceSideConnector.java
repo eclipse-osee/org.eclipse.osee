56dcdd8a41255aa4af7ec3dddccf8a56110a90ba
kaguilar <kaguilar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-10-28 16:44:17 +0000

    corrected possible concurrent modification exception

--- org.eclipse.osee.ote.connection.jini/src/org/eclipse/osee/ote/connection/jini/JiniServiceSideConnector.java
+++ org.eclipse.osee.ote.connection.jini/src/org/eclipse/osee/ote/connection/jini/JiniServiceSideConnector.java
@@ -7,6 +7,7 @@ import java.net.UnknownHostException;
 import java.rmi.Remote;
 import java.rmi.server.ExportException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Timer;
 import java.util.logging.Level;
 
@@ -29,128 +30,147 @@ import org.eclipse.osee.ote.connection.jini.util.LeaseRenewTask;
  */
 public class JiniServiceSideConnector extends JiniConnector implements
 	IJiniConnectorLink {
-   public static final String TYPE = "jini.service-end";
-   private final HashMap<ServiceRegistration, LeaseRenewTask> registrations =
-         new HashMap<ServiceRegistration, LeaseRenewTask>();
-   private final Remote service;
-   private final Remote serviceProxy;
-   private final BasicJeriExporter serviceExporter;
-   private final ServiceID serviceId;
-   private final Timer timer = new Timer();
-   private final ServiceItem serviceItem;
-   private final BasicJeriExporter linkExporter;
-   private final IJiniConnectorLink exportedThis;
-   public JiniServiceSideConnector(Remote service, EnhancedProperties props) throws UnknownHostException, ExportException {
-      super(props);
-      this.service = service;
-      serviceId = generateServiceId();
-      serviceExporter =
-            new BasicJeriExporter(TcpServerEndpoint.getInstance(Network.getValidIP().getHostAddress(), 0),
-                  new BasicILFactory(null, null, Activator.getDefault().getExportClassLoader()), false, false);
-      linkExporter = new BasicJeriExporter(TcpServerEndpoint.getInstance(
+    public static final String TYPE = "jini.service-end";
+    private final HashMap<ServiceRegistration, LeaseRenewTask> registrations = new HashMap<ServiceRegistration, LeaseRenewTask>();
+    private final Remote service;
+    private final Remote serviceProxy;
+    private final BasicJeriExporter serviceExporter;
+    private final ServiceID serviceId;
+    private final Timer timer = new Timer();
+    private final ServiceItem serviceItem;
+    private final BasicJeriExporter linkExporter;
+    private final IJiniConnectorLink exportedThis;
+
+    public JiniServiceSideConnector(Remote service, EnhancedProperties props)
+	    throws UnknownHostException, ExportException {
+	super(props);
+	this.service = service;
+	serviceId = generateServiceId();
+	serviceExporter = new BasicJeriExporter(TcpServerEndpoint.getInstance(
 		Network.getValidIP().getHostAddress(), 0), new BasicILFactory(
 		null, null, Activator.getDefault().getExportClassLoader()),
 		false, false);
-      serviceProxy = (Remote) serviceExporter.export(service);
-      exportedThis = (IJiniConnectorLink) linkExporter.export(this);
-      props.setProperty(LINK_PROPERTY, (Serializable) exportedThis);
-      serviceItem = new ServiceItem(serviceId, serviceProxy, createEntries());
-   }
+	linkExporter = new BasicJeriExporter(TcpServerEndpoint.getInstance(
+		Network.getValidIP().getHostAddress(), 0), new BasicILFactory(
+			null, null, Activator.getDefault().getExportClassLoader()),
+		false, false);
+	serviceProxy = (Remote) serviceExporter.export(service);
+	exportedThis = (IJiniConnectorLink) linkExporter.export(this);
+	props.setProperty(LINK_PROPERTY, (Serializable) exportedThis);
+	serviceItem = new ServiceItem(serviceId, serviceProxy, createEntries());
+    }
 
-   /* (non-Javadoc)
-    * @see org.eclipse.osee.connection.service.IServiceConnector#getService()
-    */
-   @Override
-   public Remote getService() {
-      return service;
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.osee.connection.service.IServiceConnector#getService()
+     */
+    @Override
+    public Remote getService() {
+	return service;
+    }
 
-   /* (non-Javadoc)
-    * @see org.eclipse.osee.connection.service.IServiceConnector#stop()
-    */
-   @Override
-   public synchronized void stop() throws Exception {
-      super.stop();
-      removeAllRegistrations();
-      serviceExporter.unexport(true);
-      linkExporter.unexport(true);
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.osee.connection.service.IServiceConnector#stop()
+     */
+    @Override
+    public synchronized void stop() throws Exception {
+	super.stop();
+	removeAllRegistrations();
+	serviceExporter.unexport(true);
+	linkExporter.unexport(true);
+    }
 
-   /**
-    * this method will cancel all current registrations of this connector
-    */
-   synchronized void removeAllRegistrations() {
-      for (ServiceRegistration registration : registrations.keySet()) {
-         try {
-            removeRegistration(registration);
-         } catch (Exception e) {
-            Activator.log(Level.SEVERE, "exception removing registration",
+    /**
+     * this method will cancel all current registrations of this connector
+     */
+    synchronized void removeAllRegistrations() {
+	for (ServiceRegistration registration : registrations.keySet()) {
+	    try {
+		removeRegistration(registration);
+	    } catch (Exception e) {
+		Activator.log(Level.SEVERE, "exception removing registration",
 			e);
-         }
-      }
-   }
+	    }
+	}
+    }
 
-   private ServiceID generateServiceId() {
-      Uuid uuid = UuidFactory.generate();
-      Long lsb = new Long(uuid.getLeastSignificantBits());
-      Long msb = new Long(uuid.getMostSignificantBits());
-      return new ServiceID(msb.longValue(), lsb.longValue());
-   }
+    private ServiceID generateServiceId() {
+	Uuid uuid = UuidFactory.generate();
+	Long lsb = new Long(uuid.getLeastSignificantBits());
+	Long msb = new Long(uuid.getMostSignificantBits());
+	return new ServiceID(msb.longValue(), lsb.longValue());
+    }
 
-   synchronized void addRegistration(ServiceRegistration registration) {
-      registrations.put(registration, new LeaseRenewTask(timer, registration));
-   }
+    synchronized void addRegistration(ServiceRegistration registration) {
+	registrations
+		.put(registration, new LeaseRenewTask(timer, registration));
+    }
 
-   synchronized void removeRegistration(ServiceRegistration registration) {
-      System.out.println("removing registration");
-      LeaseRenewTask task = registrations.remove(registration);
-      if (task != null) {
-         task.cancel();
-      }
-   }
+    synchronized void removeRegistration(ServiceRegistration registration) {
+	System.out.println("removing registration");
+	LeaseRenewTask task = registrations.remove(registration);
+	if (task != null) {
+	    task.cancel();
+	}
+    }
 
-   ServiceItem getServiceItem() {
-      return serviceItem;
-   }
+    ServiceItem getServiceItem() {
+	return serviceItem;
+    }
 
-   private synchronized void setAttributes(Entry[] entry) {
-      for (ServiceRegistration registration : registrations.keySet()) {
-         try {
-            registration.setAttributes(entry);
-         } catch (Exception ex) {
-             Activator.log(Level.SEVERE, "exception setting attributes", ex);
-            registrations.remove(registration);
-         }
-      }
-   }
+    private synchronized void setAttributes(Entry[] entry) {
+	Iterator<ServiceRegistration> iter = registrations.keySet().iterator();
+	while (iter.hasNext()) {
+	    ServiceRegistration registration = iter.next();
+	    try {
+		registration.setAttributes(entry);
+	    } catch (Exception ex) {
+		Activator.log(Level.SEVERE, "exception setting attributes", ex);
+		registrations.remove(registration);
+	    }
+	}
+    }
 
-   /* (non-Javadoc)
-    * @see org.eclipse.osee.connection.service.IServiceConnector#getType()
-    */
-   @Override
-   public String getConnectorType() {
-      return TYPE;
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.osee.connection.service.IServiceConnector#getType()
+     */
+    @Override
+    public String getConnectorType() {
+	return TYPE;
+    }
 
-   /* (non-Javadoc)
-    * @see org.eclipse.osee.ote.connection.jini.JiniConnector#setProperty(java.lang.String, java.lang.String)
-    */
-   @Override
-   public void setProperty(String key, Serializable value) {
-      super.setProperty(key, value);
-      setAttributes(createEntries());
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.osee.ote.connection.jini.JiniConnector#setProperty(java.lang
+     * .String, java.lang.String)
+     */
+    @Override
+    public void setProperty(String key, Serializable value) {
+	super.setProperty(key, value);
+	setAttributes(createEntries());
+    }
 
-   /* (non-Javadoc)
-    * @see org.eclipse.osee.connection.service.IServiceConnector#upload(java.io.File)
-    */
-   @Override
-   public URI upload(File file) throws Exception {
-      return null;
-   }
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.osee.connection.service.IServiceConnector#upload(java.io.
+     * File)
+     */
+    @Override
+    public URI upload(File file) throws Exception {
+	return null;
+    }
 
-   @Override
-   public boolean ping() {
+    @Override
+    public boolean ping() {
 	return true;
     }
 
