c2389a7a4598d0ab6dd6b026bea5023545ecd2d1
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-12-02 18:57:51 +0000

    Updated bash scripts to use lba8 server.

--- org.eclipse.osee.framework.server.admin/osee_server_bundles/runServer-ProductionDev.sh
+++ org.eclipse.osee.framework.server.admin/osee_server_bundles/runServer-ProductionDev.sh
@@ -1,6 +1,8 @@
-java -Dorg.osgi.service.http.port=8089 \
+java -Dorg.osgi.service.http.port=8091 \
 -Dosgi.compatibility.bootdelegation=true \
--Xmx512m -Dosee.db.connection.id=oracle7 \
+-Xmx512m \
+-Dosee.db.connection.id=oracle8Client \
 -Dequinox.ds.debug=true \
--Dosee.application.server.data=/lba_oseex/osee_backup/production_server_data \
--jar org.eclipse.osgi_3.4.0.v20080326.jar -console 
+-Dosee.application.server.data=/lba_oseex/osee_backup/datastore_lba8_fs \
+-Dosee.check.tag.queue.on.startup=true \
+-jar org.eclipse.osgi_3.4.0.v20080326.jar -console
