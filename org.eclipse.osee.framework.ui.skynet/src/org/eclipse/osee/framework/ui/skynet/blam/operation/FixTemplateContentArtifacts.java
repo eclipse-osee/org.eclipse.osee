4a2662a102e36461848af8da6aa5026bf4917b97
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2008-12-05 02:51:07 +0000

    Fixes for template content Blam

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/FixTemplateContentArtifacts.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/FixTemplateContentArtifacts.java
@@ -2,9 +2,12 @@ package org.eclipse.osee.framework.ui.skynet.blam.operation;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -12,13 +15,13 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.logging.Level;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.osee.framework.core.client.ClientSessionManager;
 import org.eclipse.osee.framework.core.client.server.HttpUrlBuilder;
-import org.eclipse.osee.framework.core.data.OseeInfo;
 import org.eclipse.osee.framework.core.data.OseeServerContext;
-import org.eclipse.osee.framework.core.exception.OseeAuthenticationRequiredException;
 import org.eclipse.osee.framework.db.connection.ConnectionHandlerStatement;
 import org.eclipse.osee.framework.db.connection.exception.OseeCoreException;
 import org.eclipse.osee.framework.db.connection.exception.OseeDataStoreException;
@@ -33,6 +36,7 @@ import org.eclipse.osee.framework.jdk.core.util.xml.Jaxp;
 import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.attribute.AttributeTypeManager;
 import org.eclipse.osee.framework.skynet.core.attribute.WordAttribute;
+import org.eclipse.osee.framework.ui.plugin.util.OseeData;
 import org.eclipse.osee.framework.ui.skynet.SkynetGuiPlugin;
 import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
 import org.eclipse.osee.framework.ui.skynet.render.WordTemplateRenderer;
@@ -53,30 +57,37 @@ public class FixTemplateContentArtifacts extends AbstractBlam {
    private static final String GET_ATTRS =
          "SELECT * FROM osee_attribute t1, osee_artifact t3 WHERE t1.attr_type_id = ? AND t1.art_id = t3.art_id AND t1.uri is not null";
    private static final String GET_ATTRS_TEST = GET_ATTRS + " AND t1.art_id = 3894";
-   private String applicationServerAddress;
    private Collection<String> badData = new LinkedList<String>();
    private static final String[] columnHeaders = new String[] {"Corrupted Data"};
 
+   private static File createTempFolder() {
+      File rootDirectory = OseeData.getFile("FixTemplate_" + Lib.getDateTimeString() + File.separator);
+      rootDirectory.mkdirs();
+      return rootDirectory;
+   }
+
    @Override
    public void runOperation(VariableMap variableMap, IProgressMonitor monitor) throws Exception {
-      applicationServerAddress = OseeInfo.getValue("osee.resource.server");
+      File backupFolder = createTempFolder();
+      OseeLog.log(SkynetGuiPlugin.class, Level.INFO, String.format("Backup Folder location: [%s]",
+            backupFolder.getAbsolutePath()));
 
       ArrayList<AttrData> attrDatas = loadAttrData();
       monitor.beginTask("Fix word template content", attrDatas.size());
       for (AttrData attrData : attrDatas) {
          monitor.subTask(attrData.getHrid());
-         String content = new String(getResource(attrData.getUri()));
+         Resource resource = getResource(attrData.getUri());
+
          Element rootElement = null;
          if (DEBUG) {
-            OseeLog.log(SkynetGuiPlugin.class, Level.INFO, String.format("Before Fix: %s", content));
+            OseeLog.log(SkynetGuiPlugin.class, Level.INFO, String.format("Before Fix: %s", resource.data));
          }
 
          final Collection<Element> elements = new LinkedList<Element>();
          final Collection<Element> sectPr = new LinkedList<Element>();
          boolean fixedAttribute = false;
          try {
-
-            Document doc = Jaxp.readXmlDocument("<ForFix>" + content + "</ForFix>");
+            Document doc = Jaxp.readXmlDocument("<ForFix>" + resource.data + "</ForFix>");
             rootElement = doc.getDocumentElement();
 
             NodeList nodeList = rootElement.getElementsByTagName("*");
@@ -107,20 +118,19 @@ public class FixTemplateContentArtifacts extends AbstractBlam {
          }
 
          if (fixedAttribute) {
-            //backup old files before changing them
             try {
-               HttpProcessor.put(getStorageURL(attrData.getGammaId(), attrData.getHrid(), "bck", false),
-                     Lib.stringToInputStream(content), "txt/xml", "UTF-8");
-               String fixedContent =
-                     Lib.inputStreamToString(new ByteArrayInputStream(
-                           WordTemplateRenderer.getFormattedContent(rootElement)));
+               // Backup File
+               backupResourceLocally(backupFolder, resource);
 
-               HttpProcessor.put(getStorageURL(attrData.getGammaId(), attrData.getHrid(), attrData.getUri().contains(
-                     ".xml") ? "xml" : null, true), new ByteArrayInputStream(
-                     WordTemplateRenderer.getFormattedContent(rootElement)), "txt/xml", "UTF-8");
+               // Perform Fix
+               resource.data = new String(WordTemplateRenderer.getFormattedContent(rootElement), "UTF-8");
+               resource.encoding = "UTF-8";
+
+               // UploadResource
+               uploadResource(attrData.getGammaId(), resource);
 
                if (DEBUG) {
-                  OseeLog.log(SkynetGuiPlugin.class, Level.INFO, String.format(" After Fix : %s", fixedContent));
+                  OseeLog.log(SkynetGuiPlugin.class, Level.INFO, String.format(" After Fix : %s", resource.data));
                }
             } catch (Exception ex) {
                OseeLog.log(SkynetGuiPlugin.class, Level.SEVERE, String.format(
@@ -159,30 +169,46 @@ public class FixTemplateContentArtifacts extends AbstractBlam {
       return attrData;
    }
 
-   public static URL getStorageURL(String gammaId, String artifactHrid, String extension, boolean allowOverwrite) throws OseeDataStoreException, MalformedURLException, OseeAuthenticationRequiredException {
+   private static void uploadResource(String gammaId, Resource resource) throws Exception {
+      String fileName = resource.resourceName;
       Map<String, String> parameterMap = new HashMap<String, String>();
+      parameterMap.put("sessionId", ClientSessionManager.getSessionId());
+      parameterMap.put("is.overwrite.allowed", String.valueOf(true));
       parameterMap.put("protocol", "attr");
       parameterMap.put("seed", gammaId);
-      parameterMap.put("name", artifactHrid);
-      parameterMap.put("is.overwrite.allowed", String.valueOf(allowOverwrite));
-      parameterMap.put("compress.before.saving", "true");
-      parameterMap.put("sessionId", ClientSessionManager.getSessionId());
 
-      if (Strings.isValid(extension) != false) {
+      String extension = Lib.getExtension(fileName);
+      if (Strings.isValid(extension)) {
          parameterMap.put("extension", extension);
+         int charToRemove = extension.length() + 1;
+         fileName = fileName.substring(0, fileName.length() - charToRemove);
       }
+      parameterMap.put("name", fileName);
+
+      byte[] toUpload = resource.data.getBytes(resource.encoding);
+      if (resource.wasZipped) {
+         toUpload = Lib.compressStream(new ByteArrayInputStream(toUpload), resource.entryName);
+      }
+
       String urlString =
             HttpUrlBuilder.getInstance().getOsgiServletServiceUrl(OseeServerContext.RESOURCE_CONTEXT, parameterMap);
-      return new URL(urlString);
+      HttpProcessor.put(new URL(urlString), new ByteArrayInputStream(toUpload), resource.result.getContentType(),
+            resource.result.getEncoding());
    }
 
-   public byte[] getResource(String resourcePath) throws OseeCoreException {
-      byte[] toReturn = null;
+   private Resource getResource(String resourcePath) throws OseeCoreException {
+      Resource toReturn = null;
       ByteArrayOutputStream sourceOutputStream = new ByteArrayOutputStream();
       try {
-         AcquireResult result = HttpProcessor.acquire(getAcquireUrl(resourcePath), sourceOutputStream);
+         Map<String, String> parameterMap = new HashMap<String, String>();
+         parameterMap.put("sessionId", ClientSessionManager.getSessionId());
+         parameterMap.put("uri", resourcePath);
+         String urlString =
+               HttpUrlBuilder.getInstance().getOsgiServletServiceUrl(OseeServerContext.RESOURCE_CONTEXT, parameterMap);
+
+         AcquireResult result = HttpProcessor.acquire(new URL(urlString), sourceOutputStream);
          if (result.getCode() == HttpURLConnection.HTTP_OK) {
-            toReturn = sourceOutputStream.toByteArray();
+            toReturn = new Resource(resourcePath, result, sourceOutputStream.toByteArray());
          }
       } catch (Exception ex) {
          throw new OseeWrappedException(ex);
@@ -196,24 +222,77 @@ public class FixTemplateContentArtifacts extends AbstractBlam {
       return toReturn;
    }
 
-   private URL getAcquireUrl(String uri) throws OseeCoreException, MalformedURLException {
-      Map<String, String> parameterMap = new HashMap<String, String>();
-      parameterMap.put("uri", uri);
-      parameterMap.put("decompress.before.sending", "true");
-      parameterMap.put("sessionId", ClientSessionManager.getSessionId());
+   private final class Resource {
+      private final String entryName;
+      private final String resourceName;
+      private final AcquireResult result;
+      private final byte[] rawBytes;
+      private final boolean wasZipped;
+      private final String sourcePath;
 
-      String urlString =
-            HttpUrlBuilder.getInstance().getOsgiServletServiceUrl(OseeServerContext.RESOURCE_CONTEXT, parameterMap);
-      return new URL(urlString);
+      private String data;
+      private String encoding;
+
+      private Resource(String sourcePath, AcquireResult result, byte[] rawBytes) throws IOException {
+         this.rawBytes = rawBytes;
+         this.result = result;
+         int index = sourcePath.lastIndexOf('/');
+         this.sourcePath = sourcePath;
+         this.resourceName = sourcePath.substring(index + 1, sourcePath.length());
+         this.wasZipped = result.getContentType().contains("zip");
+         if (wasZipped) {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            this.entryName = decompressStream(new ByteArrayInputStream(rawBytes), outputStream);
+            this.encoding = "UTF-8";
+            this.data = new String(outputStream.toByteArray(), encoding);
+         } else {
+            this.data = new String(rawBytes, result.getEncoding());
+            this.entryName = null;
+            this.encoding = result.getEncoding();
+         }
+      }
    }
 
-   //   private URL getAcquireUrl(String address, String uriPath) throws MalformedURLException, UnsupportedEncodingException {
-   //      if (address.endsWith("/")) {
-   //         address = address.substring(0, address.length() - 1);
-   //      }
-   //      return new URL(String.format("%s/%s?uri=%s&decompress.before.sending=true", address,
-   //            OseeServerContext.RESOURCE_CONTEXT, URLEncoder.encode(uriPath, "UTF-8")));
-   //   }
+   private static void backupResourceLocally(File backupFolder, Resource resource) throws IOException {
+      InputStream inputStream = null;
+      OutputStream outputStream = null;
+      try {
+         String path = resource.sourcePath.replace("attr://", "");
+         path = path.replaceAll("/", File.separator);
+         outputStream = new FileOutputStream(new File(backupFolder, path));
+
+         inputStream = new ByteArrayInputStream(resource.rawBytes);
+         Lib.inputStreamToOutputStream(inputStream, outputStream);
+      } finally {
+         if (inputStream != null) {
+            inputStream.close();
+         }
+         if (outputStream != null) {
+            outputStream.close();
+         }
+      }
+   }
+
+   private static String decompressStream(InputStream inputStream, OutputStream outputStream) throws IOException {
+      String zipEntryName = null;
+      ZipInputStream zipInputStream = null;
+      try {
+         zipInputStream = new ZipInputStream(inputStream);
+         ZipEntry entry = zipInputStream.getNextEntry();
+         zipEntryName = entry.getName();
+         // Transfer bytes from the ZIP file to the output file
+         byte[] buf = new byte[1024];
+         int len;
+         while ((len = zipInputStream.read(buf)) > 0) {
+            outputStream.write(buf, 0, len);
+         }
+      } finally {
+         if (zipInputStream != null) {
+            zipInputStream.close();
+         }
+      }
+      return zipEntryName;
+   }
 
    //To handle the case of sub-sections
    private boolean isBadParagraph(Element paragraph) throws OseeCoreException {
