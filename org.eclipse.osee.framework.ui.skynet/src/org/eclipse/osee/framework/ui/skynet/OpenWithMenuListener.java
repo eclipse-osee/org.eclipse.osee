7261e3672da814f5e7d6724859438892a3bbbee9
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-01-07 18:01:17 +0000

    Improved exception handeling

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/OpenWithMenuListener.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/OpenWithMenuListener.java
@@ -6,16 +6,17 @@ package org.eclipse.osee.framework.ui.skynet;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
 import org.eclipse.core.commands.Command;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.WordArtifact;
 import org.eclipse.osee.framework.ui.skynet.listener.IRebuildMenuListener;
 import org.eclipse.osee.framework.ui.skynet.render.IRenderer;
 import org.eclipse.osee.framework.ui.skynet.render.PreviewRendererData;
 import org.eclipse.osee.framework.ui.skynet.render.RendererManager;
-import org.eclipse.osee.framework.ui.skynet.util.OSEELog;
 import org.eclipse.search.ui.text.Match;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.MenuEvent;
@@ -89,7 +90,7 @@ public class OpenWithMenuListener implements MenuListener {
                   try {
                      item.setImage(previewRenderer.getImage(null));
                   } catch (Exception ex) {
-                     //                     OSEELog.logException(SkynetGuiPlugin.class, ex, false);
+                     OseeLog.log(SkynetGuiPlugin.class, Level.SEVERE, ex);
                   }
                   item.addSelectionListener(new OpenWithSelectionListener(previewRenderer, viewer, true,
                         data.getOption()));
@@ -103,7 +104,7 @@ public class OpenWithMenuListener implements MenuListener {
             try {
                image = renderer.getImage(artifacts.iterator().next());
             } catch (Exception ex) {
-               //               OSEELog.logException(SkynetGuiPlugin.class, ex, false);
+               OseeLog.log(SkynetGuiPlugin.class, Level.SEVERE, ex);
             }
 
             Command command = commandService.getCommand(renderer.getCommandId());
@@ -114,9 +115,8 @@ public class OpenWithMenuListener implements MenuListener {
                menuItem.addSelectionListener(new OpenWithSelectionListener(renderer, viewer, false));
             }
          }
-
       } catch (Exception ex) {
-         OSEELog.logException(SkynetGuiPlugin.class, ex, true);
+         OseeLog.log(SkynetGuiPlugin.class, Level.SEVERE, ex);
       }
    }
 }
