62d809d4ba274205cbfb128aa1be4ceac58dc253
kaguilar <kaguilar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-01-26 17:25:09 +0000

    corrected issue with the OSGI service not unregistering correctly

--- org.eclipse.osee.ote.connection.service/src/org/eclipse/osee/connection/service/Activator.java
+++ org.eclipse.osee.ote.connection.service/src/org/eclipse/osee/connection/service/Activator.java
@@ -7,69 +7,62 @@ import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.plugin.core.util.ExtensionDefinedObjects;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-      private ConnectionServiceImpl service;
-    private ServiceTracker connectionServiceTracker;
+	private ConnectionServiceImpl service;
+	private ServiceRegistration registration;
 
-   /*
-    * (non-Javadoc)
-    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-    */
-   public void start(BundleContext context) throws Exception {
-            service = new ConnectionServiceImpl();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext context) throws Exception {
+		service = new ConnectionServiceImpl();
 
-	// register the service
-	context.registerService(IConnectionService.class.getName(), service,
-		null);
+		// register the service
+		registration = context.registerService(IConnectionService.class.getName(), service, null);
 
-	// create a tracker and track the service
-	connectionServiceTracker = new ServiceTracker(context,
-		IConnectionService.class.getName(), null);
-	connectionServiceTracker.open();
+		// create a tracker and track the service
 
-	ExtensionDefinedObjects<IConnectorContributor> definedObjects = new ExtensionDefinedObjects<IConnectorContributor>(
-		"org.eclipse.osee.connection.service.ext",
-		"ConnectorContribution", "className");
-	try {
-	    List<IConnectorContributor> contributors = definedObjects
-		    .getObjects();
-	    for (IConnectorContributor contributor : contributors) {
+		ExtensionDefinedObjects<IConnectorContributor> definedObjects = new ExtensionDefinedObjects<IConnectorContributor>(
+				"org.eclipse.osee.connection.service.ext", "ConnectorContribution", "className");
 		try {
-		    contributor.init();
-		} catch (Exception e) {
-		    log(Level.SEVERE,
-			    "exception initializing connector contributor", e);
+			List<IConnectorContributor> contributors = definedObjects.getObjects();
+			for (IConnectorContributor contributor : contributors) {
+				try {
+					contributor.init();
+				} catch (Exception e) {
+					log(Level.SEVERE, "exception initializing connector contributor", e);
+				}
+			}
+		} catch (Exception ex) {
+			log(Level.SEVERE, "failed to process OTE runtime library provider extensions", ex);
 		}
-	    }
-	} catch (Exception ex) {
-	    log(
-		    Level.SEVERE,
-		    "failed to process OTE runtime library provider extensions",
-		    ex);
 	}
-   }
 
-   /*
-    * (non-Javadoc)
-    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-    */
-   public void stop(BundleContext context) throws Exception {
-      // close the service tracker
-	connectionServiceTracker.close();
-	connectionServiceTracker = null;
-	service.stop();
-	service = null;
-   }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		service.stop();
+		registration.unregister();
+		service = null;
+	}
 
-   public static void log(Level level, String message, Throwable t) {
-      OseeLog.log(Activator.class, level, message, t);
-   }
+	public static void log(Level level, String message, Throwable t) {
+		OseeLog.log(Activator.class, level, message, t);
+	}
 
-   public static void log(Level level, String message) {
-      log(level, message, null);
-   }
+	public static void log(Level level, String message) {
+		log(level, message, null);
+	}
 
 }
