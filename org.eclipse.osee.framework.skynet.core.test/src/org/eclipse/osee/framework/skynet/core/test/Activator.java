dc31c688f4dfcb23de0bc26d01848b28fc83306a
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-02-03 00:20:15 +0000

    "Team Workflow" - BRB9S - "Hyperlinks not working with multiple OSEE Sessions" "Team Workflow" - 89S5J - "Artifact names need to be updated in hyperlinks of word formatted content when artifact name changes" "Team Workflow" - 01KGT - "When select hyperlink in artifact 9NXQU from V13 branch, get error dialog" "Team Workflow" - HH2RJ - "missing drawings form links in page" "Team Workflow" - 903LQ - "The hyperlinks in {PRI_NAV_BEARING} fail to open the referenced artifact"

--- org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/Activator.java
+++ org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/Activator.java
@@ -5,18 +5,25 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-	}
+   private static BundleContext context;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-	}
+   /*
+    * (non-Javadoc)
+    * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+    */
+   public void start(BundleContext context) throws Exception {
+      Activator.context = context;
+   }
 
+   /*
+    * (non-Javadoc)
+    * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+    */
+   public void stop(BundleContext context) throws Exception {
+      Activator.context = null;
+   }
+
+   public static BundleContext getBundleContext() {
+      return context;
+   }
 }
---
+++ org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/nonproduction/WordMlLinkHandlerTest.java
@@ -0,0 +1,157 @@
+/*
+ * Created on Jan 28, 2009
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.framework.skynet.core.test.nonproduction;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import junit.framework.TestCase;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.jdk.core.util.Strings;
+import org.eclipse.osee.framework.skynet.core.artifact.Branch;
+import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
+import org.eclipse.osee.framework.skynet.core.linking.LinkType;
+import org.eclipse.osee.framework.skynet.core.linking.WordMlLinkHandler;
+import org.eclipse.osee.framework.skynet.core.test.Activator;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class WordMlLinkHandlerTest extends TestCase {
+
+   /**
+    * Data driven test to check document link manager link/unlink methods
+    * 
+    * @throws Exception
+    */
+   public void testLinkUnLink() throws Exception {
+      Branch branch = BranchManager.getBranch(10);
+
+      Map<String, TestData> testMap = getTestData();
+      for (String key : testMap.keySet()) {
+         TestData testData = testMap.get(key);
+
+         InputStream dataStream = null;
+         InputStream expectedStream = null;
+         try {
+            dataStream = new BufferedInputStream(testData.data.openStream());
+            expectedStream = new BufferedInputStream(testData.expected.openStream());
+
+            LinkType docType = testData.docType;
+            boolean isLinkTest = testData.isLink;
+
+            String input = Lib.inputStreamToString(dataStream);
+
+            String actual = null;
+            if (isLinkTest) {
+               actual = WordMlLinkHandler.link(docType, branch, input);
+            } else {
+               actual = WordMlLinkHandler.unlink(docType, input);
+            }
+            String expected = Lib.inputStreamToString(expectedStream);
+            assertEquals(String.format("%s: [%s] ", isLinkTest ? "Link" : "UnLink", key), expected, actual);
+         } finally {
+            if (dataStream != null) {
+               try {
+                  dataStream.close();
+               } catch (IOException ex) {
+               }
+            }
+            if (expectedStream != null) {
+               try {
+                  expectedStream.close();
+               } catch (IOException ex) {
+               }
+            }
+         }
+      }
+   }
+
+   private String getFileName(String name) {
+      int index = name.lastIndexOf("/");
+      if (index > -1) {
+         name = name.substring(index + 1, name.length());
+      }
+      if (name.endsWith(".data.xml")) {
+         name = name.substring(0, name.length() - 9);
+      }
+      if (name.endsWith(".expected.xml")) {
+         name = name.substring(0, name.length() - 13);
+      }
+      return name;
+   }
+
+   private boolean isDataFile(String name) {
+      return name != null && name.endsWith(".data.xml");
+   }
+
+   private boolean isExpectedFile(String name) {
+      return name != null && name.endsWith(".expected.xml");
+   }
+
+   private LinkType getDocType(String name) {
+      LinkType toReturn = null;
+      int index = name.lastIndexOf('.');
+      if (index > 0) {
+         try {
+            name = name.substring(index + 1, name.length());
+            toReturn = LinkType.valueOf(name.toUpperCase());
+         } catch (Exception ex) {
+            //Do nothing;
+         }
+      }
+      assertNotNull(String.format("Error getting DocType from [%s]", name), toReturn);
+      return toReturn;
+   }
+
+   private boolean isLinkTest(String name) {
+      return !name.contains("unlink");
+   }
+
+   private Map<String, TestData> getTestData() {
+      Map<String, TestData> toReturn = new LinkedHashMap<String, TestData>();
+
+      Bundle bundle = Activator.getBundleContext().getBundle();
+      Enumeration<?> urls = bundle.findEntries("data", "*.*", true);
+      while (urls.hasMoreElements()) {
+         URL url = (URL) urls.nextElement();
+         String name = getFileName(url.getPath());
+         if (Strings.isValid(name) && (url.getPath().endsWith(".data.xml") || url.getPath().endsWith(".expected.xml"))) {
+            String key = name;
+            int index = name.indexOf('.');
+            if (index > 0) {
+               key = name.substring(0, index);
+            }
+            TestData pair = toReturn.get(key);
+            if (pair == null) {
+               pair = new TestData();
+               toReturn.put(key, pair);
+            }
+            if (isDataFile(url.getPath())) {
+               pair.data = url;
+               pair.docType = getDocType(name);
+               pair.isLink = isLinkTest(name);
+            } else if (isExpectedFile(url.getPath())) {
+               pair.expected = url;
+            } else if (pair.data == null || pair.expected == null) {
+               toReturn.remove(pair);
+            }
+         }
+      }
+      return toReturn;
+   }
+   private class TestData {
+      public boolean isLink;
+      private URL data;
+      private URL expected;
+      private LinkType docType;
+   }
+}
