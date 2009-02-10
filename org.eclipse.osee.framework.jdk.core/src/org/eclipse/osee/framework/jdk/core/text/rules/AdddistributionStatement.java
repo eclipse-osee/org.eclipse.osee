a3a14cfd46fa19bea464a104a5b113b9d2ca8c88
mmasterso <mmasterso@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-02-10 00:26:07 +0000

    Sanitation patch applied from Andy Jury involving adding distro statements to classes.

--- org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/text/rules/AdddistributionStatement.java
+++ org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/text/rules/AdddistributionStatement.java
@@ -12,6 +12,7 @@ package org.eclipse.osee.framework.jdk.core.text.rules;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.eclipse.osee.framework.jdk.core.text.Rule;
 import org.eclipse.osee.framework.jdk.core.text.change.ChangeSet;
 
@@ -20,12 +21,12 @@ import org.eclipse.osee.framework.jdk.core.text.change.ChangeSet;
  */
 public class AdddistributionStatement extends Rule {
    private static final Pattern classDeclarationP =
-         Pattern.compile("^[/\\s\\*]*(?:Created on [^\n]+)?[\\s\\*]*(?:PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE)?[/\\s\\*]*package");
+         Pattern.compile("^[/\\s\\*]*((?:Created on [^\n]+)?[\\s\\*]*(?:PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE)?[/\\s\\*]*)package");
    private static final char[] distributionStatement =
          "/*******************************************************************************\n * Copyright (c) 2004, 2007 Boeing.\n * All rights reserved. This program and the accompanying materials\n * are made available under the terms of the Eclipse Public License v1.0\n * which accompanies this distribution, and is available at\n * http://www.eclipse.org/legal/epl-v10.html\n *\n * Contributors:\n *     Boeing - initial API and implementation\n *******************************************************************************/\n".toCharArray();
 
    public AdddistributionStatement() {
-      super(null);
+      super("done");
       setFileNamePattern(Pattern.compile(".*\\.java"));
    }
 
@@ -35,7 +36,7 @@ public class AdddistributionStatement extends Rule {
       Matcher matcher = classDeclarationP.matcher(seq);
       if (matcher.find()) {
          setRuleWasApplicable(true);
-         changeSet.replace(0, matcher.end(), distributionStatement);
+         changeSet.replace(0, matcher.end(1), distributionStatement);
       }
 
       return changeSet;
