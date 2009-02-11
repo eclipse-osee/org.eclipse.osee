5832c512de4d44933a8872cc2ce4d225c761aa02
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-02-11 17:49:22 +0000

    "Team Workflow" - 6W2B7 - "Create BLAM to check for errors in the default hierarchy"

---
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/CheckDefaulHierarchy.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import java.util.List;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.db.connection.exception.MultipleArtifactsExist;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.artifact.ArtifactType;
+import org.eclipse.osee.framework.skynet.core.artifact.Branch;
+import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
+import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+
+/**
+ * @author Ryan D. Brooks
+ */
+public class CheckDefaulHierarchy extends AbstractBlam {
+
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.BlamOperation#runOperation(org.eclipse.osee.framework.ui.skynet.blam.VariableMap, org.eclipse.core.runtime.IProgressMonitor)
+    */
+   @Override
+   public void runOperation(VariableMap variableMap, IProgressMonitor monitor) throws Exception {
+      Branch branch = variableMap.getBranch("Branch");
+      ArtifactType artifactType = variableMap.getArtifactType("Artifact Type");
+      List<Artifact> artifacts = ArtifactQuery.getArtifactsFromType(artifactType, branch, false);
+      for (Artifact artifact : artifacts) {
+         try {
+            if (!artifact.hasParent()) {
+               appendResultLine("\n" + artifact.getHumanReadableId() + " has no parent\n");
+            }
+         } catch (MultipleArtifactsExist ex) {
+            appendResultLine("\n" + ex.getLocalizedMessage() + "\n");
+         }
+      }
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.AbstractBlam#getXWidgetsXml()
+    */
+   @Override
+   public String getXWidgetsXml() {
+      return "<xWidgets><XWidget xwidgetType=\"XBranchSelectWidget\" displayName=\"Branch\" /><XWidget xwidgetType=\"XArtifactTypeListViewer\" displayName=\"Artifact Type\" /></xWidgets>";
+   }
+
+}
