a8a8c12b15d045c64a7c77d415756cc34f73a7ba
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-04-11 01:11:11 +0000

    "Team Workflow" - 5HPLV - "Import Test Script Traceability for Block III"

---
+++ org.eclipse.osee.define/src/org/eclipse/osee/define/traceability/action/FindTraceUnitActionDelegate.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.define.traceability.action;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osee.define.traceability.jobs.FindTraceUnitJob;
+import org.eclipse.osee.framework.plugin.core.util.Jobs;
+import org.eclipse.osee.framework.ui.skynet.commandHandlers.Handlers;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class FindTraceUnitActionDelegate implements IWorkbenchWindowActionDelegate {
+
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#dispose()
+    */
+   @Override
+   public void dispose() {
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+    */
+   @Override
+   public void init(IWorkbenchWindow window) {
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+    */
+   @Override
+   public void run(IAction action) {
+      final String jobName = "Resource To Trace Unit Artifact";
+      final List<IResource> resources = getSelectedItems();
+      Jobs.startJob(new FindTraceUnitJob(jobName, resources.toArray(new IResource[resources.size()])), true);
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+    */
+   @Override
+   public void selectionChanged(IAction action, ISelection selection) {
+   }
+
+   private List<IResource> getSelectedItems() {
+      ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getSelection();
+      if (selection instanceof StructuredSelection) {
+         List<IResource> selectedItems =
+               Handlers.processSelectionObjects(IResource.class, (StructuredSelection) selection);
+         List<IResource> toReturn = new ArrayList<IResource>();
+         for (IResource resource : selectedItems) {
+            if (resource instanceof IFile) {
+               toReturn.add(resource);
+            }
+         }
+         return toReturn;
+      }
+      return Collections.emptyList();
+   }
+}
---
+++ org.eclipse.osee.define/src/org/eclipse/osee/define/traceability/jobs/FindTraceUnitJob.java
@@ -0,0 +1,85 @@
+/*
+ * Created on Apr 10, 2009
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.define.traceability.jobs;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.IJobChangeEvent;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobChangeAdapter;
+import org.eclipse.osee.define.DefinePlugin;
+import org.eclipse.osee.define.traceability.operations.FindTraceUnitFromResource;
+import org.eclipse.osee.framework.plugin.core.util.IExceptionableRunnable;
+import org.eclipse.osee.framework.plugin.core.util.Jobs;
+import org.eclipse.osee.framework.skynet.core.artifact.Branch;
+import org.eclipse.osee.framework.ui.skynet.branch.BranchSelectionDialog;
+import org.eclipse.ui.progress.UIJob;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class FindTraceUnitJob extends Job {
+   private final IResource[] resources;
+
+   public FindTraceUnitJob(String name, IResource... resources) {
+      super(name);
+      if (resources != null) {
+         this.resources = resources;
+      } else {
+         this.resources = new IResource[0];
+      }
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
+    */
+   @Override
+   protected IStatus run(IProgressMonitor monitor) {
+      if (resources != null && resources.length > 0) {
+         FetchBranchJob job = new FetchBranchJob(getName());
+         Jobs.startJob(job, true, new JobChangeAdapter() {
+
+            @Override
+            public void done(IJobChangeEvent event) {
+               FetchBranchJob fetcherJob = (FetchBranchJob) event.getJob();
+               final Branch branch = fetcherJob.getSelectedBranch();
+               if (branch != null) {
+                  IExceptionableRunnable runnable = new IExceptionableRunnable() {
+
+                     @Override
+                     public void run(IProgressMonitor monitor) throws Exception {
+                        if (branch != null) {
+                           FindTraceUnitFromResource.search(branch, resources);
+                        }
+                     }
+                  };
+                  Jobs.run(getName(), runnable, DefinePlugin.class, DefinePlugin.PLUGIN_ID);
+               }
+            }
+         });
+      }
+      return Status.OK_STATUS;
+   }
+   private final class FetchBranchJob extends UIJob {
+      private Branch branch;
+
+      public FetchBranchJob(String name) {
+         super(name);
+         branch = null;
+      }
+
+      public IStatus runInUIThread(IProgressMonitor monitor) {
+         branch = BranchSelectionDialog.getBranchFromUser();
+         return Status.OK_STATUS;
+      }
+
+      public Branch getSelectedBranch() {
+         return branch;
+      }
+   }
+}
