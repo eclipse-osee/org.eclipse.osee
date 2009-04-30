9e02efde76a67814284f270c5143a49930273ea8
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-04-30 01:38:30 +0000

    "Team Workflow" - YL81X - "Attributes hard to set due to limitations of the table cell editors"

--- org.eclipse.osee.ats/src/org/eclipse/osee/ats/editor/SMAEditorInput.java
+++ org.eclipse.osee.ats/src/org/eclipse/osee/ats/editor/SMAEditorInput.java
@@ -10,99 +10,23 @@
  *******************************************************************************/
 package org.eclipse.osee.ats.editor;
 
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IPersistableElement;
+import org.eclipse.osee.framework.ui.skynet.artifact.editor.BaseArtifactEditorInput;
 
 /**
  * @author Donald G. Dunne
  */
-public class SMAEditorInput implements IEditorInput {
-
-   protected Artifact artifact;
+public class SMAEditorInput extends BaseArtifactEditorInput {
 
    public SMAEditorInput(Artifact artifact) {
-      this.artifact = artifact;
+      super(artifact);
    }
 
    public boolean equals(Object obj) {
       boolean equals = false;
       if (obj instanceof SMAEditorInput) {
-         SMAEditorInput otherEdInput = (SMAEditorInput) obj;
-
-         equals = (artifact == otherEdInput.artifact);
+         return (getArtifact() == ((SMAEditorInput) obj).getArtifact());
       }
       return equals;
    }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.ui.IEditorInput#exists()
-    */
-   public boolean exists() {
-      return true;
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.ui.IEditorInput#getImageDescriptor()
-    */
-   public ImageDescriptor getImageDescriptor() {
-      return null;
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.ui.IEditorInput#getName()
-    */
-   public String getName() {
-      if (artifact == null) {
-         return "No Artifact Input Provided";
-      }
-      return artifact.getVersionedName();
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.ui.IEditorInput#getPersistable()
-    */
-   public IPersistableElement getPersistable() {
-      return null;
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.ui.IEditorInput#getToolTipText()
-    */
-   public String getToolTipText() {
-      return getName();
-   }
-
-   /*
-    * (non-Javadoc)
-    * 
-    * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
-    */
-   @SuppressWarnings("unchecked")
-   public Object getAdapter(Class adapter) {
-      return null;
-   }
-
-   public Artifact getArtifact() {
-      return artifact;
-   }
-
-   /**
-    * @param artifact the artifact to set
-    */
-   public void setArtifact(Artifact artifact) {
-      this.artifact = artifact;
-   }
-
 }
