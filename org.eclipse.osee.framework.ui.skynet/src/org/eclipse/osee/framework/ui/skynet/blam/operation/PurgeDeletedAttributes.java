243b2a4dd424b99c252e86ec0a74d10d23995311
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-05-04 18:22:54 +0000

    "Team Workflow" - 2TMGH - "Cleanup un-needed production skynet types and database artifacts"

---
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/PurgeDeletedAttributes.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.artifact.ArtifactLoad;
+import org.eclipse.osee.framework.skynet.core.artifact.Branch;
+import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
+import org.eclipse.osee.framework.skynet.core.attribute.Attribute;
+import org.eclipse.osee.framework.skynet.core.attribute.AttributeType;
+import org.eclipse.osee.framework.ui.plugin.util.AIFile;
+import org.eclipse.osee.framework.ui.plugin.util.OseeData;
+import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+import org.eclipse.swt.program.Program;
+
+/**
+ * @author Ryan D. Brooks
+ */
+public class PurgeDeletedAttributes extends AbstractBlam {
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.AbstractBlam#getName()
+    */
+   @Override
+   public String getName() {
+      return "Purge Deleted Attributes";
+   }
+
+   /* (non-Javadoc)
+     * @see org.eclipse.osee.framework.ui.skynet.blam.operation.BlamOperation#runOperation(org.eclipse.osee.framework.ui.skynet.blam.VariableMap, org.eclipse.osee.framework.skynet.core.artifact.Branch, org.eclipse.core.runtime.IProgressMonitor)
+     */
+   public void runOperation(VariableMap variableMap, IProgressMonitor monitor) throws Exception {
+      Branch branch = variableMap.getBranch("Branch");
+      Collection<AttributeType> purgeAttributeTypes =
+            variableMap.getCollection(AttributeType.class, "Attribute Type(s) to purge");
+
+      List<Artifact> artifacts = ArtifactQuery.getArtifactsFromBranch(branch, ArtifactLoad.ALL_CURRENT, true);
+
+      StringBuilder strB = new StringBuilder();
+
+      for (Artifact artifact : artifacts) {
+         for (AttributeType attributeType : purgeAttributeTypes) {
+            for (Attribute<?> attribute : artifact.getAttributesIncludeDeleted(attributeType.getName())) {
+
+               strB.append(attribute.getAttributeType());
+               strB.append(";");
+               strB.append(artifact.getArtId());
+               strB.append(";");
+               strB.append(attribute.getDisplayableString());
+               strB.append("\n");
+               attribute.purge();
+
+            }
+         }
+      }
+
+      IFile iFile = OseeData.getIFile("PurgeDeletedAttributes" + Lib.getDateTimeString() + ".txt");
+      AIFile.writeToFile(iFile, strB.toString());
+      Program.launch(iFile.getLocation().toOSString());
+   }
+
+   /*
+    * (non-Javadoc)
+    * 
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.BlamOperation#getXWidgetXml()
+    */
+   @Override
+   public String getXWidgetsXml() {
+      return "<xWidgets><XWidget xwidgetType=\"XBranchSelectWidget\"  defaultValue=\"Common\" displayName=\"Branch\" /><XWidget xwidgetType=\"XAttributeTypeListViewer\" displayName=\"Attribute Type(s) to purge\" multiSelect=\"true\" /></xWidgets>";
+   }
+
+   /* (non-Javadoc)
+    * @see org.eclipse.osee.framework.ui.skynet.blam.operation.AbstractBlam#getDescriptionUsage()
+    */
+   @Override
+   public String getDescriptionUsage() {
+      return "Purge all deleted attributes of specified type(s).";
+   }
+
+   public Collection<String> getCategories() {
+      return Arrays.asList("Admin");
+   }
+}
