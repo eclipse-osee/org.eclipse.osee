d7d59879d9c78e792dacc274a129513073515741
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-06-09 17:41:31 +0000

    changes to sql statement

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/dbHealth/DuplicateHRID.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/dbHealth/DuplicateHRID.java
@@ -6,30 +6,51 @@
 package org.eclipse.osee.framework.ui.skynet.dbHealth;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.logging.Level;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.osee.framework.db.connection.ConnectionHandlerStatement;
+import org.eclipse.osee.framework.db.connection.exception.OseeDataStoreException;
 import org.eclipse.osee.framework.jdk.core.util.AHTML;
+import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
-import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
-import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
 import org.eclipse.osee.framework.ui.skynet.results.XResultData;
 
 /**
  * @author Megumi Telles
  */
 public class DuplicateHRID extends DatabaseHealthOperation {
+
    private class DuplicateHRIDs {
-      protected int artId;
-      protected int branchId;
+      protected String guid;
       protected String hrid;
-      protected Artifact art;
+      protected String artTypeName;
+   }
+
+   private class dupHridsSorter implements Comparator<DuplicateHRIDs> {
+      public dupHridsSorter() {
+         super();
+      }
+
+      @Override
+      public int compare(DuplicateHRIDs o1, DuplicateHRIDs o2) {
+         String str1 = o1.hrid;
+         String str2 = o2.hrid;
+         return str1.compareTo(str2);
+      }
    }
 
    private static final String GET_DUPLICATE_HRIDS =
-         "SELECT distinct t1.art_id, t3.branch_id, t1.human_readable_id from osee_artifact t1, osee_tx_details t3, osee_txs t4, osee_artifact_version t5 where  t3.transaction_id=t4.transaction_id and t4.gamma_id=t5.gamma_id and t3.osee_comment is null and t5.art_id=t1.art_id and t1.human_readable_id in (SELECT human_readable_id from osee_artifact t2 group by t2.human_readable_id having count(t2.human_readable_id)>1) order by t3.branch_id";
+         "SELECT t1.guid,  t1.human_readable_id,  t3.name FROM osee_artifact t1, osee_artifact_type t3 WHERE t1.human_readable_id IN  (SELECT t2.human_readable_id    FROM osee_artifact t2   GROUP BY t2.human_readable_id HAVING COUNT(t2.human_readable_id) > 1) AND t3.art_type_id = t1.art_type_id ORDER BY t1.human_readable_id";
+
+   private static final String GET_ATTR = "SELECT * from osee_attribute where value like ?";
+   private static final String GET_COMMENT = "SELECT * from osee_tx_details where osee_comment like ?";
+   private static final String GET_BRANCH_NAME = "SELECT * from osee_branch where branch_name like ?";
+   private static final String FIX_HRID = "UPDATE osee_artifact set human_readable_id=? where guid=?";
+
    boolean fixErrors = false;
    boolean processTxCurrent = true;
 
@@ -45,7 +66,6 @@ public class DuplicateHRID extends DatabaseHealthOperation {
       LinkedList<DuplicateHRIDs> diffValues = new LinkedList<DuplicateHRIDs>();
       List<Artifact> artifacts = new ArrayList<Artifact>();
       ConnectionHandlerStatement chStmt1 = new ConnectionHandlerStatement();
-      this.setShowDetailsEnabled(true);
       fixErrors = isFixOperationEnabled();
       monitor.beginTask("Clean Up Duplicate Human Readable Ids", 1);
       monitor.subTask("Querying for Duplicate Human Readable Ids");
@@ -58,15 +78,12 @@ public class DuplicateHRID extends DatabaseHealthOperation {
          while (chStmt1.next()) {
             DuplicateHRIDs duplicateHrid;
             duplicateHrid = new DuplicateHRIDs();
-            duplicateHrid.artId = chStmt1.getInt("art_id");
-            duplicateHrid.branchId = chStmt1.getInt("branch_id");
+            duplicateHrid.guid = chStmt1.getString("guid");
             duplicateHrid.hrid = chStmt1.getString("human_readable_id");
-            artifacts =
-                  ArtifactQuery.getArtifactsFromIds(Arrays.asList(duplicateHrid.artId),
-                        BranchManager.getBranch(duplicateHrid.branchId), true);
-            duplicateHrid.art = artifacts.get(0);
+            duplicateHrid.artTypeName = chStmt1.getString("name");
             diffValues.add(duplicateHrid);
          }
+         Collections.sort(diffValues, new dupHridsSorter());
       } finally {
          chStmt1.close();
       }
@@ -78,13 +95,15 @@ public class DuplicateHRID extends DatabaseHealthOperation {
       } else {
          XResultData rd = new XResultData();
          try {
-            String[] columnHeaders = new String[] {"Artifact Id", "Branch Id", "HRID", "Artifact Type"};
+            String[] columnHeaders =
+                  new String[] {"GUID", "HRID", "Artifact Type", "Attribute Hits", "Branch Hits", "Comment Hits"};
             rd.addRaw(AHTML.beginMultiColumnTable(100, 1));
             rd.addRaw(AHTML.addHeaderRowMultiColumnTable(columnHeaders));
             for (DuplicateHRIDs dup : diffValues) {
-               rd.addRaw(AHTML.addRowMultiColumnTable(new String[] {Integer.toString(dup.artId),
-                     Integer.toString(dup.branchId), XResultData.getHyperlinkForArtifactEditor(dup.hrid, dup.hrid),
-                     dup.art.getArtifactTypeName()}));
+               rd.addRaw(AHTML.addRowMultiColumnTable(new String[] {dup.guid, dup.hrid, dup.artTypeName,
+                     String.valueOf(getAdditionalCounts(GET_ATTR, dup.hrid)),
+                     String.valueOf(getAdditionalCounts(GET_BRANCH_NAME, dup.hrid)),
+                     String.valueOf(getAdditionalCounts(GET_COMMENT, dup.hrid))}));
             }
             rd.addRaw(AHTML.endMultiColumnTable());
          } finally {
@@ -93,5 +112,23 @@ public class DuplicateHRID extends DatabaseHealthOperation {
             }
          }
       }
+   }
+
+   private String getAdditionalCounts(String statement, String hrid) {
+      ConnectionHandlerStatement chStmt1 = new ConnectionHandlerStatement();
+      int count = 0;
+      try {
+         chStmt1.runPreparedQuery(10, statement, new Object[] {"%" + hrid + "%"});
+         while (chStmt1.next()) {
+            count++;
+         }
+         return String.valueOf(count);
+      } catch (OseeDataStoreException ex) {
+         OseeLog.log(DuplicateHRID.class, Level.SEVERE, ex);
+         return ex.getLocalizedMessage();
+      } finally {
+         chStmt1.close();
+      }
+
    }
 }
