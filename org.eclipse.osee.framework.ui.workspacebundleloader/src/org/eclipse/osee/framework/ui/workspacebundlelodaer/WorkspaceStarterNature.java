2c234d1753942a03348a635ad40e55f2912fee4a
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-06-17 21:59:56 +0000

    Dynamic workspace bundle loading support [Bug# 280691]

---
+++ org.eclipse.osee.framework.ui.workspacebundleloader/src/org/eclipse/osee/framework/ui/workspacebundlelodaer/WorkspaceStarterNature.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.workspacebundlelodaer;
+
+import java.util.Collection;
+import org.eclipse.core.runtime.CoreException;
+
+/**
+ * @author Robert A. Fisher
+ * @author Andrew M. Finkbeiner
+ */
+public class WorkspaceStarterNature extends JarCollectionNature {
+   public static final String NATURE_ID = "org.eclipse.osee.benchWorkspaceStarter.WorkspaceStarterNature";
+   static final String BUNDLE_PATH_ATTRIBUTE = "WorkspaceBundlePath";
+
+   public WorkspaceStarterNature() {
+      super(BUNDLE_PATH_ATTRIBUTE);
+      System.out.println("workspace starter thing 1");
+   }
+
+   public static Collection<WorkspaceStarterNature> getWorkspaceProjects() throws CoreException {
+      System.out.println("workspace starter thing 2");
+      return getWorkspaceProjects(NATURE_ID, WorkspaceStarterNature.class);
+   }
+}
