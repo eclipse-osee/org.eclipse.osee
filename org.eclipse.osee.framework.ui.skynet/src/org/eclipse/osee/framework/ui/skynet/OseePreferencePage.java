c63240fd34f74b2ff92746dd2609284941db5240
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-07-10 19:02:19 +0000

    updated obsolete comment

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/OseePreferencePage.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/OseePreferencePage.java
@@ -33,9 +33,7 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 /**
- * This class represents a preference page that is contributed to the Preferences dialog. By subclassing
- * <samp>FieldEditorPreferencePage</samp>, we can use the field support built into JFace that allows us to create a page
- * that is small and knows how to save, restore and apply itself.
+ * This class represents a preference page that is contributed to the Preferences dialog.
  * <p>
  * This page is used to modify preferences only. They are stored in the preference store that belongs to the main
  * plug-in class. That way, preferences can be accessed directly via the preference store.
