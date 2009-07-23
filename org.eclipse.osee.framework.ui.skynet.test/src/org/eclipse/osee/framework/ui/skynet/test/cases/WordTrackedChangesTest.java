75ba6eb2d6ec68aacf4c612d94e3e96fcf707ba2
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-07-23 19:50:45 +0000

    fixed setting content for wholeworddocument test

--- org.eclipse.osee.framework.ui.skynet.test/src/org/eclipse/osee/framework/ui/skynet/test/cases/WordTrackedChangesTest.java
+++ org.eclipse.osee.framework.ui.skynet.test/src/org/eclipse/osee/framework/ui/skynet/test/cases/WordTrackedChangesTest.java
@@ -12,23 +12,25 @@ package org.eclipse.osee.framework.ui.skynet.test.cases;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.osee.framework.db.connection.exception.OseeArgumentException;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
 import org.eclipse.osee.framework.logging.SevereLoggingMonitor;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.ArtifactTypeManager;
 import org.eclipse.osee.framework.skynet.core.artifact.Branch;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
 import org.eclipse.osee.framework.skynet.core.attribute.WordAttribute;
+import org.eclipse.osee.framework.skynet.core.linking.LinkType;
+import org.eclipse.osee.framework.skynet.core.linking.WordMlLinkHandler;
 import org.eclipse.osee.framework.skynet.core.test.util.FrameworkTestUtil;
 import org.eclipse.osee.framework.skynet.core.utility.Requirements;
 import org.eclipse.osee.framework.ui.skynet.render.FileRenderer;
@@ -86,7 +88,7 @@ public class WordTrackedChangesTest {
       renderer = WordEditTest.openArtifacts(artifacts);
       makeChangesToArtifact(renderer, TEST_WORD_EDIT_FILE_NAME, artifacts);
       Thread.sleep(5000);
-      assertTrue("Detected Tracked Changes Succcessfully",
+      assertTrue("Did not detect Tracked Changes Succcessfully",
             WordAttribute.getDisplayTrackedChangesErrorMessage().contains(
                   "Cannot save - Detected tracked changes on this artifact. ") == true);
       TestUtil.severeLoggingEnd(monitorLog);
@@ -119,23 +121,29 @@ public class WordTrackedChangesTest {
     */
    @org.junit.Test
    public void testWholeWordSaveWithTrackChanges() throws Exception {
-
-      List<Artifact> artifacts = new ArrayList<Artifact>();
+      WordAttribute.setNoPopUps(true);
+      InputStream inputStream = new FileInputStream(TEST_WORD_EDIT_FILE_NAME);
+      String content = Lib.inputStreamToString(inputStream);
+      LinkType linkType = LinkType.OSEE_SERVER_LINK;
       SevereLoggingMonitor monitorLog = TestUtil.severeLoggingStart();
       FileRenderer.setWorkbenchSavePopUpDisabled(true);
       Branch branch = BranchManager.getKeyedBranch(DemoSawBuilds.SAW_Bld_1.name());
       // create a new requirement artifact
       Artifact newArt = ArtifactTypeManager.addArtifact("Test Procedure WML", branch, getClass().getSimpleName());
-      newArt.persistAttributesAndRelations();
-      artifacts = Arrays.asList(newArt);
-      RendererManager.openInJob(artifacts, PresentationType.SPECIALIZED_EDIT);
-      FileRenderer renderer = RendererManager.getBestFileRenderer(PresentationType.SPECIALIZED_EDIT, newArt);
-      makeChangesToArtifact(renderer, TEST_WORD_EDIT_FILE_NAME, artifacts);
-      Thread.sleep(10000);
-      assertTrue("Detected Tracked Changes Succcessfully",
-            WordAttribute.getDisplayTrackedChangesErrorMessage().contains(
+      content = WordMlLinkHandler.unlink(linkType, newArt, content);
+      try {
+         newArt.setSoleAttributeFromString(WordAttribute.WHOLE_WORD_CONTENT, content);
+         newArt.persistAttributesAndRelations();
+      } catch (OseeArgumentException ex) {
+         if (ex.getLocalizedMessage().equals("Cannot save - Detected tracked changes on this artifact. ")) {
+            assertTrue("Did not Detect Tracked Changes", WordAttribute.getDisplayTrackedChangesErrorMessage().contains(
                   "Cannot save - Detected tracked changes on this artifact. ") == true);
-      TestUtil.severeLoggingEnd(monitorLog);
+         } else {
+            throw ex;
+         }
+      } finally {
+         TestUtil.severeLoggingEnd(monitorLog);
+      }
    }
 
    @After
