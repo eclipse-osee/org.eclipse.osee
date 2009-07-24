91e5169ad4c7a71ff5aaf016c9c036b68ffdf969
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-07-24 21:25:02 +0000

    minor changes to static variables

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/FileUiUtil.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/FileUiUtil.java
@@ -14,14 +14,15 @@ import org.eclipse.osee.framework.ui.skynet.SkynetGuiPlugin;
  * @author Megumi Telles
  */
 public class FileUiUtil {
-   public static String filenameWarningMessage =
+   public static String FILENAME_WARNING_MESSAGE =
          "\n\nis approaching a large size which may cause the opening application to error. " + "\nSuggest moving your workspace to avoid potential errors. ";
+   public final static int FILENAME_LIMIT = 215;
 
    public static boolean ensureFilenameLimit(IFile file) {
       boolean withinLimit = true;
       String absPath = file.getLocation().toFile().getAbsolutePath();
-      if (absPath.length() > 215) {
-         String warningMessage = "Your filename: \n\n" + absPath + filenameWarningMessage;
+      if (absPath.length() > FILENAME_LIMIT) {
+         String warningMessage = "Your filename: \n\n" + absPath + FILENAME_WARNING_MESSAGE;
          // need to warn user that their filename size is large and may cause the program (Word, Excel, PPT) to error
          WordUiUtil.displayWarningMessageDialog("Filename Size Warning", warningMessage);
          OseeLog.log(SkynetGuiPlugin.class, Level.WARNING, warningMessage);
