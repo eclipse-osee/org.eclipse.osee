d0068053d4f32aa29ee5949eb2f10e0d926bf904
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-07-24 21:33:30 +0000

    "Team Workflow" - 638GG - "EBM - Add finalize capability to engineering builds"

---
+++ org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/GeneralDocument.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.skynet.core.artifact;
+
+/**
+ * @author Megumi Telles
+ */
+public class GeneralDocument {
+
+   public static String ARTIFACT_TYPE = "General Document";
+
+   public static String NATIVE_CONTENT = "Native Content";
+}
