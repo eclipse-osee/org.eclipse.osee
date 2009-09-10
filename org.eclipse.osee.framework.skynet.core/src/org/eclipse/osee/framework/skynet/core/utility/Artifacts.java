0c0990bf514b401419fcf552dede319ab05de880
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-09-10 19:57:58 +0000

    Fixed display of details prior to persist

--- org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/utility/Artifacts.java
+++ org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/utility/Artifacts.java
@@ -16,11 +16,15 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
 import org.eclipse.osee.framework.core.enums.Active;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.exception.OseeDataStoreException;
 import org.eclipse.osee.framework.jdk.core.util.Collections;
+import org.eclipse.osee.framework.jdk.core.util.xml.Xml;
+import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.internal.Activator;
 import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
 
 /**
@@ -174,4 +178,40 @@ public final class Artifacts {
       }
       return children;
    }
+
+   public static String getDetailsFormText(Artifact artifact) {
+      String template = "<p><b>%s:</b> %s</p>";
+      StringBuilder sb = new StringBuilder();
+      sb.append("<form>");
+
+      if (artifact != null) {
+         sb.append(String.format(template, "GUID", Xml.escape(artifact.getGuid())));
+         sb.append(String.format(template, "HRID", Xml.escape(artifact.getHumanReadableId())));
+         sb.append(String.format(template, "Branch", Xml.escape(artifact.getBranch().toString())));
+         sb.append(String.format(template, "Branch Id", artifact.getBranch().getBranchId()));
+         sb.append(String.format(template, "Artifact Id", artifact.getArtId()));
+         sb.append(String.format(template, "Artifact Type Name", Xml.escape(artifact.getArtifactTypeName())));
+         sb.append(String.format(template, "Artifact Type Id", artifact.getArtTypeId()));
+         sb.append(String.format(template, "Gamma Id", artifact.getGammaId()));
+         sb.append(String.format(template, "Historical", artifact.isHistorical()));
+         sb.append(String.format(template, "Deleted", artifact.isDeleted()));
+         sb.append(String.format(template, "Revision",
+               (artifact.isInDb() ? artifact.getTransactionNumber() : "Not In Db")));
+         sb.append(String.format(template, "Read Only", artifact.isReadOnly()));
+         sb.append(String.format(template, "Last Modified",
+               (artifact.isInDb() ? artifact.getLastModified() : "Not In Db")));
+         try {
+            sb.append(String.format(template, "Last Modified By",
+                  (artifact.isInDb() ? artifact.getLastModifiedBy() : "Not In Db")));
+         } catch (Exception ex) {
+            OseeLog.log(Activator.class, Level.SEVERE, ex);
+            sb.append(String.format(template, "Last Modified By", "Exception " + ex.getLocalizedMessage()));
+         }
+      } else {
+         sb.append(String.format(template, "Artifact", "null"));
+      }
+      sb.append("</form>");
+      return sb.toString();
+   }
+
 }
