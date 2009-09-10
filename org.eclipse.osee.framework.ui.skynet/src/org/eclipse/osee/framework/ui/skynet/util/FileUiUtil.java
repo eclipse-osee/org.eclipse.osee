b7efe26c35cb0b4f33eff4cdbf04269f0447bd3b
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-09-10 17:30:35 +0000

    "Task" - H3YYR - "Leigh Schideler and Chris Smith - Filename Size Warning pop-up getting annoying"

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/FileUiUtil.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/FileUiUtil.java
@@ -17,6 +17,7 @@ public class FileUiUtil {
    public static String FILENAME_WARNING_MESSAGE =
          "\n\nis approaching a large size which may cause the opening application to error. " + "\nSuggest moving your workspace to avoid potential errors. ";
    public final static int FILENAME_LIMIT = 215;
+   public static boolean showAgain = true;
 
    public static boolean ensureFilenameLimit(IFile file) {
       boolean withinLimit = true;
@@ -24,7 +25,12 @@ public class FileUiUtil {
       if (absPath.length() > FILENAME_LIMIT) {
          String warningMessage = "Your filename: \n\n" + absPath + FILENAME_WARNING_MESSAGE;
          // need to warn user that their filename size is large and may cause the program (Word, Excel, PPT) to error
-         WordUiUtil.displayWarningMessageDialog("Filename Size Warning", warningMessage);
+         if (showAgain) {
+            //display warning once per session
+            WordUiUtil.displayWarningMessageDialog("Filename Size Warning", warningMessage);
+            showAgain = false;
+         }
+         //log the warning every time
          OseeLog.log(SkynetGuiPlugin.class, Level.WARNING, warningMessage);
          withinLimit = false;
       }
