c5572ef17bac109ea40671bc79160747afe325aa
rschmitt <rschmitt@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-09-29 18:59:08 +0000

    Faster test suite

---
+++ org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/FrameworkCore_Demo_Fast_Suite.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.skynet.core.test;
+
+import static org.junit.Assert.assertTrue;
+import org.eclipse.osee.framework.core.client.ClientSessionManager;
+import org.eclipse.osee.framework.skynet.core.test.branch.BranchTestSuite;
+import org.eclipse.osee.framework.skynet.core.test.cases.ArtifactTypeInheritanceTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.Artifact_setAttributeValues;
+import org.eclipse.osee.framework.skynet.core.test.cases.CsvArtifactTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.DuplicateHridTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.RelationDeletionTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.RelationOrderingTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.SevereLogMonitorTest;
+import org.eclipse.osee.framework.skynet.core.test.cases.StaticIdManagerTest;
+import org.eclipse.osee.framework.skynet.core.test.commit.ComputeNetChangeTest;
+import org.eclipse.osee.framework.skynet.core.test.commit.LoadCommitItemsFromDbTest;
+import org.eclipse.osee.framework.skynet.core.test.relation.RelationSorterTest;
+import org.eclipse.osee.framework.skynet.core.test.types.OseeCacheTestSuite;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {BranchTestSuite.class, OseeCacheTestSuite.class, ComputeNetChangeTest.class,
+      LoadCommitItemsFromDbTest.class, ArtifactTypeInheritanceTest.class, Artifact_setAttributeValues.class,
+      CsvArtifactTest.class, SevereLogMonitorTest.class, RelationDeletionTest.class, StaticIdManagerTest.class,
+      DuplicateHridTest.class, RelationOrderingTest.class, RelationSorterTest.class})
+/**
+ * @author Donald G. Dunne
+ */
+public class FrameworkCore_Demo_Fast_Suite {
+   @BeforeClass
+   public static void setUp() throws Exception {
+      assertTrue("Demo Application Server must be running.",
+            ClientSessionManager.getAuthenticationProtocols().contains("demo"));
+      assertTrue("Client must authenticate using demo protocol",
+            ClientSessionManager.getSession().getAuthenticationProtocol().equals("demo"));
+   }
+
+}
