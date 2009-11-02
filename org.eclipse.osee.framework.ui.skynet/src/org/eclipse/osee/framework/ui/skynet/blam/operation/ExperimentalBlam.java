889a1ca690b4ab44735b846d43087b28b706d059
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-11-02 16:22:53 +0000

    moved ExperimentalBlam into ui.skynet

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/ExperimentalBlam.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/ExperimentalBlam.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Boeing - initial API and implementation
  *******************************************************************************/
-package org.eclipse.osee.define.blam.operation;
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
 
 import java.util.Arrays;
 import java.util.Collection;
