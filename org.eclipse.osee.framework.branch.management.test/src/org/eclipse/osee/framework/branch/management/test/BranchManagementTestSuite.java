3dc13216f9d0905103afd8b4e4abc50c6862a8ba
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-11-10 01:38:15 +0000

    "Team Workflow" - 9F1NU - "Fix and Merge Application Server tests into MasterTestSuite"

---
+++ org.eclipse.osee.framework.branch.management.test/src/org/eclipse/osee/framework/branch/management/test/BranchManagementTestSuite.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Nov 9, 2009
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.framework.branch.management.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses( {})
+/**
+ * @author Roberto E. Escobar
+ */
+public class BranchManagementTestSuite {
+
+}
