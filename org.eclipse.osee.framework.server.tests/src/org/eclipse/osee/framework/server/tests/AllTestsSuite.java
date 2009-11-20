8d736e731b946ebc6dc4124d01023c41e5dcb0ca
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-11-20 04:26:12 +0000

    All Server Test Container

---
+++ org.eclipse.osee.framework.server.tests/src/org/eclipse/osee/framework/server/tests/AllTestsSuite.java
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.server.tests;
+
+import org.eclipse.osee.framework.branch.management.test.BranchManagementTestSuite;
+import org.eclipse.osee.framework.core.test.FrameworkCoreTestSuite;
+import org.eclipse.osee.framework.jdk.core.test.JdkCoreTestSuite;
+import org.eclipse.osee.framework.resource.locator.attribute.test.AttributeLocatorProviderTestSuite;
+import org.eclipse.osee.framework.resource.management.test.ResourceManagementTestSuite;
+import org.eclipse.osee.framework.resource.provider.attribute.test.AttributeProviderTestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Roberto E. Escobar
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses( { //
+JdkCoreTestSuite.class, //
+      FrameworkCoreTestSuite.class, //
+      BranchManagementTestSuite.class, //
+      ResourceManagementTestSuite.class, //
+      AttributeLocatorProviderTestSuite.class, //
+      AttributeProviderTestSuite.class, //
+})
+public class AllTestsSuite {
+
+}
