53fe686b25220364b91ba5790f3d77a82c6b56da
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-11-21 00:21:42 +0000

    Add base import choice

---
+++ org.eclipse.osee.framework.database.init/src/org/eclipse/osee/framework/database/init/AddBaseDbInitChoice.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.osee.framework.database.init;
+
+/**
+ * @author Ryan D. Brooks
+ */
+public class AddBaseDbInitChoice implements IAddDbInitChoice {
+   public void addDbInitChoice(GroupSelection groupSelection) {
+      DbInitConfiguration config = new DbInitConfiguration(true);
+
+      config.addOseeType("org.eclipse.osee.ote.define.OseeTypesOTE");
+      config.addOseeType("org.eclipse.osee.framework.skynet.core.OseeTypes_ATS");
+
+      groupSelection.addChoice("Base - for importing branches", config);
+   }
+}
