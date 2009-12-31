e9b52255c6c971a8b28e6bdb78285a7ae0673c2f
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2009-12-31 17:13:40 +0000

    Performance - removed unnecessary dictionary list

--- org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/OseeMainDictionary.java
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/OseeMainDictionary.java
@@ -13,10 +13,7 @@ package org.eclipse.osee.framework.ui.skynet.util;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import org.eclipse.osee.framework.logging.OseeLog;
@@ -26,24 +23,16 @@ import org.eclipse.osee.framework.ui.skynet.SkynetGuiPlugin;
  * @author Donald G. Dunne
  */
 public class OseeMainDictionary implements IOseeDictionary {
-   Set<String> dict;
-   private ArrayList<String> sortedDict;
-
-   /**
-    * @return the dict
-    */
-   public List<String> getSortedDict() {
-      return sortedDict;
-   }
+   Set<String> dictionary;
 
    public boolean isWord(String word) {
-      if (dict == null) loadDictionary();
-      boolean contains = dict.contains(word);
+      if (dictionary == null) loadDictionary();
+      boolean contains = dictionary.contains(word);
       return contains;
    }
 
    private void loadDictionary() {
-      dict = new HashSet<String>();
+      dictionary = new HashSet<String>();
       String line;
       InputStream is = null;
       BufferedReader br = null;
@@ -54,16 +43,12 @@ public class OseeMainDictionary implements IOseeDictionary {
          br = new BufferedReader(new InputStreamReader(is));
          while ((line = br.readLine()) != null) {
             line.replaceAll("[\t\r\n ]+$", "");
-            dict.add(line);
+            dictionary.add(line);
          }
          is.close();
       } catch (Exception ex) {
          OseeLog.log(SkynetGuiPlugin.class, Level.SEVERE, ex);
       }
-
-      sortedDict = new ArrayList<String>(dict.size());
-      sortedDict.addAll(dict);
-      Collections.sort(sortedDict);
    }
 
 }
