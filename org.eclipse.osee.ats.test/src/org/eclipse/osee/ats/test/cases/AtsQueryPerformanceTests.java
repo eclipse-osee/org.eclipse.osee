75bf71496e1a1db1bacee9b4e6efec004e1e9283
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-01-08 19:59:07 +0000

    production tests

--- org.eclipse.osee.ats.test/src/org/eclipse/osee/ats/test/cases/AtsQueryPerformanceTests.java
+++ org.eclipse.osee.ats.test/src/org/eclipse/osee/ats/test/cases/AtsQueryPerformanceTests.java
@@ -56,7 +56,7 @@ public class AtsQueryPerformanceTests {
       System.out.println(String.format("testTeamWorldSearchItem took %dms for %d artifacts", elapsedTime,
             artifacts.size()));
       assertTrue("No artifacts found", artifacts.size() > 0);
-      assertTrue("testTeamWorldSearchItem should take less than 9000ms", elapsedTime < 9000);
+      assertTrue("testTeamWorldSearchItem should take less than 9000ms; took " + elapsedTime + "ms", elapsedTime < 9000);
    }
 
    private User getAUser() throws OseeCoreException {
