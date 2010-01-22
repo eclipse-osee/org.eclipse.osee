acf51c1d9dad9e764873a945bd861317c9cc3882
mtelles <mtelles@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-01-22 19:01:55 +0000

    "Team Workflow" - XDQ30 - "Allow setting branch type in Branch Manager"

---
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/PromptChangeUtil.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.util;
+
+import java.util.Collection;
+import org.eclipse.osee.framework.core.enums.BranchArchivedState;
+import org.eclipse.osee.framework.core.enums.BranchState;
+import org.eclipse.osee.framework.core.enums.BranchType;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.core.model.Branch;
+import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
+import org.eclipse.osee.framework.ui.skynet.widgets.dialog.ChangeBranchArchivedStateDialog;
+import org.eclipse.osee.framework.ui.skynet.widgets.dialog.ChangeBranchStateDialog;
+import org.eclipse.osee.framework.ui.skynet.widgets.dialog.ChangeBranchTypeDialog;
+import org.eclipse.swt.widgets.Display;
+
+/**
+ * @author Megumi Telles
+ */
+public class PromptChangeUtil {
+
+   public static boolean promptChangeBranchType(final Collection<Branch> branches) throws OseeCoreException {
+      ChangeBranchTypeDialog ld = new ChangeBranchTypeDialog(Display.getCurrent().getActiveShell());
+      int result = ld.open();
+      if (result == 0) {
+         BranchType type = ld.getSelection();
+         for (Branch branch : branches) {
+            BranchManager.updateBranchType(null, branch.getId(), type);
+         }
+         return true;
+      }
+      return false;
+   }
+
+   public static boolean promptChangeBranchState(final Collection<Branch> branches) throws OseeCoreException {
+      ChangeBranchStateDialog ld = new ChangeBranchStateDialog(Display.getCurrent().getActiveShell());
+      int result = ld.open();
+      if (result == 0) {
+         BranchState state = ld.getSelection();
+         for (Branch branch : branches) {
+            BranchManager.updateBranchState(null, branch.getId(), state);
+         }
+         return true;
+      }
+      return false;
+   }
+
+   public static boolean promptChangeBranchArchivedState(final Collection<Branch> branches) throws OseeCoreException {
+      ChangeBranchArchivedStateDialog ld = new ChangeBranchArchivedStateDialog(Display.getCurrent().getActiveShell());
+      int result = ld.open();
+      if (result == 0) {
+         BranchArchivedState state = ld.getSelection();
+         for (Branch branch : branches) {
+            BranchManager.updateBranchArchivedState(null, branch.getId(), state);
+         }
+         return true;
+      }
+      return false;
+   }
+
+}
---
+++ org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/dialog/ChangeBranchArchivedStateDialog.java
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.widgets.dialog;
+
+import java.util.ArrayList;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.osee.framework.core.enums.BranchArchivedState;
+import org.eclipse.osee.framework.ui.plugin.util.AWorkbench;
+import org.eclipse.osee.framework.ui.swt.ALayout;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ListDialog;
+
+/**
+ * @author Megumi Telles
+ */
+public class ChangeBranchArchivedStateDialog extends ListDialog {
+
+   BranchArchivedState selected = null;
+
+   public ChangeBranchArchivedStateDialog(Shell parent) {
+      super(parent);
+      setContentProvider(new ArrayContentProvider());
+      setLabelProvider(new ChangeLabelProvider());
+      setInput(BranchArchivedState.values());
+      setShellStyle(getShellStyle() | SWT.RESIZE);
+      setTitle("Select Branch Archived State");
+   }
+
+   public BranchArchivedState getSelection() {
+      return (BranchArchivedState) getResult()[0];
+   }
+
+   @Override
+   protected Control createDialogArea(Composite container) {
+
+      (new Label(container, SWT.NONE)).setText("     Select Branch Archived State:");
+
+      Control c = super.createDialogArea(container);
+      GridData gd = new GridData(GridData.FILL_BOTH);
+      gd.heightHint = 80;
+      gd.widthHint = 300;
+      getTableViewer().getTable().setLayoutData(gd);
+      GridLayout layout = ALayout.getZeroMarginLayout();
+      layout.marginWidth = 20;
+      getTableViewer().getTable().getParent().setLayout(layout);
+      if (selected != null) {
+         ArrayList<Object> sel = new ArrayList<Object>();
+         sel.add(selected);
+         getTableViewer().setSelection(new StructuredSelection(sel.toArray(new Object[sel.size()])));
+         getTableViewer().getTable().setFocus();
+      }
+      return c;
+   }
+
+   @Override
+   protected void okPressed() {
+      if (getTableViewer().getSelection().isEmpty()) {
+         AWorkbench.popup("ERROR", "Must make selection.");
+         return;
+      }
+      super.okPressed();
+   }
+
+   public class ChangeLabelProvider implements ILabelProvider {
+
+      public Image getImage(Object arg0) {
+         return null;
+      }
+
+      public String getText(Object arg0) {
+         BranchArchivedState type = (BranchArchivedState) arg0;
+         return type.name();
+      }
+
+      public void addListener(ILabelProviderListener arg0) {
+      }
+
+      public void dispose() {
+      }
+
+      public boolean isLabelProperty(Object arg0, String arg1) {
+         return false;
+      }
+
+      public void removeListener(ILabelProviderListener arg0) {
+      }
+
+   }
+
+   public void setSelected(BranchArchivedState selected) {
+      this.selected = selected;
+   }
+
+}
