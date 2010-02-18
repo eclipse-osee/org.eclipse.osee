37a8f318a1784ca89ea4a43f9e6da9a4bb9475dc
afinkbein <afinkbein@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-02-18 22:04:18 +0000

    "Team Workflow" - 0RQMW - "Make OSEE application services use the messenger gateway"

--- jms.activemq.launch/src/jms/activemq/launch/RunActiveMq.java
+++ jms.activemq.launch/src/jms/activemq/launch/RunActiveMq.java
@@ -5,106 +5,97 @@
  */
 package jms.activemq.launch;
 
-import java.util.List;
+import java.util.logging.Level;
+import javax.jms.Connection;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.Topic;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.camel.CamelConnectionFactory;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jms.JmsComponent;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.model.RouteDefinition;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.osee.framework.logging.OseeLog;
 
 public class RunActiveMq implements IApplication {
-   private static String BROKER_URI;
-   private BrokerService broker;
-   private CamelConnectionFactory activeMqRemoteConnectionFactory;
-
-   private final boolean isKillable = true;
-
-   @Override
-   public Object start(IApplicationContext appContext) throws Exception {
-      broker = new BrokerService();
-      broker.setBrokerName("osee");
-      broker.setUseShutdownHook(true);
-      String[] myArgs = (String[]) appContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
-      BROKER_URI = "tcp://localhost:" + myArgs[0];
-      broker.addConnector(BROKER_URI);
-      broker.start();
-
-      if (isKillable) {
-
-         DefaultCamelContext context = new DefaultCamelContext();
-         activeMqRemoteConnectionFactory = new CamelConnectionFactory();
-         activeMqRemoteConnectionFactory.setBrokerURL(BROKER_URI);
-
-         activeMqRemoteConnectionFactory.setAlwaysSessionAsync(true);
-         activeMqRemoteConnectionFactory.setAlwaysSyncSend(false);
-         /*
-          * if we use failover: sends pend not sure about route adding, probably pend also. Need to thread those
-          * operations so we don't block applications
-          * use transport listener for lifecycle events
-          */
-         activeMqRemoteConnectionFactory.setCamelContext(context);
-         activeMqRemoteConnectionFactory.getCamelContext().addComponent("osee-jms",
-               JmsComponent.jmsComponent(activeMqRemoteConnectionFactory));
-         context.start();
-
-         activeMqRemoteConnectionFactory.getCamelContext().addRoutes(new RouteBuilder() {
+	private static String BROKER_URI;
+	private BrokerService broker;
+	
+	private boolean isKillable = true;
+   private Session session;
+   private MessageConsumer replyToConsumer;
+   private Connection connection;
+   
+	@Override
+	public Object start(IApplicationContext appContext) throws Exception {
+		broker = new BrokerService();
+		broker.setBrokerName("osee");
+		broker.setUseShutdownHook(true);
+		broker.setUseJmx(false);
+		String[] myArgs = (String[])appContext.getArguments().get(IApplicationContext.APPLICATION_ARGS);
+		BROKER_URI =  "tcp://localhost:"+myArgs[0];
+		broker.addConnector(BROKER_URI);
+		broker.start();
+		
+		if(isKillable){
+		   ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKER_URI);
+	      connection = factory.createConnection();
+	      connection.start();
+	      session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+	      Topic destination = session.createTopic("jms.kill.broker");
+	      replyToConsumer = session.createConsumer(destination);
+	      replyToConsumer.setMessageListener(new MessageListener() {
+            
             @Override
-            public void configure() {
-               from("osee-jms:" + "topic:jms.kill.broker").process(new Processor() {
-
-                  @Override
-                  public void process(Exchange exchange) throws Exception {
-                     stopBrokerInNewThread();
-                  }
-
-               });
+            public void onMessage(Message arg0) {
+               try {
+                  System.err.println("got a kill message");
+                  stopBrokerInNewThread();
+                  System.err.println("did something with a kill message");
+               } catch (Exception ex) {
+                  OseeLog.log(RunActiveMq.class, Level.SEVERE, ex);
+               }
             }
          });
+			while(broker.isStarted()){
+				Thread.sleep(1000);
+			}
+		} else {
+			while(broker.isStarted()){
+				Thread.sleep(60000);
+			}
+		}
+		return null;
+	}
 
-         while (broker.isStarted()) {
-            Thread.sleep(1000);
-         }
-      } else {
-         while (broker.isStarted()) {
-            Thread.sleep(60000);
-         }
-      }
-      return null;
-   }
+	@Override
+	public void stop() {
+		try {
+			broker.stop();
+		} catch (Exception ex) {
+			OseeLog.log(RunActiveMq.class, Level.SEVERE, ex);
+		}
+	}
 
-   @Override
-   public void stop() {
-      try {
-         broker.stop();
-      } catch (Throwable th) {
-         th.printStackTrace();
-      }
-   }
+  void stopBrokerInNewThread() throws Exception {
+     new Thread(new StopIt()).start();
+  }
 
-   void stopBrokerInNewThread() throws Exception {
-      new Thread(new StopIt()).start();
-   }
-
-   private class StopIt implements Runnable {
-      @Override
-      public void run() {
-         try {
-            broker.stop();
-            List<RouteDefinition> defs = activeMqRemoteConnectionFactory.getCamelContext().getRouteDefinitions();
-            for (RouteDefinition def : defs) {
-               def.stop();
-            }
-            activeMqRemoteConnectionFactory.getCamelContext().removeRouteDefinitions(defs);
-            activeMqRemoteConnectionFactory.getCamelContext().stop();
-         } catch (Throwable th) {
-            th.printStackTrace();
-         }
-      }
-   }
-
+  private class StopIt implements Runnable {
+     @Override
+     public void run() {
+        try {
+           System.err.println("close the connection");
+           connection.close();
+           System.err.println("stop the broker");
+           broker.stop();
+           System.err.println("done");
+        } catch (Throwable th) {
+           th.printStackTrace();
+        }
+     }
+  }
+	
 }
