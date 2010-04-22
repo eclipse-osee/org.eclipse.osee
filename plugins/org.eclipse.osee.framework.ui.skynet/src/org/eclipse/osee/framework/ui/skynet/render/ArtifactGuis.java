a0c3a68fc2b097e985f3350879b9a76531f1794c
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-04-22 18:07:15 +0000

    "Team Workflow" - 44JWQ - "Erroneous Edit Conflict warning"

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/ArtifactGuis.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/ArtifactGuis.java
@@ -10,39 +10,62 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.ui.skynet.render;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osee.framework.core.enums.BranchArchivedState;
+import org.eclipse.osee.framework.core.exception.OseeArgumentException;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.database.core.ConnectionHandler;
 import org.eclipse.osee.framework.database.core.IOseeStatement;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
-import org.eclipse.osee.framework.ui.plugin.util.Displays;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.swt.widgets.Display;
 
 public final class ArtifactGuis {
+
    private ArtifactGuis() {
+      // this empty constructor exists to prevent the default constructor from allowing public construction
    }
 
    private static final String OTHER_EDIT_SQL =
-         "select br.branch_id from osee_attribute att, osee_txs txs, osee_branch br where att.art_id = ? and att.gamma_id = txs.gamma_id and txs.branch_id = br.branch_id and txs.transaction_id <> br.baseline_transaction_id and br.branch_id <> ? and br.parent_branch_id = ? and br.archived = ?";
+         "select br.branch_id, att.gamma_id, att.attr_id from osee_attribute att, osee_txs txs, osee_branch br where att.art_id = ? and att.gamma_id = txs.gamma_id and txs.branch_id = br.branch_id and txs.transaction_id <> br.baseline_transaction_id and br.branch_id <> ? and br.parent_branch_id = ? and br.archived = ?";
+
+   public static boolean checkOtherEdit(List<Artifact> artifacts) throws OseeCoreException {
+      int modifiedCount = 0;
+      if (artifacts.isEmpty()) {
+         throw new OseeArgumentException("you must pass at least one artifact");
+      }
+
+      Set<String> otherBranches = new HashSet<String>();
+      for (Artifact artifact : artifacts) {
+         modifiedCount += getOtherEdittedBranches(artifact, otherBranches);
+      }
+      StringBuilder branchesStrb = new StringBuilder();
+      for (String branchName : otherBranches) {
+         branchesStrb.append("\n\t");
+         branchesStrb.append(branchName);
+      }
+
+      String message =
+            "%d of the %d artifacts about to be edited have already been modified on the following branches:%s\n\nDo you still wish to proceed?";
+      if (!otherBranches.isEmpty()) {
+         message = String.format(message, modifiedCount, artifacts.size(), branchesStrb.toString());
+         return MessageDialog.openConfirm(Display.getCurrent().getActiveShell(), "Confirm Edit", message);
+      }
+      return true;
+   }
 
    /**
     * Returns non-archived sibling branches that this artifact's attributes have been edited on
     * 
-    * @param artifact
     * @throws OseeCoreException
     */
-   private static Collection<Branch> getOtherEdittedBranches(Artifact artifact) throws OseeCoreException {
-      Collection<Branch> otherBranches = new LinkedList<Branch>();
-
-      // Can only be on other branches it has already been saved
+   private static int getOtherEdittedBranches(Artifact artifact, Set<String> otherBranches) throws OseeCoreException {
+      int modifiedCount = 0;
+      // Can only be on other branches if it has already been saved
       if (artifact.isInDb()) {
 
          IOseeStatement chStmt = ConnectionHandler.getStatement();
@@ -52,106 +75,15 @@ public final class ArtifactGuis {
                   branch.getParentBranch().getId(), BranchArchivedState.UNARCHIVED.getValue());
 
             while (chStmt.next()) {
-               otherBranches.add(BranchManager.getBranch(chStmt.getInt("branch_id")));
+               if (artifact.getAttributeById(chStmt.getInt("attr_id"), false).getGammaId() != chStmt.getInt("gamma_id")) {
+                  otherBranches.add(BranchManager.getBranch(chStmt.getInt("branch_id")).getShortName());
+                  modifiedCount++;
+               }
             }
          } finally {
             chStmt.close();
          }
       }
-      return otherBranches;
-   }
-
-   public static boolean checkOtherEdit(List<Artifact> artifacts) throws OseeCoreException {
-      if (artifacts.size() == 0) {
-         throw new IllegalArgumentException("you must pass at least one artifact");
-      }
-
-      boolean goAhead = true;
-
-      Set<Branch> otherBranches = new HashSet<Branch>();
-      for (Artifact artifact : artifacts) {
-         otherBranches.addAll(getOtherEdittedBranches(artifact));
-      }
-
-      if (!otherBranches.isEmpty()) {
-         StringBuilder sb = new StringBuilder();
-
-         sb.append("The artifact");
-         if (artifacts.size() > 1) {
-            sb.append('s');
-         }
-         sb.append(" about to be editted ");
-         if (artifacts.size() > 1) {
-            sb.append("have");
-         } else {
-            sb.append("has");
-         }
-         sb.append(" already been modified on the following branches:");
-         for (Branch branch : otherBranches) {
-            sb.append("\n\t" + branch.getName());
-         }
-         sb.append("\n\nDo you still want to proceed?");
-
-         synchronized (sb) {
-
-            AskQuestion question = new AskQuestion(sb, "Confirm Edit", sb.toString());
-            Displays.ensureInDisplayThread(question);
-            try {
-               while (!question.done) {
-                  sb.wait();
-               }
-            } catch (InterruptedException e) {
-            }
-            goAhead = question.isYes();
-         }
-      }
-
-      return goAhead;
-   }
-
-   private static class AskQuestion implements Runnable {
-
-      private final Object notifee;
-      private final String title;
-      private final String question;
-      private boolean yes;
-      private boolean done;
-
-      /**
-       * @param notifee
-       * @param question
-       */
-      public AskQuestion(Object notifee, String title, String question) {
-         super();
-         this.notifee = notifee;
-         this.title = title;
-         this.question = question;
-         this.yes = false;
-         this.done = false;
-      }
-
-      public void run() {
-         synchronized (notifee) {
-            yes =
-                  MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), title,
-                        question);
-            done = true;
-            notifee.notifyAll();
-         }
-      }
-
-      /**
-       * @return Returns the yes.
-       */
-      public boolean isYes() {
-         return yes;
-      }
-
-      /**
-       * @return Returns the done.
-       */
-      public boolean isDone() {
-         return done;
-      }
+      return modifiedCount;
    }
 }
