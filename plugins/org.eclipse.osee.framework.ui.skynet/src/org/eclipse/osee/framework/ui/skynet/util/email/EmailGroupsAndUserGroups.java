38bc2b11821659fe82551f6669508f2fc1898d23
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-03 17:18:32 +0000

    replaced usage of type name with type token

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/email/EmailGroupsAndUserGroups.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/util/email/EmailGroupsAndUserGroups.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.eclipse.osee.framework.core.enums.CoreArtifactTypes;
 import org.eclipse.osee.framework.core.enums.CoreRelationTypes;
 import org.eclipse.osee.framework.core.enums.PermissionEnum;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
@@ -64,13 +65,18 @@ public class EmailGroupsAndUserGroups extends XNavigateItemAction {
       if (groupTypes.contains(GroupType.Both) || groupTypes.contains(GroupType.Groups)) {
          for (Artifact art : UniversalGroup.getGroups(BranchManager.getCommonBranch())) {
             // Only add group if have read permissions
-            if (!art.getName().equals("Root Artifact") && AccessControlManager.hasPermission(art, PermissionEnum.READ)) groupOptions.add(art);
+            if (!art.getName().equals("Root Artifact") && AccessControlManager.hasPermission(art, PermissionEnum.READ)) {
+               groupOptions.add(art);
+            }
          }
       }
       if (groupTypes.contains(GroupType.Both) || groupTypes.contains(GroupType.UserGroups)) {
-         for (Artifact art : ArtifactQuery.getArtifactListFromType("User Group", BranchManager.getCommonBranch())) {
+         for (Artifact art : ArtifactQuery.getArtifactListFromType(CoreArtifactTypes.UserGroup,
+               BranchManager.getCommonBranch())) {
             // Only add group if have read permissions
-            if (!art.getName().equals("Root Artifact") && AccessControlManager.hasPermission(art, PermissionEnum.READ)) groupOptions.add(art);
+            if (!art.getName().equals("Root Artifact") && AccessControlManager.hasPermission(art, PermissionEnum.READ)) {
+               groupOptions.add(art);
+            }
          }
       }
       return groupOptions;
@@ -79,50 +85,51 @@ public class EmailGroupsAndUserGroups extends XNavigateItemAction {
    @Override
    public void run(TableLoadOption... tableLoadOptions) {
       try {
-      Set<Artifact> groupOptions = getEmailGroupsAndUserGroups(UserManager.getUser(), groupType);
-      UserGroupsCheckTreeDialog dialog = new UserGroupsCheckTreeDialog(groupOptions);
-      dialog.setTitle("Select Groups to Email");
-      if (dialog.open() == 0) {
+         Set<Artifact> groupOptions = getEmailGroupsAndUserGroups(UserManager.getUser(), groupType);
+         UserGroupsCheckTreeDialog dialog = new UserGroupsCheckTreeDialog(groupOptions);
+         dialog.setTitle("Select Groups to Email");
+         if (dialog.open() == 0) {
 
-         Set<String> emails = new HashSet<String>();
-         for (Artifact artifact : dialog.getSelection()) {
-            if (artifact.isOfType("Universal Group")) {
-               for (Artifact userArt : artifact.getRelatedArtifacts(CoreRelationTypes.Universal_Grouping__Members)) {
-                  if (userArt instanceof User) {
+            Set<String> emails = new HashSet<String>();
+            for (Artifact artifact : dialog.getSelection()) {
+               if (artifact.isOfType("Universal Group")) {
+                  for (Artifact userArt : artifact.getRelatedArtifacts(CoreRelationTypes.Universal_Grouping__Members)) {
+                     if (userArt instanceof User) {
                         if (!EmailUtil.isEmailValid((User) userArt)) {
                            OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE, String.format(
                                  "Invalid email [%s] for user [%s]; skipping", ((User) userArt).getEmail(), userArt));
                         } else {
-                     emails.add(((User) userArt).getEmail());
+                           emails.add(((User) userArt).getEmail());
+                        }
+                     }
                   }
-               }
-                  }
-            } else if (artifact.isOfType("User Group")) {
-               for (User user : artifact.getRelatedArtifacts(CoreRelationTypes.Users_User, User.class)) {
+               } else if (artifact.isOfType("User Group")) {
+                  for (User user : artifact.getRelatedArtifacts(CoreRelationTypes.Users_User, User.class)) {
                      if (!EmailUtil.isEmailValid(user)) {
                         OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE, String.format(
                               "Invalid email [%s] for user [%s]; skipping", user.getEmail(), user));
                      } else {
-                  emails.add(user.getEmail());
+                        emails.add(user.getEmail());
+                     }
+                  }
                }
             }
-         }
+            if (emails.size() == 0) {
+               AWorkbench.popup("Error", "No emails configured.");
+               return;
             }
-         if (emails.size() == 0) {
-            AWorkbench.popup("Error", "No emails configured.");
-            return;
-         }
             String emailStr = org.eclipse.osee.framework.jdk.core.util.Collections.toString(";", emails);
             if (emailStr.length() > 2048) {
                AWorkbench.popup("Email list too big for auto-open. Emails opened in editor for copy/paste.");
                ResultsEditor.open("Email Addresses", "Email Addresses", emailStr);
-            } else
+            } else {
                Program.launch("mailto:" + emailStr);
-         AWorkbench.popup("Complete", "Configured emails openened in local email client.");
-      }
+            }
+            AWorkbench.popup("Complete", "Configured emails openened in local email client.");
+         }
       } catch (OseeCoreException ex) {
          OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
-   }
+      }
 
-}
+   }
 }
