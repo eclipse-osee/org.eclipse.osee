381d6a372e21230fb2a429eec402dfc4cb7d568a
rschmitt <rschmitt@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-03 19:39:52 +0000

    Extracted DiffHandler class from MergeView

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/xmerge/MergeView.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/xmerge/MergeView.java
@@ -12,7 +12,6 @@
 package org.eclipse.osee.framework.ui.skynet.widgets.xmerge;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
@@ -279,11 +278,11 @@ public class MergeView extends ViewPart implements IActionable, IBranchEventList
    private void createDiffMenuItem(MenuManager menuManager) {
       MenuManager subMenuManager = new MenuManager("Differences", "diffTransaction");
       menuManager.add(subMenuManager);
-      createDiffItems(subMenuManager, new DiffHandler(menuManager, 1), "Show Source Branch Differences");
-      createDiffItems(subMenuManager, new DiffHandler(menuManager, 2), "Show Destination Branch Differences");
-      createDiffItems(subMenuManager, new DiffHandler(menuManager, 3), "Show Source/Destination Differences");
-      createDiffItems(subMenuManager, new DiffHandler(menuManager, 4), "Show Source/Merge Differences");
-      createDiffItems(subMenuManager, new DiffHandler(menuManager, 5), "Show Destination/Merge Differences");
+      createDiffItems(subMenuManager, new DiffHandler(menuManager, 1, mergeXWidget), "Show Source Branch Differences");
+      createDiffItems(subMenuManager, new DiffHandler(menuManager, 2, mergeXWidget), "Show Destination Branch Differences");
+      createDiffItems(subMenuManager, new DiffHandler(menuManager, 3, mergeXWidget), "Show Source/Destination Differences");
+      createDiffItems(subMenuManager, new DiffHandler(menuManager, 4, mergeXWidget), "Show Source/Merge Differences");
+      createDiffItems(subMenuManager, new DiffHandler(menuManager, 5, mergeXWidget), "Show Destination/Merge Differences");
    }
 
    private String addDiffItems(MenuManager subMenuManager, String command) {
@@ -767,180 +766,6 @@ public class MergeView extends ViewPart implements IActionable, IBranchEventList
             }
          }
 
-         return AccessControlManager.checkObjectListPermission(artifacts, PermissionEnum.READ);
-      }
-   }
-
-   private class DiffHandler extends AbstractSelectionEnabledHandler {
-      private final int diffToShow;
-      private AttributeConflict attributeConflict;
-      private ArtifactConflict artifactConflict;
-      private List<Artifact> artifacts;
-
-      public DiffHandler(MenuManager menuManager, int diffToShow) {
-         super(menuManager);
-         this.diffToShow = diffToShow;
-      }
-
-      @Override
-      public Object execute(ExecutionEvent event) throws ExecutionException {
-         try {
-            if (attributeConflict != null) {
-               switch (diffToShow) {
-                  case 1:
-                     MergeUtility.showCompareFile(
-                           MergeUtility.getStartArtifact(attributeConflict),
-                           attributeConflict.getSourceArtifact(),
-                           "Source_Diff_For_" + attributeConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                                 ":", ";") + ".xml");
-                     break;
-                  case 2:
-                     MergeUtility.showCompareFile(
-                           MergeUtility.getStartArtifact(attributeConflict),
-                           attributeConflict.getDestArtifact(),
-                           "Destination_Diff_For_" + attributeConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                                 ":", ";") + ".xml");
-                     break;
-                  case 3:
-                     MergeUtility.showCompareFile(
-                           attributeConflict.getSourceArtifact(),
-                           attributeConflict.getDestArtifact(),
-                           "Source_Destination_Diff_For_" + attributeConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                                 ":", ";") + ".xml");
-                     break;
-                  case 4:
-                     if (attributeConflict.wordMarkupPresent()) {
-                        throw new OseeCoreException(AttributeConflict.DIFF_MERGE_MARKUP);
-                     }
-                     MergeUtility.showCompareFile(
-                           attributeConflict.getSourceArtifact(),
-                           attributeConflict.getArtifact(),
-                           "Source_Merge_Diff_For_" + attributeConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                                 ":", ";") + ".xml");
-                     break;
-                  case 5:
-                     if (attributeConflict.wordMarkupPresent()) {
-                        throw new OseeCoreException(AttributeConflict.DIFF_MERGE_MARKUP);
-                     }
-                     MergeUtility.showCompareFile(
-                           attributeConflict.getDestArtifact(),
-                           attributeConflict.getArtifact(),
-                           "Destination_Merge_Diff_For_" + attributeConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                                 ":", ";") + ".xml");
-                     break;
-               }
-            } else if (artifactConflict != null) {
-               if (diffToShow == 1) {
-                  MergeUtility.showCompareFile(
-                        artifactConflict.getSourceArtifact(),
-                        MergeUtility.getStartArtifact(artifactConflict),
-                        "Source_Diff_For_" + artifactConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                              ":", ";") + ".xml");
-               }
-               if (diffToShow == 2) {
-                  MergeUtility.showCompareFile(
-                        artifactConflict.getDestArtifact(),
-                        MergeUtility.getStartArtifact(artifactConflict),
-                        "Destination_Diff_For_" + artifactConflict.getArtifact().getSafeName() + new Date().toString().replaceAll(
-                              ":", ";") + ".xml");
-               }
-            }
-         } catch (Exception ex) {
-            OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
-         }
-         return null;
-      }
-
-      @Override
-      public boolean isEnabledWithException() throws OseeCoreException {
-         artifacts = new LinkedList<Artifact>();
-         List<Conflict> conflicts = mergeXWidget.getSelectedConflicts();
-         if (conflicts.size() != 1) {
-            return false;
-         }
-         if (conflicts.get(0) instanceof AttributeConflict) {
-            attributeConflict = (AttributeConflict) conflicts.get(0);
-            artifactConflict = null;
-            try {
-               switch (diffToShow) {
-                  case 1:
-                     if (attributeConflict.getSourceArtifact() != null && MergeUtility.getStartArtifact(attributeConflict) != null) {
-                        artifacts.add(attributeConflict.getSourceArtifact());
-                        artifacts.add(MergeUtility.getStartArtifact(attributeConflict));
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 2:
-                     if (attributeConflict.getDestArtifact() != null && MergeUtility.getStartArtifact(attributeConflict) != null) {
-                        artifacts.add(attributeConflict.getDestArtifact());
-                        artifacts.add(MergeUtility.getStartArtifact(attributeConflict));
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 3:
-                     if (attributeConflict.getDestArtifact() != null && attributeConflict.getSourceArtifact() != null) {
-                        artifacts.add(attributeConflict.getSourceArtifact());
-                        artifacts.add(attributeConflict.getDestArtifact());
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 4:
-                     if (attributeConflict.getSourceArtifact() != null && attributeConflict.getArtifact() != null) {
-                        artifacts.add(attributeConflict.getSourceArtifact());
-                        artifacts.add(attributeConflict.getArtifact());
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 5:
-                     if (attributeConflict.getDestArtifact() != null && attributeConflict.getArtifact() != null) {
-                        artifacts.add(attributeConflict.getDestArtifact());
-                        artifacts.add(attributeConflict.getArtifact());
-                     } else {
-                        return false;
-                     }
-                     break;
-               }
-            } catch (Exception ex) {
-               OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
-            }
-
-         } else if (conflicts.get(0) instanceof ArtifactConflict) {
-            attributeConflict = null;
-            artifactConflict = (ArtifactConflict) conflicts.get(0);
-            try {
-               switch (diffToShow) {
-                  case 1:
-                     if (artifactConflict.getSourceArtifact() != null && conflicts.get(0).statusNotResolvable() && MergeUtility.getStartArtifact(artifactConflict) != null) {
-                        artifacts.add(artifactConflict.getSourceArtifact());
-                        artifacts.add(MergeUtility.getStartArtifact(artifactConflict));
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 2:
-                     if (artifactConflict.getDestArtifact() != null && conflicts.get(0).statusInformational() && MergeUtility.getStartArtifact(artifactConflict) != null) {
-                        artifacts.add(artifactConflict.getDestArtifact());
-                        artifacts.add(MergeUtility.getStartArtifact(artifactConflict));
-                     } else {
-                        return false;
-                     }
-                     break;
-                  case 3:
-                     return false;
-                  case 4:
-                     return false;
-                  case 5:
-                     return false;
-               }
-            } catch (Exception ex) {
-               OseeLog.log(SkynetGuiPlugin.class, OseeLevel.SEVERE_POPUP, ex);
-            }
-
-         }
          return AccessControlManager.checkObjectListPermission(artifacts, PermissionEnum.READ);
       }
    }
