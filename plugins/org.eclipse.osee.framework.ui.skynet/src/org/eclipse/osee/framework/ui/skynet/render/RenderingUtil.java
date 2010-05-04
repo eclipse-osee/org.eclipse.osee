24bd88e2dbcabe3957aaf60d9292e514015bb71d
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-04 20:12:15 +0000

    fixed ordering of start and end version of artifact used for creating Word diff

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/RenderingUtil.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/RenderingUtil.java
@@ -12,7 +12,6 @@ import java.util.Set;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
-import org.eclipse.osee.framework.core.enums.ModificationType;
 import org.eclipse.osee.framework.core.exception.OseeArgumentException;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.model.Branch;
@@ -52,25 +51,26 @@ public final class RenderingUtil {
       Artifact artFile2;
 
       if (delta.getTxDelta() == null || !delta.getTxDelta().areOnTheSameBranch()) {
-         // Assumptions - when comparing data between transactions on different branches, the start artifact will never be null;
-         ModificationType startModType = delta.getStartArtifact().getModType();
-         if (startModType.isDeleted()) {
+         // Assumptions - when comparing data between transactions on
+         // different branches, the start artifact will never be null;
+         if (delta.getStartArtifact().getModType().isDeleted()) {
             artFile1 = delta.getStartArtifact();
             artFile2 = null;
-         } else if ((startModType == ModificationType.INTRODUCED || startModType == ModificationType.NEW) && delta.getEndArtifact() == null) {
+         } else if (delta.getEndArtifact() == null) {
             artFile1 = null;
             artFile2 = delta.getStartArtifact();
-         } else {
-            artFile1 = delta.getStartArtifact();
-            artFile2 = delta.getEndArtifact();
+         } else { // case where there are new, modified, or deleted attributes (are artifact is not new or deleted)
+            // also could be introduced in both branches
+            artFile1 = delta.getEndArtifact();
+            artFile2 = delta.getStartArtifact();
          }
       } else {
-         // Assumptions - when comparing data between transactions on the same branch, the end artifact will never be null;
-         ModificationType modType = delta.getEndArtifact().getModType();
-         if (modType.isDeleted()) {
+         // Assumptions - when comparing data between transactions on the
+         // same branch, the end artifact will never be null;
+         if (delta.getEndArtifact().getModType().isDeleted()) {
             artFile1 = delta.getStartArtifact();
             artFile2 = null;
-         } else if ((modType == ModificationType.INTRODUCED || modType == ModificationType.NEW) && delta.getStartArtifact() == null) {
+         } else if (delta.getStartArtifact() == null) {
             artFile1 = null;
             artFile2 = delta.getEndArtifact();
          } else {
