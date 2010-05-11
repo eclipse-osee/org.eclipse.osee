047f0cfe30e3968743bb6b6a90a17042b295e76b
rschmitt <rschmitt@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-11 19:42:57 +0000

    Refactored tests

--- plugins/org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/relation/order/RelationOrderMergeUtilityTest.java
+++ plugins/org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/relation/order/RelationOrderMergeUtilityTest.java
@@ -11,6 +11,9 @@
 package org.eclipse.osee.framework.skynet.core.test.relation.order;
 
 import static org.eclipse.osee.framework.core.enums.CoreArtifactTypes.Artifact;
+import static org.eclipse.osee.framework.core.enums.RelationOrderBaseTypes.USER_DEFINED;
+import static org.eclipse.osee.framework.skynet.core.test.util.FrameworkTestUtil.createSimpleArtifact;
+import static org.eclipse.osee.framework.skynet.core.test.util.FrameworkTestUtil.createSimpleArtifacts;
 import java.util.List;
 import org.eclipse.osee.framework.core.data.IRelationSorterId;
 import org.eclipse.osee.framework.core.enums.CoreRelationTypes;
@@ -19,6 +22,7 @@ import org.eclipse.osee.framework.core.enums.RelationSide;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.core.model.RelationType;
+import org.eclipse.osee.framework.jdk.core.util.GUID;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
 import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
@@ -26,106 +30,103 @@ import org.eclipse.osee.framework.skynet.core.relation.RelationTypeManager;
 import org.eclipse.osee.framework.skynet.core.relation.order.RelationOrderData;
 import org.eclipse.osee.framework.skynet.core.relation.order.RelationOrderMergeUtility;
 import org.eclipse.osee.framework.skynet.core.test.util.FrameworkTestUtil;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author Ryan Schmitt
  */
 public class RelationOrderMergeUtilityTest {
+   private final CoreRelationTypes defaultHierarchy = CoreRelationTypes.Default_Hierarchical__Child;
+   private final IRelationSorterId ascOrder = RelationOrderBaseTypes.LEXICOGRAPHICAL_ASC;
+   private final IRelationSorterId descOrder = RelationOrderBaseTypes.LEXICOGRAPHICAL_DESC;
+
+   private RelationType hierType;
+   private RelationSide hierSide;
+
+   private Branch destBranch;
+
+   @Before
+   public void createBranch() throws OseeCoreException {
+      String branchName = GUID.create();
+      destBranch = BranchManager.createWorkingBranch(BranchManager.getSystemRootBranch(), branchName, null);
+      hierType = RelationTypeManager.getType(defaultHierarchy);
+      hierSide = defaultHierarchy.getSide();
+   }
+
+   @After
+   public void destroyBranch() throws OseeCoreException {
+      BranchManager.purgeBranch(destBranch);
+   }
+
    @Test
    public void testTrivialMerge() throws OseeCoreException {
-      Branch common = BranchManager.getCommonBranch();
-      Artifact parent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", common);
-      Artifact[] relatives =
-            FrameworkTestUtil.createSimpleArtifacts(Artifact, 5, "Relative", common).toArray(new Artifact[0]);
-      IRelationSorterId abc = RelationOrderBaseTypes.LEXICOGRAPHICAL_ASC;
-      CoreRelationTypes hierarchical = CoreRelationTypes.Default_Hierarchical__Child;
-      RelationType type = RelationTypeManager.getType(hierarchical);
-      RelationSide side = hierarchical.getSide();
+      Artifact parent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", destBranch);
+      Artifact[] children = createSimpleArtifacts(Artifact, 5, "Relative", destBranch).toArray(new Artifact[0]);
 
       for (int i = 0; i < 5; i++) {
-         relatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Child);
-         relatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
-         parent.addRelation(abc, hierarchical, relatives[i]);
+         setAsChild(parent, children[i], ascOrder);
       }
+      parent.persist();
       RelationOrderData mergedOrder = RelationOrderMergeUtility.mergeRelationOrder(parent, parent);
       Assert.assertNotNull(mergedOrder);
-      String sorter = mergedOrder.getCurrentSorterGuid(type, side);
-      Assert.assertEquals(RelationOrderBaseTypes.LEXICOGRAPHICAL_ASC.getGuid(), sorter);
+
+      String sorter = mergedOrder.getCurrentSorterGuid(hierType, hierSide);
+      Assert.assertEquals(ascOrder.getGuid(), sorter);
    }
 
    @Test
    public void testOrderMerge() throws OseeCoreException {
-      Branch parentBranch =
-            BranchManager.createWorkingBranch(BranchManager.getSystemRootBranch(), "RO test branch", null);
-      Artifact parent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", parentBranch);
-      Artifact[] children =
-            FrameworkTestUtil.createSimpleArtifacts(Artifact, 5, "Relative", parentBranch).toArray(new Artifact[0]);
+      Artifact destParent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", destBranch);
+      Artifact[] destChildren = createSimpleArtifacts(Artifact, 5, "Relative", destBranch).toArray(new Artifact[0]);
 
-      CoreRelationTypes defaultHierarchy = CoreRelationTypes.Default_Hierarchical__Child;
-      IRelationSorterId userDefinedOrder = RelationOrderBaseTypes.USER_DEFINED;
-
-      for (int i = 0; i < 4; i++) {
-         children[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Child);
-         children[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
-         parent.addRelation(userDefinedOrder, defaultHierarchy, children[i]);
+      for (int i = 0; i <= 3; i++) {
+         setAsChild(destParent, destChildren[i], USER_DEFINED);
       }
+      destParent.persist();
 
-      parent.persist();
-      Branch childBranch = BranchManager.createWorkingBranch(parentBranch, "Common2", null);
-      Artifact branchParent = ArtifactQuery.getArtifactFromId(parent.getGuid(), childBranch);
-      for (int i = 4; i < 5; i++) {
-         Artifact relative = ArtifactQuery.getArtifactFromId(children[i].getGuid(), childBranch);
-         relative.deleteRelations(CoreRelationTypes.Default_Hierarchical__Child);
-         relative.deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
-         branchParent.addRelation(userDefinedOrder, defaultHierarchy, relative);
-         branchParent.persist();
-      }
-      children[0].deleteAndPersist();
+      Branch sourceBranch = BranchManager.createWorkingBranch(destBranch, "Source Branch", null);
+      Artifact srcParent = ArtifactQuery.getArtifactFromId(destParent.getGuid(), sourceBranch);
+      Artifact srcChild = ArtifactQuery.getArtifactFromId(destChildren[4].getGuid(), sourceBranch);
+      setAsChild(srcParent, srcChild, USER_DEFINED);
+      srcParent.persist();
+      destChildren[0].deleteAndPersist();
 
-      RelationOrderData mergedOrder = RelationOrderMergeUtility.mergeRelationOrder(parent, branchParent);
+      RelationOrderData mergedOrder = RelationOrderMergeUtility.mergeRelationOrder(destParent, srcParent);
       Assert.assertNotNull(mergedOrder);
-      List<String> orderList =
-            mergedOrder.getOrderList(RelationTypeManager.getType(CoreRelationTypes.Default_Hierarchical__Child),
-                  CoreRelationTypes.Default_Hierarchical__Child.getSide());
-      Assert.assertTrue(orderList.size() == 4);
-      for (int i = 1; i < 5; i++) {
-         Assert.assertTrue(orderList.get(i - 1).equals(children[i].getGuid()));
-      }
 
-      BranchManager.purgeBranch(childBranch);
-      parent.deleteAndPersist();
-      for (int i = 0; i < 5; i++) {
-         children[i].deleteAndPersist();
-      }
+      List<String> orderList = mergedOrder.getOrderList(hierType, hierSide);
+      Assert.assertTrue(orderList.size() == 4);
+      Assert.assertTrue(orderList.get(0).equals(destChildren[1].getGuid()));
+      Assert.assertTrue(orderList.get(1).equals(destChildren[2].getGuid()));
+      Assert.assertTrue(orderList.get(2).equals(destChildren[3].getGuid()));
+      Assert.assertTrue(orderList.get(3).equals(destChildren[4].getGuid()));
+
+      BranchManager.purgeBranch(sourceBranch);
    }
 
    @Test
    public void testStrategyMerge() throws OseeCoreException {
-      Branch common = BranchManager.getCommonBranch();
-      Artifact ascParent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", common);
-      Artifact[] ascRelatives =
-            FrameworkTestUtil.createSimpleArtifacts(Artifact, 5, "Relative", common).toArray(new Artifact[0]);
-      Artifact descParent = FrameworkTestUtil.createSimpleArtifact(Artifact, "Parent", common);
-      Artifact[] descRelatives =
-            FrameworkTestUtil.createSimpleArtifacts(Artifact, 5, "Relative", common).toArray(new Artifact[0]);
-
-      CoreRelationTypes hierarchical = CoreRelationTypes.Default_Hierarchical__Child;
-      IRelationSorterId ascOrder = RelationOrderBaseTypes.LEXICOGRAPHICAL_ASC;
-      IRelationSorterId descOrder = RelationOrderBaseTypes.LEXICOGRAPHICAL_DESC;
+      Artifact ascParent = createSimpleArtifact(Artifact, "Parent", destBranch);
+      Artifact[] ascRelatives = createSimpleArtifacts(Artifact, 5, "Relative", destBranch).toArray(new Artifact[0]);
+      Artifact descParent = createSimpleArtifact(Artifact, "Parent", destBranch);
+      Artifact[] descRelatives = createSimpleArtifacts(Artifact, 5, "Relative", destBranch).toArray(new Artifact[0]);
 
       for (int i = 0; i < 5; i++) {
-         ascRelatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Child);
-         ascRelatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
-         descRelatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Child);
-         descRelatives[i].deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
-         ascParent.addRelation(ascOrder, hierarchical, ascRelatives[i]);
-         descParent.addRelation(descOrder, hierarchical, descRelatives[i]);
+         setAsChild(ascParent, ascRelatives[i], ascOrder);
+         setAsChild(descParent, descRelatives[i], descOrder);
       }
+
       ascParent.persist();
       descParent.persist();
       RelationOrderData mergedOrder = RelationOrderMergeUtility.mergeRelationOrder(ascParent, descParent);
       Assert.assertNull(mergedOrder);
+   }
+
+   private void setAsChild(Artifact parent, Artifact child, IRelationSorterId sorter) throws OseeCoreException {
+      child.deleteRelations(CoreRelationTypes.Default_Hierarchical__Parent);
+      parent.addRelation(sorter, CoreRelationTypes.Default_Hierarchical__Child, child);
    }
 }
