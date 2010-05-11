b5d7b6a942c33c34fc6b04c45826bb10afd922e5
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-05-11 17:49:18 +0000

    marked failures as intermittent

--- plugins/org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/cases/ConflictTest.java
+++ plugins/org.eclipse.osee.framework.skynet.core.test/src/org/eclipse/osee/framework/skynet/core/test/cases/ConflictTest.java
@@ -47,9 +47,7 @@ public class ConflictTest {
    private static final boolean DEBUG =
          "TRUE".equalsIgnoreCase(Platform.getDebugOption("org.eclipse.osee.framework.skynet.core.test/debug/Junit"));
    private static final String[] NO_TX_CURRENT_SET =
-         {
-               "SELECT distinct t1.",
-               ", txs1.branch_id FROM osee_txs txs1, ",
+         {"SELECT distinct t1.", ", txs1.branch_id FROM osee_txs txs1, ",
                " t1 WHERE txs1.gamma_id = t1.gamma_id AND txs1.tx_current = 0 %s SELECT distinct t2.",
                ", txs2.branch_id FROM osee_txs txs2, ",
                " t2 WHERE txs2.gamma_id = t2.gamma_id AND txs2.tx_current != 0"};
@@ -101,7 +99,8 @@ public class ConflictTest {
       } catch (Exception ex) {
          fail(Lib.exceptionToString(ex));
       }
-      assertEquals("Number of conflicts found is not equal to the number of conflicts expected",
+      assertEquals(
+            "(Intermittent failures - needs re-write) - Number of conflicts found is not equal to the number of conflicts expected",
             ConflictTestManager.numberOfConflicts(), conflicts.size());
       assertTrue(String.format("%d SevereLogs during test.", monitorLog.getSevereLogs().size()),
             monitorLog.getSevereLogs().size() == 0);
@@ -270,7 +269,8 @@ public class ConflictTest {
             }
             System.out.println("\n");
          }
-         assertEquals("The merge Branch does not contain the expected number of artifacts: ",
+         assertEquals(
+               "(Intermittent failures - needs re-write) - The merge Branch does not contain the expected number of artifacts: ",
                ConflictTestManager.numberOfArtifactsOnMergeBranch(), artifacts.size());
       } catch (Exception ex) {
          fail(ex.getMessage());
