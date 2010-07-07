b723ef44670e923fb3c9ae59d0f9f5aead5e31fc
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-07 22:10:44 +0000

    added distribution statment

--- plugins/org.eclipse.osee.framework.access/src/org/eclipse/osee/framework/access/internal/CMAccessProvider.java
+++ plugins/org.eclipse.osee.framework.access/src/org/eclipse/osee/framework/access/internal/CMAccessProvider.java
@@ -1,8 +1,13 @@
-/*
- * Created on Jun 28, 2010
+/*******************************************************************************
+ * Copyright (c) 2010 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
- */
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osee.framework.access.internal;
 
 import java.util.Collection;
@@ -13,56 +18,59 @@ import org.eclipse.osee.framework.core.model.AccessData;
 import org.eclipse.osee.framework.core.model.Branch;
 import org.eclipse.osee.framework.core.model.IBasicArtifact;
 
+/**
+ * @author Roberto E. Escobar
+ */
 public class CMAccessProvider implements IAccessProvider {
 
-   private final ConfigurationManagementProvider provider;
+	private final ConfigurationManagementProvider provider;
 
-   public CMAccessProvider(ConfigurationManagementProvider provider) {
-      this.provider = provider;
-   }
+	public CMAccessProvider(ConfigurationManagementProvider provider) {
+		this.provider = provider;
+	}
 
-   @Override
-   public void computeAccess(IBasicArtifact<?> userArtifact, Collection<IBasicArtifact<?>> artsToCheck, AccessData accessData) throws OseeCoreException {
+	@Override
+	public void computeAccess(IBasicArtifact<?> userArtifact, Collection<IBasicArtifact<?>> artsToCheck, AccessData accessData) throws OseeCoreException {
 
-      for (IBasicArtifact<?> artifactToCheck : artsToCheck) {
-         ConfigurationManagement management = provider.getCM(artifactToCheck);
-         AccessModel accessModel = management.getAccessModel();
-         String contextId = management.getContextId(userArtifact, artifactToCheck);
-         AccessContext context = accessModel.getContext(contextId);
-         PermissionEnum permission = context.getPermission();
-         accessData.add(artifactToCheck, permission);
-      }
-   }
+		for (IBasicArtifact<?> artifactToCheck : artsToCheck) {
+			ConfigurationManagement management = provider.getCM(artifactToCheck);
+			AccessModel accessModel = management.getAccessModel();
+			String contextId = management.getContextId(userArtifact, artifactToCheck);
+			AccessContext context = accessModel.getContext(contextId);
+			PermissionEnum permission = context.getPermission();
+			accessData.add(artifactToCheck, permission);
+		}
+	}
 
-   public static interface ConfigurationManagementProvider {
-      ConfigurationManagement getCM(IBasicArtifact<?> artifact) throws OseeCoreException;
-   }
+	public static interface ConfigurationManagementProvider {
+		ConfigurationManagement getCM(IBasicArtifact<?> artifact) throws OseeCoreException;
+	}
 
-   private final class ConfigurationManagementProviderImpl implements ConfigurationManagementProvider {
+	private final class ConfigurationManagementProviderImpl implements ConfigurationManagementProvider {
 
-      @Override
-      public ConfigurationManagement getCM(IBasicArtifact<?> artifact) throws OseeCoreException {
-         Branch branch = artifact.getBranch();
-         IBasicArtifact<?> associatedArtifact = branch.getAssociatedArtifact();
-         ConfigurationManagement cm = null; //         associatedArtifact.getCM();
-         return cm;
-      }
-   }
+		@Override
+		public ConfigurationManagement getCM(IBasicArtifact<?> artifact) throws OseeCoreException {
+			Branch branch = artifact.getBranch();
+			IBasicArtifact<?> associatedArtifact = branch.getAssociatedArtifact();
+			ConfigurationManagement cm = null; //         associatedArtifact.getCM();
+			return cm;
+		}
+	}
 
-   public static interface ConfigurationManagement {
-      String getContextId(IBasicArtifact<?> userArtifact, IBasicArtifact<?> artifactToCheck);
+	public static interface ConfigurationManagement {
+		String getContextId(IBasicArtifact<?> userArtifact, IBasicArtifact<?> artifactToCheck);
 
-      AccessModel getAccessModel();
-   }
+		AccessModel getAccessModel();
+	}
 
-   public static interface AccessContext {
+	public static interface AccessContext {
 
-      PermissionEnum getPermission();
-   }
+		PermissionEnum getPermission();
+	}
 
-   public static interface AccessModel {
+	public static interface AccessModel {
 
-      AccessContext getContext(String id) throws OseeCoreException;
-   }
+		AccessContext getContext(String id) throws OseeCoreException;
+	}
 
 }
