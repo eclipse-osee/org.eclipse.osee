7365625fca4f16529f5e6f199aa0685a4e7c478c
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-07 22:10:03 +0000

    updated year to 2010

--- plugins/org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/text/rules/AddDistributionStatement.java
+++ plugins/org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/text/rules/AddDistributionStatement.java
@@ -18,27 +18,27 @@ import org.eclipse.osee.framework.jdk.core.text.change.ChangeSet;
 /**
  * @author Ryan D. Brooks
  */
-public class AdddistributionStatement extends Rule {
-   private static final Pattern classDeclarationP =
-         Pattern.compile("^[/\\s\\*]*((?:Created on [^\n]+)?[\\s\\*]*(?:PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE)?[/\\s\\*]*)package");
-   private static final char[] distributionStatement =
-         "/*******************************************************************************\n * Copyright (c) 2004, 2009 Boeing.\n * All rights reserved. This program and the accompanying materials\n * are made available under the terms of the Eclipse Public License v1.0\n * which accompanies this distribution, and is available at\n * http://www.eclipse.org/legal/epl-v10.html\n *\n * Contributors:\n *     Boeing - initial API and implementation\n *******************************************************************************/\n".toCharArray();
+public class AddDistributionStatement extends Rule {
+	private static final Pattern classDeclarationP =
+				Pattern.compile("^[/\\s\\*]*((?:Created on [^\n]+)?[\\s\\*]*(?:PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE)?[/\\s\\*]*)package");
+	private static final char[] distributionStatement =
+				"/*******************************************************************************\n * Copyright (c) 2010 Boeing.\n * All rights reserved. This program and the accompanying materials\n * are made available under the terms of the Eclipse Public License v1.0\n * which accompanies this distribution, and is available at\n * http://www.eclipse.org/legal/epl-v10.html\n *\n * Contributors:\n *     Boeing - initial API and implementation\n *******************************************************************************/\n".toCharArray();
 
-   public AdddistributionStatement() {
-      super(null);
-      setFileNamePattern(Pattern.compile(".*\\.java"));
-   }
+	public AddDistributionStatement() {
+		super(null);
+		setFileNamePattern(Pattern.compile(".*\\.java"));
+	}
 
-   @Override
-   public ChangeSet computeChanges(CharSequence seq) {
-      ChangeSet changeSet = new ChangeSet(seq);
+	@Override
+	public ChangeSet computeChanges(CharSequence seq) {
+		ChangeSet changeSet = new ChangeSet(seq);
 
-      Matcher matcher = classDeclarationP.matcher(seq);
-      if (matcher.find()) {
-         setRuleWasApplicable(true);
-         changeSet.replace(0, matcher.end(1), distributionStatement);
-      }
+		Matcher matcher = classDeclarationP.matcher(seq);
+		if (matcher.find()) {
+			setRuleWasApplicable(true);
+			changeSet.replace(0, matcher.end(1), distributionStatement);
+		}
 
-      return changeSet;
-   }
+		return changeSet;
+	}
 }
