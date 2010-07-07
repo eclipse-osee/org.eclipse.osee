d8059d6ca82c4adfa63ab3c4eec1919cf0be784c
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-07 23:06:23 +0000

    Fixed Warnings

--- plugins/org.eclipse.osee.framework.ui.service.control/src/org/eclipse/osee/framework/ui/service/control/actions/LookupUpdates.java
+++ plugins/org.eclipse.osee.framework.ui.service.control/src/org/eclipse/osee/framework/ui/service/control/actions/LookupUpdates.java
@@ -17,6 +17,8 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import net.jini.core.lookup.ServiceID;
+import net.jini.core.lookup.ServiceRegistrar;
 import org.eclipse.osee.framework.jini.discovery.EclipseJiniClassloader;
 import org.eclipse.osee.framework.jini.discovery.IRegistrarListener;
 import org.eclipse.osee.framework.ui.service.control.managers.ReggieCache;
@@ -24,105 +26,103 @@ import org.eclipse.osee.framework.ui.service.control.renderer.IRenderer;
 import org.eclipse.osee.framework.ui.service.control.renderer.ReggieItemHandler;
 import org.eclipse.osee.framework.ui.service.control.widgets.LookupViewer;
 import org.eclipse.osee.framework.ui.service.control.widgets.ManagerMain;
-import net.jini.core.lookup.ServiceID;
-import net.jini.core.lookup.ServiceRegistrar;
 
 /**
  * @author Roberto E. Escobar
  */
 public class LookupUpdates implements IRegistrarListener {
 
-   private LookupViewer viewer;
-   private ReggieCache reggieCache;
-   private Map<ServiceID, IRenderer> handlerMap;
-   private Map<ServiceID, ServiceRegistrar> filteredReggieMap;
-   private Collection<ServiceRegistrar> unfilteredList;
+	private final LookupViewer viewer;
+	private final ReggieCache reggieCache;
+	private final Map<ServiceID, IRenderer> handlerMap;
+	private final Map<ServiceID, ServiceRegistrar> filteredReggieMap;
+	private final Collection<ServiceRegistrar> unfilteredList;
 
-   public LookupUpdates(ManagerMain mainWindow) {
-      super();
-      this.viewer = mainWindow.getLookupViewer();
-      this.handlerMap = new HashMap<ServiceID, IRenderer>();
-      this.reggieCache = ReggieCache.getEclipseInstance(EclipseJiniClassloader.getInstance());
-      this.filteredReggieMap = new HashMap<ServiceID, ServiceRegistrar>();
-      this.unfilteredList = reggieCache.getServiceRegistrars().values();
-      this.viewer.setRendererMap(handlerMap);
-      this.viewer.setInput(filteredReggieMap.values());
-      reggieCache.addListener(this);
-   }
+	public LookupUpdates(ManagerMain mainWindow) {
+		super();
+		this.viewer = mainWindow.getLookupViewer();
+		this.handlerMap = new HashMap<ServiceID, IRenderer>();
+		this.reggieCache = ReggieCache.getEclipseInstance(EclipseJiniClassloader.getInstance());
+		this.filteredReggieMap = new HashMap<ServiceID, ServiceRegistrar>();
+		this.unfilteredList = reggieCache.getServiceRegistrars().values();
+		this.viewer.setRendererMap(handlerMap);
+		this.viewer.setInput(filteredReggieMap.values());
+		reggieCache.addListener(this);
+	}
 
-   public void clear() {
-      handlerMap.clear();
-      filteredReggieMap.clear();
-   }
+	public void clear() {
+		handlerMap.clear();
+		filteredReggieMap.clear();
+	}
 
-   public void filterLookupServers(boolean displayAll) {
-      this.viewer.setInput(displayAll ? unfilteredList : filteredReggieMap.values());
-      this.viewer.refresh();
-   }
+	public void filterLookupServers(boolean displayAll) {
+		this.viewer.setInput(displayAll ? unfilteredList : filteredReggieMap.values());
+		this.viewer.refresh();
+	}
 
-   public void reggieAdded(List<ServiceRegistrar> serviceRegistrars) {
-      // System.out.println("Reggie Added: ");
-      Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
-      while (iterator.hasNext()) {
-         ServiceRegistrar reggie = iterator.next();
-         ServiceID serviceID = reggie.getServiceID();
-         if (!handlerMap.containsKey(serviceID)) {
-            handlerMap.put(serviceID, new ReggieItemHandler(reggie));
+	public void reggieAdded(List<ServiceRegistrar> serviceRegistrars) {
+		// System.out.println("Reggie Added: ");
+		Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
+		while (iterator.hasNext()) {
+			ServiceRegistrar reggie = iterator.next();
+			ServiceID serviceID = reggie.getServiceID();
+			if (!handlerMap.containsKey(serviceID)) {
+				handlerMap.put(serviceID, new ReggieItemHandler(reggie));
 
-            if (ReggieItemHandler.isAllowed(reggie)) {
-               filteredReggieMap.put(serviceID, reggie);
-            }
-         }
-      }
-      viewer.refresh();
-   }
+				if (ReggieItemHandler.isAllowed(reggie)) {
+					filteredReggieMap.put(serviceID, reggie);
+				}
+			}
+		}
+		viewer.refresh();
+	}
 
-   @SuppressWarnings("unchecked")
-   public void reggieRemoved(List<ServiceRegistrar> serviceRegistrars) {
-      // System.out.println("Reggie Removed: ");
-      Set<ServiceID> cachedReggies = handlerMap.keySet();
-      Set<ServiceID> availableReggies = new HashSet<ServiceID>();
-      Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
-      while (iterator.hasNext()) {
-         ServiceRegistrar reggie = iterator.next();
-         availableReggies.add(reggie.getServiceID());
-      }
+	@SuppressWarnings("unchecked")
+	public void reggieRemoved(List<ServiceRegistrar> serviceRegistrars) {
+		// System.out.println("Reggie Removed: ");
+		Set<ServiceID> cachedReggies = handlerMap.keySet();
+		Set<ServiceID> availableReggies = new HashSet<ServiceID>();
+		Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
+		while (iterator.hasNext()) {
+			ServiceRegistrar reggie = iterator.next();
+			availableReggies.add(reggie.getServiceID());
+		}
 
-      List discardedIds =
-            org.eclipse.osee.framework.jdk.core.util.Collections.setComplement(cachedReggies, availableReggies);
-      Iterator iterator2 = discardedIds.iterator();
-      ServiceID toProcess = null;
-      while (iterator2.hasNext()) {
-         toProcess = (ServiceID) iterator2.next();
-         handlerMap.remove(toProcess);
+		List discardedIds =
+					org.eclipse.osee.framework.jdk.core.util.Collections.setComplement(cachedReggies, availableReggies);
+		Iterator iterator2 = discardedIds.iterator();
+		ServiceID toProcess = null;
+		while (iterator2.hasNext()) {
+			toProcess = (ServiceID) iterator2.next();
+			handlerMap.remove(toProcess);
 
-         if (filteredReggieMap.containsKey(toProcess)) {
-            filteredReggieMap.remove(toProcess);
-         }
-      }
-      viewer.refresh();
-   }
+			if (filteredReggieMap.containsKey(toProcess)) {
+				filteredReggieMap.remove(toProcess);
+			}
+		}
+		viewer.refresh();
+	}
 
-   public void reggieChanged(List<ServiceRegistrar> serviceRegistrars) {
-      // System.out.println("Reggie Changed: ");
-      Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
-      while (iterator.hasNext()) {
-         ServiceRegistrar reggie = iterator.next();
-         ServiceID serviceID = reggie.getServiceID();
-         if (handlerMap.containsKey(serviceID)) {
-            handlerMap.put(serviceID, new ReggieItemHandler(reggie));
-         }
-         if (ReggieItemHandler.isAllowed(reggie)) {
-            filteredReggieMap.put(serviceID, reggie);
-         }
-      }
-      viewer.refresh();
-   }
+	public void reggieChanged(List<ServiceRegistrar> serviceRegistrars) {
+		// System.out.println("Reggie Changed: ");
+		Iterator<ServiceRegistrar> iterator = serviceRegistrars.iterator();
+		while (iterator.hasNext()) {
+			ServiceRegistrar reggie = iterator.next();
+			ServiceID serviceID = reggie.getServiceID();
+			if (handlerMap.containsKey(serviceID)) {
+				handlerMap.put(serviceID, new ReggieItemHandler(reggie));
+			}
+			if (ReggieItemHandler.isAllowed(reggie)) {
+				filteredReggieMap.put(serviceID, reggie);
+			}
+		}
+		viewer.refresh();
+	}
 
-   public void dispose() {
-      reggieCache.removeListener(this);
-      viewer.dispose();
-      handlerMap.clear();
-      filteredReggieMap.clear();
-   }
+	public void dispose() {
+		reggieCache.removeListener(this);
+		viewer.dispose();
+		handlerMap.clear();
+		filteredReggieMap.clear();
+	}
 }
