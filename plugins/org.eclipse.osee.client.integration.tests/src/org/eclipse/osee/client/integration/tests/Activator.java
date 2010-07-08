9a64e03704c16cd11ab5fb9ffb6fe7a341f8ce65
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-08 18:07:14 +0000

    fixed warnings

--- plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/Activator.java
+++ plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/Activator.java
@@ -7,10 +7,13 @@ public class Activator implements BundleActivator {
 
    public static final String PLUGIN_ID = "org.eclipse.osee.client.integration.tests";
 
+   @Override
    public void start(BundleContext context) throws Exception {
+      //
    }
 
+   @Override
    public void stop(BundleContext context) throws Exception {
+      //
    }
-
 }
