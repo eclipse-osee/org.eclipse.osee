f9b4320175fced1ac8369b4717879b98d0de6eb3
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-14 19:18:24 +0000

    Fixed dependencies

--- plugins/org.eclipse.osee.framework.lifecycle.test/src/org/eclipse/osee/framework/lifecycle/test/mock/AnotherMockHandler.java
+++ plugins/org.eclipse.osee.framework.lifecycle.test/src/org/eclipse/osee/framework/lifecycle/test/mock/AnotherMockHandler.java
@@ -15,9 +15,8 @@ package org.eclipse.osee.framework.lifecycle.test.mock;
  */
 public class AnotherMockHandler extends MockHandler {
 
-   @Override
-   public void doSomething() {
-      hasRan = true;
-      System.out.println("I am another guy");
-   }
+	@Override
+	public void doSomething() {
+		hasRan = true;
+	}
 }
