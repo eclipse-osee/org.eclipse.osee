71bdd44082840b453e18d698dfd04bf645d1ebe7
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-15 20:34:46 +0000

    "Team Workflow" - GH02N - "Search for orphans is failing"

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/search/OrphanArtifactSearch.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/artifact/search/OrphanArtifactSearch.java
@@ -22,54 +22,55 @@ import org.eclipse.osee.framework.skynet.core.relation.RelationTypeManager;
  * @author Jeff C. Phillips
  */
 public class OrphanArtifactSearch implements ISearchPrimitive {
-   private static final String LABEL = "Orphan Search: ";
-   private static final String tables = "osee_artifact";
-   private static final String sql = "t2.art_type_id =? AND art_id NOT in (SELECT t2.art_id FROM osee_relation_link t1, osee_artifact t2, osee_txs t4, (SELECT Max(t1.gamma_id) AS gamma_id, t1.rel_link_id, t2.branch_id FROM osee_relation_link t1, osee_txs t2 WHERE t1.gamma_id = t2.gamma_id AND t2.branch_id = ? GROUP BY t1.rel_link_id, t2.branch_id) t6 WHERE t1.rel_link_type_id =? AND t1.b_art_id = t2.art_id AND t1.gamma_id = t4.gamma_id AND t1.rel_link_id = t6.rel_link_id AND t4.branch_id = t6.branch_id AND t1.gamma_id = t6.gamma_id AND t4.mod_type <> " + ModificationType.DELETED.getValue() + " GROUP BY t2.art_id)";
-   private final ArtifactType aritfactType;
-   private final int relationTypeId;
+	private static final String LABEL = "Orphan Search: ";
+	private static final String tables = "osee_artifact";
+	private static final String sql =
+				"art_type_id =? AND art_id NOT in (SELECT t2.art_id FROM osee_relation_link t1, osee_artifact t2, osee_txs t4, (SELECT Max(t1.gamma_id) AS gamma_id, t1.rel_link_id, t2.branch_id FROM osee_relation_link t1, osee_txs t2 WHERE t1.gamma_id = t2.gamma_id AND t2.branch_id = ? GROUP BY t1.rel_link_id, t2.branch_id) t6 WHERE t1.rel_link_type_id =? AND t1.b_art_id = t2.art_id AND t1.gamma_id = t4.gamma_id AND t1.rel_link_id = t6.rel_link_id AND t4.branch_id = t6.branch_id AND t1.gamma_id = t6.gamma_id AND t4.mod_type <> " + ModificationType.DELETED.getValue() + " GROUP BY t2.art_id)";
+	private final ArtifactType aritfactType;
+	private final int relationTypeId;
 
-   public OrphanArtifactSearch(ArtifactType aritfactType) throws OseeCoreException {
-      this.aritfactType = aritfactType;
-      this.relationTypeId = RelationTypeManager.getType("Default Hierarchical").getId();
-   }
+	public OrphanArtifactSearch(ArtifactType aritfactType) throws OseeCoreException {
+		this.aritfactType = aritfactType;
+		this.relationTypeId = RelationTypeManager.getType("Default Hierarchical").getId();
+	}
 
-   public String getCriteriaSql(List<Object> dataList, Branch branch) {
-      dataList.add(aritfactType.getId());
-      dataList.add(branch.getId());
-      dataList.add(relationTypeId);
+	public String getCriteriaSql(List<Object> dataList, Branch branch) {
+		dataList.add(aritfactType.getId());
+		dataList.add(branch.getId());
+		dataList.add(relationTypeId);
 
-      return sql;
-   }
+		return sql;
+	}
 
-   public String getArtIdColName() {
-      return "art_id";
-   }
+	public String getArtIdColName() {
+		return "art_id";
+	}
 
-   public String getTableSql(List<Object> dataList, Branch branch) {
-      return tables;
-   }
+	public String getTableSql(List<Object> dataList, Branch branch) {
+		return tables;
+	}
 
-   @Override
-   public String toString() {
-      return LABEL + aritfactType.getName();
-   }
+	@Override
+	public String toString() {
+		return LABEL + aritfactType.getName();
+	}
 
-   public static OrphanArtifactSearch getPrimitive(String storageString) {
-      storageString = storageString.replace(LABEL, "");
-      if (storageString.endsWith(";")) {
-         storageString = storageString.substring(0, storageString.length() - 1);
-      }
-      OrphanArtifactSearch search = null;
-      try {
-         ArtifactType artifactType = ArtifactTypeManager.getType(storageString);
-         search = new OrphanArtifactSearch(artifactType);
-      } catch (OseeCoreException ex) {
-         throw new IllegalStateException("Value for " + OrphanArtifactSearch.class.getSimpleName() + " not parsable");
-      }
-      return search;
-   }
+	public static OrphanArtifactSearch getPrimitive(String storageString) {
+		storageString = storageString.replace(LABEL, "");
+		if (storageString.endsWith(";")) {
+			storageString = storageString.substring(0, storageString.length() - 1);
+		}
+		OrphanArtifactSearch search = null;
+		try {
+			ArtifactType artifactType = ArtifactTypeManager.getType(storageString);
+			search = new OrphanArtifactSearch(artifactType);
+		} catch (OseeCoreException ex) {
+			throw new IllegalStateException("Value for " + OrphanArtifactSearch.class.getSimpleName() + " not parsable");
+		}
+		return search;
+	}
 
-   public String getStorageString() {
-      return LABEL + aritfactType.getName() + ";";
-   }
+	public String getStorageString() {
+		return LABEL + aritfactType.getName() + ";";
+	}
 }
