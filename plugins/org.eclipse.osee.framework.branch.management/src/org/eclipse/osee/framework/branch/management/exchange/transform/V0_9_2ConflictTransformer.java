fe56273475917b413f5ef08c8f77d73dedf3c7bd
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-18 20:35:25 +0000

    fix bug in branch import conversion where non-artifact gamma ids were converted to null

--- plugins/org.eclipse.osee.framework.branch.management/src/org/eclipse/osee/framework/branch/management/exchange/transform/V0_9_2ConflictTransformer.java
+++ plugins/org.eclipse.osee.framework.branch.management/src/org/eclipse/osee/framework/branch/management/exchange/transform/V0_9_2ConflictTransformer.java
@@ -30,12 +30,17 @@ public class V0_9_2ConflictTransformer extends SaxTransformer {
    public void startElementFound(String uri, String localName, String qName, Attributes attributes) throws XMLStreamException {
       writer.writeStartElement(localName);
       for (int i = 0; i < attributes.getLength(); i++) {
+         String value = null;
          if (attributes.getLocalName(i).equals("source_gamma_id") || attributes.getLocalName(i).equals("dest_gamma_id")) {
             Long netGammaId = artifactGammaToNetGammaId.get(Long.valueOf(attributes.getValue(i)));
-            writer.writeAttribute(attributes.getLocalName(i), String.valueOf(netGammaId));
-         } else {
-            writer.writeAttribute(attributes.getLocalName(i), attributes.getValue(i));
+            if (netGammaId != null) {
+               value = String.valueOf(netGammaId);
+            }
          }
+         if (value == null) {
+            value = attributes.getValue(i);
+         }
+         writer.writeAttribute(attributes.getLocalName(i), value);
       }
    }
 }
