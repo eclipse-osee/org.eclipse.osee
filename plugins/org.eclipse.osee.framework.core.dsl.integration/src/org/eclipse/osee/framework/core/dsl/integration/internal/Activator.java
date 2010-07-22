291eb5536c4c8a42c0eaebcbb61939bc2cf0ad93
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-22 23:21:24 +0000

    Core Dsl Updates for Access Control

--- plugins/org.eclipse.osee.framework.core.dsl.integration/src/org/eclipse/osee/framework/core/dsl/integration/internal/Activator.java
+++ plugins/org.eclipse.osee.framework.core.dsl.integration/src/org/eclipse/osee/framework/core/dsl/integration/internal/Activator.java
@@ -1,11 +1,34 @@
-/*
- * Created on Jul 22, 2010
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
- */
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osee.framework.core.dsl.integration.internal;
 
-public class Activator {
+import org.eclipse.osee.framework.core.util.ServiceDependencyTracker;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
 
 	public static final String PLUGIN_ID = "org.eclipse.osee.framework.core.dsl.integration";
+
+	private ServiceDependencyTracker dependencyTracker;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		dependencyTracker = new ServiceDependencyTracker(context, new OseeModelingServiceRegHandler());
+		dependencyTracker.open();
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		Lib.close(dependencyTracker);
+	}
 }
---
+++ plugins/org.eclipse.osee.framework.core.dsl.integration/src/org/eclipse/osee/framework/core/dsl/integration/internal/OseeModelingServiceRegHandler.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.core.dsl.integration.internal;
+
+import java.util.Map;
+import org.eclipse.osee.framework.core.dsl.oseeDsl.OseeDslFactory;
+import org.eclipse.osee.framework.core.message.IOseeModelingService;
+import org.eclipse.osee.framework.core.services.IOseeCachingService;
+import org.eclipse.osee.framework.core.services.IOseeCachingServiceFactory;
+import org.eclipse.osee.framework.core.services.IOseeModelFactoryService;
+import org.eclipse.osee.framework.core.util.AbstractTrackingHandler;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class OseeModelingServiceRegHandler extends AbstractTrackingHandler {
+
+	private static final Class<?>[] SERVICE_DEPENDENCIES = new Class<?>[] {
+				//
+				IOseeCachingService.class, //
+				IOseeModelFactoryService.class, //
+				IOseeCachingServiceFactory.class //
+				};
+
+	private ServiceRegistration registration;
+
+	@Override
+	public Class<?>[] getDependencies() {
+		return SERVICE_DEPENDENCIES;
+	}
+
+	@Override
+	public void onActivate(BundleContext context, Map<Class<?>, Object> services) {
+		IOseeModelFactoryService modelFactoryService = getService(IOseeModelFactoryService.class, services);
+		IOseeCachingService cachingService = getService(IOseeCachingService.class, services);
+		IOseeCachingServiceFactory cachingFactoryService = getService(IOseeCachingServiceFactory.class, services);
+
+		IOseeModelingService service =
+					new OseeModelingServiceImpl(modelFactoryService, cachingService, cachingFactoryService,
+								OseeDslFactory.eINSTANCE);
+		registration = context.registerService(IOseeModelingService.class.getName(), service, null);
+	}
+
+	@Override
+	public void onDeActivate() {
+		if (registration != null) {
+			registration.unregister();
+		}
+	}
+
+}
