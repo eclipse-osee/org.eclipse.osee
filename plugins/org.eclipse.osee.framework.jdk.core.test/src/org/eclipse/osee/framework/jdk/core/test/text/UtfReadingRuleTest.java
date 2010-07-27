16858412e7bdfd66e16e490bb6461e083051594a
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-07-27 20:14:18 +0000

    Fixed test to avoid accessing test input files directly which was creating problems when bundle is in a jar.

--- plugins/org.eclipse.osee.framework.jdk.core.test/src/org/eclipse/osee/framework/jdk/core/test/text/UtfReadingRuleTest.java
+++ plugins/org.eclipse.osee.framework.jdk.core.test/src/org/eclipse/osee/framework/jdk/core/test/text/UtfReadingRuleTest.java
@@ -10,26 +10,95 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.jdk.core.test.text;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.nio.charset.UnsupportedCharsetException;
 import java.util.Stack;
 import junit.framework.TestCase;
 import org.eclipse.osee.framework.jdk.core.text.Rule;
 import org.eclipse.osee.framework.jdk.core.text.change.ChangeSet;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 /**
  * Tests ability to read files in charsets, particularly UTF8
  * 
- * @link:Rule
- * @link:Lib
+ * <pre>
+ * {@link Rule} 
+ * {@link Lib}
+ * </pre>
+ * 
  * @author Karol M. Wilk
  */
 public final class UtfReadingRuleTest extends TestCase {
 
-   private static class Utf8TestRule extends Rule {
+   private static final String FILE_INPUT = "utf8_input.xml";
+
+   @org.junit.Rule
+   public TemporaryFolder tempFolder = new TemporaryFolder();
+
+   private File getInputFile() throws IOException {
+      File outfile = tempFolder.newFile("utf8_input_copy.xml");
+      URL url = UtfReadingRuleTest.class.getResource(FILE_INPUT);
+      InputStream inputStream = null;
+      try {
+         inputStream = new BufferedInputStream(url.openStream());
+         Lib.inputStreamToFile(inputStream, outfile);
+      } finally {
+         Lib.close(inputStream);
+      }
+      return outfile;
+   }
+
+   private final Utf8TestRule rule = new Utf8TestRule();
+
+   @Test(expected = NullPointerException.class)
+   public void testWrongFileName() throws IOException {
+      rule.process(new File("./notexistentFile.txt"));
+   }
+
+   @Test
+   public void testCharset() {
+      Stack<String> charsetStack = new Stack<String>();
+      charsetStack.add("UnknownCharset");
+      charsetStack.add("UTF-8");
+
+      while (!charsetStack.isEmpty()) {
+         try {
+            File inputFile = getInputFile();
+            rule.setCharsetString(charsetStack.pop());
+            rule.process(inputFile);
+         } catch (Exception ex) {
+            Assert.assertTrue("unexpected/wrong exception thrown testCharset(), Exception: " + ex.toString(),
+               ex instanceof UnsupportedCharsetException || ex instanceof UnsupportedEncodingException);
+         }
+      }
+   }
+
+   @Test
+   public void testUtf8ReadData() {
+      String expectedUtf8String = "<w:t>â‚¬</w:t>";
+      try {
+         File inputFile = getInputFile();
+         rule.process(inputFile);
+      } catch (UnsupportedCharsetException ex) {
+         Assert.assertTrue(true);
+      } catch (Exception ex) {
+         Assert.fail("unexpected/wrong exception thrown testCharset()");
+      }
+
+      //trim off extra data
+      String actual = rule.getLastOutput().toString().trim();
+      Assert.assertEquals(expectedUtf8String, actual);
+   }
+
+   private static final class Utf8TestRule extends Rule {
       private CharSequence lastOutput;
 
       public Utf8TestRule() {
@@ -45,56 +114,5 @@ public final class UtfReadingRuleTest extends TestCase {
       public CharSequence getLastOutput() {
          return lastOutput;
       }
-   }
-
-   private static final String FILE_INPUT = "utf8_input.xml";
-
-   private final Utf8TestRule rule = new Utf8TestRule();
-
-   @Test
-   public void testWrongFileName() {
-      try {
-         rule.process(new File("./notexistentFile.txt"));
-      } catch (NullPointerException ex) {
-         Assert.assertTrue(true);
-      } catch (Exception ex) {
-         Assert.fail("unexpected/wrong exception thrown testWrongFileName()");
-      }
-   }
-
-   @Test
-   public void testCharset() {
-      Stack<String> charsetStack = new Stack<String>();
-      charsetStack.add("UnknownCharset");
-      charsetStack.add("UTF-8");
-
-      while (!charsetStack.isEmpty()) {
-         try {
-            File inputFile = new File(UtfReadingRuleTest.class.getResource(FILE_INPUT).toURI());
-            rule.setCharsetString(charsetStack.pop());
-            rule.process(inputFile);
-         } catch (Exception ex) {
-            Assert.assertTrue("unexpected/wrong exception thrown testCharset(), Exception: " + ex.toString(),
-               ex instanceof UnsupportedCharsetException || ex instanceof UnsupportedEncodingException);
-         }
-      }
-   }
-
-   @Test
-   public void testUtf8ReadData() {
-      String expectedUtf8String = "<w:t>â‚¬</w:t>";
-      try {
-         File inputFile = new File(UtfReadingRuleTest.class.getResource(FILE_INPUT).toURI());
-         rule.setCharsetString("UTF8");
-         rule.process(inputFile);
-      } catch (UnsupportedCharsetException ex) {
-         Assert.assertTrue(true);
-      } catch (Exception ex) {
-         Assert.fail("unexpected/wrong exception thrown testCharset()");
-      }
-
-      //trim off extra data
-      String actual = rule.getLastOutput().toString().trim();
-      Assert.assertEquals(expectedUtf8String, actual);
    }
 }
