e0a7144d2858e2c8b11eff4cc91f499f203e286e
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-03 23:54:41 +0000

    converted more string based type references to use type tokens

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/workflow/WorkItemAttributes.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/workflow/WorkItemAttributes.java
@@ -10,37 +10,30 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.ui.skynet.widgets.workflow;
 
+import org.eclipse.osee.framework.core.data.IAttributeType;
+import org.eclipse.osee.framework.core.data.NamedIdentity;
+
 /**
  * @author Donald G. Dunne
  */
-public class WorkItemAttributes {
+public class WorkItemAttributes extends NamedIdentity implements IAttributeType {
 
-   public static WorkItemAttributes WORK_ID = new WorkItemAttributes("Work Id");
-   public static WorkItemAttributes WORK_PARENT_ID = new WorkItemAttributes("Work Parent Id");
-   public static WorkItemAttributes WORK_DATA = new WorkItemAttributes("Work Data");
-   public static WorkItemAttributes WORK_DESCRIPTION = new WorkItemAttributes("Work Description");
-   public static WorkItemAttributes WORK_PAGE_NAME = new WorkItemAttributes("Work Page Name");
-   public static WorkItemAttributes WORK_TYPE = new WorkItemAttributes("Work Type");
-   public static WorkItemAttributes TRANSITION = new WorkItemAttributes("Transition");
-   public static WorkItemAttributes START_PAGE = new WorkItemAttributes("Start Page");
+   public static final WorkItemAttributes WORK_DATA = new WorkItemAttributes("AAMFEbyhslOoQf3+hyAA", "Work Data");
+   public static final WorkItemAttributes WORK_ID = new WorkItemAttributes("AAMFEb0hXw933Zrje2QA", "Work Id");
+   public static final WorkItemAttributes WORK_TYPE = new WorkItemAttributes("AAMFEb2f9nh42sjMHfAA", "Work Type");
+   public static final WorkItemAttributes WORK_DESCRIPTION = new WorkItemAttributes("AAMFEb57TkhPHyzOLDwA",
+      "Work Description");
+   public static final WorkItemAttributes WORK_PARENT_ID = new WorkItemAttributes("AAMFEb8R5y9WcjD5hcwA",
+      "Work Parent Id");
+   public static final WorkItemAttributes WORK_PAGE_NAME = new WorkItemAttributes("AAMFEb+vCyCjKbzzHoQA",
+      "Work Page Name");
+   public static final WorkItemAttributes WORK_TRANSITION =
+      new WorkItemAttributes("AAMFEcBLGB5U+55hJrQA", "Transition");
+   public static final WorkItemAttributes WORK_START_PAGE =
+      new WorkItemAttributes("AAMFEcDfggQLaAdLlpQA", "Start Page");
 
-   private final String attributeTypeName;
-
-   public WorkItemAttributes(String name, String attributeTypeName) {
-      this.attributeTypeName = attributeTypeName;
-   }
-
-   /**
-    * Creates attribute with displayName = "<name>" and storeName = "osee.wi.<name>"
-    * 
-    * @param name
-    */
-   public WorkItemAttributes(String name) {
-      this(name, "osee.wi." + name);
-   }
-
-   public String getAttributeTypeName() {
-      return attributeTypeName;
+   private WorkItemAttributes(String guid, String name) {
+      super(guid, "osee.wi." + name);
    }
 
 }
