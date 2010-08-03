b26da5cb0e829e981c8876284b82e26b86cb1156
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-03 23:47:06 +0000

    converted more string based type references to use type tokens

--- plugins/org.eclipse.osee.ats.test/src/org/eclipse/osee/ats/test/util/SMATestUtil.java
+++ plugins/org.eclipse.osee.ats.test/src/org/eclipse/osee/ats/test/util/SMATestUtil.java
@@ -13,7 +13,7 @@ package org.eclipse.osee.ats.test.util;
 import java.util.Arrays;
 import java.util.Collection;
 import junit.framework.Assert;
-import org.eclipse.osee.ats.artifact.ATSAttributes;
+import org.eclipse.osee.ats.artifact.AtsAttributeTypes;
 import org.eclipse.osee.ats.artifact.StateMachineArtifact;
 import org.eclipse.osee.ats.artifact.TeamWorkFlowArtifact.DefaultTeamState;
 
@@ -29,18 +29,18 @@ public class SMATestUtil {
 
    public static void validateSMAs(Collection<? extends StateMachineArtifact> smas, String stateName, int totalPercent, double hoursSpent) throws Exception {
       for (StateMachineArtifact sma : smas) {
-         Assert.assertEquals("Current State wronf for " + sma.getHumanReadableId(),
+         Assert.assertEquals("Current State wrong for " + sma.getHumanReadableId(),
             sma.getStateMgr().getCurrentStateName(), stateName);
          if (sma.isCancelledOrCompleted()) {
             Assert.assertEquals("ats.CurrentState wrong " + sma.getHumanReadableId(),
                sma.getStateMgr().getCurrentStateName() + ";;;",
-               sma.getSoleAttributeValue(ATSAttributes.CURRENT_STATE_ATTRIBUTE.getStoreName()));
+               sma.getSoleAttributeValue(AtsAttributeTypes.ATS_CURRENT_STATE));
          }
          Assert.assertEquals("Percent wrong for " + sma.getHumanReadableId(), sma.getWorldViewPercentCompleteTotal(),
             totalPercent);
          Assert.assertEquals("Hours Spent wrong for " + sma.getHumanReadableId(), sma.getWorldViewHoursSpentTotal(),
             hoursSpent);
-         for (String stateValue : sma.getAttributesToStringList(ATSAttributes.STATE_ATTRIBUTE.getStoreName())) {
+         for (String stateValue : sma.getAttributesToStringList(AtsAttributeTypes.ATS_CURRENT_STATE)) {
             if (stateValue.startsWith(DefaultTeamState.Completed.name())) {
                Assert.assertEquals("ats.State wrong " + sma.getHumanReadableId(), stateValue, "Completed;;;");
             } else if (stateValue.startsWith(DefaultTeamState.Cancelled.name())) {
