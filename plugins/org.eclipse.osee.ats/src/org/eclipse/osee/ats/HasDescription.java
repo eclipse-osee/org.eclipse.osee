a0dc86a59f7d171e70963f40fcd7244f5482ed19
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-04 00:02:05 +0000

    converted more string based type references to use type tokens

---
+++ plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/HasDescription.java
@@ -0,0 +1,11 @@
+/*
+ * Created on Jul 30, 2010
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.ats;
+
+public interface HasDescription {
+
+   String getDescription();
+}
