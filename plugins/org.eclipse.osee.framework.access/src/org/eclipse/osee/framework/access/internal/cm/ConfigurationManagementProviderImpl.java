1e2a1b6b12455bb9016d0706a507629250534796
rescobar <rescobar@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-05 04:06:53 +0000

    "Team Workflow" - YGHW1 - "Block changes to requirements unless done under an appropriate action."

--- plugins/org.eclipse.osee.framework.access/src/org/eclipse/osee/framework/access/internal/cm/ConfigurationManagementProviderImpl.java
+++ plugins/org.eclipse.osee.framework.access/src/org/eclipse/osee/framework/access/internal/cm/ConfigurationManagementProviderImpl.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osee.framework.access.internal.cm;
 
 import java.util.Collection;
@@ -7,12 +17,17 @@ import org.eclipse.osee.framework.core.services.ConfigurationManagement;
 import org.eclipse.osee.framework.core.services.ConfigurationManagementProvider;
 import org.eclipse.osee.framework.core.services.HasConfigurationManagement;
 
+/**
+ * @author Roberto E. Escobar
+ */
 public class ConfigurationManagementProviderImpl implements ConfigurationManagementProvider {
 
    private final Collection<ConfigurationManagement> cmServices;
+   private final ConfigurationManagement defaultCM;
 
-   public ConfigurationManagementProviderImpl(Collection<ConfigurationManagement> cmServices) {
+   public ConfigurationManagementProviderImpl(ConfigurationManagement defaultCM, Collection<ConfigurationManagement> cmServices) {
       this.cmServices = cmServices;
+      this.defaultCM = defaultCM;
    }
 
    @Override
@@ -23,15 +38,18 @@ public class ConfigurationManagementProviderImpl implements ConfigurationManagem
          cmToReturn = cmContainer.getCM();
       } else {
          for (ConfigurationManagement cmService : cmServices) {
-            if (cmService.isApplicable(userArtifact, object)) {
-               cmToReturn = cmService;
-               break;
+            if (!cmService.equals(defaultCM)) {
+               if (cmService.isApplicable(userArtifact, object)) {
+                  cmToReturn = cmService;
+                  break;
+               }
             }
          }
          if (cmToReturn == null) {
-            cmToReturn = null; //TODO: provide Default
+            cmToReturn = defaultCM;
          }
       }
       return cmToReturn;
    }
+
 }
