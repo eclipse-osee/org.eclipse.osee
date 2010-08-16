73f4488c1eff0239cb86495a1046e71942551e56
ddunne <ddunne@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-16 18:31:31 +0000

    cleanup code warnings

--- plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/data/IOseeBranch.java
+++ plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/data/IOseeBranch.java
@@ -14,4 +14,5 @@ package org.eclipse.osee.framework.core.data;
  * @author Ryan D. Brooks
  */
 public interface IOseeBranch extends Identity, Named {
+   // composition interface requires no additional methods,fields
 }
