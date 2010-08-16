c4321d60d38671e92d47fd30cae5c61bf57f1323
rbrooks <rbrooks@ee007c2a-0a25-0410-9ab9-bf268980928c>
2010-08-16 04:04:20 +0000

    replaced deprecated call to IWorkspaceRoot.findFilesForLocation with IWorkspaceRoot.findFilesForLocationURI

--- plugins/org.eclipse.osee.framework.plugin.core/src/org/eclipse/osee/framework/plugin/core/util/AIFile.java
+++ plugins/org.eclipse.osee.framework.plugin.core/src/org/eclipse/osee/framework/plugin/core/util/AIFile.java
@@ -10,6 +10,7 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.plugin.core.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -18,7 +19,7 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.osee.framework.core.exception.OseeArgumentException;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.exception.OseeExceptions;
 import org.eclipse.osee.framework.jdk.core.util.io.Streams;
@@ -29,6 +30,7 @@ import org.eclipse.osee.framework.jdk.core.util.io.Streams;
 public final class AIFile {
 
    private AIFile() {
+      // utility class
    }
 
    public static void writeToFile(IFile file, InputStream in) throws OseeCoreException {
@@ -59,12 +61,15 @@ public final class AIFile {
       }
    }
 
-   public static IFile constructIFile(String fullPath) {
+   public static IFile constructIFile(String fullPath) throws OseeArgumentException {
       IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-      IFile[] files = workspaceRoot.findFilesForLocation(Path.fromOSString(fullPath));
-      if (files == null || files.length == 0) {
-         return null;
+      IFile[] files = workspaceRoot.findFilesForLocationURI(new File(fullPath).toURI());
+
+      if (files.length == 1) {
+         return files[0];
+      } else {
+         throw new OseeArgumentException(String.format(
+            "expected findFilesForLocationURI for %s to find 1 file but %d found", fullPath, files.length));
       }
-      return workspaceRoot.findFilesForLocation(Path.fromOSString(fullPath))[0];
    }
 }
