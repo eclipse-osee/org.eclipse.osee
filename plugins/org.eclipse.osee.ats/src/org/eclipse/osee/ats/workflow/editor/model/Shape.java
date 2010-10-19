90ace39c57d27ebbc4e92ee4b4c07c80e488e10e
rbrooks <ryan.d.brooks@boeing.com>
2010-10-19 09:15:48 -0700

    refactor: Replace garbled distribution statements with correct text

--- plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/workflow/editor/model/Shape.java
+++ plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/workflow/editor/model/Shape.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 Donald G. Dunne and others.
-�* All rights reserved. This program and the accompanying materials
-�* are made available under the terms of the Eclipse Public License v1.0
-�* which accompanies this distribution, and is available at
-�* http://www.eclipse.org/legal/epl-v10.html
-�*
-�* Contributors:
-�*����Donald G. Dunne - initial API and implementation
-�*******************************************************************************/
+ * Copyright (c) 2010 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osee.ats.workflow.editor.model;
 
 import java.util.ArrayList;
