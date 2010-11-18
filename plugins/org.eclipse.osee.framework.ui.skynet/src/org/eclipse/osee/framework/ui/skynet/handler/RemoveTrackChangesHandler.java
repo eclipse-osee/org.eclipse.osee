3c1a19484d81dac5eed86816beded30e9c1441c6
rescobar <roberto.e.escobar@boeing.com>
2010-11-18 16:33:48 -0700

    refinement: Update RemoveTrackChangesHandler no popups on tests

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/handler/RemoveTrackChangesHandler.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/handler/RemoveTrackChangesHandler.java
@@ -13,7 +13,8 @@ package org.eclipse.osee.framework.ui.skynet.handler;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.IStatusHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.osee.framework.jdk.core.type.MutableInteger;
+import org.eclipse.osee.framework.jdk.core.type.MutableBoolean;
+import org.eclipse.osee.framework.ui.skynet.render.RenderingUtil;
 import org.eclipse.osee.framework.ui.swt.Displays;
 import org.eclipse.ui.PlatformUI;
 
@@ -23,18 +24,23 @@ import org.eclipse.ui.PlatformUI;
 public class RemoveTrackChangesHandler implements IStatusHandler {
    @Override
    public Object handleStatus(IStatus status, Object source) {
-      final MutableInteger result = new MutableInteger(0);
+      final MutableBoolean isOkToRemove = new MutableBoolean(false);
       final String message = (String) source;
 
-      Displays.pendInDisplayThread(new Runnable() {
-         @Override
-         public void run() {
-            if (MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-               "Confirm Removal Of Track Changes ", message)) {
-               result.setValue(1);
+      if (RenderingUtil.arePopupsAllowed()) {
+         Displays.pendInDisplayThread(new Runnable() {
+            @Override
+            public void run() {
+               boolean doesUserConfirm =
+                  MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+                     "Confirm Removal Of Track Changes ", message);
+               isOkToRemove.setValue(doesUserConfirm);
             }
-         }
-      });
-      return result.getValue() == 1;
+         });
+      } else {
+         // For Test Purposes
+         isOkToRemove.setValue(true);
+      }
+      return isOkToRemove.getValue();
    }
 }
