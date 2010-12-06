846800958961aa62e98cc314f02a9252b3784a59
ddunne <donald.g.dunne@boeing.com>
2010-12-06 09:21:05 -0700

    refinement: Remove deprecated off Strings.isValid(String)

--- plugins/org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/util/Strings.java
+++ plugins/org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/util/Strings.java
@@ -17,85 +17,81 @@ package org.eclipse.osee.framework.jdk.core.util;
  * @author Karol M. Wilk
  */
 public class Strings {
-	private final static String EMPTY_STRING = "";
+   private final static String EMPTY_STRING = "";
 
-	private Strings() {
-		// Utility class
-	}
+   private Strings() {
+      // Utility class
+   }
 
-	/**
-	 * Don't remove this function without coordinating the change with the OTE source code. 
-	 */
-	@Deprecated
-	public static boolean isValid(String value) {
-		return value != null && value.length() > 0;
-	}
+   /**
+    * Don't remove this function without coordinating the change with the OTE source code.
+    */
+   public static boolean isValid(String value) {
+      return value != null && value.length() > 0;
+   }
 
-	public static boolean isValid(CharSequence value) {
-		return value != null && value.length() > 0;
-	}
+   public static boolean isValid(CharSequence value) {
+      return value != null && value.length() > 0;
+   }
 
-	public static String emptyString() {
-		return EMPTY_STRING;
-	}
+   public static String emptyString() {
+      return EMPTY_STRING;
+   }
 
-	/**
-	 * This method adjusts '&'-containing strings to break the keyboard shortcut
-	 * ("Accelerator") feature some widgets offer, where &Test will make Alt+T a
-	 * shortcut. This method breaks the accelerator by escaping ampersands.
-	 * 
-	 * @return a string with doubled ampersands.
-	 */
-	public static String escapeAmpersands(String stringWithAmp) {
-		if (isValid(stringWithAmp)) {
-			return stringWithAmp.replace("&", "&&");
-		} else {
-			return null;
-		}
-	}
+   /**
+    * This method adjusts '&'-containing strings to break the keyboard shortcut ("Accelerator") feature some widgets
+    * offer, where &Test will make Alt+T a shortcut. This method breaks the accelerator by escaping ampersands.
+    * 
+    * @return a string with doubled ampersands.
+    */
+   public static String escapeAmpersands(String stringWithAmp) {
+      if (isValid(stringWithAmp)) {
+         return stringWithAmp.replace("&", "&&");
+      } else {
+         return null;
+      }
+   }
 
-	public static String intern(String str) {
-		if (str == null) {
-			return null;
-		}
-		return str.intern();
-	}
+   public static String intern(String str) {
+      if (str == null) {
+         return null;
+      }
+      return str.intern();
+   }
 
-	/**
-	 * Will truncate string if necessary and add "..." to end if addDots and
-	 * truncated
-	 */
-	public static String truncate(String value, int length, boolean addDots) {
-		if (value == null) {
-			return emptyString();
-		}
-		String toReturn = value;
-		if (Strings.isValid(value) && value.length() > length) {
-			int len = addDots && length - 3 > 0 ? length - 3 : length;
-			toReturn = value.substring(0, Math.min(length, len))
-					+ (addDots ? "..." : emptyString());
-		}
-		return toReturn;
-	}
+   /**
+    * Will truncate string if necessary and add "..." to end if addDots and truncated
+    */
+   public static String truncate(String value, int length, boolean addDots) {
+      if (value == null) {
+         return emptyString();
+      }
+      String toReturn = value;
+      if (Strings.isValid(value) && value.length() > length) {
+         int len = addDots && length - 3 > 0 ? length - 3 : length;
+         toReturn = value.substring(0, Math.min(length, len)) + (addDots ? "..." : emptyString());
+      }
+      return toReturn;
+   }
 
-	public static String truncate(String value, int length) {
-		return truncate(value, length, false);
-	}
+   public static String truncate(String value, int length) {
+      return truncate(value, length, false);
+   }
 
-	public static String unquote(String nameReference) {
-		String toReturn = nameReference;
-		if (Strings.isValid(nameReference) && nameReference.contains("\"")) {
-			toReturn = nameReference.replaceAll("\\\"", emptyString());
-		}
-		return toReturn;
-	}
+   public static String unquote(String nameReference) {
+      String toReturn = nameReference;
+      if (Strings.isValid(nameReference) && nameReference.contains("\"")) {
+         toReturn = nameReference.replaceAll("\\\"", emptyString());
+      }
+      return toReturn;
+   }
 
-	public static String quote(String nameReference) {
-		String toReturn = nameReference;
-		if (Strings.isValid(nameReference)) {
-			toReturn = String.format("\"%s\"", nameReference);
-		}
-		return toReturn;
-	}
+   public static String quote(String nameReference) {
+      String toReturn = nameReference;
+      if (Strings.isValid(nameReference)) {
+         toReturn = String.format("\"%s\"", nameReference);
+      }
+      return toReturn;
+   }
 
 }
