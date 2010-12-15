e778460dc46b3065046810d057fc0717be73a062
rescobar <roberto.e.escobar@boeing.com>
2010-12-14 17:40:15 -0700

    bug[ats_JGX6U]: Create help context reference object

---
+++ plugins/org.eclipse.osee.framework.ui.plugin/src/org/eclipse/osee/framework/ui/plugin/util/HelpContext.java
@@ -0,0 +1,78 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.plugin.util;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public final class HelpContext {
+   private final String name;
+   private final String pluginId;
+
+   protected HelpContext(String pluginId, String name) {
+      this.pluginId = pluginId;
+      this.name = name;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public String getPluginId() {
+      return pluginId;
+   }
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((name == null) ? 0 : name.hashCode());
+      result = prime * result + ((pluginId == null) ? 0 : pluginId.hashCode());
+      return result;
+   }
+
+   @Override
+   public boolean equals(Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (getClass() != obj.getClass()) {
+         return false;
+      }
+      HelpContext other = (HelpContext) obj;
+      if (name == null) {
+         if (other.name != null) {
+            return false;
+         }
+      } else if (!name.equals(other.name)) {
+         return false;
+      }
+      if (pluginId == null) {
+         if (other.pluginId != null) {
+            return false;
+         }
+      } else if (!pluginId.equals(other.pluginId)) {
+         return false;
+      }
+      return true;
+   }
+
+   public String asReference() {
+      return asReference(pluginId, name);
+   }
+
+   public static String asReference(String pluginId, String name) {
+      return String.format("%s.%s", pluginId, name);
+   }
+}
--- plugins/org.eclipse.osee.framework.ui.plugin/src/org/eclipse/osee/framework/ui/plugin/util/HelpUtil.java
+++ plugins/org.eclipse.osee.framework.ui.plugin/src/org/eclipse/osee/framework/ui/plugin/util/HelpUtil.java
@@ -10,36 +10,80 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.ui.plugin.util;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.help.IWorkbenchHelpSystem;
 
 public class HelpUtil {
 
+   private static final Map<String, HelpContext> contexts = new ConcurrentHashMap<String, HelpContext>();
+
+   public static void setHelp(Control control, HelpContext context) {
+      getHelpSystem().setHelp(control, context.asReference());
+   }
+
+   public static void setHelp(IAction action, HelpContext context) {
+      getHelpSystem().setHelp(action, context.asReference());
+   }
+
+   public static void setHelp(Menu menu, HelpContext context) {
+      getHelpSystem().setHelp(menu, context.asReference());
+   }
+
+   public static void setHelp(MenuItem menuItem, HelpContext context) {
+      getHelpSystem().setHelp(menuItem, context.asReference());
+   }
+
+   public static void displayHelp(HelpContext context) {
+      getHelpSystem().displayHelp(context.asReference());
+   }
+
+   @Deprecated
    public static void setHelp(Control control, String name, String library) {
-      PlatformUI.getWorkbench().getHelpSystem().setHelp(control, library + "." + name);
+      setHelp(control, asContext(library, name));
    }
 
+   @Deprecated
    public static void setHelp(IAction action, String name, String library) {
-      PlatformUI.getWorkbench().getHelpSystem().setHelp(action, library + "." + name);
+      setHelp(action, asContext(library, name));
    }
 
+   @Deprecated
    public static void setHelp(Menu menu, String name, String library) {
-      PlatformUI.getWorkbench().getHelpSystem().setHelp(menu, library + "." + name);
+      setHelp(menu, asContext(library, name));
    }
 
+   @Deprecated
    public static void setHelp(MenuItem menuItem, String name, String library) {
-      PlatformUI.getWorkbench().getHelpSystem().setHelp(menuItem, library + "." + name);
+      setHelp(menuItem, asContext(library, name));
    }
 
+   @Deprecated
    public static void displayHelp(String name, String library) {
-      PlatformUI.getWorkbench().getHelpSystem().displayHelp(library + "." + name);
+      displayHelp(asContext(library, name));
    }
 
+   @Deprecated
    public static void displayHelp(String contextId) {
-      PlatformUI.getWorkbench().getHelpSystem().displayHelp();
+      getHelpSystem().displayHelp(contextId);
    }
 
+   public static IWorkbenchHelpSystem getHelpSystem() {
+      return PlatformUI.getWorkbench().getHelpSystem();
+   }
+
+   public static HelpContext asContext(String pluginId, String name) {
+      String key = HelpContext.asReference(pluginId, name);
+      HelpContext context = contexts.get(key);
+      if (context == null) {
+         context = new HelpContext(pluginId, name);
+         contexts.put(context.asReference(), context);
+      }
+      return context;
+   }
 }
