c96062cfa3d5c3291e0f140e727e4ccec32ba35b
rbrooks <ryan.d.brooks@boeing.com>
2011-01-04 12:27:56 -0700

    refactor: Remove usage of obsolete method AIFile.constructIFile

--- plugins/org.eclipse.osee.framework.plugin.core/src/org/eclipse/osee/framework/plugin/core/util/AIFile.java
+++ plugins/org.eclipse.osee.framework.plugin.core/src/org/eclipse/osee/framework/plugin/core/util/AIFile.java
@@ -10,14 +10,12 @@
  *******************************************************************************/
 package org.eclipse.osee.framework.plugin.core.util;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.osee.framework.core.exception.OseeArgumentException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.osee.framework.core.exception.OseeCoreException;
 import org.eclipse.osee.framework.core.exception.OseeExceptions;
 import org.eclipse.osee.framework.jdk.core.util.io.Streams;
@@ -57,20 +55,7 @@ public final class AIFile {
       }
    }
 
-   public static IFile constructIFile(File file) throws OseeArgumentException {
-      IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-      IFile[] files = workspaceRoot.findFilesForLocationURI(file.toURI());
-
-      if (files.length == 1) {
-         return files[0];
-      } else {
-         throw new OseeArgumentException(
-            String.format("expected findFilesForLocationURI for %s to find 1 file but %d found",
-               file.getAbsolutePath(), files.length));
-      }
-   }
-
-   public static IFile constructIFile(String fullPath) throws OseeArgumentException {
-      return constructIFile(new File(fullPath));
+   public static IFile constructIFile(String fullPath) {
+      return ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(new Path(fullPath));
    }
 }
