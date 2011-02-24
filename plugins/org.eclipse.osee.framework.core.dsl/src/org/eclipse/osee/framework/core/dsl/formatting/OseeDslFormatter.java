1bc51a6888b111d9dec49e07a981adc4d64318f1
rescobar <roberto.e.escobar@boeing.com>
2011-02-24 15:36:41 -0700

    feature[ats_YGHW1]: Improve dsl formatter

--- plugins/org.eclipse.osee.framework.core.dsl/src/org/eclipse/osee/framework/core/dsl/formatting/OseeDslFormatter.java
+++ plugins/org.eclipse.osee.framework.core.dsl/src/org/eclipse/osee/framework/core/dsl/formatting/OseeDslFormatter.java
@@ -47,7 +47,6 @@ public class OseeDslFormatter extends AbstractDeclarativeFormatter implements II
       "defaultValue",
       "fileExtension",
       "taggerId",
-      "artifact",
       "accessContext",
       "ALLOW",
       "DENY",
