fd460a0195ddc82a1c61377664f6f0e9c9c83bb0
rescobar <roberto.e.escobar@boeing.com>
2011-06-24 12:25:17 -0700

    refinement[bgz_349095]: Add multipart/mixed

--- plugins/org.eclipse.osee.mail/src/org/eclipse/osee/mail/MailUtils.java
+++ plugins/org.eclipse.osee.mail/src/org/eclipse/osee/mail/MailUtils.java
@@ -41,6 +41,7 @@ public final class MailUtils {
       mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml");
       mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
       mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+      mc.addMailcap("multipart/mixed;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
       mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
       return mc;
    }
