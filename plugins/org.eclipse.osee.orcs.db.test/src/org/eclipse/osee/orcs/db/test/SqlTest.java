9abd2e4fd14ca8637a975c798c340e0cae7e5a1c
rescobar <roberto.e.escobar@boeing.com>
2011-09-26 15:57:59 -0700

    feature[ats_Q9NLC]: Mavenize xserver build

---
+++ plugins/org.eclipse.osee.orcs.db.test/src/org/eclipse/osee/orcs/db/test/SqlTest.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.orcs.db.test;
+
+import org.junit.Assert;
+
+/**
+ * @author Roberto E. Escobar
+ */
+public class SqlTest {
+
+   @org.junit.Test
+   public void testOne() {
+      Assert.assertEquals("dummy test", 1, 1);
+   }
+}
