3fe0c43120655b5d6bafe22c46846758ba8093bb
rescobar <roberto.e.escobar@boeing.com>
2011-09-30 11:56:37 -0700

    feature[ats_Q9NLC]: Comment out compile problems in orcs.mock

--- plugins/org.eclipse.osee.orcs.mock/src/org/eclipse/osee/orcs/mock/FakeArtifact.java
+++ plugins/org.eclipse.osee.orcs.mock/src/org/eclipse/osee/orcs/mock/FakeArtifact.java
@@ -5,65 +5,57 @@
  */
 package org.eclipse.osee.orcs.mock;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import org.eclipse.osee.display.presenter.internal.WebProgramsPresenter.IFakeArtifact;
-import org.eclipse.osee.framework.core.data.IAttributeType;
-import org.eclipse.osee.framework.core.data.IRelationTypeSide;
-import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
 
-public class FakeArtifact implements IFakeArtifact {
-
-   private final Map<IRelationTypeSide, Collection<IFakeArtifact>> relationMap =
-      new HashMap<IRelationTypeSide, Collection<IFakeArtifact>>();
-   private final Map<IAttributeType, String> attributeMap = new HashMap<IAttributeType, String>();
-   private final String guid;
-
-   public FakeArtifact(String name, String guid) {
-      attributeMap.put(CoreAttributeTypes.Name, name);
-      this.guid = guid;
-   }
-
-   public void addRelation(IRelationTypeSide relationType, IFakeArtifact artifact) {
-      Collection<IFakeArtifact> related;
-      if (relationMap.containsKey(relationType)) {
-         related = relationMap.get(relationType);
-      } else {
-         related = new LinkedList<IFakeArtifact>();
-      }
-      related.add(artifact);
-      relationMap.put(relationType, related);
-   }
-
-   @Override
-   public List<IFakeArtifact> getRelatedArtifacts(IRelationTypeSide relationSide) {
-      return new LinkedList<IFakeArtifact>(relationMap.get(relationSide));
-   }
-
-   @Override
-   public IFakeArtifact getRelatedArtifact(IRelationTypeSide relationSide) {
-      if (relationMap.containsKey(relationSide)) {
-         return relationMap.get(relationSide).iterator().next();
-      } else {
-         return null;
-      }
-   }
-
-   @Override
-   public String getName() {
-      return attributeMap.get(CoreAttributeTypes.Name);
-   }
-
-   @Override
-   public String getGuid() {
-      return guid;
-   }
-
-   public void setSoleAttributeFromString(IAttributeType type, String value) {
-      attributeMap.put(type, value);
-   }
+public class FakeArtifact {
+   //implements IFakeArtifact {
+   //
+   //   private final Map<IRelationTypeSide, Collection<IFakeArtifact>> relationMap =
+   //      new HashMap<IRelationTypeSide, Collection<IFakeArtifact>>();
+   //   private final Map<IAttributeType, String> attributeMap = new HashMap<IAttributeType, String>();
+   //   private final String guid;
+   //
+   //   public FakeArtifact(String name, String guid) {
+   //      attributeMap.put(CoreAttributeTypes.Name, name);
+   //      this.guid = guid;
+   //   }
+   //
+   //   public void addRelation(IRelationTypeSide relationType, IFakeArtifact artifact) {
+   //      Collection<IFakeArtifact> related;
+   //      if (relationMap.containsKey(relationType)) {
+   //         related = relationMap.get(relationType);
+   //      } else {
+   //         related = new LinkedList<IFakeArtifact>();
+   //      }
+   //      related.add(artifact);
+   //      relationMap.put(relationType, related);
+   //   }
+   //
+   //   @Override
+   //   public List<IFakeArtifact> getRelatedArtifacts(IRelationTypeSide relationSide) {
+   //      return new LinkedList<IFakeArtifact>(relationMap.get(relationSide));
+   //   }
+   //
+   //   @Override
+   //   public IFakeArtifact getRelatedArtifact(IRelationTypeSide relationSide) {
+   //      if (relationMap.containsKey(relationSide)) {
+   //         return relationMap.get(relationSide).iterator().next();
+   //      } else {
+   //         return null;
+   //      }
+   //   }
+   //
+   //   @Override
+   //   public String getName() {
+   //      return attributeMap.get(CoreAttributeTypes.Name);
+   //   }
+   //
+   //   @Override
+   //   public String getGuid() {
+   //      return guid;
+   //   }
+   //
+   //   public void setSoleAttributeFromString(IAttributeType type, String value) {
+   //      attributeMap.put(type, value);
+   //   }
 
 }
