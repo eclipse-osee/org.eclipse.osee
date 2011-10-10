e0c4df996d4de06766f2fb45fdfe8a3cb2f5d911
rescobar <roberto.e.escobar@boeing.com>
2011-10-10 16:23:25 -0700

    feature[ats_Q9NLC]: Implement orcs exception

--- plugins/org.eclipse.osee.orcs/src/org/eclipse/osee/orcs/OrcsException.java
+++ plugins/org.eclipse.osee.orcs/src/org/eclipse/osee/orcs/OrcsException.java
@@ -1,10 +1,47 @@
-/*
- * Created on Oct 3, 2011
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
- */
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.osee.orcs;
 
+import org.eclipse.osee.framework.jdk.core.util.Collections;
+
+/**
+ * @author Roberto E. Escobar
+ */
 public class OrcsException extends Exception {
 
+   private static final long serialVersionUID = -7326541420696736796L;
+
+   public OrcsException(String message, Object... args) {
+      super(formatMessage(message, args));
+   }
+
+   public OrcsException(Throwable cause, String message) {
+      super(formatMessage(message), cause);
+   }
+
+   public OrcsException(Throwable cause, String message, Object... args) {
+      super(formatMessage(message, args), cause);
+   }
+
+   public OrcsException(Throwable cause) {
+      super(cause);
+   }
+
+   private static String formatMessage(String message, Object... args) {
+      try {
+         return String.format(message, args);
+      } catch (RuntimeException ex) {
+         return String.format(
+            "Exception message could not be formatted: [%s] with the following arguments [%s].  Cause [%s]", message,
+            Collections.toString(",", args), ex.toString());
+      }
+   }
 }
