6ab347fe48c0b6081f4f8a279d6f0473e3f2bddf
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2011-11-09 18:10:23 -0700

    feature[ats_ZPXX0]: Allow Commit Into working branch for none admin users

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/commandHandlers/branch/commit/CommitIntoHandler.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/commandHandlers/branch/commit/CommitIntoHandler.java
@@ -13,6 +13,7 @@ package org.eclipse.osee.framework.ui.skynet.commandHandlers.branch.commit;
 import java.util.Collections;
 import java.util.List;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.osee.framework.access.AccessControlManager;
@@ -27,6 +28,7 @@ import org.eclipse.osee.framework.ui.plugin.util.AWorkbench;
 import org.eclipse.osee.framework.ui.skynet.branch.BranchSelectionDialog;
 import org.eclipse.osee.framework.ui.skynet.commandHandlers.Handlers;
 import org.eclipse.osee.framework.ui.skynet.widgets.dialog.CheckBoxDialog;
+import org.eclipse.osee.framework.ui.swt.Displays;
 
 /**
  * @author Jeff C. Phillips
@@ -45,32 +47,42 @@ public class CommitIntoHandler extends CommitHandler {
          (IStructuredSelection) AWorkbench.getActivePage().getActivePart().getSite().getSelectionProvider().getSelection();
       Branch sourceBranch = Handlers.getBranchesFromStructuredSelection(selection).iterator().next();
 
-      List<Branch> branches =
-         BranchManager.getBranches(BranchArchivedState.UNARCHIVED, BranchType.WORKING, BranchType.BASELINE);
-      Collections.sort(branches);
-
-      branches.remove(sourceBranch);
-      BranchSelectionDialog branchSelection =
-         new BranchSelectionDialog(String.format("Source Branch [%s]\n\nSelect Destination Branch", sourceBranch),
-            branches);
-      int result = branchSelection.open();
-      if (result == Window.OK) {
-         CheckBoxDialog dialog =
-            new CheckBoxDialog("Commit Into", String.format(
-               "Commit from\n\nSource Branch: [%s]\n\ninto\n\nDestination Branch: [%s]", sourceBranch,
-               branchSelection.getSelected()), "Archive Source Branch");
-         if (dialog.open() == 0) {
-            Jobs.startJob(new CommitJob(sourceBranch, branchSelection.getSelected(), dialog.isChecked()));
-         }
+      BranchType[] allowedTypes;
+      if (AccessControlManager.isOseeAdmin()) {
+         allowedTypes = new BranchType[] {BranchType.WORKING, BranchType.BASELINE};
+      } else {
+         allowedTypes = new BranchType[] {BranchType.WORKING};
       }
 
+      List<Branch> branches = BranchManager.getBranches(BranchArchivedState.UNARCHIVED, allowedTypes);
+      Collections.sort(branches);
+      branches.remove(sourceBranch);
+
+      if (!branches.isEmpty()) {
+         BranchSelectionDialog branchSelection =
+            new BranchSelectionDialog(String.format("Source Branch [%s]\n\nSelect Destination Branch", sourceBranch),
+               branches);
+         int result = branchSelection.open();
+         if (result == Window.OK) {
+            CheckBoxDialog dialog =
+               new CheckBoxDialog("Commit Into", String.format(
+                  "Commit from\n\nSource Branch: [%s]\n\ninto\n\nDestination Branch: [%s]", sourceBranch,
+                  branchSelection.getSelected()), "Archive Source Branch");
+            if (dialog.open() == 0) {
+               Jobs.startJob(new CommitJob(sourceBranch, branchSelection.getSelected(), dialog.isChecked()));
+            }
+         }
+      } else {
+         MessageDialog.openWarning(Displays.getActiveShell(), "Commit Into",
+            "No valid destination branches found.\n\nPlease contact an OSEE Admin from more information.");
+      }
       return null;
    }
 
    @Override
-   public boolean isEnabledWithException(IStructuredSelection structuredSelection) throws OseeCoreException {
+   public boolean isEnabledWithException(IStructuredSelection structuredSelection) {
       List<? extends IOseeBranch> branches = Handlers.getBranchesFromStructuredSelection(structuredSelection);
-      return branches.size() == 1 && AccessControlManager.isOseeAdmin();
+      return branches.size() == 1;
    }
 
 }
