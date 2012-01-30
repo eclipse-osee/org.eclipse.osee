2032d716277771423f4a7fd4f9383c9edd422bde
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2012-01-30 10:01:41 -0700

    feature[ats_AGHDK]: Add dialog before renaming test cases

--- plugins/org.eclipse.osee.define/src/org/eclipse/osee/define/traceability/operations/TraceResourceDropOperation.java
+++ plugins/org.eclipse.osee.define/src/org/eclipse/osee/define/traceability/operations/TraceResourceDropOperation.java
@@ -14,6 +14,9 @@ import java.io.File;
 import java.net.URI;
 import java.nio.CharBuffer;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -39,19 +42,26 @@ import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
  */
 public class TraceResourceDropOperation extends AbstractOperation {
 
+   public static interface RenameConfirmer {
+
+      boolean acceptUpdate(Map<Artifact, String> nameUpdateRequired);
+   }
+
    private final Collection<URI> resources;
    private final IResourceLocator locator;
    private final IRelationTypeSide relTypeSide;
    private final Artifact requirement;
    private final boolean persistChanges;
+   private final RenameConfirmer confirmer;
 
-   public TraceResourceDropOperation(Collection<URI> resources, IRelationTypeSide relTypeSide, Artifact requirement, IResourceLocator locator, boolean persistChanges) {
+   public TraceResourceDropOperation(Collection<URI> resources, IRelationTypeSide relTypeSide, Artifact requirement, IResourceLocator locator, boolean persistChanges, RenameConfirmer confirmer) {
       super("Trace Resource Drop Operation", Activator.PLUGIN_ID);
       this.relTypeSide = relTypeSide;
       this.requirement = requirement;
       this.resources = resources;
       this.locator = locator;
       this.persistChanges = persistChanges;
+      this.confirmer = confirmer;
    }
 
    @Override
@@ -62,20 +72,37 @@ public class TraceResourceDropOperation extends AbstractOperation {
          if (persistChanges) {
             transaction = TransactionManager.createTransaction(branch, "TraceResourceDrop");
          }
+
+         Map<Artifact, String> nameUpdateRequired = new TreeMap<Artifact, String>();
+
          for (URI resource : resources) {
             File file = new File(resource);
             if (!file.isDirectory()) {
-               processFile(file, transaction);
+               processFile(file, transaction, nameUpdateRequired);
             }
          }
-         if (persistChanges) {
+
+         boolean isOk = true;
+         if (!nameUpdateRequired.isEmpty()) {
+            if (confirmer != null) {
+               isOk = confirmer.acceptUpdate(nameUpdateRequired);
+            }
+
+            if (isOk) {
+               for (Entry<Artifact, String> entry : nameUpdateRequired.entrySet()) {
+                  entry.getKey().setName(entry.getValue());
+               }
+            }
+         }
+
+         if (persistChanges && isOk) {
             requirement.persist(transaction);
             transaction.execute();
          }
       }
    }
 
-   private void processFile(File file, SkynetTransaction transaction) throws Exception {
+   private void processFile(File file, SkynetTransaction transaction, Map<Artifact, String> nameUpdateRequired) throws Exception {
       CharBuffer fileBuffer = Lib.fileToCharBuffer(file);
       IFileStore fileStore = EFS.getStore(file.toURI());
       String name = locator.getIdentifier(fileStore, fileBuffer).getName();
@@ -104,8 +131,9 @@ public class TraceResourceDropOperation extends AbstractOperation {
             tagger.addSourceTag(file.toURI(), testUnitArtifact.getGuid());
          }
       }
+
       if (!testUnitArtifact.getName().equals(name)) {
-         testUnitArtifact.setName(name);
+         nameUpdateRequired.put(testUnitArtifact, name);
       }
 
       requirement.addRelation(relTypeSide, testUnitArtifact);
