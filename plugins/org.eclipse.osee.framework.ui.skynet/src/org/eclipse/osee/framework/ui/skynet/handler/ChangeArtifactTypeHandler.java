fbc171249d56a532d3302e6154269a33e2fae508
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2012-07-20 18:24:09 -0700

    bug[ats_Z4MD8]: Disable dialog for ChangeArtifactType during tests

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/handler/ChangeArtifactTypeHandler.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/handler/ChangeArtifactTypeHandler.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang.mutable.MutableBoolean;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.IStatusHandler;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osee.framework.ui.skynet.render.RenderingUtil;
 import org.eclipse.osee.framework.ui.swt.Displays;
 import org.eclipse.ui.PlatformUI;
 
@@ -23,21 +24,25 @@ import org.eclipse.ui.PlatformUI;
 public class ChangeArtifactTypeHandler implements IStatusHandler {
    @Override
    public Object handleStatus(IStatus status, Object source) {
-      final MutableBoolean result = new MutableBoolean(false);
-      final String message = (String) source;
+      boolean toReturn = false;
 
-      Runnable runnable = new Runnable() {
-         @Override
-         public void run() {
-            if (MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-               "Confirm Artifact Type Change ", message)) {
-               result.setValue(true);
+      if (RenderingUtil.arePopupsAllowed()) {
+         final MutableBoolean result = new MutableBoolean(false);
+         final String message = (String) source;
+         Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+               if (MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+                  "Confirm Artifact Type Change ", message)) {
+                  result.setValue(true);
+               }
             }
-         }
-
-      };
-
-      Displays.pendInDisplayThread(runnable);
-      return result.getValue();
+         };
+         Displays.pendInDisplayThread(runnable);
+         toReturn = result.booleanValue();
+      } else {
+         toReturn = true;
+      }
+      return toReturn;
    }
 }
