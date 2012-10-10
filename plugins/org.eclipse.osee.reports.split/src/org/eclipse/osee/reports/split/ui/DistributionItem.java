4a029b20d2712619915fa73ef1b17777f44d9ffc
Sumy Joseph <sumy.joseph@in.bosch.com>
2012-10-10 16:53:27 -0700

    feature[bgz_391218]: Add project management work distribution report

---
+++ plugins/org.eclipse.osee.reports.split/src/org/eclipse/osee/reports/split/ui/DistributionItem.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2012 Robert Bosch Engineering and Business Solutions Ltd India. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.osee.reports.split.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.osee.ats.AtsImage;
+import org.eclipse.osee.ats.api.version.IAtsVersion;
+import org.eclipse.osee.ats.core.client.config.store.VersionArtifactStore;
+import org.eclipse.osee.ats.util.widgets.dialog.TeamVersionListDialog;
+import org.eclipse.osee.framework.core.enums.Active;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateComposite.TableLoadOption;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItem;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItemAction;
+import org.eclipse.osee.framework.ui.skynet.results.IResultsEditorProvider;
+import org.eclipse.osee.framework.ui.skynet.results.IResultsEditorTab;
+import org.eclipse.osee.framework.ui.skynet.results.ResultsEditor;
+import org.eclipse.osee.reports.split.model.AIDistributionEntry;
+import org.eclipse.osee.reports.split.model.DistributionModel;
+import org.eclipse.osee.reports.split.model.StateDistributionEntry;
+import org.eclipse.osee.reports.split.model.TeamDistributionEntry;
+import org.eclipse.osee.reports.split.ui.ai.AITab;
+import org.eclipse.osee.reports.split.ui.state.StateTab;
+import org.eclipse.osee.reports.split.ui.team.TeamTab;
+
+/**
+ * Class to create the navigate item in the UI and to populate the model
+ * 
+ * @author Chandan Bandemutt
+ */
+public class DistributionItem extends XNavigateItemAction {
+
+  /**
+   * Constructor that calls super
+   * 
+   * @param parent :
+   */
+  public DistributionItem(final XNavigateItem parent) {
+    super(parent, "Distribution Reports", AtsImage.REPORT);
+  }
+
+  @Override
+  public void run(final TableLoadOption... tableLoadOptions) throws Exception {
+    // Get input from the user.
+    TeamVersionListDialog dlg = new TeamVersionListDialog(Active.Both);
+    int open = dlg.open();
+    if (open == 0) {
+      populateModel(dlg);
+      ResultsEditor.open(new IResultsEditorProvider() {
+
+        @Override
+        public String getEditorName() {
+          return "Distribution Reports";
+        }
+
+        @Override
+        public List<IResultsEditorTab> getResultsEditorTabs() {
+          List<IResultsEditorTab> tabs = new ArrayList<IResultsEditorTab>();
+          tabs.add(new AITab());
+          tabs.add(new TeamTab());
+          tabs.add(new StateTab());
+          return tabs;
+        }
+
+      });
+    }
+  }
+
+  private boolean populateModel(final TeamVersionListDialog dlg) {
+    try {
+      // AI Split
+    	IAtsVersion version = (IAtsVersion) dlg.getSelectedVersion();
+    	VersionArtifactStore artifactStore = new VersionArtifactStore(version);
+    	Artifact artifact = artifactStore.getArtifact();
+    	
+          AIDistributionEntry aiSplitEntry = new AIDistributionEntry(artifact);
+          aiSplitEntry.computeAISplit();
+
+          TeamDistributionEntry teamSplitEntry = new TeamDistributionEntry(artifact);
+          teamSplitEntry.computeTeamSplit();
+
+          StateDistributionEntry stateSplitEntry = new StateDistributionEntry(artifact);
+          stateSplitEntry.computeStateSplit();
+
+          DistributionModel.setAiSplitEntry(aiSplitEntry);
+          DistributionModel.setTeamSplitEntry(teamSplitEntry);
+          DistributionModel.setStateSplitEntry(stateSplitEntry);
+
+    }
+    catch (OseeCoreException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+      return false;
+    }
+
+    return true;
+  }
+
+}
---
+++ plugins/org.eclipse.osee.reports.split/src/org/eclipse/osee/reports/split/ui/team/TeamTab.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2012 Robert Bosch Engineering and Business Solutions Ltd India. All rights reserved. This program and
+ * the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies
+ * this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.eclipse.osee.reports.split.ui.team;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.eclipse.birt.report.viewer.utilities.WebViewer;
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.ui.skynet.results.IResultsEditorTab;
+import org.eclipse.osee.framework.ui.skynet.results.ResultsEditor;
+import org.eclipse.osee.reports.split.Activator;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.widgets.Composite;
+import org.osgi.framework.Bundle;
+
+
+/**
+ * Class to create teh tab in UI
+ * 
+ * @author Chandan Bandemutt
+ */
+public class TeamTab implements IResultsEditorTab {
+
+  private Browser browser;
+
+  @SuppressWarnings("deprecation")
+  @Override
+  public Composite createTab(final Composite parent, final ResultsEditor resultsEditor) throws OseeCoreException {
+
+    String path = getReportPath();
+    this.browser = new Browser(parent, SWT.NONE);
+    WebViewer.display(path, WebViewer.HTML, this.browser, "frameset");
+    this.browser.refresh();
+    return this.browser;
+  }
+
+  /**
+   * @return the report Path
+   */
+  public String getReportPath() {
+    String path = null;
+    try {
+      Bundle bundle = Platform.getBundle(getPluginID());
+      URL url = FileLocator.find(bundle, new Path("reports/" + getReport()), null);
+      path = FileLocator.toFileURL(url).getPath();
+    }
+    catch (MalformedURLException me) {
+      System.out.println("Fehler bei URL " + me.getStackTrace());
+      return null;
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
+    return path;
+  }
+
+  /**
+   * @return the plugin ID
+   */
+  public String getPluginID() {
+    return Activator.PLUGIN_ID;
+  }
+
+  /**
+   * @return the report name
+   */
+  public String getReport() {
+    return "teamWorkDistribution.rptdesign";
+  }
+
+  @Override
+  public String getTabName() {
+    return "Team-Work";
+  }
+
+}
