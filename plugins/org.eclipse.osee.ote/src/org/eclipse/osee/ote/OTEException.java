4d6e2c79198a4e82e4b56ec911bae9fa5ad9d7c0
Andrew M Finkbeiner <andrew.m.finkbeiner@boeing.com>
2013-03-18 15:16:03 -0700

    feature[ats_5Z6FP]: Add OTE api
    Change-Id: I8c24c4ff6a2ec0ea5d250fe7799217c5d53789b1

---
+++ plugins/org.eclipse.osee.ote/src/org/eclipse/osee/ote/OTEException.java
@@ -0,0 +1,7 @@
+package org.eclipse.osee.ote;
+
+public class OTEException extends Exception {
+
+   private static final long serialVersionUID = 8430838531056913404L;
+
+}
---
+++ plugins/org.eclipse.osee.ote/src/org/eclipse/osee/ote/OTEFuture.java
@@ -0,0 +1,7 @@
+package org.eclipse.osee.ote;
+
+import java.util.concurrent.Future;
+
+public interface OTEFuture<V> extends Future<V> {
+
+}
