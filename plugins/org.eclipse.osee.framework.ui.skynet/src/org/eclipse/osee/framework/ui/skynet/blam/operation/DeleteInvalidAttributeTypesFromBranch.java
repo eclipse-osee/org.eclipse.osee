955620ec8f23932db22f9ca95a6ad5f815706c8a
megumi.telles <megumi.telles@boeing.com>
2013-04-01 09:22:12 -0700

    bug[ats_98K7D]: Created BLAM to handle deletion of invalid attributes types.
    - BLAM that takes comma separated HRIDS, a branch and selected attribute
      types to delete
    
    - BLAM UI xml file to separate the widgets of the BLAM
    
    - Updates to the plugin to include the BLAM
    
    Change-Id: I69bc5e7571482560e6d300af63fb5bc65247c7e0

---
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/DeleteInvalidAttributeTypesFromBranch.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.core.model.Branch;
+import org.eclipse.osee.framework.core.model.type.AttributeType;
+import org.eclipse.osee.framework.core.util.Conditions;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.artifact.Attribute;
+import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
+import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
+import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
+import org.eclipse.osee.framework.ui.skynet.blam.AbstractBlam;
+import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+import org.eclipse.osee.framework.ui.skynet.internal.Activator;
+
+/**
+ * @author Megumi Telles
+ */
+public class DeleteInvalidAttributeTypesFromBranch extends AbstractBlam {
+
+   private static final String ARTIFACT_IDS_WIDGET_NAME = "List of Artifact HRIDS (comma separated)";
+   private static final String DESCRIPTION =
+      "Provide list of artifact HRIDs that contain an invalid attribute type to delete from the selected branch";
+
+   public DeleteInvalidAttributeTypesFromBranch() {
+      super(null, DESCRIPTION, BlamUiSource.FILE);
+   }
+
+   @Override
+   public String getName() {
+      return "Delete Invalid Attribute Types From Branch";
+   }
+
+   @Override
+   public void runOperation(VariableMap variableMap, IProgressMonitor monitor) throws OseeCoreException {
+      Branch branch = variableMap.getBranch("Branch");
+      List<AttributeType> attributeTypes = variableMap.getAttributeTypes("Attribute Type");
+      String input = variableMap.getString(ARTIFACT_IDS_WIDGET_NAME);
+      Conditions.checkNotNullOrEmpty(input, ARTIFACT_IDS_WIDGET_NAME);
+      List<String> inputArtIds = Arrays.asList(input.split("[,\\s]+"));
+      List<Artifact> arts = ArtifactQuery.getArtifactListFromIds(inputArtIds, branch);
+      if (arts != null && !arts.isEmpty()) {
+         SkynetTransaction transaction =
+            TransactionManager.createTransaction(branch, "BLAM: Delete invalid attribute type");
+         for (AttributeType attrType : attributeTypes) {
+            deleteInvalidAttributeType(arts, attrType, transaction);
+         }
+         transaction.execute();
+      }
+
+   }
+
+   private void deleteInvalidAttributeType(List<Artifact> artifacts, AttributeType attrType, SkynetTransaction transaction) throws OseeCoreException {
+      for (Artifact art : artifacts) {
+         if (!art.isAttributeTypeValid(attrType)) {
+            delete(art, attrType);
+            art.persist(transaction);
+         } else {
+            logf("Artifact [%s] attribute type [%s] is valid - did not delete\n", art, attrType);
+         }
+      }
+   }
+
+   private void delete(Artifact art, AttributeType attrType) throws OseeCoreException {
+      List<Attribute<?>> attrs = art.getAttributes(false);
+      for (Attribute<?> attr : attrs) {
+         if (attr.isOfType(attrType)) {
+            attr.delete();
+            logf("Artifact [%s] attribute type [%s] deleted: [%s]\n", art, attrType, attr);
+         }
+      }
+   }
+
+   @Override
+   public String getXWidgetsXml() throws OseeCoreException {
+      return getXWidgetsXmlFromUiFile(getClass().getSimpleName(), Activator.PLUGIN_ID);
+   }
+
+   @Override
+   public Collection<String> getCategories() {
+      return Arrays.asList("Admin");
+   }
+}
