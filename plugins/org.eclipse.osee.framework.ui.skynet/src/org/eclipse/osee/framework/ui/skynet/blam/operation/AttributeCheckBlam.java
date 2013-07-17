1b616743df583e2737c1674b9f4be7f3ee82179e
Angel Avila <Angel.Avila2@boeing.com>
2013-07-16 17:15:31 -0700

    feature[ats_704W1]: Create AttributeCheckBlam
    Change-Id: I4c067b71067655b9849ecd6a27448459371844b6

---
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/AttributeCheckBlam.java
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import org.eclipse.osee.framework.core.data.IAttributeType;
+import org.eclipse.osee.framework.core.operation.IOperation;
+import org.eclipse.osee.framework.core.operation.OperationLogger;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.ui.skynet.blam.AbstractBlam;
+import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
+
+/**
+ * @author Angel Avila
+ */
+
+public class AttributeCheckBlam extends AbstractBlam {
+   private static final String ARTIFACTS = "Drag in Artifacts to look in";
+   private static final String ATTRIBUTE = "Attribute Type to check";
+   private static final String MULTIPLE_VALUES = "Multiple values allowed for this attribute?";
+   private static final String CHANGE_INCONSISTENT_VALUES = "Change artifacts with inconsistent values?";
+
+   @Override
+   public IOperation createOperation(VariableMap variableMap, OperationLogger logger) throws Exception {
+      IAttributeType attributeToCheck = variableMap.getAttributeType(ATTRIBUTE);
+      List<Artifact> artifacts = variableMap.getArtifacts(ARTIFACTS);
+      boolean changeValues = variableMap.getBoolean(CHANGE_INCONSISTENT_VALUES);
+      boolean multipleValues = variableMap.getBoolean(MULTIPLE_VALUES);
+
+      return new AttributeCheckOperation(logger, artifacts, attributeToCheck, changeValues, multipleValues);
+   }
+
+   @Override
+   public String getDescriptionUsage() {
+      return "Check the consitency of Attribute values under a specified Folder Artifact.";
+   }
+
+   @Override
+   public String getXWidgetsXml() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("<xWidgets>");
+      sb.append("<XWidget xwidgetType=\"XListDropViewer\" displayName=\"");
+      sb.append(ARTIFACTS);
+      sb.append("\" />");
+
+      sb.append("<XWidget xwidgetType=\"XCheckBox\" displayName=\"");
+      sb.append(CHANGE_INCONSISTENT_VALUES);
+      sb.append("\" labelAfter=\"true\" horizontalLabel=\"true\" />");
+
+      sb.append("<XWidget xwidgetType=\"XCheckBox\" displayName=\"");
+      sb.append(MULTIPLE_VALUES);
+      sb.append("\" labelAfter=\"true\" horizontalLabel=\"true\" />");
+
+      sb.append("<XWidget xwidgetType=\"XAttributeTypeMultiChoiceSelect\" displayName=\"");
+      sb.append(ATTRIBUTE);
+      sb.append("\" multiSelect=\"false\" />");
+
+      sb.append("</xWidgets>");
+      return sb.toString();
+   }
+
+   @Override
+   public Collection<String> getCategories() {
+      return Arrays.asList("Reports");
+   }
+
+}
---
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/blam/operation/AttributeCheckOperation.java
@@ -0,0 +1,161 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.ui.skynet.blam.operation;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.core.data.IAttributeType;
+import org.eclipse.osee.framework.core.data.IOseeBranch;
+import org.eclipse.osee.framework.core.exception.OseeCoreException;
+import org.eclipse.osee.framework.core.model.Branch;
+import org.eclipse.osee.framework.core.operation.AbstractOperation;
+import org.eclipse.osee.framework.core.operation.OperationLogger;
+import org.eclipse.osee.framework.jdk.core.type.CountingMap;
+import org.eclipse.osee.framework.jdk.core.type.MutableInteger;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
+import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
+import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
+import org.eclipse.osee.framework.ui.skynet.internal.Activator;
+
+/**
+ * @author Angel Avila
+ */
+
+public class AttributeCheckOperation extends AbstractOperation {
+
+   private final List<Artifact> folders;
+   private final IAttributeType attribute;
+   private boolean changeValues;
+   private final boolean multipleValuesRequested;
+   private boolean multipleValuesFound;
+
+   public AttributeCheckOperation(OperationLogger logger, List<Artifact> artifacts, IAttributeType attribute, boolean changeValues, boolean multipleRequested) {
+      super("Attribute Check ", Activator.PLUGIN_ID, logger);
+      this.folders = artifacts;
+      this.attribute = attribute;
+      this.changeValues = changeValues;
+      this.multipleValuesRequested = multipleRequested;
+   }
+
+   @Override
+   protected void doWork(IProgressMonitor monitor) throws OseeCoreException {
+      for (Artifact folder : folders) {
+         if (!monitor.isCanceled()) {
+            logf("\n\n**************CHECKING IN [%s]***************", folder);
+            List<Artifact> allDescendants = folder.getDescendants();
+            String popularValue = findMostPopularAttributeValue(allDescendants, monitor);
+            compareAllChildrenWithPopularValue(allDescendants, popularValue, folder, monitor);
+         }
+      }
+   }
+
+   private String findMostPopularAttributeValue(List<Artifact> allDescendants, IProgressMonitor monitor) throws OseeCoreException {
+      CountingMap<String> countingMap = new CountingMap<String>();
+
+      for (Artifact child : allDescendants) {
+         if (!monitor.isCanceled()) {
+            List<String> attributeValues = child.getAttributeValues(attribute);
+            for (String attributeValue : attributeValues) {
+               if (multipleValuesRequested) {
+                  if (attributeValues.size() > 1) {
+                     multipleValuesFound = true;
+                  }
+               }
+               countingMap.put(attributeValue);
+            }
+         }
+      }
+
+      Set<Entry<String, MutableInteger>> counts = countingMap.getCounts();
+      String mostPopular = "";
+      int mostPopularCount = 0;
+
+      for (Entry<String, MutableInteger> count : counts) {
+         if (!monitor.isCanceled()) {
+            int countValueInt = count.getValue().getValue();
+            if (countValueInt > mostPopularCount && !mostPopular.equals(count.getKey())) {
+               mostPopular = count.getKey();
+               mostPopularCount = countValueInt;
+            }
+         }
+      }
+
+      return mostPopular;
+   }
+
+   @SuppressWarnings("unchecked")
+   private void compareAllChildrenWithPopularValue(List<Artifact> allDescendants, String popularValue, Artifact folder, IProgressMonitor monitor) throws OseeCoreException {
+      if (!allDescendants.isEmpty()) {
+         IOseeBranch tempBranch = allDescendants.get(0).getBranch();
+         Branch branch = BranchManager.getBranch(tempBranch);
+         SkynetTransaction transaction = TransactionManager.createTransaction(branch, "Attribute Check Blam");
+         if (!branch.getBranchType().isWorkingBranch()) {
+            changeValues = false;
+            logf("\n\nCANNOT MAKE ANY CHANGES BECAUSE ARTIFACTS ARE NOT ON A WORKING BRANCH");
+         }
+
+         if (multipleValuesRequested != multipleValuesFound) {
+            changeValues = false;
+            logf("=============WARNING==============");
+            logf("NO CHANGES WILL BE MADE\n");
+            if (multipleValuesRequested) {
+               logf(
+                  "No artifacts with multiple values for [%s] were found under the selected folder(s).  All attributes will be expected to have exactly ONE value.",
+                  attribute);
+               logf(String.format(
+                  "To allow this blam to set multiple values, please change an artifact under the folder [%s] to contain multiple values using Artifact Editor.",
+                  folder));
+            }
+            if (multipleValuesFound) {
+               logf(String.format(
+                  "Artifacts with multiple values for [%s] were found under the selected folder [%s], however, the \'Multiple values allowed for this attribute?\' check box was not selected.",
+                  attribute, folder));
+            }
+         }
+
+         for (Artifact art : allDescendants) {
+            if (!monitor.isCanceled()) {
+               List<String> attributeValues = art.getAttributeValues(attribute);
+               List<String> newValues = attributeValues;
+               if (!attributeValues.isEmpty()) {
+                  boolean changesNeeded = false;
+                  if (multipleValuesRequested && !attributeValues.contains(popularValue)) {
+                     logf("[%s] does not contain the value [%s] for [%s]", art, popularValue, attribute);
+                     newValues.add(popularValue);
+                     changesNeeded = true;
+                  } else if (!multipleValuesRequested && !attributeValues.equals(Collections.singletonList(popularValue))) {
+                     logf("[%s]'s value for [%s] is NOT equal to [%s]", art, attribute, popularValue);
+                     newValues = Arrays.asList(popularValue);
+                     changesNeeded = true;
+                  }
+                  if (changeValues && changesNeeded) {
+                     art.setAttributeValues(attribute, newValues);
+                     art.persist(transaction);
+                     logf("------>Value of attribute [%s] in [%s] changed from [%s] to [%s]", attribute, art,
+                        attributeValues, newValues);
+                  }
+               } else {
+                  logf("[%s] has NO values for [%s].  No changes made", art, attribute);
+               }
+            }
+         }
+         if (changeValues) {
+            transaction.execute();
+         }
+
+      }
+   }
+}
