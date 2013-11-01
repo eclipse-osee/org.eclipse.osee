4b023c7f3a96eaa27ee5f38a79b84c2926bae82c
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2013-10-31 23:48:42 -0700

    refinement: Create DrainingIterator datatype
    Change-Id: Ib6d229604868fd80733cf73bacca3146c9e42e5e

---
+++ plugins/org.eclipse.osee.framework.jdk.core/src/org/eclipse/osee/framework/jdk/core/type/DrainingIterator.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.jdk.core.type;
+
+import java.util.Iterator;
+
+/**
+ * Immediately removes each element from the underlying iterator when it is returned from calling next(). One use for a
+ * DrainingIterator is when an iterator from a ConcurrentHashMap (i.e. ConcurrentHashMap.values().iterator()) is passed
+ * to another method for iteration and the subsequent removal of each element is required. In that case, this class is
+ * more than just a convenience because it is not safe to simply call ConcurrentHashMap.clear() after the iterator
+ * finishes since the ConcurrentHashMap may well have been added to concurrently and this may or may not be reflected in
+ * the iterator. Thus new additions to the ConcurrentHashMap might otherwise get cleared with out ever being accessed
+ * through iteration.
+ * 
+ * @author Ryan D. Brooks
+ */
+public class DrainingIterator<T> implements Iterator<T>, Iterable<T> {
+   private final Iterator<T> iterator;
+
+   public DrainingIterator(Iterator<T> iterator) {
+      this.iterator = iterator;
+   }
+
+   public DrainingIterator(Iterable<T> iterable) {
+      this.iterator = iterable.iterator();
+   }
+
+   @Override
+   public boolean hasNext() {
+      return iterator.hasNext();
+   }
+
+   @Override
+   public T next() {
+      T value = iterator.next();
+      iterator.remove();
+      return value;
+   }
+
+   @Override
+   /**
+    * It is always unnecessary to call remove from a DrainingIterator since every call to next has already called remove.  This method always throws UnsupportedOperationException.
+    */
+   public void remove() {
+      throw new UnsupportedOperationException(
+         "It is always unnecessary to call remove from a DrainingIterator since every call to next has already called remove.");
+   }
+
+   @Override
+   public Iterator<T> iterator() {
+      return this;
+   }
+}
