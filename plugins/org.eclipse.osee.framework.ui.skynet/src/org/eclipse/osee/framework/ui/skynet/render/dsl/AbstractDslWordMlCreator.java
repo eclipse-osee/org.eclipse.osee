f150a5889819e9084963f6354b1455466ffd375c
megumi.telles <megumi.telles@boeing.com>
2014-01-17 14:24:43 -0700

    bug[ats_ATS4926]: Update abstract wordml creator
    Change-Id: I9c3bfc837b174294a4bc3ccf1a95a8553a31b587

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/dsl/AbstractDslWordMlCreator.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/dsl/AbstractDslWordMlCreator.java
@@ -21,15 +21,15 @@ import org.eclipse.osee.framework.ui.skynet.render.PresentationType;
  */
 public abstract class AbstractDslWordMlCreator {
 
-   private static final String[] XML_CHARS = new String[] {"[&]", "[<]", "[>]", "[\"]"};
+   private static final String[] XML_CHARS = new String[] {"&", "<", ">", "\""};
    private static final String[] XML_ESCAPES = new String[] {"&amp;", "&lt;", "&gt;", "&quot;"};
-   private static String emptyParagraph = "<w:p/>";
-   private static String paragraphEnd = "</w:t></w:r></w:p>";
-   private static String start =
+   public static String emptyParagraph = "<w:p/>";
+   public static String paragraphEnd = "</w:t></w:r></w:p>";
+   public static String paragraphStart =
          "<w:p><w:pPr><w:pStyle w:val=\"reqlang1\"/><w:spacing w:before=\"0\" w:after=\"0\"/></w:pPr><w:r><w:t>";
    private static final String[] TAB_ARRAY =
-         new String[] {"\r\t\t\t\t\t\t\t\t", "\r\t\t\t\t\t\t\t", "\r\t\t\t\t\t\t", "\r\t\t\t\t\t", "\r\t\t\t\t",
-               "\r\t\t\t", "\r\t\t", "\r\t"};
+         new String[] {"\t\t\t\t\t\t\t\t", "\t\t\t\t\t\t\t", "\t\t\t\t\t\t", "\t\t\t\t\t", "\t\t\t\t",
+               "\t\t\t", "\t\t", "\t"};
    private static final String[] TAB_STYLE_ARRAY =
          new String[] {"w:val=\"reqlang9\"", "w:val=\"reqlang8\"", "w:val=\"reqlang7\"", "w:val=\"reqlang6\"",
                "w:val=\"reqlang5\"", "w:val=\"reqlang4\"", "w:val=\"reqlang3\"", "w:val=\"reqlang2\""};
@@ -47,8 +47,8 @@ public abstract class AbstractDslWordMlCreator {
       wordml = wordml.replace("\n\r", "\r");
       wordml = wordml.replace("\n", "\r");
       wordml = replaceTabs(wordml);
-      wordml = wordml.replace("\r", paragraphEnd + start);
-      wordml = start + wordml;
+      wordml = wordml.replace("\r", paragraphEnd + paragraphStart);
+      wordml = paragraphStart + wordml;
       wordml = wordml + paragraphEnd;
 
       if (addEndParagraph) {
@@ -60,7 +60,7 @@ public abstract class AbstractDslWordMlCreator {
    public String replaceTabs(String wordml) {
       for (int x = 0; x < TAB_ARRAY.length; x++) {
          wordml =
-            wordml.replace(TAB_ARRAY[x], (paragraphEnd + start).replace("w:val=\"reqlang1\"", TAB_STYLE_ARRAY[x]));
+            wordml.replace(TAB_ARRAY[x], (paragraphEnd + paragraphStart).replace("w:val=\"reqlang1\"", TAB_STYLE_ARRAY[x]));
       }
       return wordml;
    }
