5eb6f1ca8c9cfc5e23866ec80039883d8514159f
megumi.telles <megumi.telles@boeing.com>
2014-05-06 12:27:13 -0700

    feature[ats_ATS43715]: Updates to conversion of baseline guid to uuid
    Change-Id: I2517d62bc17158e33a8b98559440f1059cc7d792

--- plugins/org.eclipse.osee.ats.impl/src/org/eclipse/osee/ats/impl/internal/convert/ConvertBaselineGuidToBaselineUuid.java
+++ plugins/org.eclipse.osee.ats.impl/src/org/eclipse/osee/ats/impl/internal/convert/ConvertBaselineGuidToBaselineUuid.java
@@ -10,40 +10,33 @@
  *******************************************************************************/
 package org.eclipse.osee.ats.impl.internal.convert;
 
-import static org.eclipse.osee.framework.core.enums.CoreBranches.COMMON;
 import java.util.List;
 import org.eclipse.osee.ats.api.data.AtsArtifactTypes;
 import org.eclipse.osee.ats.api.data.AtsAttributeTypes;
-import org.eclipse.osee.ats.api.util.IAtsDatabaseConversion;
 import org.eclipse.osee.ats.impl.internal.util.AtsUtilServer;
 import org.eclipse.osee.framework.core.data.IAttributeType;
 import org.eclipse.osee.framework.core.data.TokenFactory;
-import org.eclipse.osee.framework.core.enums.SystemUser;
 import org.eclipse.osee.framework.core.util.XResultData;
 import org.eclipse.osee.framework.database.IOseeDatabaseService;
-import org.eclipse.osee.framework.jdk.core.util.Conditions;
+import org.eclipse.osee.framework.jdk.core.util.Strings;
 import org.eclipse.osee.orcs.OrcsApi;
 import org.eclipse.osee.orcs.data.ArtifactReadable;
 import org.eclipse.osee.orcs.data.BranchReadable;
 import org.eclipse.osee.orcs.transaction.TransactionBuilder;
-import org.eclipse.osee.orcs.transaction.TransactionFactory;
 
 /**
  * See description below
  * 
  * @author Donald G Dunne
  */
-public class ConvertBaselineGuidToBaselineUuid implements IAtsDatabaseConversion {
+public class ConvertBaselineGuidToBaselineUuid extends AbstractConvertGuidToUuid {
 
-   private final OrcsApi orcsApi;
    // Leave this attribute definition and conversion for other OSEE sites to convert
    public static final IAttributeType BaselineBranchGuid = TokenFactory.createAttributeType(0x10000000000000A9L,
       "ats.Baseline Branch Guid");
-   private final IOseeDatabaseService dbService;
 
    public ConvertBaselineGuidToBaselineUuid(IOseeDatabaseService dbService, OrcsApi orcsApi) {
-      this.dbService = dbService;
-      this.orcsApi = orcsApi;
+      super(dbService, orcsApi);
    }
 
    @Override
@@ -51,43 +44,42 @@ public class ConvertBaselineGuidToBaselineUuid implements IAtsDatabaseConversion
       if (reportOnly) {
          data.log("REPORT ONLY - Changes not persisted\n");
       }
-      if (!orcsApi.getOrcsTypes(null).getAttributeTypes().exists(AtsAttributeTypes.BaselineBranchUuid)) {
+      if (!getOrcsApi().getOrcsTypes(null).getAttributeTypes().exists(AtsAttributeTypes.BaselineBranchUuid)) {
          data.logError("ats.BaselineBranchUuid is not configured for this database");
          return;
       }
-      TransactionFactory txFactory = orcsApi.getTransactionFactory(null);
-      TransactionBuilder tx =
-         txFactory.createTransaction(COMMON, AtsUtilServer.getArtifactByGuid(orcsApi, SystemUser.OseeSystem.getGuid()),
-            getName());
+      TransactionBuilder tx = createTransactionBuilder();
       int numChanges = 0;
-      for (ArtifactReadable art : orcsApi.getQueryFactory(null).fromBranch(AtsUtilServer.getAtsBranch()).andTypeEquals(
+      for (ArtifactReadable art : getOrcsApi().getQueryFactory(null).fromBranch(AtsUtilServer.getAtsBranch()).andTypeEquals(
          AtsArtifactTypes.Version, AtsArtifactTypes.TeamDefinition).andExists(BaselineBranchGuid).getResults()) {
          List<String> attributeValues = art.getAttributeValues(BaselineBranchGuid);
          for (String guid : attributeValues) {
-            BranchReadable branch = null;
-            try {
-               branch =
-                  orcsApi.getQueryFactory(null).branchQuery().andUuids(getBranchIdLegacy(guid)).getResults().getExactlyOne();
-            } catch (Exception ex) {
-               // do nothing
-            }
-            if (branch == null) {
-               data.logErrorWithFormat("Branch with guid %s can't be found", guid);
-            } else {
-               String uuid = art.getSoleAttributeAsString(AtsAttributeTypes.BaselineBranchUuid, null);
-               if (uuid == null || !Long.valueOf(uuid).equals(branch.getLocalId())) {
-                  if (uuid == null) {
-                     data.logWithFormat("Adding uuid attribute of value %d to artifact type [%s] name [%s] id [%s]\n",
-                        branch.getLocalId(), art.getArtifactType(), art.getName(), art.getGuid());
-                  } else if (!Long.valueOf(uuid).equals(branch.getLocalId())) {
-                     data.logWithFormat(
-                        "Updating uuid attribute of value %d to artifact type [%s] name [%s] id [%s]\n",
-                        branch.getLocalId(), art.getArtifactType(), art.getName(), art.getGuid());
-                  }
-                  numChanges++;
-                  if (!reportOnly) {
-                     tx.setSoleAttributeValue(art, AtsAttributeTypes.BaselineBranchUuid,
-                        String.valueOf(branch.getLocalId()));
+            if (!guid.isEmpty()) {
+               BranchReadable branch = null;
+               try {
+                  branch = getBranch(guid);
+               } catch (Exception ex) {
+                  // do nothing
+               }
+               if (branch == null) {
+                  data.logErrorWithFormat("Branch with guid %s can't be found", guid);
+               } else {
+                  long branchUuid = branch.getUuid();
+                  String uuid = art.getSoleAttributeAsString(AtsAttributeTypes.BaselineBranchUuid, null);
+                  if (!Strings.isValid(uuid) || isUuidDifferent(uuid, branchUuid)) {
+                     if (!Strings.isValid(uuid)) {
+                        data.logWithFormat(
+                           "Adding uuid attribute of value %d to artifact type [%s] name [%s] id [%s]\n", branchUuid,
+                           art.getArtifactType(), art.getName(), art.getGuid());
+                     } else if (isUuidDifferent(uuid, branchUuid)) {
+                        data.logWithFormat(
+                           "Updating uuid attribute of value %d to artifact type [%s] name [%s] id [%s]\n", branchUuid,
+                           art.getArtifactType(), art.getName(), art.getGuid());
+                     }
+                     numChanges++;
+                     if (!reportOnly) {
+                        tx.setSoleAttributeValue(art, AtsAttributeTypes.BaselineBranchUuid, String.valueOf(branchUuid));
+                     }
                   }
                }
             }
@@ -99,17 +91,6 @@ public class ConvertBaselineGuidToBaselineUuid implements IAtsDatabaseConversion
       } else {
          data.log("\n" + numChanges + " Need to be Changed");
       }
-   }
-
-   private final String SELECT_BRANCH_ID_BY_GUID = "select branch_id from osee_branch where branch_guid = ?";
-
-   /**
-    * Temporary method till all code uses branch uuid. Remove after 0.17.0
-    */
-   public long getBranchIdLegacy(String branchGuid) {
-      Long longId = dbService.runPreparedQueryFetchObject(0L, SELECT_BRANCH_ID_BY_GUID, branchGuid);
-      Conditions.checkExpressionFailOnTrue(longId <= 0, "Error getting branch_id for branch: [%s]", branchGuid);
-      return longId;
    }
 
    @Override
@@ -128,5 +109,9 @@ public class ConvertBaselineGuidToBaselineUuid implements IAtsDatabaseConversion
    @Override
    public String getName() {
       return "ConvertBaselineGuidToBaselineUuid";
+   }
+
+   private boolean isUuidDifferent(String uuid, long branchUuid) {
+      return Strings.isValid(uuid) && !Long.valueOf(uuid).equals(branchUuid);
    }
 }
