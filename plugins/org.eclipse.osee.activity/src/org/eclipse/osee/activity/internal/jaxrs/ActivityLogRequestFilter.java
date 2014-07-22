e66ee0e919c711fa9b8a41fa6c772e2fa9ab30e9
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2014-07-22 12:15:51 -0700

    bug[ats_ATS79782]: Activity Logger throws NPE
    Change-Id: I4f395cdd562241e3f8cfb0a3871cccc3bd1090e7

--- plugins/org.eclipse.osee.activity/src/org/eclipse/osee/activity/internal/jaxrs/ActivityLogRequestFilter.java
+++ plugins/org.eclipse.osee.activity/src/org/eclipse/osee/activity/internal/jaxrs/ActivityLogRequestFilter.java
@@ -10,26 +10,24 @@
  *******************************************************************************/
 package org.eclipse.osee.activity.internal.jaxrs;
 
-import static org.eclipse.osee.activity.ActivityConstants.ERROR_MSG__MISSING_ACTIVITY_HEADER;
-import static org.eclipse.osee.activity.ActivityConstants.HTTP_HEADER__ACTIVITY_ENTRY_ID;
+import javax.annotation.Priority;
+import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.ContainerResponseContext;
-import javax.ws.rs.container.ContainerResponseFilter;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.Response.StatusType;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.ext.Provider;
 import org.eclipse.osee.activity.ActivityConstants;
 import org.eclipse.osee.activity.api.Activity;
 import org.eclipse.osee.activity.api.ActivityLog;
-import org.eclipse.osee.framework.jdk.core.util.Strings;
 import org.eclipse.osee.logger.Log;
 
 /**
  * @author Ryan D. Brooks
  */
+@PreMatching
+@Priority(Priorities.HEADER_DECORATOR)
 @Provider
-public class ActivityLogFilter implements ContainerRequestFilter, ContainerResponseFilter {
+public class ActivityLogRequestFilter implements ContainerRequestFilter {
 
    private Log logger;
    private ActivityLog activityLog;
@@ -57,34 +55,9 @@ public class ActivityLogFilter implements ContainerRequestFilter, ContainerRespo
          Long entryId =
             activityLog.createActivityThread(Activity.JAXRS_METHOD_CALL, accountId, serverId, clientId, message);
 
-         context.getHeaders().addFirst(ActivityConstants.HTTP_HEADER__ACTIVITY_ENTRY_ID, String.valueOf(entryId));
+         context.setProperty(ActivityConstants.HTTP_HEADER__ACTIVITY_ENTRY_ID, entryId);
       } catch (Throwable th) {
          logger.error(th, "Error during ActivityContainerRequestFilter");
-      }
-   }
-
-   /**
-    * Called after a resource method is executed
-    */
-   @Override
-   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-      try {
-         String activityHeader = requestContext.getHeaders().getFirst(HTTP_HEADER__ACTIVITY_ENTRY_ID);
-         if (!Strings.isValid(activityHeader)) {
-            activityLog.createUpdateableEntry(Activity.JAXRS_METHOD_CALL_FILTER_ERROR,
-               ERROR_MSG__MISSING_ACTIVITY_HEADER);
-         } else {
-            Long entryId = Long.parseLong(activityHeader);
-
-            StatusType statusType = responseContext.getStatusInfo();
-            if (statusType.getFamily() == Status.Family.SUCCESSFUL) {
-               activityLog.completeEntry(entryId);
-            } else {
-               activityLog.endEntryAbnormally(entryId, responseContext.getStatus());
-            }
-         }
-      } catch (Throwable th) {
-         logger.error(th, "Error during ActivityContainerResponseFilter");
       }
    }
 
---
+++ plugins/org.eclipse.osee.activity/src/org/eclipse/osee/activity/internal/jaxrs/ActivityLogResponseFilter.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.activity.internal.jaxrs;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response.StatusType;
+import javax.ws.rs.ext.Provider;
+import org.eclipse.osee.activity.ActivityConstants;
+import org.eclipse.osee.activity.api.Activity;
+import org.eclipse.osee.activity.api.ActivityLog;
+import org.eclipse.osee.logger.Log;
+
+/**
+ * @author Ryan D. Brooks
+ */
+@Provider
+public class ActivityLogResponseFilter implements ContainerResponseFilter {
+
+   private Log logger;
+   private ActivityLog activityLog;
+
+   public void setActivityLogger(ActivityLog activityLog) {
+      this.activityLog = activityLog;
+   }
+
+   public void setLogger(Log logger) {
+      this.logger = logger;
+   }
+
+   /**
+    * Called after a resource method is executed
+    */
+   @Override
+   public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+      try {
+         Long entryId = (Long) requestContext.getProperty(ActivityConstants.HTTP_HEADER__ACTIVITY_ENTRY_ID);
+         if (entryId != null) {
+            StatusType statusType = responseContext.getStatusInfo();
+            if (statusType.getFamily() == Status.Family.SUCCESSFUL) {
+               activityLog.completeEntry(entryId);
+            } else {
+               activityLog.endEntryAbnormally(entryId, responseContext.getStatus());
+            }
+         } else {
+            // Response was called without a matching request
+            activityLog.createUpdateableEntry(Activity.JAXRS_METHOD_CALL_FILTER_ERROR,
+               ActivityConstants.ERROR_MSG__MISSING_ACTIVITY_HEADER);
+         }
+      } catch (Throwable th) {
+         logger.error(th, "Error during ActivityContainerResponseFilter");
+      }
+   }
+}
