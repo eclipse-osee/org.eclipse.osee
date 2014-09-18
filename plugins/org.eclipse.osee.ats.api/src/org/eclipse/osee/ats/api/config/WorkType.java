6880fd4498eed5fb5c4c54b8db0c2ee83968e2c8
donald.g.dunne <donald.g.dunne@boeing.com>
2014-09-18 08:54:21 -0700

    feature[ats_ATS72141]: CPA: Create WorkType
    Change-Id: Ia35d60407742a9d0ada032be108e72f8b76cce21

---
+++ plugins/org.eclipse.osee.ats.api/src/org/eclipse/osee/ats/api/config/WorkType.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ats.api.config;
+
+/**
+ * WorkTypes used for Category field for ATS Config objects
+ * 
+ * @author Donald G. Dunne
+ */
+public enum WorkType {
+   Code,
+   Test,
+   Requirements,
+   SW_Design,
+   SW_TechAppr,
+   Test_Procedures,
+   SubSystems,
+   ICDs,
+   PIDS,
+   All;
+}
