6ee3fa21a2328c1c291dfb49fd98be0e2e93f2be
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2014-10-22 11:04:39 -0700

    feature[ats_ATS98292]: Create ORCS Script DSL
    Change-Id: I897d40ca7dcaf526403ab236539f02c4d09590f2

---
+++ plugins/org.eclipse.osee.orcs.script.dsl/xtend-gen/org/eclipse/osee/orcs/script/dsl/generator/OrcsScriptDslGenerator.java
@@ -0,0 +1,15 @@
+/*
+ * by Xtext
+ */
+package org.eclipse.osee.orcs.script.dsl.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+@SuppressWarnings("all")
+public class OrcsScriptDslGenerator implements IGenerator {
+   @Override
+   public void doGenerate(final Resource resource, final IFileSystemAccess fsa) {
+   }
+}
