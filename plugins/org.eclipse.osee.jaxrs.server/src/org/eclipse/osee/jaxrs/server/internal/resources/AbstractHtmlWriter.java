a07f4a90925ec75d0cd16d5f8596bc3ff294f659
Roberto E. Escobar <roberto.e.escobar@boeing.com>
2014-11-05 12:56:47 -0700

    refinment: Provide fallback html view
    Change-Id: Iad74d79dec4fd08c3f5d1823821c10119e07b143

--- plugins/org.eclipse.osee.jaxrs.server/src/org/eclipse/osee/jaxrs/server/internal/resources/AbstractHtmlWriter.java
+++ plugins/org.eclipse.osee.jaxrs.server/src/org/eclipse/osee/jaxrs/server/internal/resources/AbstractHtmlWriter.java
@@ -12,9 +12,13 @@ package org.eclipse.osee.jaxrs.server.internal.resources;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -44,9 +48,46 @@ public abstract class AbstractHtmlWriter<T> extends AbstractMessageBodyWriter<T>
       ViewModel model = asViewModel(data);
       MessageBodyWriter<ViewModel> writer =
          providers.getMessageBodyWriter(ViewModel.class, genericType, annotations, mediaType);
-      writer.writeTo(model, ViewModel.class, genericType, annotations, mediaType, httpHeaders, entityStream);
+      if (writer != null) {
+         writer.writeTo(model, ViewModel.class, genericType, annotations, mediaType, httpHeaders, entityStream);
+      } else {
+         writeHtml(model, entityStream);
+      }
    }
 
    public abstract ViewModel asViewModel(T data);
+
+   /**
+    * Fall-back HTML - used when templating system is not initialized.
+    */
+   private void writeHtml(ViewModel model, OutputStream output) throws IOException {
+      Writer writer = new OutputStreamWriter(output);
+      Map<String, Object> asMap = model.asMap();
+      writer.write("<!DOCTYPE HTML>");
+      writer.write("<html lang=\"en\"><head><link rel=\"stylesheet\" href=\"/lib/css/bootstrap.min.css\">");
+      writer.write("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"></meta>");
+      writer.write("<meta charset=\"UTF-8\"></head>");
+      writer.write("<body>");
+      writer.write("<div class=\"container\">");
+      writer.write("<h1>OSEE - ");
+      writer.write(model.getViewId());
+      writer.write("</h1>");
+      writer.write("<div class=\"container\" class=\"panel-body\">");
+      writer.write("<div class=\"lead\">");
+      writer.write("Templating system is not initialized. Using default template.");
+      writer.write("</div>");
+      writer.write("<table class=\"table table-striped\">");
+      writer.write("<thead><tr><th>Key</th><th>Value</th></tr></thead>");
+      writer.write("<tbody>");
+      for (Entry<String, Object> entry : asMap.entrySet()) {
+         writer.write("<tr><td>");
+         writer.write(entry.getKey());
+         writer.write("</td><td>");
+         writer.write(String.valueOf(entry.getValue()));
+         writer.write("</td></tr>");
+      }
+      writer.write("</tbody></table></div></div></body></html>");
+      writer.flush();
+   }
 
 }
