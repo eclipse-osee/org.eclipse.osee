7b1b0dd59799f58e80d70258ab1d862b3af582f4
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2015-01-26 14:53:34 -0700

    feature: Create database health rest api
    Change-Id: I33c42fd80d189f428cf2c81d1bdfca982d75aa4f

---
+++ plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/DbHeathApplication.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.orcs.db.internal;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import org.eclipse.osee.jdbc.JdbcService;
+import org.eclipse.osee.orcs.db.internal.health.DatabaseHealthResource;
+
+/**
+ * @author Ryan D. Brooks
+ */
+@ApplicationPath("db")
+public final class DbHeathApplication extends Application {
+
+   private JdbcService jdbcService;
+
+   private final Set<Object> singletons = new HashSet<Object>();
+
+   public void setJdbcService(JdbcService jdbcService) {
+      this.jdbcService = jdbcService;
+   }
+
+   @Override
+   public Set<Object> getSingletons() {
+      return singletons;
+   }
+
+   public void start(Map<String, Object> properties) {
+      singletons.add(new DatabaseHealthResource(properties, jdbcService));
+   }
+
+   public void stop() {
+      singletons.clear();
+   }
+}
---
+++ plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/health/DatabaseHealthResource.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.orcs.db.internal.health;
+
+import java.util.Map;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.osee.jdbc.JdbcClient;
+import org.eclipse.osee.jdbc.JdbcService;
+import org.eclipse.osee.orcs.db.internal.callable.PurgeUnusedBackingDataAndTransactions;
+
+/**
+ * @author Ryan D. Brooks
+ */
+@Path("/health")
+public final class DatabaseHealthResource {
+   private final Map<String, Object> properties;
+   private final JdbcClient jdbcClient;
+
+   public DatabaseHealthResource(Map<String, Object> properties, JdbcService jdbcService) {
+      this.properties = properties;
+      this.jdbcClient = jdbcService.getClient();
+   }
+
+   @Path("purgeunused")
+   @GET
+   @Produces(MediaType.TEXT_HTML)
+   public String deleteObsoleteData() {
+      PurgeUnusedBackingDataAndTransactions app = new PurgeUnusedBackingDataAndTransactions(jdbcClient);
+      int[] counts = app.purge();
+
+      StringBuilder strB = new StringBuilder();
+      for (int count : counts) {
+         strB.append(count);
+         strB.append("<br />");
+      }
+      return strB.toString();
+   }
+
+}
