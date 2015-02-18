0da544a7f3123a122c35e1074a9dba1085d4a454
donald.g.dunne <donald.g.dunne@boeing.com>
2015-02-18 08:54:51 -0700

    refinement: Remove unused TaskComposite.handleDeleteTask
    Change-Id: Id0966d36a3ed1c13104c01330095f96acdb743e7

--- plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/task/TaskComposite.java
+++ plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/task/TaskComposite.java
@@ -13,7 +13,6 @@ package org.eclipse.osee.ats.task;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -21,7 +20,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.nebula.widgets.xviewer.customize.CustomizeData;
 import org.eclipse.osee.ats.actions.OpenNewAtsTaskEditorAction.IOpenNewAtsTaskEditorHandler;
@@ -35,7 +33,6 @@ import org.eclipse.osee.ats.core.client.task.TaskManager;
 import org.eclipse.osee.ats.core.client.team.TeamWorkFlowArtifact;
 import org.eclipse.osee.ats.core.client.util.AtsChangeSet;
 import org.eclipse.osee.ats.core.client.util.AtsTaskCache;
-import org.eclipse.osee.ats.core.util.AtsUtilCore;
 import org.eclipse.osee.ats.editor.SMAEditor;
 import org.eclipse.osee.ats.internal.Activator;
 import org.eclipse.osee.ats.internal.AtsClientService;
@@ -44,15 +41,11 @@ import org.eclipse.osee.ats.world.WorldContentProvider;
 import org.eclipse.osee.ats.world.WorldLabelProvider;
 import org.eclipse.osee.ats.world.WorldXViewer;
 import org.eclipse.osee.ats.world.WorldXViewerEventManager;
-import org.eclipse.osee.framework.core.operation.Operations;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.osee.framework.logging.OseeLevel;
 import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.ArtifactData;
-import org.eclipse.osee.framework.skynet.core.artifact.PurgeArtifacts;
-import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
-import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
 import org.eclipse.osee.framework.skynet.core.utility.Artifacts;
 import org.eclipse.osee.framework.ui.plugin.util.AWorkbench;
 import org.eclipse.osee.framework.ui.skynet.action.RefreshAction.IRefreshActionHandler;
@@ -76,7 +69,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Tree;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * @author Donald G. Dunne
@@ -150,65 +142,6 @@ public class TaskComposite extends Composite implements IWorldViewerEventHandler
       if (Widgets.isAccessible(taskXViewer.getTree())) {
          this.taskArts.clear();
          add(iXTaskViewer.getTaskArtifacts());
-      }
-   }
-
-   public void handleDeleteTask() {
-      final List<TaskArtifact> items = getSelectedTaskArtifactItems();
-      if (items.isEmpty()) {
-         AWorkbench.popup("ERROR", "No Tasks Selected");
-         return;
-      }
-      StringBuilder builder = new StringBuilder();
-      if (items.size() > 15) {
-         builder.append("Are you sure you wish to delete " + items.size() + " Tasks?\n\n");
-      } else {
-         builder.append("Are you sure you wish to delete ");
-         if (items.size() == 1) {
-            builder.append("this Task?\n\n");
-         } else {
-            builder.append("these Tasks?\n\n");
-         }
-         for (TaskArtifact taskItem : items) {
-            builder.append("\"" + taskItem.getName() + "\"\n");
-         }
-
-         builder.append("\n\nNote: Workflow will be saved.");
-
-      }
-      boolean delete =
-         MessageDialog.openQuestion(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Delete Task",
-            builder.toString());
-      if (delete) {
-         try {
-            SkynetTransaction transaction =
-               TransactionManager.createTransaction(AtsUtilCore.getAtsBranch(), "Delete Tasks");
-            // Done for concurrent modification purposes
-            ArrayList<TaskArtifact> delItems = new ArrayList<TaskArtifact>();
-            ArrayList<TaskArtifact> tasksNotInDb = new ArrayList<TaskArtifact>();
-            delItems.addAll(items);
-            for (TaskArtifact taskArt : delItems) {
-               SMAEditor.close(Collections.singleton(taskArt), false);
-               if (taskArt.isInDb()) {
-                  taskArt.deleteAndPersist(transaction);
-               } else {
-                  tasksNotInDb.add(taskArt);
-               }
-            }
-            transaction.execute();
-
-            AtsTaskCache.decache((AbstractTaskableArtifact) iXTaskViewer.getAwa());
-            taskXViewer.remove(items.toArray(new Object[items.size()]));
-            taskArts.removeAll(items);
-
-            if (tasksNotInDb.size() > 0) {
-               Operations.executeWorkAndCheckStatus(new PurgeArtifacts(tasksNotInDb));
-               refreshActionHandler();
-            }
-            iXTaskViewer.getEditor().onDirtied();
-         } catch (Exception ex) {
-            OseeLog.log(Activator.class, OseeLevel.SEVERE_POPUP, ex);
-         }
       }
    }
 
