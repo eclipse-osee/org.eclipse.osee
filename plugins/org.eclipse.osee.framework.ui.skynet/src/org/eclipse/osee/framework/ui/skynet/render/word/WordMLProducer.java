2b8674a826d623e8a749ee1164ae9f2af17f35e9
megumi.telles <megumi.telles@boeing.com>
2015-03-02 12:07:17 -0700

    bug[ats_ATS168817]: Fix list reset in publish
    Change-Id: Id8b1fa1620643267951622c258f1d6473dfff49d

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/word/WordMLProducer.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/render/word/WordMLProducer.java
@@ -225,10 +225,12 @@ public class WordMLProducer extends Producer {
    public void resetListValue() throws OseeCoreException {
       // extra paragraph needed to support WORD's bug to add in a trailing zero when using field codes
       startParagraph();
+      addWordMl(LISTNUM_FIELD_HEAD);
+      endParagraph();
 
+      startParagraph();
       append("<w:r>");
       append("</w:r>");
-
       //The listnum also acts a template delimiter to know when to remove unwanted content.
       addWordMl(LISTNUM_FIELD);
       endParagraph();
