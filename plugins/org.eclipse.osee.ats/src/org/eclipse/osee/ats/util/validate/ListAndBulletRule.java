156f2c24f58813c67ee4b2f5656c06e1a9b1f5ac
megumi.telles <megumi.telles@boeing.com>
2015-10-29 08:22:19 -0700

    bug[ats_ATS253972]: Create validation rule for bullets and lists
    Change-Id: If861de1b3aa5d8a136b5051b1a2834afa43ee811

---
+++ plugins/org.eclipse.osee.ats/src/org/eclipse/osee/ats/util/validate/ListAndBulletRule.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ats.util.validate;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.osee.framework.core.data.IArtifactType;
+import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
+import org.eclipse.osee.framework.core.model.type.ArtifactType;
+import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.ui.skynet.results.XResultDataUI;
+
+/**
+ * @author Megumi Telles
+ */
+public class ListAndBulletRule extends AbstractValidationRule {
+
+   private static final String MORE_ON_FORMATTING =
+      "https://apache.msc.az.boeing.com/wiki/lba/index.php/OSEE/ATS/LBA/User_Guide#What_is_.22Validate_Requirement_Changes.22.2C_how_do_I_run_it_and_complete_review.3F";
+   private static final Pattern NORMAL_LIST_BULLET_STYLE = Pattern.compile("<w:pPr><w:listPr>.+?</w:listPr></w:pPr>");
+   private final IArtifactType artifactType;
+
+   public ListAndBulletRule(IArtifactType artifactType) {
+      this.artifactType = artifactType;
+   }
+
+   public boolean hasArtifactType(ArtifactType artType) {
+      return artType.inheritsFrom(artifactType);
+   }
+
+   @Override
+   protected ValidationResult validate(Artifact artToValidate, IProgressMonitor monitor) throws OseeCoreException {
+      Collection<String> errorMessages = new ArrayList<>();
+      boolean validationPassed = true;
+      String wtc = artToValidate.getSoleAttributeValue(CoreAttributeTypes.WordTemplateContent, "");
+
+      Matcher match = NORMAL_LIST_BULLET_STYLE.matcher(wtc);
+      if (match.find()) {
+         errorMessages.add(ValidationReportOperation.getRequirementHyperlink(
+            artToValidate) + " (" + artToValidate.getGammaId() + ")" + " is not using lists or bullets associated with an expected style." + " See " + getHyperLink());
+         validationPassed = false;
+      }
+
+      return new ValidationResult(errorMessages, validationPassed);
+   }
+
+   @Override
+   public String getRuleDescription() {
+      return "<b>Formatting Check: </b>" + "Ensure lists and bullets use an expected style (" + getHyperLink() + ")" + " in the artifact(s)";
+   }
+
+   private String getHyperLink() {
+      return XResultDataUI.getHyperlinkUrlInternal("More on Formatting/Editing", MORE_ON_FORMATTING);
+   }
+
+   @Override
+   public String getRuleTitle() {
+      return "Formatting Check:";
+   }
+
+}
