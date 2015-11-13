21a12609ebaac1b6730a7095870bd5e19373a074
Andy Jury <andy.jury@boeing.com>
2015-11-13 15:44:08 -0700

    feature[ats_ATS258705]: Make class loading more robust and efficient.

--- plugins/org.eclipse.osee.ote.core/src/org/eclipse/osee/ote/core/OseeURLClassLoader.java
+++ plugins/org.eclipse.osee.ote.core/src/org/eclipse/osee/ote/core/OseeURLClassLoader.java
@@ -13,6 +13,9 @@ package org.eclipse.osee.ote.core;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLStreamHandlerFactory;
+import java.util.logging.Level;
+import org.eclipse.osee.framework.logging.OseeLog;
+import org.eclipse.osee.framework.plugin.core.util.ExportClassLoader;
 
 /**
  * @author Andrew M. Finkbeiner
@@ -20,26 +23,53 @@ import java.net.URLStreamHandlerFactory;
 public class OseeURLClassLoader extends URLClassLoader {
 
    private final String name;
+   private final ExportClassLoader exportClassLoader;
 
    public OseeURLClassLoader(String name, URL[] urls, ClassLoader parent) {
       super(urls, parent);
       this.name = name;
       GCHelper.getGCHelper().addRefWatch(this);
-
+      exportClassLoader = ExportClassLoader.getInstance();
+      
    }
 
    public OseeURLClassLoader(String name, URL[] urls) {
       super(urls);
       GCHelper.getGCHelper().addRefWatch(this);
       this.name = name;
+      exportClassLoader = ExportClassLoader.getInstance();
    }
 
    public OseeURLClassLoader(String name, URL[] urls, ClassLoader parent, URLStreamHandlerFactory factory) {
       super(urls, parent, factory);
       GCHelper.getGCHelper().addRefWatch(this);
       this.name = name;
+      exportClassLoader = ExportClassLoader.getInstance();
    }
 
+   @Override
+   public Class<?> loadClass(String clazz) throws ClassNotFoundException{
+      try {
+         return exportClassLoader.loadClass(clazz);
+      } catch (Exception ex2) {
+         int timesTriedToLoad = 0;
+         while(timesTriedToLoad < 10){
+            try {
+               return super.loadClass(clazz);
+            } catch (ClassNotFoundException ex) {
+               System.out.println("Retrying to load from OseeURLClassLoader for class = "+ clazz);
+               timesTriedToLoad++; //Try to load again
+               try {
+                  Thread.sleep(1);
+               } catch (InterruptedException ex1) {
+                  OseeLog.log(OseeURLClassLoader.class, Level.SEVERE, ex1.toString(), ex1);
+               }
+            }
+         }
+         throw new ClassNotFoundException("Class = " + clazz);
+      }
+   }
+   
    @Override
    public String toString() {
       return this.getClass().getName() + " [ " + name + " ] ";
