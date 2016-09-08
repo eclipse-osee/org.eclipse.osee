0f5f86cd557fb676154a12458df07e96cf6c62bf
donald.g.dunne <donald.g.dunne@boeing.com>
2016-09-08 13:37:23 -0700

    bug[ats_ATS313932]: Add JaxAtsUsers is support of action
    Change-Id: I8e7056c36f6d2d0b2453765d4ef9eba77981e7f7

---
+++ plugins/org.eclipse.osee.ats.api/src/org/eclipse/osee/ats/api/user/JaxAtsUsers.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ats.api.user;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Donald G. Dunne
+ */
+public class JaxAtsUsers {
+
+   private List<JaxAtsUser> users = new LinkedList<JaxAtsUser>();
+
+   public JaxAtsUsers() {
+      // for jax-rs
+   }
+
+   public List<JaxAtsUser> getUsers() {
+      return users;
+   }
+
+   public void setUsers(List<JaxAtsUser> users) {
+      this.users = users;
+   }
+
+}
