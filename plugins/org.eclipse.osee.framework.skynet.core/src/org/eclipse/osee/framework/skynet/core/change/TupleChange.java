060547bab5741450bc1e38e47af9b4ab08cce1f8
megumi.telles <megumi.telles@boeing.com>
2016-10-12 13:59:13 -0700

    refinement: Use tuple change type in TupleChange

--- plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/change/TupleChange.java
+++ plugins/org.eclipse.osee.framework.skynet.core/src/org/eclipse/osee/framework/skynet/core/change/TupleChange.java
@@ -74,6 +74,6 @@ public class TupleChange extends Change {
 
    @Override
    public LoadChangeType getChangeType() {
-      return LoadChangeType.attribute;
+      return LoadChangeType.tuple;
    }
 }
