642e11b5978b33de77456e411e5988359024452a
donald.g.dunne <donald.g.dunne@boeing.com>
2016-11-18 10:14:45 -0700

    refinement: Enhance README and add runme.sh
    Change-Id: I2f859671b2083b317f28a72f76fff2ae72de2685

--- plugins/org.eclipse.osee.dep.parent/runme.sh
+++ plugins/org.eclipse.osee.dep.parent/runme.sh
@@ -1,4 +1,3 @@
-1. Edit your .bashrc and change MAVEN_OPTS to -Xmx3G
-2. Execute this (or run runme.sh)
+#!/bin/bash
 mvn clean verify -Declipse-ip-site=file:./../../../org.eclipse.ip/org.eclipse.ip.p2/target/repository
 
