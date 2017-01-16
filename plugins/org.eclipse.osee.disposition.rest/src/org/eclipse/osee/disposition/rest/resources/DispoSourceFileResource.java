0b4e592e489e909d452ba9bf10e870bd3680561c
Angel Avila <angel.avila2@boeing.com>
2017-01-16 12:15:18 -0700

    bug: Fix Coverage get Source File regex trim
    Change-Id: Ibd2ccc5663b9033e0d9cf59a7782e70029789271

--- plugins/org.eclipse.osee.disposition.rest/src/org/eclipse/osee/disposition/rest/resources/DispoSourceFileResource.java
+++ plugins/org.eclipse.osee.disposition.rest/src/org/eclipse/osee/disposition/rest/resources/DispoSourceFileResource.java
@@ -59,8 +59,9 @@ public class DispoSourceFileResource {
    @GET
    @Produces(MediaType.APPLICATION_OCTET_STREAM)
    public Response getDispoItemsById(@PathParam("fileName") String fileName) {
+      String origFileName = fileName;
       if (!fileName.endsWith(".LIS")) {
-         fileName = fileName.replaceAll("\\.(ada|c).*", ".LIS");
+         fileName = fileName.replaceAll("\\.(ada($|\\.)|c($|\\.)).*", ".LIS");
       }
 
       DispoSet set = dispoApi.getDispoSetById(branch, setId);
