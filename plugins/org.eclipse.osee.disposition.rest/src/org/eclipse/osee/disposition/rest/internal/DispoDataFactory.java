917e0510a93cdd3f6a2f63de79bc0cb0f4859ea4
Angel Avila <angel.avila2@boeing.com>
2017-04-28 15:31:37 -0700

    bug: Fix abort flag not being set properly on Dispo Import
    Change-Id: I8e8dab7ebf9d2d658ee01c6b26fb79394bd4a6ef

--- plugins/org.eclipse.osee.disposition.rest/src/org/eclipse/osee/disposition/rest/internal/DispoDataFactory.java
+++ plugins/org.eclipse.osee.disposition.rest/src/org/eclipse/osee/disposition/rest/internal/DispoDataFactory.java
@@ -83,7 +83,6 @@ public class DispoDataFactory {
          itemToInit.setAssignee("UnAssigned");
       }
 
-      itemToInit.setAborted(false);
       itemToInit.setNeedsRerun(false);
       itemToInit.setNeedsReview(false);
 
