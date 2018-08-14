7ec0ceb22498581a191eb65ff37134877a5d20b5
donald.g.dunne <donald.g.dunne@boeing.com>
2018-08-14 12:17:57 -0700

    feature[ats_TW10398]: Create BranchJsonWriters
    Change-Id: I7e8a19a902590c1d8abb831a0932559d5ba59908

---
+++ plugins/org.eclipse.osee.orcs.rest/src/org/eclipse/osee/orcs/rest/internal/writers/BranchJsonWriter.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.orcs.rest.internal.writers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+import org.eclipse.osee.framework.core.data.Branch;
+import org.eclipse.osee.framework.core.util.JsonUtil;
+import org.eclipse.osee.orcs.OrcsApi;
+
+/**
+ * @author Donald G. Dunne
+ */
+@Provider
+public class BranchJsonWriter implements MessageBodyWriter<Branch> {
+   private JsonFactory jsonFactory;
+   private OrcsApi orcsApi;
+
+   public void setOrcsApi(OrcsApi orcsApi) {
+      this.orcsApi = orcsApi;
+   }
+
+   public void start() {
+      jsonFactory = JsonUtil.getFactory();
+   }
+
+   public void stop() {
+      jsonFactory = null;
+   }
+
+   @Override
+   public long getSize(Branch data, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+      return -1;
+   }
+
+   @Override
+   public boolean isWriteable(Class<?> clazz, Type genericType, Annotation[] annotations, MediaType mediaType) {
+      boolean isWriteable = Branch.class.isAssignableFrom(clazz);
+      return isWriteable;
+   }
+
+   @Override
+   public void writeTo(Branch branch, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+      JsonGenerator writer = null;
+      try {
+         writer = jsonFactory.createJsonGenerator(entityStream);
+         writer.writeStartObject();
+         writer.writeNumberField("id", branch.getId());
+         writer.writeStringField("name", branch.getName());
+         writer.writeStringField("viewId", branch.getViewId().getIdString());
+         writer.writeEndObject();
+      } finally {
+         if (writer != null) {
+            writer.flush();
+         }
+      }
+   }
+}
