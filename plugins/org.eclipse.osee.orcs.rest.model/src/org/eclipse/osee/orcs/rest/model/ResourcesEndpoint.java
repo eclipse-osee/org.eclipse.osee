d25978e66f047d93a74e2c512cb7cfca02b6b403
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2019-03-13 00:10:53 -0700

    refinement: Remove xml usage from ResourcesEndpoint
    Change-Id: I03baee60dff14abaf29f8047ce163ae133bbfd46

--- plugins/org.eclipse.osee.orcs.rest.model/src/org/eclipse/osee/orcs/rest/model/ResourcesEndpoint.java
+++ plugins/org.eclipse.osee.orcs.rest.model/src/org/eclipse/osee/orcs/rest/model/ResourcesEndpoint.java
@@ -11,6 +11,7 @@
 package org.eclipse.osee.orcs.rest.model;
 
 import java.io.InputStream;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -18,6 +19,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -33,6 +35,7 @@ public interface ResourcesEndpoint {
 
    @POST
    @Path("{protocol}/{resourceId}")
+   @Consumes({MediaType.APPLICATION_JSON})
    Response saveResource(InputStream inputStream, //
       @PathParam("protocol") String protocol, //
       @PathParam("resourceId") String resourceId, //
