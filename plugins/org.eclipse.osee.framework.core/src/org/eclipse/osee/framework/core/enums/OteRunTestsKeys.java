9f6104ecad60ce9390f12d1a9f09b99458138724
Andy Jury <andy.jury@boeing.com>
2019-05-31 11:01:47 -0700

    feature[AMS9554]: Add new OTE enum
    Change-Id: I9b7c59c0b3f7bd503d0afcc0e14bf88d0f0748df

---
+++ plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/enums/OteRunTestsKeys.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Boeing.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.framework.core.enums;
+
+/**
+ * @author Andrew M. Finkbeiner
+ */
+public enum OteRunTestsKeys {
+   distributionStatement,
+   uut_path,
+   classpath,
+   version_repositoryType,
+   version_location,
+   version_revision,
+   version_lastAuthor,
+   version_lastModificationDate,
+   version_modifiedFlag,
+   serverOutfilePath,
+   serverOutfileJsonPath,
+   serverOutfileDataPath,
+   serverOutfileDataFolderPath,
+   uut_output_path,
+   uut_debug,
+   testClass,
+   clientOutfilePath,
+   testVersion,
+   serverOutfileFolderOverride,
+   uut_partition_array,
+   uut_paths_array,
+   uut_rate_array;
+   
+}
