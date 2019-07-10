141b492a58e67cebf6f5c41fa88c28296f706d15
donald.g.dunne <donald.g.dunne@boeing.com>
2019-07-09 19:42:01 -0700

    feature[TW14501]: In support of action
    Change-Id: I37f4e2ae612a62da6b8388136c7004bdb1e72559

---
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/XAbstractSignDateAndByButton.java
@@ -0,0 +1,113 @@
+/*
+ * Created on July 9, 2009
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.framework.ui.skynet.widgets;
+
+import java.util.Date;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.osee.framework.core.data.AttributeTypeId;
+import org.eclipse.osee.framework.core.enums.SystemUser;
+import org.eclipse.osee.framework.core.operation.Operations;
+import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.jdk.core.util.DateUtil;
+import org.eclipse.osee.framework.logging.OseeLevel;
+import org.eclipse.osee.framework.logging.OseeLog;
+import org.eclipse.osee.framework.skynet.core.User;
+import org.eclipse.osee.framework.skynet.core.UserManager;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
+import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
+import org.eclipse.osee.framework.ui.skynet.internal.Activator;
+import org.eclipse.osee.framework.ui.swt.Displays;
+import org.eclipse.osee.framework.ui.swt.ImageManager;
+import org.eclipse.osee.framework.ui.swt.KeyedImage;
+import org.eclipse.swt.SWT;
+
+/**
+ * XWidget that provides a label and button. Upon pressing button, user and date will be stored and displayed. Widget
+ * must be extended to provide text and attrs to store date and user. Storage will be in storeArt. NOTE: User is stored
+ * by User artifact id.
+ *
+ * @author Donald G. Dunne
+ */
+public abstract class XAbstractSignDateAndByButton extends XButtonWithLabelDam {
+
+   private final AttributeTypeId signDateAttrType;
+   private final AttributeTypeId signByAttrType;
+
+   public XAbstractSignDateAndByButton(String label, String toolTip, AttributeTypeId signDateAttrType, AttributeTypeId signByAttrUser, KeyedImage keyedImage) {
+      super(label, toolTip, ImageManager.getImage(keyedImage));
+      this.signDateAttrType = signDateAttrType;
+      this.signByAttrType = signByAttrUser;
+      addListener();
+   }
+
+   protected void addListener() {
+      addXModifiedListener(listener);
+   }
+
+   @Override
+   protected String getResultsText() {
+      Date date = getArtifact().getSoleAttributeValue(signDateAttrType, null);
+      if (date != null) {
+         User user = UserManager.getUserByArtId(
+            getArtifact().getSoleAttributeValue(signByAttrType, SystemUser.UnAssigned.getId()));
+         labelWidget.setForeground(Displays.getSystemColor(SWT.COLOR_BLACK));
+         return String.format("signed by %s on %s", user.getName(), DateUtil.getDateNow(date, DateUtil.MMDDYYHHMM));
+      }
+      resultsLabelWidget.setForeground(Displays.getSystemColor(SWT.COLOR_RED));
+      return "Not Yet Signed";
+   }
+
+   private final XModifiedListener listener = new XModifiedListener() {
+      @Override
+      public void widgetModified(org.eclipse.osee.framework.ui.skynet.widgets.XWidget widget) {
+         handleSelection();
+      }
+   };
+
+   protected void handleSelection() {
+      try {
+         if (!MessageDialog.openConfirm(Displays.getActiveShell(), getLabel(), getSignMessage())) {
+            return;
+         }
+         Job signJob = new Job(getSignMessage()) {
+
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+               setSigned();
+               return Status.OK_STATUS;
+            }
+         };
+         Operations.scheduleJob(signJob, false, Job.SHORT, null);
+      } catch (OseeCoreException ex) {
+         OseeLog.log(Activator.class, OseeLevel.SEVERE_POPUP, ex);
+      }
+   };
+
+   protected String getSignMessage() {
+      return "Sign " + getLabel() + "?";
+   }
+
+   protected void setSigned() {
+      SkynetTransaction tx =
+         TransactionManager.createTransaction(getArtifact().getBranch(), "Set signed for " + getLabel());
+      Artifact storeArt = getArtifact();
+      storeArt.setSoleAttributeValue(signByAttrType, UserManager.getUser().getIdString());
+      storeArt.setSoleAttributeValue(signDateAttrType, new Date());
+      tx.addArtifact(storeArt);
+      tx.execute();
+   }
+
+   @Override
+   public boolean isEditable() {
+      return true;
+   }
+
+}
