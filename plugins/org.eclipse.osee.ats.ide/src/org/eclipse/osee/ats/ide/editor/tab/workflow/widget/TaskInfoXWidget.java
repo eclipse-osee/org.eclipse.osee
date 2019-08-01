032a1a78a8cdb5a8df4741d02bb4808c38d22b3f
Jeremy A. Midvidy <jeremy.a.midvidy@boeing.com>
2019-08-01 16:27:56 -0700

    feature[TW14501]: Add task summary widget
    Change-Id: Icc5fb585bb57a31ed7ae55591fd0fcf7bf21de87
    Signed-off-by: Jeremy A. Midvidy <jeremy.a.midvidy@boeing.com>

--- plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/editor/tab/workflow/widget/TaskInfoXWidget.java
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/editor/tab/workflow/widget/TaskInfoXWidget.java
@@ -11,11 +11,13 @@
 package org.eclipse.osee.ats.ide.editor.tab.workflow.widget;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.logging.Level;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.osee.ats.api.util.IAtsChangeSet;
 import org.eclipse.osee.ats.api.workdef.IStateToken;
+import org.eclipse.osee.ats.api.workdef.StateColor;
 import org.eclipse.osee.ats.api.workflow.IAtsTask;
 import org.eclipse.osee.ats.api.workflow.IAtsTeamWorkflow;
 import org.eclipse.osee.ats.api.workflow.transition.IAtsTransitionManager;
@@ -25,6 +27,7 @@ import org.eclipse.osee.ats.core.workflow.transition.TransitionFactory;
 import org.eclipse.osee.ats.core.workflow.transition.TransitionHelper;
 import org.eclipse.osee.ats.ide.internal.Activator;
 import org.eclipse.osee.ats.ide.internal.AtsClientService;
+import org.eclipse.osee.ats.ide.workdef.StateColorToSwtColor;
 import org.eclipse.osee.ats.ide.workflow.task.TaskArtifact;
 import org.eclipse.osee.ats.ide.workflow.task.TaskStates;
 import org.eclipse.osee.ats.ide.workflow.teamwf.TeamWorkFlowArtifact;
@@ -37,8 +40,11 @@ import org.eclipse.osee.framework.ui.skynet.widgets.XLabelValueBase;
 import org.eclipse.osee.framework.ui.swt.Displays;
 import org.eclipse.osee.framework.ui.swt.Widgets;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.IMessageManager;
@@ -51,12 +57,16 @@ public class TaskInfoXWidget extends XLabelValueBase {
    private final IStateToken forState;
    private final IManagedForm managedForm;
    private final TeamWorkFlowArtifact teamWf;
+   private final Composite compositeLabel;
+   private final Composite composite;
 
-   public TaskInfoXWidget(IManagedForm managedForm, final TeamWorkFlowArtifact teamWf, final IStateToken forState, Composite composite, int horizontalSpan) {
-      super("\"" + forState.getName() + "\" State Tasks");
+   public TaskInfoXWidget(IManagedForm managedForm, final TeamWorkFlowArtifact teamWf, final IStateToken forState, Composite fromComposite, int horizontalSpan) {
+      super("Tasks");
       this.managedForm = managedForm;
       this.teamWf = teamWf;
       this.forState = forState;
+      this.compositeLabel = fromComposite.getParent();
+      this.composite = fromComposite;
       setToolTip("Tasks must be completed before transtion.  Select \"Task\" tab to view tasks");
       setFillHorizontally(true);
       createWidgets(managedForm, composite, horizontalSpan);
@@ -78,9 +88,11 @@ public class TaskInfoXWidget extends XLabelValueBase {
          dispose();
       }
       try {
-         if (AtsClientService.get().getTaskService().hasTasks(teamWf, forState)) {
-            setValueText(getStatus(teamWf, forState));
+         if (AtsClientService.get().getTaskService().hasTasks(teamWf)) {
+            dispose();
+            getStateStatus(teamWf, forState);
          } else {
+            dispose();
             setValueText("No Tasks Created");
          }
          if (areTasksComplete(teamWf, forState).isFalse()) {
@@ -115,19 +127,43 @@ public class TaskInfoXWidget extends XLabelValueBase {
       return Result.TrueResult;
    }
 
-   private String getStatus(TeamWorkFlowArtifact teamWf, IStateToken state) {
-      int completed = 0, cancelled = 0, inWork = 0;
-      for (IAtsTask task : AtsClientService.get().getTaskService().getTasks(teamWf, state)) {
-         if (task.isCompleted()) {
-            completed++;
-         } else if (task.isCancelled()) {
-            cancelled++;
-         } else {
-            inWork++;
-         }
+   private void getStateStatus(final TeamWorkFlowArtifact currWf, final IStateToken currState) {
+      if (!this.getValueText().equals("")) {
+         return;
       }
-      return String.format("Total: %d - InWork: %d - Completed: %d - Cancelled: %d",
-         AtsClientService.get().getTaskService().getTasks(teamWf, state).size(), inWork, completed, cancelled);
+      Composite tasksCompositeLabel = new Composite(this.compositeLabel, SWT.LEFT);
+      HashMap<String, Integer> countingMap = new HashMap<String, Integer>();
+      HashMap<String, Integer> colorMap = new HashMap<String, Integer>();
+      int numStates = 0;
+      for (IAtsTask task : AtsClientService.get().getTaskService().getTasks(currWf)) {
+         String currTaskName = task.getStateDefinition().getName();
+         if (countingMap.containsKey(currTaskName)) {
+            countingMap.put(currTaskName, countingMap.get(currTaskName) + 1);
+         } else {
+            countingMap.put(currTaskName, 1);
+            StateColor currColor = task.getStateDefinition().getColor();
+            if (currColor == null) {
+               currColor = StateColor.BLACK;
+            }
+            colorMap.put(currTaskName, StateColorToSwtColor.convert(currColor));
+         }
+         numStates++;
+      }
+      int gridSpaces = numStates;
+      if (numStates < 3) {
+         gridSpaces = 3;
+      }
+      tasksCompositeLabel.setLayout(new GridLayout(gridSpaces, false));
+      tasksCompositeLabel.setLayoutData(new GridData(SWT.NONE, SWT.NONE, true, false));
+      setValueText(String.format(" Total: %d  |  Current State \"%s\": %s", numStates, currState.getName(),
+         AtsClientService.get().getTaskService().getTasks(currWf, currState).size()));
+      Label leftAlignLab = new Label(tasksCompositeLabel, SWT.NONE);
+      leftAlignLab.setText("          ");
+      for (String key : countingMap.keySet()) {
+         Label lab = new Label(tasksCompositeLabel, SWT.NONE);
+         lab.setText("\"" + key + "\"" + ": " + countingMap.get(key) + " ");
+         lab.setForeground(Displays.getSystemColor(colorMap.get(key)));
+      }
    }
 
    public void addAdminRightClickOption() {
