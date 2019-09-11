ba7429b130d32aa6f07de9f868b0c8bd663a15e2
Jeremy A. Midvidy <jeremy.a.midvidy@boeing.com>
2019-09-11 14:52:52 -0700

    feature[TW14501]: Fix WorkDefinitionViewer to display relevant workdefs
    Change-Id: I2ec901f4f49ccff80d94c9ed8a51dea2815325db
    Signed-off-by: Jeremy A. Midvidy <jeremy.a.midvidy@boeing.com>

--- plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/workdef/internal/AtsWorkDefinitionProvider.java
+++ plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/workdef/internal/AtsWorkDefinitionProvider.java
@@ -1,9 +1,12 @@
 package org.eclipse.osee.ats.core.workdef.internal;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import org.eclipse.osee.ats.api.workdef.IAtsWorkDefinition;
 import org.eclipse.osee.ats.api.workdef.IAtsWorkDefinitionProvider;
+import org.eclipse.osee.ats.core.internal.AtsApiService;
 import org.eclipse.osee.ats.core.workdef.internal.workdefs.WorkDefGoal;
 import org.eclipse.osee.ats.core.workdef.internal.workdefs.WorkDefReviewDecision;
 import org.eclipse.osee.ats.core.workdef.internal.workdefs.WorkDefReviewPeerDemoSwDesign;
@@ -27,8 +30,10 @@ public class AtsWorkDefinitionProvider implements IAtsWorkDefinitionProvider {
 
    @Override
    public Collection<IAtsWorkDefinition> getWorkDefinitions() {
+      // return empty if not AtsDB
+      List<IAtsWorkDefinition> ret = new ArrayList<IAtsWorkDefinition>();
       // @formatter:off
-         return Arrays.asList(
+         ret.addAll(Arrays.asList(
             new WorkDefGoal().build(),
             new WorkDefReviewDecision().build(),
             new WorkDefReviewPeerToPeer().build(),
@@ -37,14 +42,22 @@ public class AtsWorkDefinitionProvider implements IAtsWorkDefinitionProvider {
             new WorkDefTaskDefault().build(),
             new WorkDefTeamAtsConfig2Example().build(),
             new WorkDefTeamDefault().build(),
-            new WorkDefTeamSimple().build(),
+            new WorkDefTeamSimple().build()));
+         if (isDemoDb()) {
+            ret.addAll(Arrays.asList(
             new WorkDefTeamDemoCode().build(),
             new WorkDefTeamDemoReq().build(),
             new WorkDefTeamDemoSwDesign().build(),
             new WorkDefTeamDemoTest().build(),
             new WorkDefTaskDemoSwDesign().build(),
-            new WorkDefReviewPeerDemoSwDesign().build());
+            new WorkDefReviewPeerDemoSwDesign().build()));
+      }
       // @formatter:on
-
+      return ret;
    }
+
+   public boolean isDemoDb() {
+      return AtsApiService.get().getUserService().getUserById("3333") != null;
+   }
+
 }
