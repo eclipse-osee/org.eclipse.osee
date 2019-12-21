8e416a5c02ac8b9985ea641f0cffb0a179164c52
Ryan D. Brooks <ryan.d.brooks@boeing.com>
2019-12-21 10:49:57 -0700

    refactor: Remove unused types DemoDslArtifact and DemoDslAttribute
    Change-Id: I9f5ee639e1c4a744d72c9b7be881dd2f9f06d715

--- plugins/org.eclipse.osee.client.demo/src/org/eclipse/osee/client/demo/DemoOseeTypes.java
+++ plugins/org.eclipse.osee.client.demo/src/org/eclipse/osee/client/demo/DemoOseeTypes.java
@@ -13,22 +13,13 @@ package org.eclipse.osee.client.demo;
 import static org.eclipse.osee.client.demo.ClientDemoTypeTokenProvider.clientDemo;
 import static org.eclipse.osee.framework.core.enums.CoreArtifactTypes.Artifact;
 import static org.eclipse.osee.framework.core.enums.CoreAttributeTypes.Partition;
-import javax.ws.rs.core.MediaType;
 import org.eclipse.osee.framework.core.data.ArtifactTypeToken;
-import org.eclipse.osee.framework.core.data.AttributeTypeString;
 
 /**
  * @author Roberto E. Escobar
  */
 public interface DemoOseeTypes {
-
-   // @formatter:off
-   AttributeTypeString DemoDslAttribute = clientDemo.createStringNoTag(1153126013769613777L, "Demo DSL Attribute", MediaType.TEXT_PLAIN, "");
-
-   ArtifactTypeToken DemoArtifactWithSelectivePartition = clientDemo.add(clientDemo.artifactType(86L, "Demo Artifact With Selective Partition", false, Artifact)
-      .atLeastOne(Partition, " ", 3458764513820541309L));
-   ArtifactTypeToken DemoDslArtifact = clientDemo.add(clientDemo.artifactType(204526342635554L, "Demo DSL Artifact", false, Artifact)
-      .any(DemoDslAttribute, ""));
-   // @formatter:on
-
+   ArtifactTypeToken DemoArtifactWithSelectivePartition =
+      clientDemo.add(clientDemo.artifactType(86L, "Demo Artifact With Selective Partition", false, Artifact).atLeastOne(
+         Partition, " ", 3458764513820541309L));
 }
