64e180cec1fcef28f987297558bdeb90091b9c5b
donald.g.dunne <donald.g.dunne@boeing.com>
2021-08-24 10:07:31 -0700

    feature[TW19351]: Allow User Management by User Group
    Change-Id: I5317e70dc7579188dda38e91a5ab4f15edb4bdd5

--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/user/UserNavigateViewItems.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/user/UserNavigateViewItems.java
@@ -13,25 +13,22 @@
 
 package org.eclipse.osee.framework.ui.skynet.user;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import org.eclipse.osee.framework.core.enums.Active;
+import org.eclipse.osee.framework.core.enums.CoreUserGroups;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
 import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.osee.framework.plugin.core.util.ExtensionDefinedObjects;
 import org.eclipse.osee.framework.ui.plugin.xnavigate.IXNavigateCommonItem;
-import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateCommonItems;
 import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItem;
 import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItemAction;
 import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateViewItems;
 import org.eclipse.osee.framework.ui.skynet.FrameworkImage;
-import org.eclipse.osee.framework.ui.skynet.internal.ServiceUtil;
 import org.eclipse.osee.framework.ui.skynet.blam.operation.CreateNewUser;
 import org.eclipse.osee.framework.ui.skynet.blam.operation.PopulateUserGroupBlam;
 import org.eclipse.osee.framework.ui.skynet.internal.Activator;
-import org.eclipse.osee.framework.ui.skynet.util.DbConnectionUtility;
+import org.eclipse.osee.framework.ui.skynet.internal.ServiceUtil;
 import org.eclipse.osee.framework.ui.skynet.widgets.xnavigate.XNavigateItemBlam;
 
 /**
@@ -40,70 +37,42 @@ import org.eclipse.osee.framework.ui.skynet.widgets.xnavigate.XNavigateItemBlam;
 public class UserNavigateViewItems implements XNavigateViewItems, IXNavigateCommonItem {
 
    private final static UserNavigateViewItems instance = new UserNavigateViewItems();
-   private final List<XNavigateItem> items = new CopyOnWriteArrayList<>();
-   private boolean ensurePopulatedRanOnce = false;
 
    public static UserNavigateViewItems getInstance() {
       return instance;
    }
 
    @Override
-   public List<XNavigateItem> getSearchNavigateItems() {
-      ensurePopulated();
-      return items;
-   }
-
-   private synchronized void ensurePopulated() {
-      if (!ensurePopulatedRanOnce) {
-         if (DbConnectionUtility.areOSEEServicesAvailable().isFalse()) {
-            return;
-         }
-         this.ensurePopulatedRanOnce = true;
-
-         try {
-            addOseeUserSectionChildren(null);
-
-            XNavigateCommonItems.addCommonNavigateItems(items, Arrays.asList(getSectionId()));
-         } catch (OseeCoreException ex) {
-            OseeLog.log(Activator.class, Level.SEVERE, ex);
-         }
-      }
-   }
-
-   public void addOseeUserSectionChildren(XNavigateItem parentItem) {
+   public void createCommonSection(List<XNavigateItem> items, List<String> excludeSectionIds) {
       try {
-         items.add(new XNavigateItemAction(parentItem, new OpenUsersInMassEditor("Open Active Users", Active.Active),
-            FrameworkImage.USER));
-         items.add(new XNavigateItemAction(parentItem, new OpenUsersInMassEditor("Open All Users", Active.Both),
-            FrameworkImage.USER));
+         boolean admin = ServiceUtil.accessControlService().isOseeAdmin();
+         boolean inUserGroup =
+            ServiceUtil.getOseeClient().getAccessControlService().getUserGroupService().isInUserGroup(
+               CoreUserGroups.UserMgmtAdmin);
 
-         if (ServiceUtil.accessControlService().isOseeAdmin()) {
-            items.add(new XNavigateItemBlam(parentItem, new CreateNewUser(), FrameworkImage.ADD_GREEN));
-            items.add(new XNavigateItemBlam(parentItem, new PopulateUserGroupBlam(), FrameworkImage.GROUP));
+         XNavigateItem userMgmtItem = null;
+         if (admin || inUserGroup) {
+            userMgmtItem = new XNavigateItem(null, "User Management", FrameworkImage.USER);
+            items.add(userMgmtItem);
+            new XNavigateItemAction(userMgmtItem, new OpenUsersInMassEditor("Open Active Users", Active.Active),
+               FrameworkImage.USER);
+            new XNavigateItemAction(userMgmtItem, new OpenUsersInMassEditor("Open All Users", Active.Both),
+               FrameworkImage.USER);
+         }
+
+         if (admin) {
+            new XNavigateItemBlam(userMgmtItem, new CreateNewUser(), FrameworkImage.ADD_GREEN);
+            new XNavigateItemBlam(userMgmtItem, new PopulateUserGroupBlam(), FrameworkImage.GROUP);
          }
 
          ExtensionDefinedObjects<IUserNavigateItem> objects = new ExtensionDefinedObjects<>(
             "org.eclipse.osee.framework.ui.skynet.UserNavigateItem", "UserNavigateItem", "classname");
          for (IUserNavigateItem newItem : objects.getObjects()) {
-            for (XNavigateItem item : newItem.getNavigateItems(parentItem)) {
+            for (XNavigateItem item : newItem.getNavigateItems(userMgmtItem, admin, inUserGroup)) {
                items.add(item);
             }
          }
 
-      } catch (OseeCoreException ex) {
-         OseeLog.log(Activator.class, Level.SEVERE, ex);
-      }
-   }
-
-   @Override
-   public void createCommonSection(List<XNavigateItem> items, List<String> excludeSectionIds) {
-      try {
-         boolean admin = ServiceUtil.accessControlService().isOseeAdmin();
-         if (admin) {
-            XNavigateItem reviewItem = new XNavigateItem(null, "User Management", FrameworkImage.USER);
-            addOseeUserSectionChildren(reviewItem);
-            items.add(reviewItem);
-         }
       } catch (OseeCoreException ex) {
          OseeLog.log(Activator.class, Level.SEVERE, "Can't create User Management section");
       }
@@ -112,5 +81,10 @@ public class UserNavigateViewItems implements XNavigateViewItems, IXNavigateComm
    @Override
    public String getSectionId() {
       return "Users";
+   }
+
+   @Override
+   public List<XNavigateItem> getSearchNavigateItems() {
+      return java.util.Collections.emptyList();
    }
 }
