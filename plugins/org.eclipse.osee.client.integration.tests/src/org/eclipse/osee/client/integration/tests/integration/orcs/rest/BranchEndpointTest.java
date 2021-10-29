69c6e975be8f7c7a3793a89ba6328c935172b7f8
donald.g.dunne <donald.g.dunne@boeing.com>
2021-10-28 18:17:56 -0700

    refactor[TW19782]: Remove usage of UserId from branch creation
    Change-Id: Ie546de03e24b3c27e5016ba2f1a91cd0f83a8b56

--- plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/integration/orcs/rest/BranchEndpointTest.java
+++ plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/integration/orcs/rest/BranchEndpointTest.java
@@ -47,6 +47,7 @@ import org.eclipse.osee.framework.core.enums.DemoUsers;
 import org.eclipse.osee.framework.core.util.OsgiUtil;
 import org.eclipse.osee.framework.jdk.core.result.XResultData;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.skynet.core.UserManager;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.BranchManager;
 import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
