2cdcaf163c5f5aaa9a3a8a6db11b135a31193e77
donald.g.dunne <donald.g.dunne@boeing.com>
2021-11-05 08:28:36 -0700

    bug[TW19760]: Fix demo admin toggle
    Change-Id: Iec7e715bb648d271f1e18b7a2e3bb88b3ded6ed9

--- plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/util/internal/AtsApiIdeImpl.java
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/util/internal/AtsApiIdeImpl.java
@@ -63,6 +63,8 @@ import org.eclipse.osee.framework.core.client.OseeClientProperties;
 import org.eclipse.osee.framework.core.data.ArtifactTypeToken;
 import org.eclipse.osee.framework.core.util.OsgiUtil;
 import org.eclipse.osee.framework.plugin.core.util.Jobs;
+import org.eclipse.osee.framework.skynet.core.UserManager;
+import org.eclipse.osee.framework.skynet.core.access.UserServiceImpl;
 
 /**
  * @author Donald G. Dunne
@@ -138,6 +140,8 @@ public class AtsApiIdeImpl extends AtsApiImpl implements AtsApiIde {
       // clear client config cache (read from server)
       getConfigService().getConfigurations();
       getUserService().clearCaches();
+      UserManager.clearCache();
+      UserServiceImpl.clearCache();
 
       super.clearCaches();
 
