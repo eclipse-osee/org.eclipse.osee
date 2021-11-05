2f46190f3889259680f57f12b724906f74a2803a
donald.g.dunne <donald.g.dunne@boeing.com>
2021-11-05 10:30:00 -0700

    bug[TW19760]: Add missing AtsXHyperlinkTriStateBooleanValidator
    Change-Id: Ibfb60eef1e0c853a43f1f613d6f30fe88e027273

---
+++ plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/validator/AtsXHyperlinkTriStateBooleanValidator.java
@@ -0,0 +1,39 @@
+/*********************************************************************
+ * Copyright (c) 2021 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+
+package org.eclipse.osee.ats.core.validator;
+
+import org.eclipse.osee.ats.api.AtsApi;
+import org.eclipse.osee.ats.api.IAtsWorkItem;
+import org.eclipse.osee.ats.api.util.IValueProvider;
+import org.eclipse.osee.ats.api.workdef.IAtsStateDefinition;
+import org.eclipse.osee.ats.api.workdef.WidgetResult;
+import org.eclipse.osee.ats.api.workdef.model.WidgetDefinition;
+
+/**
+ * @author Donald G. Dunne
+ */
+public class AtsXHyperlinkTriStateBooleanValidator extends AtsXWidgetValidator {
+
+   @Override
+   public WidgetResult validateTransition(IAtsWorkItem workItem, IValueProvider provider, WidgetDefinition widgetDef, IAtsStateDefinition fromStateDef, IAtsStateDefinition toStateDef, AtsApi atsApi) {
+      WidgetResult result = WidgetResult.Success;
+      if ("XHyperlinkTriStateBooleanDam".equals(widgetDef.getXWidgetName())) {
+         result = validateWidgetIsRequired(provider, widgetDef, fromStateDef, toStateDef);
+         if (!result.isSuccess()) {
+            return result;
+         }
+      }
+      return result;
+   }
+}
