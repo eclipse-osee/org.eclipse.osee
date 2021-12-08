0df22d35942b23177bf9aebe903f560a02c24841
donald.g.dunne <donald.g.dunne@boeing.com>
2021-12-08 06:47:52 -0700

    feature[TW19867]: Add notification debugging
    Change-Id: I991457a1c2443d9b49b5bc7a9e929507ff446624

---
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/navigate/TestEmailSend.java
@@ -0,0 +1,67 @@
+/*********************************************************************
+ * Copyright (c) 2004, 2007 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+
+package org.eclipse.osee.ats.ide.navigate;
+
+import java.util.Arrays;
+import org.eclipse.osee.ats.api.AtsApi;
+import org.eclipse.osee.ats.ide.internal.Activator;
+import org.eclipse.osee.ats.ide.internal.AtsApiService;
+import org.eclipse.osee.framework.jdk.core.result.XResultData;
+import org.eclipse.osee.framework.jdk.core.util.AHTML;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.logging.OseeLevel;
+import org.eclipse.osee.framework.logging.OseeLog;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateComposite.TableLoadOption;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItem;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItemAction;
+import org.eclipse.osee.framework.ui.skynet.FrameworkImage;
+import org.eclipse.osee.framework.ui.skynet.notify.OseeEmail;
+import org.eclipse.osee.framework.ui.skynet.notify.OseeEmail.BodyType;
+import org.eclipse.osee.framework.ui.skynet.results.XResultDataUI;
+
+/**
+ * @author Donald G. Dunne
+ */
+public class TestEmailSend extends XNavigateItemAction {
+
+   AtsApi atsApi;
+
+   public TestEmailSend() {
+      super("Test Email Send", FrameworkImage.EMAIL, XNavigateItem.EMAIL_NOTIFICATIONS);
+      atsApi = AtsApiService.get();
+   }
+
+   @Override
+   public void run(TableLoadOption... tableLoadOptions) {
+      try {
+         XResultData rd = new XResultData();
+         rd.log("Send Test Email - Client");
+         try {
+            OseeEmail emailMessage = new OseeEmail(Arrays.asList("donald.g.dunne@boeing.com"),
+               "donald.g.dunne@boeing.com", "donald.g.dunne@boeing.com", "Test Email - Client",
+               AHTML.simplePage(AHTML.bold("Hello World - this should be bold")), BodyType.Html);
+            emailMessage.send();
+         } catch (Exception ex) {
+            rd.error(Lib.exceptionToString(ex));
+         }
+         XResultDataUI.report(rd, "Send Test Email - Client");
+
+         rd = atsApi.getServerEndpoints().getNotifyEndpoint().sendEmail();
+         XResultDataUI.report(rd, "Send Test Email - Server");
+
+      } catch (Exception ex) {
+         OseeLog.log(Activator.class, OseeLevel.SEVERE_POPUP, ex);
+      }
+   }
+}
--- plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/notify/AtsNotifyEndpointImpl.java
+++ plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/notify/AtsNotifyEndpointImpl.java
@@ -13,10 +13,15 @@
 
 package org.eclipse.osee.ats.rest.internal.notify;
 
+import java.util.Arrays;
 import javax.ws.rs.core.Response;
 import org.eclipse.osee.ats.api.AtsApi;
 import org.eclipse.osee.ats.api.notify.AtsNotificationCollector;
 import org.eclipse.osee.ats.api.notify.AtsNotifyEndpointApi;
+import org.eclipse.osee.ats.rest.internal.notify.OseeEmail.BodyType;
+import org.eclipse.osee.framework.jdk.core.result.XResultData;
+import org.eclipse.osee.framework.jdk.core.util.AHTML;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
 
 /**
  * @author Donald G. Dunne
@@ -36,5 +41,20 @@ public class AtsNotifyEndpointImpl implements AtsNotifyEndpointApi {
 
       atsApi.getNotificationService().sendNotifications(notifications);
       return Response.ok().build();
+   }
+
+   @Override
+   public XResultData sendEmail() {
+      XResultData rd = new XResultData();
+      rd.log("Send Test Email - Server");
+      try {
+         OseeEmail emailMessage = new OseeEmail(Arrays.asList("donald.g.dunne@boeing.com"), "donald.g.dunne@boeing.com",
+            "donald.g.dunne@boeing.com", "Test Email - Server",
+            AHTML.simplePage(AHTML.bold("Hello World - this should be bold")), BodyType.Html, atsApi);
+         emailMessage.send();
+      } catch (Exception ex) {
+         rd.error(Lib.exceptionToString(ex));
+      }
+      return rd;
    }
 }
