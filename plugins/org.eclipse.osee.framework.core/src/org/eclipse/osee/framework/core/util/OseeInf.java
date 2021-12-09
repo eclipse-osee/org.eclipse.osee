e0aaa786e7901b2b871c42b191887ebdfa163390
Michael P. Masterson <michael.p.masterson@boeing.com>
2021-12-09 13:24:00 -0700

    feature: Adding new way to load OseeInf resources
    This will allow for downloading resources using the classloader
    instead of depending on the bundle.  This is mostly useful for those
    cases where you are trying to load from OSEE-INF in a workspace
    instead of via a compiled product.
    
    Change-Id: I99022fd8a7df01974eb034411d497d4476d9f860

--- plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/util/OseeInf.java
+++ plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/util/OseeInf.java
@@ -26,12 +26,38 @@ import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
  */
 public class OseeInf {
 
+   public static final String ROOT_DIR = "OSEE-INF/";
+
    public static InputStream getResourceAsStream(String path, Class<?> classFromBundle) {
-      return OsgiUtil.getResourceAsStream(classFromBundle, "OSEE-INF/" + path);
+      return OsgiUtil.getResourceAsStream(classFromBundle, ROOT_DIR + path);
+   }
+
+   /**
+    * Uses the default classloader for the provided object to load the resource.
+    * 
+    * @param filePath File path of resource to load. Sometimes the classloader will assume OSEE-INF is the root dir of
+    * the path and sometimes it doesn't. If the resource doesn't exist at the root then the OSEE-INF dir is prepended to
+    * the path and tried again.
+    * @param obj Object whose classloader is to be used
+    * @return The resource found at the classloader root or in OSEE-INF, in that order.
+    * @throws OseeCoreException If resource not found
+    */
+   public static InputStream getResourceAsStream(String filePath, Object obj) throws OseeCoreException {
+      ClassLoader classLoader = obj.getClass().getClassLoader();
+      InputStream resourceAsStream = classLoader.getResourceAsStream(filePath);
+      if (resourceAsStream == null) {
+         resourceAsStream = classLoader.getResourceAsStream(ROOT_DIR + filePath);
+      }
+
+      if (resourceAsStream != null) {
+         return resourceAsStream;
+      } else {
+         throw new OseeCoreException("Resource not found at %s nor %s", filePath, ROOT_DIR + filePath);
+      }
    }
 
    public static String getResourceContents(String path, Class<?> classFromBundle) {
-      return OsgiUtil.getResourceAsString(classFromBundle, "OSEE-INF/" + path);
+      return OsgiUtil.getResourceAsString(classFromBundle, ROOT_DIR + path);
    }
 
    public static File getResourceAsFile(String path, Class<?> clazz) {
@@ -40,11 +66,11 @@ public class OseeInf {
          String uri = new URI(url.toString().replace(" ", "%20")).getPath();
          return new File(uri);
       } catch (Exception ex) {
-         throw new OseeCoreException(ex, "Error getting resource [%s] as file", path);
+         throw OseeCoreException.wrap(ex);
       }
    }
 
    public static URL getResourceAsUrl(String path, Class<?> clazz) {
-      return OsgiUtil.getResourceAsUrl(clazz, "OSEE-INF/" + path);
+      return OsgiUtil.getResourceAsUrl(clazz, ROOT_DIR + path);
    }
 }
