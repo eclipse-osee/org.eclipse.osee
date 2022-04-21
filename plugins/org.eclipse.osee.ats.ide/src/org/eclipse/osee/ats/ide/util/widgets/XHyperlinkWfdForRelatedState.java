b110a3b09839986332962ad3984809737dee89b1
donald.g.dunne <donald.g.dunne@boeing.com>
2022-04-21 12:30:16 -0700

    bug[TW20350]: Add XHyperlinkWfdForRelatedStateDam
    Change-Id: Ibcf0140d1d7320c4bdfe90e308bae41afb60d757

---
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/util/widgets/XHyperlinkWfdForRelatedState.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Boeing.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.osee.ats.ide.util.widgets;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.eclipse.osee.ats.ide.column.RelatedToStateColumn;
+import org.eclipse.osee.framework.ui.skynet.widgets.XHyperlinkWithFilteredDialog;
+
+/**
+ * @author Donald G. Dunne
+ */
+public class XHyperlinkWfdForRelatedState extends XHyperlinkWithFilteredDialog<String> {
+
+   private final Collection<String> selectable;
+
+   public XHyperlinkWfdForRelatedState(Collection<String> selectable) {
+      super("Related State");
+      this.selectable = selectable;
+      setToolTip(RelatedToStateColumn.RELATED_TO_STATE_SELECTION);
+   }
+
+   @Override
+   public Collection<String> getSelectable() {
+      if (selectable != null) {
+         return selectable;
+      } else {
+         return Collections.emptyList();
+      }
+   }
+
+}
