9a9413afe3123480f9b69890d21330c193c79394
loren.k.ashley <loren.k.ashley@boeing.com>
2022-05-11 14:57:23 -0700

    feature[TW20440]: Sync - Client Side JUnit for ENUMERATED Values
    Change-Id: I6831172ff63a66be7829103970c555f2f9719d4e

--- plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/integration/skynet/core/utils/TestUtil.java
+++ plugins/org.eclipse.osee.client.integration.tests/src/org/eclipse/osee/client/integration/tests/integration/skynet/core/utils/TestUtil.java
@@ -15,13 +15,18 @@ package org.eclipse.osee.client.integration.tests.integration.skynet.core.utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
 import org.eclipse.osee.framework.core.data.ApplicabilityId;
 import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.data.ArtifactToken;
 import org.eclipse.osee.framework.core.data.ArtifactTypeToken;
+import org.eclipse.osee.framework.core.data.AttributeTypeGeneric;
 import org.eclipse.osee.framework.core.data.BranchId;
 import org.eclipse.osee.framework.core.data.BranchToken;
 import org.eclipse.osee.framework.core.data.GammaId;
@@ -33,13 +38,20 @@ import org.eclipse.osee.framework.core.enums.ModificationType;
 import org.eclipse.osee.framework.skynet.core.OseeSystemArtifacts;
 import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
 import org.eclipse.osee.framework.skynet.core.artifact.ArtifactTypeManager;
+import org.eclipse.osee.framework.skynet.core.artifact.Attribute;
 import org.eclipse.osee.framework.skynet.core.relation.RelationLink;
 import org.eclipse.osee.framework.skynet.core.utility.ConnectionHandler;
+import org.eclipse.osee.orcs.rest.model.ArtifactEndpoint;
+import org.eclipse.osee.orcs.rest.model.RelationEndpoint;
 import org.junit.Assert;
 
 /**
+ * Class of static utility methods to aid with client integration testing.
+ *
  * @author Donald G. Dunne
+ * @author Loren K. Ashley
  */
+
 public final class TestUtil {
 
    private TestUtil() {
@@ -47,8 +59,104 @@ public final class TestUtil {
    }
 
    /**
+    * Compares two attribute values using the {@link #equals} method unless both objects are instances of the
+    * {@link Date} class and then the method {@link Date#compareTo} is used.
+    *
+    * @param expectedValue the expected value, must not be <code>null</code>.
+    * @param actualValue the value to be compared, may be <code>null</code>.
+    * @return true, when the values are equal; otherwise, <code>false</code>.
+    */
+
+   public static boolean compareAttributeValue(Object expectedValue, Object actualValue) {
+      Objects.requireNonNull(expectedValue);
+      //@formatter:off
+      return
+         Objects.nonNull( actualValue )
+            ? ( expectedValue instanceof Date )
+                 ? ( actualValue instanceof Date )
+                      ? (((Date) expectedValue).compareTo( (Date) actualValue) == 0)
+                      : false
+                 : expectedValue.equals( actualValue )
+            : false;
+      //@formatter:on
+   }
+
+   /**
+    * {@link Attribute}s on the <code>attributeList</code> with a value that matches a value on the
+    * <code>expectedValue</code> are removed. The matching expected value is also removed from the
+    * <code>expectedValueList</code>. At the completion of this method the <code>attributeList</code> will contain
+    * {@link Attribute}s with values that did not match a value on the <code>expectedValueList</code>. The
+    * <code>expectedValueList</code> will only contain the values that did not match an {@link Attribute} value on the
+    * <code>attributeList</code>.
+    *
+    * @param attributeList the list of {@link Attribute}s to be compared.
+    * @param expectedValueList the list of expected attribute values.
+    */
+
+   public static void compareAttributeValues(List<Attribute<?>> attributeList, List<Object> expectedValueList) {
+      for (int i = 0; i < expectedValueList.size(); i++) {
+         if (TestUtil.removeFirstAttributeMatch(attributeList, expectedValueList.get(i))) {
+            expectedValueList.remove(i--);
+         }
+      }
+   }
+
+   /**
+    * Creates a new artifact under the specified parent on the specified branch.
+    *
+    * @param artifactEndpoint The REST API end point for artifact functions.
+    * @param parentBranchId The branch to create the new artifact upon.
+    * @param parentArtifactId The hierarchical parent of the artifact to be created.
+    * @param childArtifactTypeToken when creating a child artifact, the type of artifact to create.
+    * @param childName the name of the child artifact.
+    * @return The {@link ArtifactToken} (identifier) for the newly created artifact.
+    */
+
+   private static ArtifactToken createChildArtifactToken(ArtifactEndpoint artifactEndpoint, BranchId parentBranchId, ArtifactId parentArtifactId, ArtifactTypeToken childArtifactTypeToken, String childName) {
+      return artifactEndpoint.createArtifact(parentBranchId, childArtifactTypeToken, parentArtifactId, childName);
+   }
+
+   /**
+    * Creates the specified number of attributes of the specified type on the provided artifact. All of the artifact's
+    * attributes of the specified type are returned. The returned list will also contain previously existing attributes.
+    *
+    * @param artifact the {@link Artifact} to create attributes on.
+    * @param attributeTypeGeneric the type of attributes to be created.
+    * @param count the number of attributes to be created.
+    * @return a list of all the artifact's attributes of the specified type.
+    */
+
+   public static List<Attribute<?>> createAttributes(Artifact artifact, AttributeTypeGeneric<?> attributeTypeGeneric, int count) {
+      for (int i = 0; i < count; i++) {
+         artifact.addAttribute(attributeTypeGeneric);
+      }
+
+      @SuppressWarnings("unchecked")
+      var attributes = (List<Attribute<?>>) (Object) artifact.getAttributes(attributeTypeGeneric);
+
+      return attributes;
+   }
+
+   public static List<RelationLink> createLinks(int total, BranchId branch) {
+      List<RelationLink> links = new ArrayList<>();
+      for (int index = 0; index < total; index++) {
+         RelationLink link = createRelationLink(RelationId.valueOf(index), ArtifactId.valueOf(index + 1),
+            ArtifactId.valueOf(index + 2), branch, CoreRelationTypes.Allocation);
+         links.add(link);
+      }
+      return links;
+   }
+
+   public static RelationLink createRelationLink(RelationId relationId, ArtifactId artA, ArtifactId artB, BranchId branch, RelationTypeToken relationType) {
+      return new RelationLink(ArtifactToken.valueOf(artA, BranchToken.valueOf(branch)),
+         ArtifactToken.valueOf(artB, BranchToken.valueOf(branch)), branch, relationType, relationId, GammaId.valueOf(0),
+         "relation: " + relationId, ModificationType.MODIFIED, ApplicabilityId.BASE);
+   }
+
+   /**
     * Creates a simple artifact and adds it to the root artifact default hierarchical relation
     */
+
    public static Artifact createSimpleArtifact(ArtifactTypeToken artifactType, String name, BranchToken branch) {
       Artifact softArt = ArtifactTypeManager.addArtifact(artifactType, branch);
       softArt.setName(name);
@@ -68,6 +176,76 @@ public final class TestUtil {
       return arts;
    }
 
+   /**
+    * Gets the attributes of the specified type from the artifact.
+    *
+    * @param artifact the artifact to get attribute from.
+    * @param attributeTypeGeneric the type of attributes to get.
+    * @return when the artifact has attributes of the specified type, a {@link Optional} with a {@link List} of the
+    * {@link Attribute<?>} objects; otherwise, an empty {@link Optional}.
+    */
+
+   public static Optional<List<Attribute<?>>> getAttributes(Artifact artifact, AttributeTypeGeneric<?> attributeTypeGeneric) {
+      @SuppressWarnings("unchecked")
+      var attributes = (List<Attribute<?>>) (Object) artifact.getAttributes(attributeTypeGeneric);
+
+      return (attributes.size() >= 1) ? Optional.of(attributes) : Optional.empty();
+   }
+
+   /**
+    * Gets the {@link ArtifactToken} of a hierarchical child artifact by the child artifact's name.
+    *
+    * @param relationEndpoint REST API end point for relationships.
+    * @param parentArtifactId the {@link ArtifactId} of the artifact to find a child of.
+    * @param childName the name of the child artifact to find.
+    * @return If found, an {@link Optional} containing the {@link ArtifactToken} of the child; otherwise, an empty
+    * {@link Optional}.
+    */
+
+   public static Optional<ArtifactToken> getChildArtifactTokenByName(RelationEndpoint relationEndpoint, ArtifactId parentArtifactId, String childName) {
+      return relationEndpoint.getRelatedHierarchy(parentArtifactId).stream().filter(
+         artifact -> artifact.getName().equals(childName)).findFirst();
+   }
+
+   /**
+    * When the artifact has no attributes of the specified type, creates and returns the specified number of attributes;
+    * otherwise, returns all of the artifacts attributes of the specified type.
+    *
+    * @param artifact the artifact to get or create attributes from or for.
+    * @param attributeTypeGeneric the type of attribute to get or create.
+    * @param count when attributes are created, the number of attributes to create.
+    * @return a list of the artifact's existing attributes of the specified type or a list of the newly created
+    * attributes.
+    */
+
+   public static List<Attribute<?>> getOrCreateAttributes(Artifact artifact, AttributeTypeGeneric<?> attributeTypeGeneric, int count) {
+      return TestUtil.getAttributes(artifact, attributeTypeGeneric).orElseGet(
+         () -> TestUtil.createAttributes(artifact, attributeTypeGeneric, count));
+   }
+
+   /**
+    * Gets or creates a hierarchical child of the specified artifact.
+    *
+    * @param relationEndpoint REST API end point for relationships used to get the hierarchical children of the parent
+    * artifact.
+    * @param artifactEndpoint The REST API end point used to create the child artifact when necessary.
+    * @param branchId the branch to create the artifact on, when necessary.
+    * @param parentArtifactId the hierarchical parent artifact identifier.
+    * @param childArtifactTypeToken when creating a child artifact, the type of artifact to create.
+    * @param childName the name of the child artifact.
+    * @return the {@link ArtifactToken} of the existing or newly created hierarchical child with the specified name.
+    */
+
+   public static ArtifactToken getOrCreateChildArtifactTokenByName(RelationEndpoint relationEndpoint, ArtifactEndpoint artifactEndpoint, BranchId branchId, ArtifactId parentArtifactId, ArtifactTypeToken childArtifactTypeToken, String childName) {
+      return TestUtil.getChildArtifactTokenByName(relationEndpoint, parentArtifactId, childName).orElseGet(
+         () -> TestUtil.createChildArtifactToken(artifactEndpoint, branchId, parentArtifactId, childArtifactTypeToken,
+            childName));
+   }
+
+   private static int getTableRowCount(String tableName) {
+      return ConnectionHandler.getJdbcClient().fetch(0, "SELECT count(1) FROM " + tableName);
+   }
+
    public static Map<String, Integer> getTableRowCounts(String... tables) {
       Map<String, Integer> data = new HashMap<>();
       for (String tableName : tables) {
@@ -76,24 +254,25 @@ public final class TestUtil {
       return data;
    }
 
-   private static int getTableRowCount(String tableName) {
-      return ConnectionHandler.getJdbcClient().fetch(0, "SELECT count(1) FROM " + tableName);
-   }
+   /**
+    * Removes the first attribute with a value that compares with the provided object using the method
+    * {@link TestUtil#compareAttributeValue}.
+    *
+    * @param attributeList the list of attributes.
+    * @param object the attribute value to search for.
+    * @return <code>true</code>, when an attribute value match was found and an attribute was removed from the list;
+    * otherwise, <code>false</code>.
+    */
 
-   public static RelationLink createRelationLink(RelationId relationId, ArtifactId artA, ArtifactId artB, BranchId branch, RelationTypeToken relationType) {
-      return new RelationLink(ArtifactToken.valueOf(artA, BranchToken.valueOf(branch)),
-         ArtifactToken.valueOf(artB, BranchToken.valueOf(branch)), branch, relationType, relationId, GammaId.valueOf(0),
-         "relation: " + relationId, ModificationType.MODIFIED, ApplicabilityId.BASE);
-   }
-
-   public static List<RelationLink> createLinks(int total, BranchId branch) {
-      List<RelationLink> links = new ArrayList<>();
-      for (int index = 0; index < total; index++) {
-         RelationLink link = createRelationLink(RelationId.valueOf(index), ArtifactId.valueOf(index + 1),
-            ArtifactId.valueOf(index + 2), branch, CoreRelationTypes.Allocation);
-         links.add(link);
+   private static boolean removeFirstAttributeMatch(List<Attribute<?>> attributeList, Object object) {
+      for (int i = 0, l = attributeList.size(); i < l; i++) {
+         if (TestUtil.compareAttributeValue(object, attributeList.get(i).getValue())) {
+            attributeList.remove(i);
+            return true;
+         }
       }
-      return links;
+
+      return false;
    }
 
    public static void setEveryOtherToDeleted(Collection<RelationLink> sourceLinks) {
@@ -114,4 +293,65 @@ public final class TestUtil {
       int expected = sourceLinks.isEmpty() ? 0 : sourceLinks.size() / 2;
       Assert.assertEquals("Deleted relation link count did not match", expected, deletedCounts);
    }
+
+   /**
+    * Sets an artifact's attribute values of the specified attribute type to the values on the expected value list. Any
+    * attributes with values of the specified attribute type that are not on expected value list are removed from the
+    * artifact or have their value changed to a value on the expected value list. Attributes will be created and added
+    * to the artifact when the artifact has less attribute values of the specified attribute type than values on the
+    * expected value list.
+    *
+    * @param artifact the {@link Artifact} to set attribute values
+    * @param attributeTypeGeneric the attribute type to set the values for
+    * @param expectedValueList a list of the expected attribute values
+    * @param attributeSetter a {@link BiConsumer} used to assign an attribute's value
+    */
+
+   public static void setAttributeValues(Artifact artifact, AttributeTypeGeneric<?> attributeTypeGeneric, List<Object> expectedValueList, BiConsumer<Attribute<?>, Object> attributeSetter) {
+      var attributeList = TestUtil.getOrCreateAttributes(artifact, attributeTypeGeneric, expectedValueList.size());
+
+      /*
+       * Matching attributes and expected values are removed from both lists
+       */
+
+      TestUtil.compareAttributeValues(attributeList, expectedValueList);
+
+      if ((attributeList.size() == 0) && (expectedValueList.size() == 0)) {
+         /*
+          * Noting missing, nothing extra, done
+          */
+
+         return;
+      }
+
+      /*
+       * Remove spare attributes
+       */
+
+      for (int i = 0, l = attributeList.size() - expectedValueList.size(); i < l; i++) {
+         artifact.deleteAttribute(attributeList.get(0));
+         attributeList.remove(0);
+      }
+
+      /*
+       * Change values in remaining attributes
+       */
+
+      for (int i = 0, l = attributeList.size(); i < l; i++) {
+         attributeSetter.accept(attributeList.get(i), expectedValueList.get(0));
+         expectedValueList.remove(0);
+      }
+
+      /*
+       * Add missing attributes
+       */
+
+      for (int i = 0, l = expectedValueList.size(); i < l; i++) {
+         artifact.addAttribute(attributeTypeGeneric, expectedValueList.get(i));
+      }
+
+   }
+
 }
+
+/* EOF */
