2cdc43359cad59c09a138884314bf7168c604b93
vaibhav.y.patel <vaibhav.y.patel@boeing.com>
2022-10-07 14:02:28 -0700

    feature[TW21763]: AI - Delete realigned Actionable Items
    Change-Id: Iba8f2c0edb76c39470d4fe9c6e8522af19d3063c

--- plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/RealignActionableItemsBlam.java
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/RealignActionableItemsBlam.java
@@ -13,9 +13,11 @@
 
 package org.eclipse.osee.ats.ide;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.osee.ats.api.AtsApi;
 import org.eclipse.osee.ats.api.IAtsObject;
@@ -39,6 +41,7 @@ import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavItemCat;
 import org.eclipse.osee.framework.ui.skynet.blam.AbstractBlam;
 import org.eclipse.osee.framework.ui.skynet.blam.VariableMap;
 import org.eclipse.osee.framework.ui.skynet.results.XResultDataUI;
+import org.eclipse.osee.framework.ui.skynet.widgets.XCheckBox;
 import org.eclipse.osee.framework.ui.skynet.widgets.XListDropViewer;
 import org.eclipse.osee.framework.ui.skynet.widgets.XModifiedListener;
 import org.eclipse.osee.framework.ui.skynet.widgets.XWidget;
@@ -56,8 +59,11 @@ public class RealignActionableItemsBlam extends AbstractBlam {
    private final static String ACTIONABLE_ITEMS_FROM = "Actionable Item(s) From";
    private final static String ACTIONABLE_ITEM_TO = "Actionable Item To";
    private final static String PERSIST = "Persist? (Read Description before checking the box)";
+   private final static String DELETE = "Delete? (Read Description before checking the box)";
    private XActionableItemCombo xActionableItemCombo;
    private XListDropViewer xListDropViewer;
+   private XCheckBox xPersistCheckBox;
+   private XCheckBox xDeleteCheckBox;
 
    @Override
    public String getName() {
@@ -70,6 +76,7 @@ public class RealignActionableItemsBlam extends AbstractBlam {
       wb.andWidget(ACTIONABLE_ITEMS_FROM, "XListDropViewer").endWidget();
       wb.andWidget(ACTIONABLE_ITEM_TO, "XActionableItemCombo").endWidget();
       wb.andWidget(PERSIST, "XCheckBox").endWidget();
+      wb.andWidget(DELETE, "XCheckBox").endWidget();
       return wb.getItems();
    }
 
@@ -89,13 +96,14 @@ public class RealignActionableItemsBlam extends AbstractBlam {
       Collection<ArtifactToken> actionableItemsFrom = getActionableItemsFrom();
       IAtsActionableItem actionableItemTo = getSelectedActionableItemTo();
       boolean isPersistAllowed = variableMap.getBoolean(PERSIST);
+      boolean isDeleteAllowed = variableMap.getBoolean(DELETE);
 
-      if (actionableItemTo.isValid() && actionableItemTo.getTeamDefinition().isValid()) {
-         realignAiAndTdForActionsIfAllowed(actionableItemsFrom, actionableItemTo, isPersistAllowed);
+      if (actionableItemTo.isValid() && actionableItemTo.getTeamDefinition() != null) {
+         realignAiAndTdForActionsIfAllowed(actionableItemsFrom, actionableItemTo, isPersistAllowed, isDeleteAllowed);
       } else {
          StringBuilder err = new StringBuilder();
          err.append("Selected Actionable Item To: " + actionableItemTo.toString());
-         err.append(" Or Related Team Definition: " + actionableItemTo.getTeamDefinition().toString());
+         err.append(" Or Related Team Definition: " + actionableItemTo.getTeamDefinition());
          err.append(" NOT VALID");
          log(err.toString());
       }
@@ -103,40 +111,68 @@ public class RealignActionableItemsBlam extends AbstractBlam {
       monitor.done();
    }
 
-   private void realignAiAndTdForActionsIfAllowed(Collection<ArtifactToken> actionableItemsFrom, IAtsActionableItem actionableItemTo, boolean isPersistAllowed) {
+   private void realignAiAndTdForActionsIfAllowed(Collection<ArtifactToken> actionableItemsFrom, IAtsActionableItem actionableItemTo, boolean isPersistAllowed, boolean isDeleteAllowed) {
       IAtsChangeSet changes = atsApi.createChangeSet("Realign AI/TD References for actions");
       List<ArtifactToken> teamWfs = atsApi.getQueryService().getArtifactsFromAttributeValues(
          AtsAttributeTypes.ActionableItemReference, actionableItemsFrom, CoreBranches.COMMON);
+      List<Long> aiIdsFrom = actionableItemsFrom.stream().map(ArtifactToken::getId).collect(Collectors.toList());
 
       final XResultData resultData = new XResultData();
       resultData.addRaw(getName());
-      resultData.addRaw("\n Related actions counts: " + teamWfs.size());
+      resultData.addRaw("\n Related actions counts: " + teamWfs.size() + "\n");
       resultData.addRaw(AHTML.beginMultiColumnTable(0, 1));
       String[] titleRow = {"Action Title", "Id", "AI From", "AI To", "TD From", "TD To"};
       resultData.addRaw(AHTML.addHeaderRowMultiColumnTable(titleRow));
 
       for (ArtifactToken teamWf : teamWfs) {
-         String currentlyAssignedAI = new ActionableItemsColumn(atsApi).getColumnText((IAtsObject) teamWf);
+         new ActionableItemsColumn(atsApi);
+         Collection<IAtsActionableItem> currentlyAssignedAIs =
+            ActionableItemsColumn.getActionableItems((IAtsObject) teamWf);
          String currentlyAssignedTD = new TeamColumn(atsApi).getColumnText((IAtsObject) teamWf);
          String currentlyAssignedAtsId = new AtsIdColumn(atsApi).getColumnText((IAtsObject) teamWf);
 
-         resultData.addRaw(AHTML.addRowMultiColumnTable(teamWf.getName(), currentlyAssignedAtsId, currentlyAssignedAI,
-            actionableItemTo.getName(), currentlyAssignedTD, actionableItemTo.getTeamDefinition().getName()));
-         if (isPersistAllowed) {
-            changes.setSoleAttributeValue(teamWf, AtsAttributeTypes.ActionableItemReference, actionableItemTo);
-            changes.setSoleAttributeValue(teamWf, AtsAttributeTypes.TeamDefinitionReference,
-               actionableItemTo.getTeamDefinition());
+         resultData.addRaw(AHTML.addRowMultiColumnTable(teamWf.getName(), currentlyAssignedAtsId,
+            Collections.toString(", ", currentlyAssignedAIs), actionableItemTo.getName(), currentlyAssignedTD,
+            actionableItemTo.getTeamDefinition().getName()));
+
+         if (isPersistAllowed && !resultData.isErrors()) {
+            try {
+               List<Object> assignAIsTo = new ArrayList<Object>();
+               assignAIsTo.add(actionableItemTo);
+               for (IAtsActionableItem currentlyAssignedAI : currentlyAssignedAIs) {
+                  if (!aiIdsFrom.contains(currentlyAssignedAI.getId())) {
+                     assignAIsTo.add(currentlyAssignedAI);
+                  }
+               }
+               changes.setAttributeValues(teamWf, AtsAttributeTypes.ActionableItemReference, assignAIsTo);
+               if (atsApi.getAttributeResolver().getAttributeCount(teamWf,
+                  AtsAttributeTypes.TeamDefinitionReference) > 0) {
+                  changes.setSoleAttributeValue(teamWf, AtsAttributeTypes.TeamDefinitionReference,
+                     actionableItemTo.getTeamDefinition());
+               }
+            } catch (Exception ex) {
+               resultData.errorf(ex.toString());
+               resultData.errorf("%s - %s has unknown problem.\n", currentlyAssignedAtsId, teamWf.getName());
+            }
          }
       }
+
+      if (isDeleteAllowed && isPersistAllowed && !resultData.isErrors()) {
+         for (ArtifactToken actionableItemFrom : actionableItemsFrom) {
+            changes.deleteArtifact(actionableItemFrom);
+         }
+         changes.executeIfNeeded();
+      } else if (isPersistAllowed && !resultData.isErrors()) {
+         changes.executeIfNeeded();
+      }
+
       Displays.ensureInDisplayThread(new Runnable() {
          @Override
          public void run() {
             XResultDataUI.report(resultData, "Results for " + getName());
          }
       });
-      if (isPersistAllowed) {
-         changes.executeIfNeeded();
-      }
+
    }
 
    @Override
@@ -148,9 +184,12 @@ public class RealignActionableItemsBlam extends AbstractBlam {
          "Team Definition will get realign to the related Team Definition of the selected \"Actionable Item To\". ");
       description.append(
          "It will re-assign team workflows to the selected \"Actionable Item To\" and related Team Definition. ");
-      description.append("Note: Run BLAM without checking the persist checkbox first. It will generate report. ");
       description.append(
-         "If report data looks correct, then check the persist checkbox and run the BLAM to execute changes. ");
+         "\n Note: Run BLAM without checking the persist and delete checkboxes first. It will generate report. ");
+      description.append(
+         "If report data looks correct, then check the persist and delete checkboxes and run the BLAM to execute changes. ");
+      description.append(
+         "\n Warning: When delete checkbox is checked, it will delete \"Actionable Item(s) From\" from OSEE. ");
       return description.toString();
    }
 
@@ -161,6 +200,22 @@ public class RealignActionableItemsBlam extends AbstractBlam {
          xActionableItemCombo = (XActionableItemCombo) xWidget;
       } else if (xWidget.getLabel().equals(ACTIONABLE_ITEMS_FROM)) {
          xListDropViewer = (XListDropViewer) xWidget;
+      } else if (xWidget.getLabel().equals(PERSIST)) {
+         xPersistCheckBox = (XCheckBox) xWidget;
+         xPersistCheckBox.addXModifiedListener(new XModifiedListener() {
+            @Override
+            public void widgetModified(XWidget widget) {
+               if (xPersistCheckBox.isSelected()) {
+                  xDeleteCheckBox.setEditable(true);
+               } else {
+                  xDeleteCheckBox.setEditable(false);
+               }
+            }
+         });
+
+      } else if (xWidget.getLabel().equals(DELETE)) {
+         xDeleteCheckBox = (XCheckBox) xWidget;
+         xDeleteCheckBox.setEditable(false);
       }
    }
 
