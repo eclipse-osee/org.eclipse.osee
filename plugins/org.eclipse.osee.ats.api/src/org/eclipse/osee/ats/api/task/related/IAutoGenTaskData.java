db65ad20973c167911cf28cbaa1a0c8865767bd6
Ryan T. Baldwin <ryan.t.baldwin@boeing.com>
2023-01-05 07:53:57 -0600

    refactor[TW22372]: Add missing dist statements to Java files
    Change-Id: Iecb34454f5d39f4d9b7c12f1a94a3be457bf0d8b

--- plugins/org.eclipse.osee.ats.api/src/org/eclipse/osee/ats/api/task/related/IAutoGenTaskData.java
+++ plugins/org.eclipse.osee.ats.api/src/org/eclipse/osee/ats/api/task/related/IAutoGenTaskData.java
@@ -1,8 +1,15 @@
-/*
- * Created on Apr 9, 2020
+/*********************************************************************
+ * Copyright (c) 2023 Boeing
  *
- * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
- */
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
 package org.eclipse.osee.ats.api.task.related;
 
 import org.eclipse.osee.ats.api.config.WorkType;
--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/workflow/task/AtsLbaRpcrTaskRendererTest.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/workflow/task/AtsLbaRpcrTaskRendererTest.java
@@ -1,8 +1,15 @@
-/*
- * Created on 2013
+/*********************************************************************
+ * Copyright (c) 2023 Boeing
  *
- * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
- */
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
 package org.eclipse.osee.ats.ide.integration.tests.ats.workflow.task;
 
 import static org.junit.Assert.assertTrue;
