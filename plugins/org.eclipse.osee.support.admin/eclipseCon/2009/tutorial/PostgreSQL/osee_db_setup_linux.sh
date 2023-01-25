fa1bbaeba8d090cd278702a624136048cfd1f9ac
donald.g.dunne <donald.g.dunne@boeing.com>
2023-01-25 06:06:28 -0700

    feature[TW22399]: IP - Clean up org.eclipse.osee.support.admin
    Change-Id: Ide6a2dd96a75cd954fae84873cc4aa126a1d746c

--- plugins/org.eclipse.osee.support.admin/eclipseCon/2009/tutorial/PostgreSQL/osee_db_setup_linux.sh
+++
@@ -1,2 +0,0 @@
-cp pgpass.conf ~/.pgpass
-/opt/PostgreSQL/8.3/bin/psql -a -e -f osee.sql -U postgres
--- plugins/org.eclipse.osee.support.admin/eclipseCon/2009/tutorial/PostgreSQL/osee_db_setup_mac.sh
+++
@@ -1,2 +0,0 @@
-cp pgpass_mac.conf ~/.pgpass
-/Library/PostgreSQL/8.3/bin/psql -a -e -f ./osee.sql -U postgres
