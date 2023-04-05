b798e0570584df2a1c3e97cd9010a1632ec4c6b0
donald.g.dunne <donald.g.dunne@boeing.com>
2023-04-05 09:01:28 -0700

    feature[TW22047]: Cancel Safety Workflows and remove auto-create
    Change-Id: I79ec95115d6f30dda13a205853ed530a72f2f26e

--- plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/agile/AgileEndpointImpl.java
+++ plugins/org.eclipse.osee.ats.rest/src/org/eclipse/osee/ats/rest/internal/agile/AgileEndpointImpl.java
@@ -114,8 +114,6 @@ import org.eclipse.osee.framework.jdk.core.type.IResourceRegistry;
 import org.eclipse.osee.framework.jdk.core.type.OseeArgumentException;
 import org.eclipse.osee.framework.jdk.core.util.AHTML;
 import org.eclipse.osee.framework.jdk.core.util.Conditions;
-import org.eclipse.osee.framework.jdk.core.util.ElapsedTime;
-import org.eclipse.osee.framework.jdk.core.util.ElapsedTime.Units;
 import org.eclipse.osee.framework.jdk.core.util.GUID;
 import org.eclipse.osee.framework.jdk.core.util.Lib;
 import org.eclipse.osee.framework.jdk.core.util.NamedComparator;
@@ -974,7 +972,6 @@ public class AgileEndpointImpl implements AgileEndpointApi {
 
    @Override
    public List<AgileItem> getBacklogItems(long teamId) {
-      ElapsedTime time = new ElapsedTime("getBacklogItems");
       List<AgileItem> items = new LinkedList<>();
       IAgileTeam team = atsApi.getAgileService().getAgileTeam(teamId);
       IAgileBacklog backlog = atsApi.getAgileService().getAgileBacklog(team);
@@ -994,7 +991,6 @@ public class AgileEndpointImpl implements AgileEndpointApi {
             items.add(item);
          }
       }
-      time.end(Units.SEC);
       return items;
    }
 
