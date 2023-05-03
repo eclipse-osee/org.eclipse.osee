206c2434519a4df0b9b141c88b37d7656fac64fc
donald.g.dunne <donald.g.dunne@boeing.com>
2023-05-02 19:19:15 -0700

    bug[TW22543]: Remove injected atsApi from AtsTaskProviders
    Change-Id: Ibaf6410270159445a7d24a658dfad0ed5fda50f6

--- plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/task/AbstractAtsTaskProvider.java
+++ plugins/org.eclipse.osee.ats.core/src/org/eclipse/osee/ats/core/task/AbstractAtsTaskProvider.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.osee.ats.core.task;
 
-import org.eclipse.osee.ats.api.AtsApi;
 import org.eclipse.osee.ats.api.data.AtsAttributeTypes;
 import org.eclipse.osee.ats.api.task.IAtsTaskProvider;
 import org.eclipse.osee.ats.api.task.JaxAtsTask;
@@ -18,25 +17,22 @@ import org.eclipse.osee.ats.api.task.create.ChangeReportTaskData;
 import org.eclipse.osee.ats.api.task.create.ChangeReportTaskMatch;
 import org.eclipse.osee.ats.api.task.create.ChangeReportTaskTeamWfData;
 import org.eclipse.osee.ats.api.workflow.IAtsTask;
+import org.eclipse.osee.ats.core.internal.AtsApiService;
 
 /**
  * @author Donald G. Dunne
  */
 public abstract class AbstractAtsTaskProvider implements IAtsTaskProvider {
 
-   protected AtsApi atsApi;
-
-   public void setAtsApi(AtsApi atsApi) {
-      this.atsApi = atsApi;
-   }
-
    @Override
    public boolean isAutoGen(IAtsTask task) {
-      return atsApi.getAttributeResolver().getSoleAttributeValue(task, AtsAttributeTypes.TaskAutoGen, false);
+      return AtsApiService.get().getAttributeResolver().getSoleAttributeValue(task, AtsAttributeTypes.TaskAutoGen,
+         false);
    }
 
    @Override
-   public void addToAutoGeneratingTask(ChangeReportTaskData crtd, ChangeReportTaskTeamWfData crttwd, ChangeReportTaskMatch taskMatch, JaxAtsTask task) {
+   public void addToAutoGeneratingTask(ChangeReportTaskData crtd, ChangeReportTaskTeamWfData crttwd,
+      ChangeReportTaskMatch taskMatch, JaxAtsTask task) {
       // do nothing
    }
 
