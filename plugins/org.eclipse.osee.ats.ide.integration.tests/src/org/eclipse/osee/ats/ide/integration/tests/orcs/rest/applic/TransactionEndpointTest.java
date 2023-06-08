f90ccf751f549d259eeda4e9db707dfdfcd7fa67
Torin Grenda <torin.n.grenda.civ@us.navy.mil>
2023-06-08 08:14:45 -0700

    feature[TW23021]: Add testing for attribute transfer
    Change-Id: If14b80e81aeaa0dfd57fb9fda24ddac76ce83879

--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/orcs/rest/applic/TransactionEndpointTest.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/orcs/rest/applic/TransactionEndpointTest.java
@@ -12,13 +12,15 @@
  **********************************************************************/
 package org.eclipse.osee.ats.ide.integration.tests.orcs.rest.applic;
 
-import static org.eclipse.osee.framework.core.enums.CoreArtifactTokens.DefaultHierarchyRoot;
 import static org.eclipse.osee.framework.core.enums.CoreArtifactTypes.Component;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
@@ -27,11 +29,16 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status.Family;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.osee.ats.ide.integration.tests.skynet.core.utils.Asserts;
 import org.eclipse.osee.ats.ide.util.ServiceUtil;
 import org.eclipse.osee.framework.core.JaxRsApi;
 import org.eclipse.osee.framework.core.data.ArtifactId;
-import org.eclipse.osee.framework.core.data.ArtifactToken;
+import org.eclipse.osee.framework.core.data.ArtifactTypeToken;
+import org.eclipse.osee.framework.core.data.AttributeTypeId;
+import org.eclipse.osee.framework.core.data.AttributeTypeToken;
 import org.eclipse.osee.framework.core.data.BranchId;
+import org.eclipse.osee.framework.core.data.TransactionId;
 import org.eclipse.osee.framework.core.data.TransactionToken;
 import org.eclipse.osee.framework.core.enums.CoreArtifactTokens;
 import org.eclipse.osee.framework.core.enums.CoreArtifactTypes;
@@ -39,11 +46,17 @@ import org.eclipse.osee.framework.core.enums.CoreAttributeTypes;
 import org.eclipse.osee.framework.core.enums.DemoBranches;
 import org.eclipse.osee.framework.core.model.change.ChangeItem;
 import org.eclipse.osee.framework.core.model.change.ChangeType;
+import org.eclipse.osee.framework.core.operation.IOperation;
 import org.eclipse.osee.framework.core.util.OseeInf;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.skynet.core.artifact.Artifact;
+import org.eclipse.osee.framework.skynet.core.artifact.ArtifactTypeManager;
+import org.eclipse.osee.framework.skynet.core.artifact.Attribute;
 import org.eclipse.osee.framework.skynet.core.artifact.search.ArtifactQuery;
+import org.eclipse.osee.framework.skynet.core.transaction.SkynetTransaction;
 import org.eclipse.osee.framework.skynet.core.transaction.TransactionManager;
-import org.eclipse.osee.orcs.rest.model.ArtifactEndpoint;
+import org.eclipse.osee.framework.skynet.core.utility.PurgeTransactionOperation;
 import org.eclipse.osee.orcs.rest.model.TransactionEndpoint;
 import org.eclipse.osee.orcs.rest.model.transaction.TransactionBuilderData;
 import org.junit.BeforeClass;
@@ -55,13 +68,11 @@ import org.junit.Test;
 public class TransactionEndpointTest {
 
    private static TransactionEndpoint transactionEndpoint;
-   private static ArtifactEndpoint artifactEndpoint;
    private static JaxRsApi jaxRsApi;
 
    @BeforeClass
    public static void testSetup() {
       transactionEndpoint = ServiceUtil.getOseeClient().getTransactionEndpoint();
-      artifactEndpoint = ServiceUtil.getOseeClient().getArtifactEndpoint(DemoBranches.SAW_PL);
       jaxRsApi = ServiceUtil.getOseeClient().jaxRsApi();
    }
 
@@ -117,177 +128,123 @@ public class TransactionEndpointTest {
 
    @Test
    public void testAddDateAttribute() {
-      //Create a new Date/Time Attribute on a new Artifact
-      ArtifactId parentArtifact = DefaultHierarchyRoot;
-      String name = getClass().getSimpleName() + " Date";
-      String currentTime = Long.valueOf(new Date().getTime()).toString();
-      ArtifactToken artifactToken =
-         artifactEndpoint.createArtifact(DemoBranches.SAW_PL, CoreArtifactTypes.GitCommit, parentArtifact, name);
-      artifactEndpoint.setSoleAttributeValue(DemoBranches.SAW_PL, artifactToken, CoreAttributeTypes.GitCommitAuthorDate,
-         currentTime);
-
-      //Get all changes done to the artifact and find the latest tx and the tx before the artifact was created
-      List<ChangeItem> changes = transactionEndpoint.getArtifactHistory(artifactToken, DemoBranches.SAW_PL);
-      if (!changes.isEmpty()) {
-         TransactionToken startingTx =
-            TransactionManager.getPriorTransaction(changes.get(0).getCurrentVersion().getTransactionToken());
-         TransactionToken currentTx = getMaxTransaction(changes);
-         TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
-         txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-            //Write out the diff in create_tx.json format and read/import it in on a different branch
-            String json = mapper.writeValueAsString(txData);
-            Response response =
-               jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
-            assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-         } catch (JsonProcessingException ex) {
-            throw new OseeCoreException("Failed to write txData as json in testAddDateAttribute");
-         }
-      } else {
-         throw new OseeCoreException("Test transfer date attribute failed getting changes");
-      }
+      Date currentDate = new Date();
+      String json = setupTransferJson(CoreArtifactTypes.GitCommit,
+         AttributeTypeId.valueOf(CoreAttributeTypes.GitCommitAuthorDate.getId()), currentDate);
+      testWrapUp(json, CoreAttributeTypes.GitCommitAuthorDate, currentDate);
    }
 
    @Test
    public void testAddBooleanAttribute() {
-      //Create a new Boolean Attribute on a new Artifact
-      ArtifactId parentArtifact = DefaultHierarchyRoot;
-      String name = getClass().getSimpleName() + " Boolean";
-      ArtifactToken artifactToken = artifactEndpoint.createArtifact(DemoBranches.SAW_PL,
-         CoreArtifactTypes.OseeTypeDefinition, parentArtifact, name);
-      artifactEndpoint.setSoleAttributeValue(DemoBranches.SAW_PL, artifactToken, CoreAttributeTypes.Active,
-         Boolean.TRUE.toString());
-
-      //Get all changes done to the artifact and find the latest tx and the tx before the artifact was created
-      List<ChangeItem> changes = transactionEndpoint.getArtifactHistory(artifactToken, DemoBranches.SAW_PL);
-      if (!changes.isEmpty()) {
-         TransactionToken startingTx =
-            TransactionManager.getPriorTransaction(changes.get(0).getCurrentVersion().getTransactionToken());
-         TransactionToken currentTx = getMaxTransaction(changes);
-         TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
-         txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-            //Write out the diff in create_tx.json format and read/import it in on a different branch
-            String json = mapper.writeValueAsString(txData);
-            Response response =
-               jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
-            assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-         } catch (JsonProcessingException ex) {
-            throw new OseeCoreException("Failed to write txData as json in testAddBooleanAttribute");
-         }
-      } else {
-         throw new OseeCoreException("Test transfer boolean attribute failed getting changes");
-      }
+      boolean testBoolean = true;
+      String json = setupTransferJson(CoreArtifactTypes.OseeTypeDefinition,
+         AttributeTypeId.valueOf(CoreAttributeTypes.Active.getId()), testBoolean);
+      testWrapUp(json, CoreAttributeTypes.Active, testBoolean);
    }
 
    @Test
    public void testAddStringAttribute() {
-      //Create a new String Attribute on a new Artifact
-      ArtifactId parentArtifact = DefaultHierarchyRoot;
-      String name = getClass().getSimpleName() + " String";
-      ArtifactToken artifactToken =
-         artifactEndpoint.createArtifact(DemoBranches.SAW_PL, CoreArtifactTypes.Artifact, parentArtifact, name);
-      artifactEndpoint.setSoleAttributeValue(DemoBranches.SAW_PL, artifactToken, CoreAttributeTypes.Annotation,
-         "Test Add String Attribute");
-
-      //Get all changes done to the artifact and find the latest tx and the tx before the artifact was created
-      List<ChangeItem> changes = transactionEndpoint.getArtifactHistory(artifactToken, DemoBranches.SAW_PL);
-      if (!changes.isEmpty()) {
-         TransactionToken startingTx =
-            TransactionManager.getPriorTransaction(changes.get(0).getCurrentVersion().getTransactionToken());
-         TransactionToken currentTx = getMaxTransaction(changes);
-         TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
-         txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-            //Write out the diff in create_tx.json format and read/import it in on a different branch
-            String json = mapper.writeValueAsString(txData);
-            Response response =
-               jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
-            assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-         } catch (JsonProcessingException ex) {
-            throw new OseeCoreException("Failed to write txData as json in testAddStringAttribute");
-         }
-      } else {
-         throw new OseeCoreException("Test transfer string attribute failed getting changes");
-      }
+      String testString = "Test Add String Attribute";
+      String json = setupTransferJson(CoreArtifactTypes.Artifact,
+         AttributeTypeId.valueOf(CoreAttributeTypes.Annotation.getId()), testString);
+      testWrapUp(json, CoreAttributeTypes.Annotation, testString);
    }
 
    @Test
    public void testAddIntegerAttribute() {
-      //Create a new Integer Attribute on a new Artifact
-      ArtifactId parentArtifact = DefaultHierarchyRoot;
-      String name = getClass().getSimpleName() + " Integer";
-      ArtifactToken artifactToken =
-         artifactEndpoint.createArtifact(DemoBranches.SAW_PL, CoreArtifactTypes.Breaker, parentArtifact, name);
-      artifactEndpoint.setSoleAttributeValue(DemoBranches.SAW_PL, artifactToken, CoreAttributeTypes.CircuitBreakerId,
-         Integer.valueOf(10).toString());
-
-      //Get all changes done to the artifact and find the latest tx and the tx before the artifact was created
-      List<ChangeItem> changes = transactionEndpoint.getArtifactHistory(artifactToken, DemoBranches.SAW_PL);
-      if (!changes.isEmpty()) {
-         TransactionToken startingTx =
-            TransactionManager.getPriorTransaction(changes.get(0).getCurrentVersion().getTransactionToken());
-         TransactionToken currentTx = getMaxTransaction(changes);
-         TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
-         txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-            //Write out the diff in create_tx.json format and read/import it in on a different branch
-            String json = mapper.writeValueAsString(txData);
-            Response response =
-               jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
-            assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-         } catch (JsonProcessingException ex) {
-            throw new OseeCoreException("Failed to write txData as json in testAddIntegerAttribute");
-         }
-      } else {
-         throw new OseeCoreException("Test transfer integer attribute failed getting changes");
-      }
+      int testInt = 10;
+      String json = setupTransferJson(CoreArtifactTypes.Breaker,
+         AttributeTypeId.valueOf(CoreAttributeTypes.CircuitBreakerId.getId()), testInt);
+      testWrapUp(json, CoreAttributeTypes.CircuitBreakerId, testInt);
    }
 
    @Test
    public void testAddArtifactIdAttribute() {
-      //Create a new ArtifactId Attribute on a new Artifact
-      ArtifactId parentArtifact = DefaultHierarchyRoot;
-      String name = getClass().getSimpleName() + " ArtifactId";
-      ArtifactToken artifactToken = artifactEndpoint.createArtifact(DemoBranches.SAW_PL,
-         CoreArtifactTypes.CertificationBaselineEvent, parentArtifact, name);
-      artifactEndpoint.setSoleAttributeValue(DemoBranches.SAW_PL, artifactToken, CoreAttributeTypes.BaselinedBy,
-         ArtifactId.valueOf(1234321L).getIdString());
+      ArtifactId testArtId = ArtifactId.valueOf(1234321L);
+      String json = setupTransferJson(CoreArtifactTypes.CertificationBaselineEvent,
+         AttributeTypeId.valueOf(CoreAttributeTypes.BaselinedBy.getId()), ArtifactId.valueOf(testArtId));
+      testWrapUp(json, CoreAttributeTypes.BaselinedBy, testArtId);
+   }
 
-      //Get all changes done to the artifact and find the latest tx and the tx before the artifact was created
-      List<ChangeItem> changes = transactionEndpoint.getArtifactHistory(artifactToken, DemoBranches.SAW_PL);
-      if (!changes.isEmpty()) {
-         TransactionToken startingTx =
-            TransactionManager.getPriorTransaction(changes.get(0).getCurrentVersion().getTransactionToken());
-         TransactionToken currentTx = getMaxTransaction(changes);
-         TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
-         txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
-         ObjectMapper mapper = new ObjectMapper();
-         try {
-            //Write out the diff in create_tx.json format and read/import it in on a different branch
-            String json = mapper.writeValueAsString(txData);
-            Response response =
-               jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
-            assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
-         } catch (JsonProcessingException ex) {
-            throw new OseeCoreException("Failed to write txData as json in testAddArtifactIdAttribute");
-         }
-      } else {
-         throw new OseeCoreException("Test transfer ArtifactId attribute failed getting changes");
+  //Still need to add ability to handle binary data
+  /**
+   @Test
+   public void testAddBinaryDataAttribute() {
+      String json;
+      try {
+         InputStream testBinaryData = Lib.stringToInputStream("Test Binary Data Attribute");
+         json = setupTransferJson(CoreArtifactTypes.ImageArtifact,
+            AttributeTypeId.valueOf(CoreAttributeTypes.ImageContent.getId()), testBinaryData);
+      } catch (UnsupportedEncodingException ex) {
+         throw new OseeCoreException("Failed to add attribute during testAddBinaryDataAttribute");
+      }
+      Response response = jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
+      assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
+   }
+   */
+
+   /**
+    * Helper method that will create a new Artifact with a single added attribute and return the Transfer Json created
+    * from the exportTxsDiff
+    */
+   private String setupTransferJson(ArtifactTypeToken artType, AttributeTypeId attrType, Object value) {
+      //Create a new Artifact of type artType on SAWL_PL_Working_Branch
+      SkynetTransaction transaction = TransactionManager.createTransaction(DemoBranches.SAW_PL_Working_Branch,
+         TransactionEndpointTest.class.getName() + attrType.getIdString());
+      Artifact artifact = ArtifactTypeManager.addArtifact(artType, DemoBranches.SAW_PL_Working_Branch);
+      //Check if the artifact already has an attribute of the supplied type, then create a new Attribute on the Artifact of type attrType with supplied value
+      if (!artifact.getAttributes(attrType).isEmpty()) {
+         artifact.deleteAttributes(attrType);
+      }
+      artifact.addAttribute(attrType, value);
+      //Commit transaction and return the newly created TransactionToken
+      transaction.addArtifact(artifact);
+      TransactionToken currentTx = transaction.execute();
+      TransactionToken startingTx = TransactionManager.getPriorTransaction(currentTx);
+      TransactionBuilderData txData = transactionEndpoint.exportTxsDiff(startingTx, currentTx);
+      //Switch branches in TransactionBuilderData so the transfer will be onto a different branch
+      txData.setBranch(DemoBranches.SAW_PL_Hardening_Branch.getIdString());
+      ObjectMapper mapper = new ObjectMapper();
+      String json;
+      try {
+         json = mapper.writeValueAsString(txData);
+      } catch (JsonProcessingException ex) {
+         throw new OseeCoreException("Failed to write txData as json in TransactionEndpointTest");
+      }
+      try {
+         purge(currentTx);
+      } catch (Exception ex) {
+         throw new OseeCoreException("Failed to purge Transaction:" + currentTx.getIdString());
+      }
+      return json;
+   }
+
+   /**
+    * Helper method that tests to ensure the new attribute is on the artifact from the created transaction and then
+    * cleans up the newly added transaction
+    */
+   private void testWrapUp(String json, AttributeTypeToken attrType, Object expectedValue) {
+      Response response = jaxRsApi.newTarget("orcs/txs").request(MediaType.APPLICATION_JSON).post(Entity.json(json));
+      assertEquals(Family.SUCCESSFUL, response.getStatusInfo().getFamily());
+      try {
+         //Pull all the important bits out of the readTree
+         JsonNode readTree = jaxRsApi.readTree(Lib.inputStreamToString((InputStream) response.getEntity()));
+         ArtifactId artId = ArtifactId.valueOf(readTree.get("results").get("ids").get(0).asLong());
+         BranchId branchId = BranchId.valueOf(readTree.get("tx").get("branchId").asLong());
+         TransactionId txId = TransactionId.valueOf(readTree.get("tx").get("id").asLong());
+         //Find the artifact with the new Attribute and ensure the expectedValue is on the artifact
+         Artifact art = ArtifactQuery.getArtifactFromId(artId, branchId);
+         List<Attribute<Object>> attributes = art.getAttributesByValue(attrType, expectedValue);
+         assertTrue(!attributes.isEmpty());
+         //Cleanup and purge the Transaction on the new branch
+         purge(TransactionToken.valueOf(txId, branchId));
+      } catch (Exception ex) {
+         throw new OseeCoreException("Failed to convert response to String");
       }
    }
 
-   private TransactionToken getMaxTransaction(List<ChangeItem> changes) {
-      TransactionToken maxTx = changes.get(0).getCurrentVersion().getTransactionToken();
-      for (ChangeItem change : changes) {
-         if (change.getCurrentVersion().getTransactionToken().isGreaterThan(maxTx)) {
-            maxTx = change.getCurrentVersion().getTransactionToken();
-         }
-      }
-      return maxTx;
+   private void purge(TransactionToken transactionId) throws Exception {
+      IOperation operation = PurgeTransactionOperation.getPurgeTransactionOperation(transactionId);
+      Asserts.assertOperation(operation, IStatus.OK);
    }
 }
