3f8da5d3f256759c2c7838b31a0a6ed3ec066b20
Baily Roberts <Baily.Roberts@boeing.com>
2023-07-25 10:56:17 -0700

    feature[TW22988]: Launch demo server through bazel
    Change-Id: I7559ee66be8ab77dd1e6c0f16ee34cffed8def6c
    Signed-off-by: Baily Roberts <Baily.Roberts@boeing.com>

---
+++ plugins/org.eclipse.osee.server.p2/runDemo.bat
@@ -0,0 +1,9 @@
+java -server ^
+-Xmx3G ^
+-Dorg.osgi.service.http.port=8089 ^
+-Dlogback.configurationFile=logback-dev.xml ^
+-Dorg.eclipse.equinox.http.jetty.context.sessioninactiveinterval=3600 ^
+-Dcm.config.uri="etc/osee.hsql.json" ^
+-Dosee.authentication.protocol=demo ^
+-Dosee.application.server.data="demo/binary_data" ^
+-jar plugins/org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar -console -consoleLog
