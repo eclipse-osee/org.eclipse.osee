f9961dc0dbd778703f725ef7b5eb6aee511922b0
David W. Miller <david.w.miller6@boeing.com>
2023-08-14 14:49:26 -0700

    refinement[TW23627]: Refactor transfer file to simplify calling on server
    Change-Id: I7d80c6ae843cb5dabd6dec9d8cc4ff01fb8cd4ba

--- plugins/org.eclipse.osee.orcs.rest.model/src/org/eclipse/osee/orcs/rest/model/transaction/TransactionBuilderDataFactory.java
+++ plugins/org.eclipse.osee.orcs.rest.model/src/org/eclipse/osee/orcs/rest/model/transaction/TransactionBuilderDataFactory.java
@@ -12,35 +12,52 @@
  **********************************************************************/
 package org.eclipse.osee.orcs.rest.model.transaction;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import org.eclipse.osee.framework.core.OrcsTokenService;
+import org.eclipse.osee.framework.core.data.ApplicabilityId;
 import org.eclipse.osee.framework.core.data.ArtifactId;
 import org.eclipse.osee.framework.core.data.ArtifactReadable;
+import org.eclipse.osee.framework.core.data.ArtifactToken;
+import org.eclipse.osee.framework.core.data.ArtifactTypeToken;
 import org.eclipse.osee.framework.core.data.AttributeTypeGeneric;
 import org.eclipse.osee.framework.core.data.BranchId;
+import org.eclipse.osee.framework.core.data.GammaId;
+import org.eclipse.osee.framework.core.data.RelationTypeToken;
 import org.eclipse.osee.framework.core.data.TransactionId;
 import org.eclipse.osee.framework.core.data.TransactionToken;
 import org.eclipse.osee.framework.core.enums.ModificationType;
+import org.eclipse.osee.framework.core.enums.RelationSide;
 import org.eclipse.osee.framework.core.model.change.ChangeItem;
 import org.eclipse.osee.framework.core.model.change.ChangeType;
 import org.eclipse.osee.framework.core.model.change.ChangeVersion;
 import org.eclipse.osee.framework.jdk.core.result.XResultData;
+import org.eclipse.osee.framework.jdk.core.type.NamedId;
+import org.eclipse.osee.framework.jdk.core.type.OseeArgumentException;
 import org.eclipse.osee.framework.jdk.core.type.OseeCoreException;
+import org.eclipse.osee.framework.jdk.core.type.OseeStateException;
 import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.jdk.core.util.Strings;
 import org.eclipse.osee.orcs.OrcsApi;
+import org.eclipse.osee.orcs.transaction.TransactionBuilder;
 import org.eclipse.osee.orcs.data.TransactionReadable;
 
 public class TransactionBuilderDataFactory {
    private final OrcsApi orcsApi;
+   private final OrcsTokenService tokenService;
    private BranchId currentBranch;
    private TransactionId currentTransaction;
    private final HashMap<ArtifactId, ArtifactSortContainer> workingArtsById = new HashMap<>();
@@ -51,6 +68,7 @@ public class TransactionBuilderDataFactory {
 
    public TransactionBuilderDataFactory(OrcsApi orcsApi) {
       this.orcsApi = orcsApi;
+      this.tokenService = orcsApi.tokenService();
    }
 
    public TransactionBuilderData loadFromChanges(TransactionId txId1, TransactionId txId2) {
@@ -100,6 +118,29 @@ public class TransactionBuilderDataFactory {
          txId1.getIdString(), txId2.getIdString()));
       }
       return tbd;
+   }
+
+   public TransactionBuilder loadFromJson(String json) {
+      if (Objects.isNull(json)) {
+         throw new OseeArgumentException("Invalid String in loadFromJson");
+      }
+      JsonNode readTree = orcsApi.jaxRsApi().readTree(json);
+      BranchId branch = BranchId.valueOf(readTree.get("branch").asLong());
+      Map<String, ArtifactToken> artifactsByName = new HashMap<>();
+      Map<String, ArtifactToken> artifactsByKeys = new HashMap<>();
+
+      String txComment = readTree.get("txComment").asText();
+      if (Strings.isInValid(txComment)) {
+         txComment = "create transaction REST call";
+      }
+      TransactionBuilder tx = orcsApi.getTransactionFactory().createTransaction(branch, txComment);
+      createArtifacts(readTree, artifactsByName, artifactsByKeys, tx);
+      modifyArtifacts(readTree, artifactsByName, tx);
+      deleteArtifacts(readTree, tx);
+      deleteRelations(readTree, tx);
+      addRelations(readTree, artifactsByKeys, tx);
+
+      return tx;
    }
 
    private boolean isGoodChange(ChangeItem change) {
@@ -427,5 +468,323 @@ public class TransactionBuilderDataFactory {
          results.errorf("tuple changes not handled for change: %s", change);
       }
       return tbd;
+   }
+
+   // transferred from TransactionBuilderMessageReader to here to simplify api calls
+   private void createArtifacts(JsonNode root, Map<String, ArtifactToken> artifactsByName,
+      Map<String, ArtifactToken> artifactsByKeys, TransactionBuilder tx) {
+      if (root.has("createArtifacts")) {
+         for (JsonNode artifactJson : root.get("createArtifacts")) {
+            ApplicabilityId appId;
+            ArtifactId artId;
+            ArtifactToken artifact;
+            if (artifactJson.has("applicabilityId")) {
+               appId = ApplicabilityId.valueOf(artifactJson.get("applicabilityId").asLong());
+            } else {
+               appId = ApplicabilityId.BASE;
+            }
+            ArtifactTypeToken artifactType = getArtifactType(artifactJson);
+            if (artifactJson.has("id")) {
+               artId = ArtifactId.valueOf(artifactJson.get("id").asLong());
+               artifact = tx.createArtifact(artifactType, artifactJson.get("name").asText(), artId, appId);
+            } else {
+               artifact = tx.createArtifact(artifactType, artifactJson.get("name").asText(), appId);
+            }
+            artifactsByName.put(artifact.getName(), artifact);
+
+            if (artifactJson.has("key")) {
+               artifactsByKeys.put(artifactJson.get("key").asText(), artifact);
+            }
+
+            readAttributes(tx, artifactJson, artifact, "attributes");
+            readrelations(tx, artifactsByName, artifactsByKeys, artifactJson, artifact);
+         }
+      }
+   }
+
+   private void modifyArtifacts(JsonNode root, Map<String, ArtifactToken> artifactsByName, TransactionBuilder tx) {
+      if (root.has("modifyArtifacts")) {
+         for (JsonNode artifactJson : root.get("modifyArtifacts")) {
+            ArtifactToken artifact = orcsApi.getQueryFactory().fromBranch(tx.getBranch()).andUuid(
+               artifactJson.get("id").asLong()).asArtifactToken();
+            artifactsByName.put(artifact.getName(), artifact);
+
+            if (artifactJson.has("applicabilityId")) {
+               tx.setApplicability(artifact, ApplicabilityId.valueOf(artifactJson.get("applicabilityId").asLong()));
+            }
+
+            readAttributes(tx, artifactJson, artifact, "setAttributes");
+            addAttributes(tx, artifactJson, artifact, "addAttributes");
+            deleteAttributes(tx, artifactJson, artifact, "deleteAttributes");
+         }
+      }
+   }
+
+   private void deleteArtifacts(JsonNode root, TransactionBuilder tx) {
+      if (root.has("deleteArtifacts")) {
+         for (JsonNode artifactId : root.get("deleteArtifacts")) {
+            tx.deleteArtifact(ArtifactId.valueOf(artifactId.asLong()));
+         }
+      }
+   }
+
+   private void deleteRelations(JsonNode root, TransactionBuilder tx) {
+      if (root.has("deleteRelations")) {
+         for (JsonNode relation : root.get("deleteRelations")) {
+            RelationTypeToken relationType = getRelationType(relation);
+            ArtifactId artA = ArtifactId.valueOf(relation.get("aArtId").asLong());
+            ArtifactId artB = ArtifactId.valueOf(relation.get("bArtId").asLong());
+            if (artA.isValid() && artB.isValid()) {
+               if (relation.has("gamma")) {
+                  tx.unrelate(artA, relationType, artB, GammaId.valueOf(relation.get("gamma").asText()));
+               } else {
+                  tx.unrelate(artA, relationType, artB);
+               }
+            }
+         }
+      }
+      if (root.has("deleteInvalidRelations")) {
+         for (JsonNode relation : root.get("deleteInvalidRelations")) {
+            ArtifactId artA = ArtifactId.valueOf(relation.get("validArt").asLong());
+            ArtifactId artB = ArtifactId.valueOf(relation.get("invalidArt").asLong());
+            if (artA.isValid() && artB.isValid()) {
+               tx.unrelateFromInvalidArtifact(artA, artB);
+            }
+         }
+      }
+   }
+
+   private void addRelations(JsonNode root, Map<String, ArtifactToken> artifactsByKeys, TransactionBuilder tx) {
+      if (root.has("addRelations")) {
+         for (JsonNode relation : root.get("addRelations")) {
+            RelationTypeToken relationType = getRelationType(relation);
+
+            String rationale = relation.has("rationale") ? relation.get("rationale").asText() : "";
+            ArtifactId relatedArtifact = relation.has("relatedArtifact") ? ArtifactId.valueOf(
+               relation.get("relatedArtifact").asLong()) : ArtifactId.SENTINEL;
+            String afterArtifact = relation.has("afterArtifact") ? relation.get("afterArtifact").asText() : "end";
+
+            ArtifactId artA;
+            ArtifactId artB;
+            if (artifactsByKeys.containsKey(relation.get("aArtId").asText())) {
+               artA = ArtifactId.valueOf(artifactsByKeys.get(relation.get("aArtId").asText()).getId());
+            } else {
+               artA = ArtifactId.valueOf(relation.get("aArtId").asLong());
+            }
+            if (artifactsByKeys.containsKey(relation.get("bArtId").asText())) {
+               artB = ArtifactId.valueOf(artifactsByKeys.get(relation.get("bArtId").asText()).getId());
+            } else {
+               artB = ArtifactId.valueOf(relation.get("bArtId").asLong());
+            }
+
+            boolean hasGamma = relation.has("gamma");
+            if (relationType.isNewRelationTable() && !hasGamma) {
+               tx.relate(artA, relationType, artB, relatedArtifact, afterArtifact);
+            } else if (!hasGamma) {
+               tx.relate(artA, relationType, artB, rationale);
+            } else if (relationType.isNewRelationTable()) {
+               tx.relate(artA, relationType, artB, relatedArtifact, afterArtifact,
+                  GammaId.valueOf(relation.get("gamma").asText()));
+            } else {
+               tx.relate(artA, relationType, artB, rationale, GammaId.valueOf(relation.get("gamma").asText()));
+            }
+         }
+      }
+
+   }
+
+   private <R extends NamedId> R getToken(JsonNode node, Function<Long, R> getById, Function<String, R> getByName) {
+      JsonNode id = node.get("typeId");
+      if (id == null) {
+         JsonNode typeNode = node.get("typeName");
+         if (typeNode == null || Strings.isInValid(typeNode.asText())) {
+            throw new OseeArgumentException("The type must be specified");
+         }
+         return getByName.apply(typeNode.asText());
+      }
+      return getById.apply(id.asLong());
+   }
+
+   private void readAttributes(TransactionBuilder tx, JsonNode artifactJson, ArtifactToken artifact,
+      String attributesNodeName) {
+      if (artifactJson.has(attributesNodeName)) {
+         for (JsonNode attribute : artifactJson.get(attributesNodeName)) {
+            AttributeTypeGeneric<?> attributeType = getAttributeType(attribute);
+            JsonNode value = attribute.get("value");
+            boolean hasGamma = attribute.has("gamma");
+            if (value.isArray()) {
+               ArrayList<String> values = new ArrayList<>();
+               ArrayList<InputStream> streams = new ArrayList<>();
+               ArrayList<GammaId> gammas = new ArrayList<>();
+               for (JsonNode attrValue : value) {
+                  if (attributeType.isInputStream()) {
+                     Decoder decoder = Base64.getDecoder();
+                     ByteArrayInputStream bais = new ByteArrayInputStream(decoder.decode(attrValue.asText()));
+                     streams.add(bais);
+                  } else {
+                     values.add(attrValue.asText());
+                  }
+               }
+               if (hasGamma) {
+                  JsonNode gamma = attribute.get("gamma");
+                  if (gamma.isArray()) {
+                     //if it isn't an array we probably don't want to try to process the gammas
+                     for (JsonNode gammaValue : gamma) {
+                        gammas.add(GammaId.valueOf(gammaValue.asText()));
+                     }
+                  }
+
+               }
+               if (!values.isEmpty() && !hasGamma) {
+                  tx.setAttributesFromStrings(artifact, attributeType, values);
+               }
+               if (!streams.isEmpty() && !hasGamma) {
+                  tx.setAttributesFromValues(artifact, attributeType, streams);
+               }
+               if (!values.isEmpty() && hasGamma && !gammas.isEmpty()) {
+                  tx.setAttributesFromStrings(artifact, attributeType, values, gammas);
+               }
+               if (!streams.isEmpty() && hasGamma && !gammas.isEmpty()) {
+                  tx.setAttributesFromValues(artifact, attributeType, streams, gammas);
+               }
+            } else if (hasGamma) {
+               JsonNode gamma = attribute.get("gamma");
+               tx.setSoleAttributeFromString(artifact, attributeType, value.asText(), GammaId.valueOf(gamma.asText()));
+            } else {
+               tx.setSoleAttributeFromString(artifact, attributeType, value.asText());
+            }
+         }
+      }
+   }
+
+   private void addAttributes(TransactionBuilder tx, JsonNode artifactJson, ArtifactToken artifact,
+      String attributesNodeName) {
+      if (artifactJson.has(attributesNodeName)) {
+         for (JsonNode attribute : artifactJson.get(attributesNodeName)) {
+            AttributeTypeGeneric<?> attributeType = getAttributeType(attribute);
+            JsonNode value = attribute.get("value");
+            if (value.isArray()) {
+               for (JsonNode attrValue : value) {
+                  tx.createAttribute(artifact, attributeType, attrValue);
+               }
+            } else {
+               tx.createAttribute(artifact, attributeType, value.asText());
+            }
+         }
+      }
+   }
+
+   private void deleteAttributes(TransactionBuilder tx, JsonNode artifactJson, ArtifactToken artifact,
+      String attributesNodeName) {
+      if (artifactJson.has(attributesNodeName)) {
+         for (JsonNode attribute : artifactJson.get(attributesNodeName)) {
+            if (attribute.has("gamma")) {
+               tx.deleteAttributes(artifact, getAttributeType(attribute),
+                  GammaId.valueOf(attribute.get("gamma").asText()));
+            } else {
+               tx.deleteAttributes(artifact, getAttributeType(attribute));
+            }
+         }
+      }
+   }
+
+   private void readrelations(TransactionBuilder tx, Map<String, ArtifactToken> artifactsByName,
+      Map<String, ArtifactToken> artifactsByKey, JsonNode artifactJson, ArtifactToken artifact) {
+      if (artifactJson.has("relations")) {
+         for (JsonNode relation : artifactJson.get("relations")) {
+            RelationTypeToken relationType = getRelationType(relation);
+            if (relation.isTextual() || relation.isArray()) {
+               relate(tx, artifactsByName, artifactsByKey, relation, relationType, artifact, RelationSide.SIDE_A, "",
+                  ArtifactId.SENTINEL, "end");
+            } else if (relation.isObject()) {
+               String rationale = relation.has("rationale") ? relation.get("rationale").asText() : "";
+               ArtifactId relatedArtifact = relation.has("relatedArtifact") ? ArtifactId.valueOf(
+                  relation.get("relatedArtifact").asLong()) : ArtifactId.SENTINEL;
+               String afterArtifact = relation.has("afterArtifact") ? relation.get("afterArtifact").asText() : "end";
+
+               if (relation.has("sideA")) {
+                  relate(tx, artifactsByName, artifactsByKey, relation.get("sideA"), relationType, artifact,
+                     RelationSide.SIDE_B, rationale, relatedArtifact, afterArtifact);
+               }
+               if (relation.has("sideB")) { //both sides are allowed for the same relation entry
+                  relate(tx, artifactsByName, artifactsByKey, relation.get("sideB"), relationType, artifact,
+                     RelationSide.SIDE_A, rationale, relatedArtifact, afterArtifact);
+               }
+            } else {
+               throw new OseeStateException("Json Node of unexpected type %s", relation.getNodeType());
+            }
+         }
+      }
+   }
+
+   private void relate(TransactionBuilder tx, Map<String, ArtifactToken> artifactsByName,
+      Map<String, ArtifactToken> artifactsByKey, JsonNode relations, RelationTypeToken relationType,
+      ArtifactToken artifact, RelationSide side, String rationale, ArtifactId relatedArtifact, String afterArtifact) {
+      if (relations.isTextual()) {
+         relateOne(tx, artifactsByName, artifactsByKey, relations, relationType, artifact, side, rationale,
+            relatedArtifact, afterArtifact);
+      } else if (relations.isArray()) {
+         for (JsonNode name : relations) {
+            relateOne(tx, artifactsByName, artifactsByKey, name, relationType, artifact, side, rationale,
+               relatedArtifact, afterArtifact);
+         }
+      } else {
+         throw new OseeStateException("Json Node of unexpected type %s", relations.getNodeType());
+      }
+   }
+
+   private void relate(TransactionBuilder tx, RelationTypeToken relationType, ArtifactToken artifact, RelationSide side,
+      String rationale, ArtifactId otherArtifact, ArtifactId relatedArtifact, String afterArtifact) {
+      if (side.isSideA()) {
+         if (relationType.isNewRelationTable()) {
+            tx.relate(artifact, relationType, otherArtifact, relatedArtifact, afterArtifact);
+         } else {
+            tx.relate(artifact, relationType, otherArtifact, rationale);
+         }
+      } else {
+         if (relationType.isNewRelationTable()) {
+            tx.relate(otherArtifact, relationType, artifact, relatedArtifact, afterArtifact);
+         } else {
+
+            tx.relate(otherArtifact, relationType, artifact, rationale);
+         }
+      }
+   }
+
+   private void relateOne(TransactionBuilder tx, Map<String, ArtifactToken> artifactsByName,
+      Map<String, ArtifactToken> artifactsByKey, JsonNode relation, RelationTypeToken relationType,
+      ArtifactToken artifact, RelationSide side, String rationale, ArtifactId relatedArtifact, String afterArtifact) {
+      ArtifactId otherArtifact;
+      if (Strings.isNumeric(relation.asText(""))) {
+         otherArtifact = ArtifactId.valueOf(relation.asLong());
+      } else {
+         otherArtifact = getArtifactByName(tx, artifactsByName, artifactsByKey, relation.asText());
+      }
+      relate(tx, relationType, artifact, side, rationale, otherArtifact, relatedArtifact, afterArtifact);
+   }
+
+   private ArtifactToken getArtifactByName(TransactionBuilder tx, Map<String, ArtifactToken> artifactsByName,
+      Map<String, ArtifactToken> artifactsByKey, String name) {
+      ArtifactToken artifact = artifactsByName.get(name);
+      if (artifact != null) {
+         return artifact;
+      }
+      artifact = artifactsByKey.get(name);
+      if (artifact != null) {
+         return artifact;
+      }
+      return orcsApi.getQueryFactory().fromBranch(tx.getBranch()).andNameEquals(name).asArtifactToken();
+   }
+
+   private ArtifactTypeToken getArtifactType(JsonNode artifactJson) {
+      return getToken(artifactJson, tokenService::getArtifactType, tokenService::getArtifactType);
+   }
+
+   private AttributeTypeGeneric<?> getAttributeType(JsonNode attribute) {
+      return getToken(attribute, tokenService::getAttributeType, tokenService::getAttributeType);
+   }
+
+   private RelationTypeToken getRelationType(JsonNode relation) {
+      return getToken(relation, tokenService::getRelationType, tokenService::getRelationType);
    }
 }
