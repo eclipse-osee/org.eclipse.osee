460ce928bcfa564ae6bd12c54952c86b407456a1
Baily Roberts <Baily.Roberts@boeing.com>
2024-03-13 11:41:19 -0700

    refactor[TW25540]: Parallelize Ats Int tests
    Signed-off-by: Baily Roberts <Baily.Roberts@boeing.com>

---
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/framework/skynet/core/artifact/SkynetCoreArtifact_Suite.java
@@ -0,0 +1,34 @@
+/*********************************************************************
+ * Copyright (c) 2014 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+
+package org.eclipse.osee.ats.ide.integration.tests.framework.skynet.core.artifact;
+
+import org.eclipse.osee.ats.ide.demo.DemoUtil;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Donald G. Dunne
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ //
+   ArtifactTypeManagerTest.class, //
+   ArtifactTest.class, //
+})
+public class SkynetCoreArtifact_Suite {
+   @BeforeClass
+   public static void setUp() throws Exception {
+      DemoUtil.checkDbInitAndPopulateSuccess();
+   }
+}
