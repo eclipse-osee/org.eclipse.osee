9f1d272109edcf5c7f059989a54cdb1b849ae284
Dunne (US), Donald G <donald.g.dunne@boeing.com>
2024-05-16 12:17:52 -0700

    feature[TW23865]: Web Portal - Add web export to file
    Change-Id: Iee203930bc0c57643a03b2b40d61259cfcde0a4d

--- plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/editor/tab/workflow/section/goal/OpenWebExportAction.java
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/editor/tab/workflow/section/goal/OpenWebExportAction.java
@@ -13,10 +13,16 @@
 
 package org.eclipse.osee.ats.ide.editor.tab.workflow.section.goal;
 
+import java.io.File;
+import java.io.IOException;
+import java.rmi.activation.Activator;
+import java.util.logging.Level;
 import org.eclipse.osee.ats.api.util.AtsImage;
 import org.eclipse.osee.ats.ide.editor.WorkflowEditor;
 import org.eclipse.osee.ats.ide.internal.AtsApiService;
 import org.eclipse.osee.ats.ide.workflow.goal.GoalArtifact;
+import org.eclipse.osee.framework.jdk.core.util.Lib;
+import org.eclipse.osee.framework.logging.OseeLog;
 import org.eclipse.swt.program.Program;
 
 /**
@@ -33,6 +39,15 @@ public class OpenWebExportAction extends AbstractWebExportAction {
       String server = AtsApiService.get().getApplicationServerBase();
 
       String url = String.format("%s/ats/world/coll/%s/export", server, goalArt.getIdString());
+      String html =
+         goalArt.getAtsApi().getServerEndpoints().getWorldEndpoint().getCollectionExport(goalArt.getArtifactId());
+      File outFile = new File("export.html");
+      try {
+         Lib.writeStringToFile(html, outFile);
+         Program.launch(outFile.getAbsolutePath());
+      } catch (IOException ex) {
+         OseeLog.log(Activator.class, Level.SEVERE, Lib.exceptionToString(ex));
+      }
       Program.launch(url);
    }
 
