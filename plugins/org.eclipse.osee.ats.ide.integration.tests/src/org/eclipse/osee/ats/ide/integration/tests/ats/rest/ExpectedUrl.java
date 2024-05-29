9f3d70c983a01eef82fb62afa8b0e9481cc04056
Dunne (US), Donald G <donald.g.dunne@boeing.com>
2024-05-29 13:09:02 -0700

    feature[TW26538]: Add ATS REST test coverage report
    Change-Id: If13a9d7ce80422815dc813fdb6a328dac0195572

---
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/rest/ExpectedUrl.java
@@ -0,0 +1,65 @@
+/*********************************************************************
+ * Copyright (c) 2024 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+package org.eclipse.osee.ats.ide.integration.tests.ats.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpectedUrl {
+
+   public List<String> expectedCrudTypes = new ArrayList<>();
+   public String expectedUrl;
+   private ActualUrl actualMatch = null;
+
+   public ExpectedUrl() {
+   }
+
+   @Override
+   public String toString() {
+      return "RestUrl [expectedUrl=" + expectedUrl //
+         + ", expectedCruds=" + expectedCrudTypes + "]";
+   }
+
+   public void addExpectedCrudType(String match) {
+      expectedCrudTypes.add(match);
+   }
+
+   public String getExpectedUrl() {
+      return expectedUrl;
+   }
+
+   public void setExpectedUrl(String expectedUrl) {
+      this.expectedUrl = expectedUrl;
+   }
+
+   public boolean isMatch() {
+      return actualMatch != null;
+   }
+
+   public ActualUrl getActualMatch() {
+      return actualMatch;
+   }
+
+   public List<String> getExpectedCrudTypes() {
+      return expectedCrudTypes;
+   }
+
+   public void setExpectedCrudTypes(List<String> expectedCrudTypes) {
+      this.expectedCrudTypes = expectedCrudTypes;
+   }
+
+   public void setActualMatch(ActualUrl actualMatch) {
+      this.actualMatch = actualMatch;
+   }
+
+}
