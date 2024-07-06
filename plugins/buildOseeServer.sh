e1b4706c04485dc05bb32498b81fd2639a791b4e
Baily Roberts <Baily.Roberts@boeing.com>
2024-07-06 14:28:20 -0700

    refactor[TW26648]: Cleanup build files
    Signed-off-by: Baily Roberts <Baily.Roberts@boeing.com>

--- plugins/buildOseeServer.sh
+++ plugins/buildOseeServer.sh
@@ -1,3 +1,3 @@
 bazel build org.eclipse.osee.server.p2:osee_server_zip;rm -rf osee_server_zip;unzip bazel-bin/org.eclipse.osee.server.p2/org.eclipse.osee.server.p2.zip -d osee_server_zip;
-cd osee_server_zip/osee_server
+cd osee_server_zip/
 
