cae8f44c147a9168c06d93542bb3d70e43787677
Puckett (US), Jaden W <jaden.w.puckett@boeing.com>
2024-09-18 14:46:12 -0700

    feature[TW27164]: Implement JNI for BAT Rust Methods
    Change-Id: I790264ed859b9e26881b0ca13cb8cc22d0fb5b06

---
+++ plugins/org.eclipse.osee.framework.core/src/org/eclipse/osee/framework/core/applicability/BatJavaRustFfi.java
@@ -0,0 +1,10 @@
+/*
+ * Created on Sep 18, 2024
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.osee.framework.core.applicability;
+
+public interface BatJavaRustFfi {
+
+}
