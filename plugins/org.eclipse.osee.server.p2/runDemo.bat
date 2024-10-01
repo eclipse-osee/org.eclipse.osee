9c417b86fdf75890c7dfcc5b4fb9f33fc2cb0a4d
Campanella (US), Zachary A <zachary.a.campanella@boeing.com>
2024-10-01 13:50:26 -0700

    feature[TW26874]: update org.eclipse.equinox.launcher versions in commands
    Change-Id: I55eefefa3073d4617ef421104bf32a113f0ecb1a

--- plugins/org.eclipse.osee.server.p2/runDemo.bat
+++ plugins/org.eclipse.osee.server.p2/runDemo.bat
@@ -6,4 +6,4 @@ java -server ^
 -Dcm.config.uri="etc/osee.hsql.json" ^
 -Dosee.authentication.protocol=demo ^
 -Dosee.application.server.data="demo/binary_data" ^
--jar plugins/org.eclipse.equinox.launcher_1.5.700.v20200207-2156.jar -console -consoleLog
+-jar plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar -console -consoleLog
