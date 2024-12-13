07bc5e55fdc905d2ee25aae5f92c7c0d41cbaab4
Audrey Denk <audrey.e.denk@boeing.com>
2024-12-13 12:25:57 -0700

    feature[TW26865]: Create MIM specific workflow - fix pr test
    Change-Id: I2f119eb200f96f8760627247914da383635682c1

--- web/apps/osee/playwright/specs/mim/tests/peer-review.e2e-spec.ts
+++ web/apps/osee/playwright/specs/mim/tests/peer-review.e2e-spec.ts
@@ -217,8 +217,6 @@ test('commit branches', async ({ page }) => {
 		.click();
 	await page.getByRole('menuitem', { name: 'Transition to Review' }).click();
 	await page.getByRole('button', { name: 'Review', exact: true }).click();
-	await page.getByRole('menuitem', { name: 'Approve Transition to' }).click();
-	await page.getByRole('button', { name: 'Review', exact: true }).click();
 	await page.getByRole('menuitem', { name: 'Commit Branch' }).click();
 
 	await expect(
@@ -244,8 +242,6 @@ test('commit branches', async ({ page }) => {
 		.getByRole('button')
 		.click();
 	await page.getByRole('menuitem', { name: 'Transition to Review' }).click();
-	await page.getByRole('button', { name: 'Review', exact: true }).click();
-	await page.getByRole('menuitem', { name: 'Approve Transition to' }).click();
 	await page.getByRole('button', { name: 'Review', exact: true }).click();
 	await page.getByRole('menuitem', { name: 'Commit Branch' }).click();
 	await page.getByRole('button', { name: 'Close Peer Review' }).click();
