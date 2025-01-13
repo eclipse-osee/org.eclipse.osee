46eee106a1d0e8150e1d27aba19001c668942664
Audrey Denk <audrey.e.denk@boeing.com>
2025-01-13 12:04:41 -0700

    feature[TW27811]:  Test updates
    Change-Id: Ic69171e543527bbb96eed2a4518996b41644f997
    Signed-off-by: Audrey Denk <audrey.e.denk@boeing.com>

--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/skynet/core/BranchStateTest.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/skynet/core/BranchStateTest.java
@@ -235,7 +235,13 @@ public class BranchStateTest {
       BranchToken workingBranch = null;
       try {
          workingBranch = BranchManager.createWorkingBranch(SAW_Bld_1, originalBranchName);
+         Artifact change = ArtifactTypeManager.addArtifact(CoreArtifactTypes.SoftwareRequirementMsWord, workingBranch,
+            "A commit change");
+         change.persist(getClass().getSimpleName());
 
+         Artifact workingBranchRoot = OseeSystemArtifacts.getDefaultHierarchyRootArtifact(workingBranch);
+         workingBranchRoot.addChild(change);
+         workingBranchRoot.persist(getClass().getSimpleName());
          // Update the branch
          ConflictResolverOperation resolverOperation =
             new ConflictResolverOperation("Test 1 Resolver", BranchStateTest.class.getCanonicalName()) {
@@ -250,7 +256,7 @@ public class BranchStateTest {
          UpdateBranchOperation operation = new UpdateBranchOperation(workingBranch, resolverOperation);
          Asserts.assertOperation(operation, IStatus.OK);
 
-         Assert.assertEquals(BranchState.DELETED, BranchManager.getState(workingBranch));
+         Assert.assertEquals(BranchState.REBASELINED, BranchManager.getState(workingBranch));
          Assert.assertEquals(Artifact.SENTINEL, BranchManager.getAssociatedArtifact(workingBranch));
 
          BranchToken newWorkingBranch = operation.getNewBranch();
