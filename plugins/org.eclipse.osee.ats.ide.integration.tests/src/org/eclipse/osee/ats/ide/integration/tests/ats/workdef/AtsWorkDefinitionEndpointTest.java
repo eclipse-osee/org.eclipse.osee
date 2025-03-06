908ad464e5f5b7bcb0ddcbd970134a9c299f9aff
Puckett (US), Jaden W <jaden.w.puckett@boeing.com>
2025-03-06 13:34:21 -0700

    bug[TW28170]: Add simple demo requirements actionable item and workflow definition

--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/workdef/AtsWorkDefinitionEndpointTest.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/workdef/AtsWorkDefinitionEndpointTest.java
@@ -30,7 +30,7 @@ public class AtsWorkDefinitionEndpointTest extends AbstractRestTest {
    public void testAllWorkDefs() {
       String url = "ats/workdef";
       JsonNode jsonNode = getJsonNode(url);
-      Assert.assertEquals(22, jsonNode.size());
+      Assert.assertEquals(23, jsonNode.size());
    }
 
    @Test
