ddbfb0ea989036e13b83268115668bcad2e9d47b
Dunne (US), Donald G <donald.g.dunne@boeing.com>
2025-07-23 08:42:33 -0700

    feature[TW28573]: Add AttrDiffReport
    Change-Id: I0035b62443ae1a28e49b556631534687f6216546

---
+++ plugins/org.eclipse.osee.ats.api/src/org/eclipse/osee/ats/api/report/AtsReportEndpointApi.java
@@ -0,0 +1,37 @@
+/*********************************************************************
+ * Copyright (c) 2025 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+package org.eclipse.osee.ats.api.report;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Donald G. Dunne
+ */
+@Path("report")
+public interface AtsReportEndpointApi {
+
+   /**
+    * @param date like 2025-07-15
+    * @param artTypeId
+    * @param attrTypeIds - comma delimited attr type ids
+    */
+   @Path("AttrDiffReport")
+   @GET
+   @Produces(MediaType.TEXT_HTML)
+   public String getAttrDiffReport(@QueryParam("date") String date, @QueryParam("artTypeId") String artTypeId,
+      @QueryParam("attrTypeIds") String attrTypeIds);
+}
