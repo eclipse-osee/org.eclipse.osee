55a80f50bd215814e7ae477f6f73e28ea505cffa
Dunne (US), Donald G <donald.g.dunne@boeing.com>
2026-01-19 21:22:36 -0700

    feature[TW29258]: PR - Create ImportWorkflowApplicabilities
    Change-Id: I0d0a8667d23ce229e51e75e2319e7dc4c15ba671

---
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/util/Import/ImportWorkflowApplicabilities.java
@@ -0,0 +1,183 @@
+/*********************************************************************
+ * Copyright (c) 2025 Boeing
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Boeing - initial API and implementation
+ **********************************************************************/
+
+package org.eclipse.osee.ats.ide.util.Import;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import org.eclipse.jface.window.Window;
+import org.eclipse.osee.ats.api.AtsApi;
+import org.eclipse.osee.ats.api.workflow.IAtsTeamWorkflow;
+import org.eclipse.osee.ats.ide.internal.AtsApiService;
+import org.eclipse.osee.ats.ide.navigate.AtsNavigateViewItems;
+import org.eclipse.osee.ats.ide.util.ServiceUtil;
+import org.eclipse.osee.framework.core.data.ApplicabilityToken;
+import org.eclipse.osee.framework.core.data.ArtifactId;
+import org.eclipse.osee.framework.core.data.BranchToken;
+import org.eclipse.osee.framework.core.data.TransactionToken;
+import org.eclipse.osee.framework.jdk.core.result.XResultData;
+import org.eclipse.osee.framework.jdk.core.type.HashCollection;
+import org.eclipse.osee.framework.jdk.core.util.Strings;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateComposite.TableLoadOption;
+import org.eclipse.osee.framework.ui.plugin.xnavigate.XNavigateItemAction;
+import org.eclipse.osee.framework.ui.skynet.FrameworkImage;
+import org.eclipse.osee.framework.ui.skynet.results.XResultDataUI;
+import org.eclipse.osee.framework.ui.skynet.widgets.dialog.BranchEntryEntryDialog;
+import org.eclipse.osee.orcs.rest.model.ApplicabilityEndpoint;
+
+/**
+ * @author Donald G. Dunne
+ */
+public class ImportWorkflowApplicabilities extends XNavigateItemAction {
+
+   private static final String FINAL_IMPORT_CHANGES = "Final Import Changes:";
+   private BranchToken branch;
+   private AtsApi atsApi;
+   private XResultData rd;
+   private StringBuffer applics;
+   private boolean persist = false;
+   private Map<String, ApplicabilityToken> applicNameToTok;
+
+   public ImportWorkflowApplicabilities() {
+      super("Import Workflow Applicabilities from Branch", FrameworkImage.IMPORT, AtsNavigateViewItems.ATS_IMPORT);
+   }
+
+   @Override
+   public void run(TableLoadOption... tableLoadOptions) throws Exception {
+      atsApi = AtsApiService.get();
+      BranchEntryEntryDialog dialog = new BranchEntryEntryDialog(getName(), getDescription(),
+         "Multi-lined: <art id>,<applic name>", "Commit Comment");
+      dialog.setFillVertically(true);
+      dialog.setFillVertically2(false);
+      dialog.setEntry2(getName());
+      dialog.addCheckbox("Persist");
+      if (dialog.open() == Window.OK) {
+         rd = new XResultData();
+         rd.log(getName());
+         rd.log("\n<b>Search for \"" + FINAL_IMPORT_CHANGES + "\" to see final changes/results.</b>\n");
+         importApplic(dialog, rd);
+         rd.addRaw("\n\n" + applics.toString());
+         XResultDataUI.report(rd, getName());
+         persist = dialog.isChecked();
+      }
+   }
+
+   private void importApplic(BranchEntryEntryDialog dialog, XResultData rd) {
+      branch = dialog.getBranch();
+      if (branch == null) {
+         rd.error("Branch must be selected");
+         return;
+      }
+      if (rd.isErrors()) {
+         return;
+      }
+      rd.logf("Branch: %s\n", (branch == null ? "Not Selected" : branch.toStringWithId()));
+      getApplicabilitesFromBranch();
+      rd.logf("Commit Comment: [%s]\n", dialog.getEntry2());
+      HashCollection<ApplicabilityToken, ArtifactId> applicToTeamWfArt = new HashCollection<>();
+      ApplicabilityEndpoint commonBranchApplicEp =
+         ServiceUtil.getOseeClient().getApplicabilityEndpoint(atsApi.getAtsBranch());
+
+      if (Strings.isValid(dialog.getEntry())) {
+         for (String line : dialog.getEntry().split("[\n\r]+")) {
+            String[] split = line.split(",");
+            if (split.length != 2) {
+               rd.errorf("Invalid line: [%s]\n", line);
+            } else {
+               String artId = split[0];
+               String importApplicName = split[1];
+               rd.logf("\nimport line: [%s]\n", line);
+               rd.logf("--- artId: [%s]\n", artId);
+               rd.logf("--- importApplicName: [%s]\n", importApplicName);
+               if (Strings.isNotNumeric(artId)) {
+                  rd.error("Applic Id [%s] isn't numeric");
+               } else {
+                  IAtsTeamWorkflow teamWf = atsApi.getQueryService().getTeamWf(Long.valueOf(artId));
+                  if (teamWf == null) {
+                     rd.error("Art Id [%s] isn't team wf");
+                  } else {
+                     rd.logf("--- teamWf %s\n", teamWf.toStringWithId());
+                     ApplicabilityToken currApplic = commonBranchApplicEp.getApplicabilityToken(teamWf.getArtifactId());
+                     rd.logf("--- curr applic [%s]\n", currApplic);
+                     if (importApplicName.equals(currApplic.getName())) {
+                        rd.logf("--- NO CHANGE\n");
+                     } else {
+                        ApplicabilityToken importApplicTok = applicNameToTok.get(importApplicName);
+                        if (importApplicTok == null) {
+                           rd.errorf("Invalid Import Applic Name [%s], No Applic Found on selected Branch\n",
+                              importApplicName);
+                        } else {
+                           rd.warningf("--- Curr [%s]; Set New Applic [%s] (on persist)\n", currApplic,
+                              importApplicName);
+                           applicToTeamWfArt.put(importApplicTok, teamWf.getArtifactId());
+                        }
+                     }
+                  }
+               }
+            }
+         }
+         Set<Entry<ApplicabilityToken, List<ArtifactId>>> entrySet = applicToTeamWfArt.entrySet();
+         rd.logf("\n\n");
+         rd.logf(FINAL_IMPORT_CHANGES);
+         rd.logf("\n");
+         if (applicToTeamWfArt.isEmpty()) {
+            rd.log("No Changes To Make");
+         } else {
+            for (Entry<ApplicabilityToken, List<ArtifactId>> entry : entrySet) {
+               rd.logf("Set Applic [%s] to teamWfs %s (on persit)\n", entry.getKey(), entry.getValue());
+               if (persist) {
+                  if (rd.isErrors()) {
+                     rd.errorf("Import Aborted Due To Errors\n");
+                  } else {
+                     TransactionToken tx = commonBranchApplicEp.setApplicability(entry.getKey(), entry.getValue());
+                     if (tx.isValid()) {
+                        rd.logf("---Transaction %s\n", tx.toString());
+                     } else {
+                        rd.errorf("--- Set did not succeed\n");
+                     }
+                  }
+               } else {
+                  rd.log("Report Only - No Persist");
+               }
+            }
+         }
+      } else {
+         rd.errorf("Must enter valid lines\n");
+      }
+   }
+
+   private void getApplicabilitesFromBranch() {
+      ApplicabilityEndpoint applicEp = ServiceUtil.getOseeClient().getApplicabilityEndpoint(branch);
+      Collection<ApplicabilityToken> applicToks = applicEp.getApplicabilityTokens();
+      applicNameToTok = new HashMap<>(100);
+      applics = new StringBuffer("Applicabilities found on selected branch: \n");
+      for (ApplicabilityToken appTok : applicToks) {
+         applics.append(String.format("--- Applic: %s\n", appTok.toStringWithId()));
+         applicNameToTok.put(appTok.getName(), appTok);
+      }
+   }
+
+   @Override
+   public String getDescription() {
+      return "This will retrieve the valid Applicability Tokens from the selected branch and then set the" //
+         + " appropriate applicability id from that branch as the applicability id for the TeamWf." //
+         + " \n\nThis is uncommon! \n\nExample Case: A Team Workflow, like a Problem Report, needs to be marked as" //
+         + " one of the applicabilies from the Product Line branch to enable a Build Memo by Config / Applicability" //
+         + " to be generated or to see what Problems exist for a current Config or Applicability.";
+   }
+
+}
--- plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/dialog/BranchEntryEntryDialog.java
+++ plugins/org.eclipse.osee.framework.ui.skynet/src/org/eclipse/osee/framework/ui/skynet/widgets/dialog/BranchEntryEntryDialog.java
@@ -15,11 +15,15 @@ package org.eclipse.osee.framework.ui.skynet.widgets.dialog;
 
 import org.eclipse.osee.framework.core.data.BranchToken;
 import org.eclipse.osee.framework.ui.skynet.widgets.XBranchSelectWidget;
+import org.eclipse.osee.framework.ui.skynet.widgets.XCheckBox;
 import org.eclipse.osee.framework.ui.skynet.widgets.XModifiedListener;
 import org.eclipse.osee.framework.ui.skynet.widgets.XText;
 import org.eclipse.osee.framework.ui.skynet.widgets.XWidget;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Listener;
 
@@ -36,6 +40,10 @@ public class BranchEntryEntryDialog extends EntryDialog {
    private final String text2Label;
    private Listener okListener;
    private BranchToken branch;
+   private boolean fillVertically2 = true;
+   private boolean addCheckBox = false;
+   private String checkboxMessage;
+   private boolean checked = false;
 
    public BranchEntryEntryDialog(String dialogTitle, String dialogMessage, String text1Label, String text2Label) {
       super(dialogTitle, dialogMessage);
@@ -59,7 +67,7 @@ public class BranchEntryEntryDialog extends EntryDialog {
 
       text2 = new XText(text2Label);
       text2.setFillHorizontally(true);
-      if (isFillVertically()) {
+      if (isFillVertically2()) {
          text2.setFillVertically(true);
          text2.setHeight(100);
          text.setHeight(100);
@@ -79,6 +87,9 @@ public class BranchEntryEntryDialog extends EntryDialog {
 
       branchWidget = new XBranchSelectWidget("Branch");
       branchWidget.createWidgets(customAreaParent, 1);
+      if (branch != null && branch.isValid()) {
+         branchWidget.setSelection(branch);
+      }
       branchWidget.addXModifiedListener(new XModifiedListener() {
 
          @Override
@@ -86,6 +97,31 @@ public class BranchEntryEntryDialog extends EntryDialog {
             branch = branchWidget.getData();
          }
       });
+
+      if (addCheckBox) {
+         final XCheckBox checkbox = new XCheckBox(checkboxMessage);
+         checkbox.setFillHorizontally(true);
+         checkbox.setFocus();
+         checkbox.setDisplayLabel(false);
+         checkbox.set(checked);
+         checkbox.createWidgets(customAreaParent, 1);
+
+         SelectionListener selectionListener = new SelectionAdapter() {
+
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+               handleModified();
+               checked = checkbox.isSelected();
+            }
+         };
+         checkbox.addSelectionListener(selectionListener);
+
+      }
+
+   }
+
+   public boolean isChecked() {
+      return checked;
    }
 
    public String getEntry2() {
@@ -113,6 +149,27 @@ public class BranchEntryEntryDialog extends EntryDialog {
 
    public BranchToken getBranch() {
       return branch;
+   }
+
+   public boolean isFillVertically2() {
+      return fillVertically2;
+   }
+
+   public void setFillVertically2(boolean fillVertically2) {
+      this.fillVertically2 = fillVertically2;
+   }
+
+   public void addCheckbox(String checkboxMessage) {
+      this.addCheckBox = true;
+      this.checkboxMessage = checkboxMessage;
+   }
+
+   public void setChecked(boolean checked) {
+      this.checked = checked;
+   }
+
+   public void setBranch(BranchToken branch) {
+      this.branch = branch;
    }
 
 }
