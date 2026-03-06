bb45b8e8c4eef9f00fa850d5a31cd93be67209ed
Kris Graham <kgraha16@asu.edu>
2026-03-06 09:53:59 -0700

    Task 193 Implemented the export button to download HTML file with the data from the Search Results Table

--- web/apps/osee/src/app/ple/artifact-explorer/advanced-search-page/advanced-search-page.component.ts
+++ web/apps/osee/src/app/ple/artifact-explorer/advanced-search-page/advanced-search-page.component.ts
@@ -1923,4 +1923,16 @@ export class AdvancedSearchPageComponent implements OnInit {
 				this.relatedNames.set(result.id, names);
 			});
 	}
+	
+	onExport(): void {
+		const tableHTML = this.generateHTMLTable();
+		const fullHTML = this.buildHTMLDocument(tableHTML);
+		const blob = new Blob([fullHTML], { type: 'text/html;charset=utf-8;' });
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = 'advanced-search-results.html';
+		a.click();
+		URL.revokeObjectURL(url);
+	}
 }
