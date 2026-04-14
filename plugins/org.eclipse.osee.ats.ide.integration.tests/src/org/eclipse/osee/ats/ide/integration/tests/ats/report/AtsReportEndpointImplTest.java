ed5410df5cca1c6de698987b570d88f71605cd06
Grumb1e <168458991+Grumb1e@users.noreply.github.com>
2026-04-13 17:06:18 -0700

    feature[TW29454]: Update And Automate Build Memo Generation (#548)

--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/report/AtsReportEndpointImplTest.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/report/AtsReportEndpointImplTest.java
@@ -13,9 +13,13 @@
 
 package org.eclipse.osee.ats.ide.integration.tests.ats.report;
 
+import java.util.Arrays;
 import java.util.Date;
 import org.eclipse.osee.ats.api.data.AtsArtifactTypes;
 import org.eclipse.osee.ats.api.data.AtsAttributeTypes;
+import org.eclipse.osee.ats.api.report.BuildMemoRequest;
+import org.eclipse.osee.ats.api.report.BuildMemoRequest.BuildMemoCommit;
+import org.eclipse.osee.ats.api.report.BuildMemoRequest.BuildMemoRepository;
 import org.eclipse.osee.ats.ide.integration.tests.AtsApiService;
 import org.eclipse.osee.ats.ide.integration.tests.ats.resource.AbstractRestTest;
 import org.eclipse.osee.ats.ide.util.AtsApiIde;
@@ -30,6 +34,61 @@ import org.junit.Test;
  * @author Donald G. Dunne
  */
 public class AtsReportEndpointImplTest extends AbstractRestTest {
+
+   @Test
+   public void testGetBuildMemo() {
+      AtsApiIde atsApi = AtsApiService.get();
+
+      BuildMemoRequest request = new BuildMemoRequest();
+      request.setTitle("Test Build Memo");
+      request.setFromTag("v1.0.0");
+      request.setToTag("v2.0.0");
+      request.setAllowedCommitTypes(Arrays.asList("FEATURE", "BUG"));
+
+      BuildMemoRepository repo = new BuildMemoRepository();
+      repo.setName("test-repo");
+      repo.setFromTag("v1.0.0");
+      repo.setToTag("v2.0.0");
+      request.setRepositories(Arrays.asList(repo));
+
+      BuildMemoCommit featureCommit = new BuildMemoCommit();
+      featureCommit.setType("FEATURE");
+      featureCommit.setId("FEAT-001");
+      featureCommit.setTitle("Add new feature");
+      featureCommit.setMessage("Implemented new feature");
+      featureCommit.setRepository("test-repo");
+
+      BuildMemoCommit bugCommit = new BuildMemoCommit();
+      bugCommit.setType("BUG");
+      bugCommit.setId("BUG-042");
+      bugCommit.setTitle("Fix null pointer");
+      bugCommit.setMessage("Fixed NPE in handler");
+      bugCommit.setRepository("test-repo");
+
+      BuildMemoCommit filteredCommit = new BuildMemoCommit();
+      filteredCommit.setType("REFACTOR");
+      filteredCommit.setId("REF-099");
+      filteredCommit.setTitle("Cleanup code");
+      filteredCommit.setMessage("Removed dead code");
+      filteredCommit.setRepository("test-repo");
+
+      request.setCommits(Arrays.asList(featureCommit, bugCommit, filteredCommit));
+
+      String html = atsApi.getServerEndpoints().getReportEp().getBuildMemo(request);
+
+      Assert.assertTrue(html.contains("Test Build Memo"));
+      Assert.assertTrue(html.contains("test-repo"));
+      Assert.assertTrue(html.contains("v1.0.0"));
+      Assert.assertTrue(html.contains("v2.0.0"));
+      Assert.assertTrue(html.contains("FEAT-001"));
+      Assert.assertTrue(html.contains("Add new feature"));
+      Assert.assertTrue(html.contains("BUG-042"));
+      Assert.assertTrue(html.contains("Fix null pointer"));
+      Assert.assertFalse("REFACTOR commits should be filtered out", html.contains("REF-099"));
+      Assert.assertFalse("REFACTOR commits should be filtered out", html.contains("Cleanup code"));
+      Assert.assertTrue("Should contain Expand All button", html.contains("Expand All"));
+      Assert.assertTrue("Should contain toggleAll function", html.contains("toggleAll"));
+   }
 
    @Test
    public void testGetAttrDiffReport() throws Exception {
