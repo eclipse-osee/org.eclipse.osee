82f3621fe01b3daf6ac0d229ffbd41693d5653bf
Audrey Denk <audrey.e.denk@boeing.com>
2026-04-20 10:55:01 -0700

    bug[TW29461]: ICD - Update test
    Change-Id: I5ab3711edcc1717c94b00198b8d1e84434d80ecb
    Signed-off-by: Audrey Denk <audrey.e.denk@boeing.com>

--- web/apps/osee/playwright/specs/mim/tests/impactedConnections.e2e-spec.ts
+++ web/apps/osee/playwright/specs/mim/tests/impactedConnections.e2e-spec.ts
@@ -16,20 +16,28 @@ import { createWorkingBranchFromPL, enableEditMode } from '../utils/helpers';
 test('test', async ({ page }) => {
 	await page.setViewportSize({ width: 1200, height: 900 });
 	await page.goto('http://localhost:4200/ple/messaging/connections');
+	await page.waitForTimeout(500);
 	await createWorkingBranchFromPL(page, 'Validation Impact');
+	await page.waitForTimeout(500);
 	await enableEditMode(page);
+	await page.waitForTimeout(500);
+
+	await page
+		.getByTestId('link-Connection A-B')
+		.getByText('Connection A-B')
+		.click();
+	await page.waitForTimeout(500);
+	await page.getByRole('combobox', { name: 'Rates' }).click();
+	await page.getByRole('combobox', { name: 'Rates' }).fill('10');
+	await page.getByRole('option', { name: '10' }).click();
 	await page.getByRole('button').filter({ hasText: 'menu' }).click();
-	await page.getByRole('link', { name: 'Structures' }).click();
-	await page.getByRole('button', { name: 'Structure' }).click();
-	await page.getByRole('link', { name: 'Message 1 > Submessage' }).click();
-	await page.locator('#mat-input-49').click();
-	await page.locator('#mat-input-49').fill('Test Desc');
-	await page.getByRole('button').filter({ hasText: 'menu' }).click();
+	await page.getByText('Product Line Engineering').click();
+	await page.getByText('MIM').click();
 	await page.getByRole('link', { name: 'Reports' }).click();
 	await page
 		.getByRole('link', { name: 'Validate Connection Impact' })
 		.click();
-	await expect(page.locator('mat-row')).toContainText(
-		'Connection A-B - Assumed Target Connection'
-	);
+	await expect(
+		page.getByRole('cell', { name: 'Connection A-B - Assumed' })
+	).toBeVisible();
 });
