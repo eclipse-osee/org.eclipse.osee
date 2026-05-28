66973d64ee2b2f5c7845cb54a7baac6ccc509480
Dunne (US), Donald G <donald.g.dunne@boeing.com>
2026-05-28 12:02:03 -0700

    feature[TW29549]: ATS Quick Search to Search Results
    Change-Id: I403fc19c7c08739ac85bd13fadab3e13b270d0e1

--- plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/search/quick/AtsQuickSearchData.java
+++ plugins/org.eclipse.osee.ats.ide/src/org/eclipse/osee/ats/ide/search/quick/AtsQuickSearchData.java
@@ -13,13 +13,19 @@
 
 package org.eclipse.osee.ats.ide.search.quick;
 
+import org.eclipse.osee.ats.ide.internal.AtsApiService;
+import org.eclipse.osee.framework.core.data.BranchId;
+
 /**
  * @author Donald G. Dunne
  */
-class AtsQuickSearchData {
+public class AtsQuickSearchData {
    private final String searchStr;
    private boolean includeCompleteCancelled = false;
    private final String name;
+   private boolean includeDeleted = false;
+   private boolean caseSensitive = false;
+   private boolean useEclipseSearchView = false;
 
    public AtsQuickSearchData(String name, String searchStr, boolean includeCompleteCancelled) {
       this.name = name;
@@ -33,6 +39,38 @@ class AtsQuickSearchData {
 
    public String getSearchStr() {
       return searchStr;
+   }
+
+   public boolean isIncludeDeleted() {
+      return includeDeleted;
+   }
+
+   public void setIncludeDeleted(boolean includeDeleted) {
+      this.includeDeleted = includeDeleted;
+   }
+
+   public boolean isCaseSensitive() {
+      return caseSensitive;
+   }
+
+   public void setCaseSensitive(boolean caseSensitive) {
+      this.caseSensitive = caseSensitive;
+   }
+
+   public boolean isUseEclipseSearchView() {
+      return useEclipseSearchView;
+   }
+
+   public void setUseEclipseSearchView(boolean useEclipseSearchView) {
+      this.useEclipseSearchView = useEclipseSearchView;
+   }
+
+   public BranchId getBranch() {
+      return AtsApiService.get().getAtsBranch();
+   }
+
+   public String getName() {
+      return name;
    }
 
    @Override
