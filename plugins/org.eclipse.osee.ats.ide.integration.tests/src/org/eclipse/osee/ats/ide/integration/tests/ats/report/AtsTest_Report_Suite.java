7ed5fe9874c541f8f37f39708af3d0fe5319a7fa
Jaden Puckett <110552794+jadenpuckett@users.noreply.github.com>
2026-06-10 09:09:43 -0700

    bug[TW29572]: Update software requirements volatility metrics report to use orcs query instead of ats cache version no load relations query (#596)

--- plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/report/AtsTest_Report_Suite.java
+++ plugins/org.eclipse.osee.ats.ide.integration.tests/src/org/eclipse/osee/ats/ide/integration/tests/ats/report/AtsTest_Report_Suite.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AtsReportEndpointImplTest.class})
+@Suite.SuiteClasses({AtsReportEndpointImplTest.class, SoftwareReqVolatilityMetricsTest.class})
 /**
  * @author Donald G. Dunne
  */
