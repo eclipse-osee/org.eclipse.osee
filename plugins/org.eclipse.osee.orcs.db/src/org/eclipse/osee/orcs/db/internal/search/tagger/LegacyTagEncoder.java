8bda971bad902f0daa71e91bfbcb8a5b8df9a756
Audrey Denk <audrey.e.denk@boeing.com>
2026-08-18 14:05:34 -0700

    feature[TW29566]: search tag update - update per findings
    Change-Id: I3b39d4b88d4577bc887d2a0e3dbc36378cc198a6
    Signed-off-by: Audrey Denk <audrey.e.denk@boeing.com>

--- plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/search/tagger/LegacyTagEncoder.java
+++ plugins/org.eclipse.osee.orcs.db/src/org/eclipse/osee/orcs/db/internal/search/tagger/LegacyTagEncoder.java
@@ -1,5 +1,5 @@
 /*********************************************************************
- * Copyright (c) 2004, 2007 Boeing
+ * Copyright (c) 2026 Boeing
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -21,7 +21,6 @@ import java.util.Arrays;
  * <p>
  * Remove this class once all release tracks have migrated to hash-based search via osee_search_tags_hash.
  *
- * @author Roberto E. Escobar
  */
 public class LegacyTagEncoder {
 
